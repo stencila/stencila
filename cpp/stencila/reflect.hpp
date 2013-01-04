a124202b75c3c4557c8f0d7f707c04e0ec485dad
Susan Devira <susan.devira@stenci.la>
2013-01-04 13:00:49 +1300

    documentation update

--- cpp/stencila/reflect.hpp
+++ cpp/stencila/reflect.hpp
@@ -53,16 +53,30 @@ template<class Derived>
 class Reflection {
 public:
 
+    //! @brief 
+    //! @param name
+    //! @param value
+    //! @param desc
+    //! @return 
     template<typename Value>
     Derived& data(const char* name, Value* value,const char* desc = 0){
         return static_cast<Derived&>(*this);
     }
 
+    //! @brief 
+    //! @param name
+    //! @param value
+    //! @param desc
+    //! @return 
     template<typename Value>
     Derived& method(const char* name, Value value, const char* desc = 0){
       return static_cast<Derived&>(*this);
     }
     
+    //! @brief 
+    //! @param refector
+    //! @param object   
+    //! @return 
     template<typename Object>
     Derived& mirror(const std::true_type& is_refector,Object& object){
         Derived& self = static_cast<Derived&>(*this);
@@ -70,12 +84,19 @@ public:
         return self;
     }
     
+    //! @brief 
+    //! @param reflector
+    //! @param object
+    //! @return 
     template<typename Object>
     Derived& mirror(const std::false_type& is_reflector,Object& object){
         Derived& self = static_cast<Derived&>(*this);
         return self;
     }
     
+    //! @brief 
+    //! @param object
+    //! @return 
     template<typename Object>
     Derived& mirror(Object& object) {
         Derived& self = static_cast<Derived&>(*this);
@@ -83,6 +104,7 @@ public:
         return self;
     }
 
+    
     template<typename Object>
     Derived& mirror(void) {
         Derived& self = static_cast<Derived&>(*this);
@@ -97,6 +119,11 @@ private:
 
 public:
 
+    //! @brief 
+    //! @param object
+    //! @param name
+    //! @param status
+    //! @return 
     template<typename Object>
     Type& mirror(Object& object) {
         int status;
@@ -106,10 +133,15 @@ public:
 
     using Reflection<Type>::mirror;
 
+    //! @brief 
+    //! @return 
     std::string type(void) const {
         return type_;
     }
 
+    //! @brief 
+    //! @param object
+    //! @return 
     template<typename Object>
     static std::string type(Object& object){
         Type type;
@@ -124,10 +156,16 @@ private:
 
 public:
 
+    //! @brief 
     Repr(void):
         repr_(""){
     }
     
+    //! @brief 
+    //! @param name 
+    //! @param value
+    //! @param desc
+    //! @return 
     template<typename Value>
     Repr& data(const char* name, Value* value, const char* desc = 0){
         repr_ += std::string(name) + ":";
@@ -138,6 +176,10 @@ public:
         
     using Reflection<Repr>::mirror;
     
+    //! @brief 
+    //! @param reflector
+    //! @param object
+    //! @return 
     template<typename Object>
     Repr& mirror(const std::true_type& is_reflector,Object& object){
         repr_ += Type::type(object) + "{";
@@ -146,17 +188,25 @@ public:
         return *this;
     }
     
+    //! @brief 
+    //! @param reflector
+    //! @param object
+    //! @return 
     template<typename Object>
     Repr& mirror(const std::false_type& is_reflector,Object& object){
         std::string printed = print()<<object;
         repr_ += printed;
         return *this;
     }
-    
+    //! @brief 
+    //! @return 
     std::string repr(void) const {
         return repr_;
     }
     
+    //! @brief 
+    //! @param object
+    //! @return 
     template<typename Object>
     static std::string repr(Object& object){
         Repr repr;
@@ -171,22 +221,37 @@ private:
 
 public:
 
+    //! @brief 
+    //! @param name
+    //! @param value
+    //! @param desc
+    //! @return 
     template<typename Value>
     Keys& data(const char* name, Value* value, const char* desc = 0){
         keys_.push_back(name);
         return *this;
     }
     
+    //! @brief 
+    //! @param name
+    //! @param value
+    //! @param desc
+    //! @return 
     template<typename Value>
     Keys& method(const char* name, Value value, const char* desc = 0){
         keys_.push_back(name);
         return *this;
     }
   
+    //! @brief 
+    //! @return 
     std::vector<std::string> keys(void) const {
         return keys_;
     }
     
+    //! @brief 
+    //! @param object
+    //! @return 
     template<typename Object>
     static std::vector<std::string> keys(Object& object){
         Keys keys;
@@ -202,27 +267,46 @@ private:
 
 public:
 
+    //! @brief 
+    //! @param name
+    //! @return 
     Has(const std::string& name):
         name_(name),
         has_(false){
     }
 
+    //! @brief 
+    //! @param name
+    //! @param value    
+    //! @param desc
+    //! @return 
     template<typename Value>
     Has& data(const char* name, Value* value, const char* desc = 0){
         if(not has_) has_ = name==name_;
         return *this;
     }
-    
+     
+     //! @brief 
+     //! @param name
+     //! @param value
+     //! @param desc
+     //! @return 
     template<typename Value>
     Has& method(const char* name, Value value, const char* desc = 0){
         if(not has_) has_ = name==name_;
         return *this;
     }
 
+    //! @brief 
+    //! @return 
     bool has(void) const {
         return has_;
     }
     
+    //! @brief 
+    //! @param object
+    //! @param name
+    //! @return 
     template<typename Object>
     static bool has(Object& object,const std::string& name){
         Has has(name);
@@ -239,12 +323,20 @@ private:
     
 public:
 
+    //! @brief 
+    //! @param name
+    //! @return 
     Get(const std::string& name):
         name_(name),
         object_(0),
         type_(""){
     }
 
+    //! @brief 
+    //! @param name
+    //! @param value
+    //! @param desc
+    //! @return 
     template<typename Value>
     Get& data(const char* name, Value* value, const char* desc = 0){
         if(name==name_){
@@ -254,16 +346,20 @@ public:
         return *this;
     }
     
+    //! @brief 
     void exception(void) const {
         STENCILA_THROW(Exception,"object does not have key:"+name_);
     }
     
+    //! @brief 
     void* object(void) const {
         if(object_) return object_;
         else exception();
         return 0;
     }
-
+    
+    //! @brief 
+    //! @return 
     std::string type(void) const {
         if(object_) return type_;
         else exception();
@@ -278,21 +374,34 @@ template<class Class> class Dispatch;
 
 template<>
 class Dispatch<void>{
-public:                                           
+public:
+    //! @brief 
+    //! @return 
     virtual Proxy create(void);
     
+    //! @brief 
+    //! @return 
     virtual std::string type(void* object){
         return "void";
     };
     
+    //! @brief 
+    //! @return 
     virtual std::vector<std::string> keys(void* object){
         return {};
     };
     
+    //! @brief 
+    //! @param name 
+    //! @return 
     virtual bool has(void* object, const std::string& name){
             return false;
     };
     
+    //! @brief 
+    //! @param object
+    //! @param name
+    //! @return 
     virtual Proxy get(void* object, const std::string& name);
     
     virtual std::string repr(void* object){
@@ -306,20 +415,33 @@ public:
 
     Proxy create(void);
 
+    //! @brief 
+    //! @return 
     std::string type(void* object){
         return Type::type(*static_cast<Class*>(object));
     };
 
+    //! @brief 
+    //! @return 
     std::vector<std::string> keys(void* object){
         return Keys::keys(*static_cast<Class*>(object));
     };
 
+    //! @brief 
+    //! @param name
+    //! @return 
     bool has(void* object, const std::string& name){
         return Has::has(*static_cast<Class*>(object),name);
     };
 
+    //! @brief 
+    //! @param object   
+    //! @param name
+    //! @return 
     Proxy get(void* object, const std::string& name);
 
+    //! @brief
+    //! @return  
     std::string repr(void* object){
         return Repr::repr(*static_cast<Class*>(object));
     }
@@ -331,6 +453,7 @@ private:
     std::map<std::string,Dispatch<void>*>* classes_;
     static Dispatch<void> dispatch_void_;
 
+    //! @brief 
     void init_(void){
         if(not inited_){
             classes_ = new std::map<std::string,Dispatch<void>*>;
@@ -346,21 +469,28 @@ public:
     ~Registry(){
         delete classes_;
     }
-
+    
+    //! @brief 
+    //! @param dispatch
     template<typename Class>
     void add(Dispatch<void>* dispatch){
         init_();
         const std::string name = Type().mirror<Class>().type();
         classes_->insert(std::pair<std::string,Dispatch<void>*>(name,dispatch));
     }
-
+    
+    //! @brief 
+    //! @param name
+    //! @return 
     Dispatch<void>* get(const std::string& name){
     init_();
         auto i = classes_->find(name);
         if(i==classes_->end()) return &dispatch_void_;
         else return i->second;
     }
-
+    
+    //! @brief 
+    //! @return 
     std::vector<std::string> types(void){
         std::vector<std::string> types;
         for(auto i=classes_->begin();i!=classes_->end();i++) types.push_back(i->first);
@@ -392,35 +522,51 @@ private:
     Dispatch<void>* dispatcher_;
     
 public:
+    //! @brief 
+    //! @param dispatcher
+    //! @return 
     Proxy(void* object, Dispatch<void>* dispatcher):
         object_(object),
         dispatcher_(dispatcher){
     }
-    
+    //! @brief 
     void* object(void){
         return object_;
     }
 
+    //! @brief 
+    //! @return 
     std::string type(void) const {
         return dispatcher_->type(object_);
     }
-
+    
+    //! @brief  
     std::vector<std::string> keys(void) const {
         return dispatcher_->keys(object_);
     }
 
+    //! @brief 
+    //! @param name
+    //! @return 
     bool has(const std::string& name){
         return dispatcher_->has(object_,name);
     };
 
+    //! @brief 
+    //! @param name
+    //! @return 
     Proxy get(const std::string& name){
         return dispatcher_->get(object_,name);
     }
 
+//! @brief 
+//! @param name
+//! @return 
     Proxy operator[](const std::string& name){
         return get(name);
     }
-
+    //! @brief 
+    //! @return 
     std::string repr(void) const {
         return dispatcher_->repr(object_);
     }
@@ -432,6 +578,10 @@ inline Proxy Dispatch<void>::create(void){
     STENCILA_THROW(Exception,"unable to create Proxy for Dispatch<void>");
 };
 
+//! @brief 
+//! @param object
+//! @param name
+//! @return 
 inline Proxy Dispatch<void>::get(void* object, const std::string& name){
     STENCILA_THROW(Exception,"no keys");
 };
@@ -444,6 +594,10 @@ Proxy Dispatch<Class>::create(void){
     return Proxy(new Class,this);
 };
 
+//! @brief 
+//! @param object
+//! @param name
+//! @return 
 template<class Class>
 Proxy Dispatch<Class>::get(void* object, const std::string& name){
     Get get(name);
