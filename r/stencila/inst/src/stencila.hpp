d7a259c1d0b7c950d0123b36055e1d0e45eca7c5
Nokome Bentley <nokome.bentley@stenci.la>
2014-04-09 06:50:24 +1200

    Tidies up R macros

--- r/stencila/inst/src/stencila.hpp
+++ r/stencila/inst/src/stencila.hpp
@@ -2,50 +2,20 @@
 
 #include <Rcpp.h>
 
-//Use Rcpp as (conversion from R to C++) and wrap (conversion from C++ to R)
-//functions for converting standard types, eg strings, vectors etc
+/*
+Conversion of atomic and standard library objects eg strings, vectors etc
+between C++ and R.
+
+Use Rcpp as (conversion from R to C++) and wrap (conversion from C++ to R).
+*/
 using Rcpp::as;
 using Rcpp::wrap;
 
-//! @{
-//! @name Macros etc
-//! Some macros and constants used for brevity and consistency in coding R functions.
-//! STENCILA_R_TRY and STENCILA_R_CATCH based on BEGIN_RCPP and END_RCPP macros.
-
-const decltype(R_NilValue) nil = R_NilValue;
-
-#define STENCILA_R_TO(TYPE,POINTER) \
-	to<TYPE>(POINTER,#TYPE)
-
-#define STENCILA_R_FUNC extern "C" SEXP
-
-#define STENCILA_R_TRY \
-    try {
-        
-#define STENCILA_R_CATCH \
-    } catch(std::exception& __ex__ ){ \
-        forward_exception_to_r( __ex__ ); \
-    } catch(...){ \
-        ::Rf_error( "Unknown C++ exception" ); \
-    }
-
-#define STENCILA_R_BEGIN \
-    try {
-
-#define STENCILA_R_END \
-    STENCILA_R_CATCH \
-    return nil;
-    
-//! @}
-
-/*! 
-@{
-@name Conversion functions
-
+/*
 Conversion of Stencila objects between C++ and R
 
 For Stencila classes use the usual "externalptr" mechanism
-but with the addition of externalptr's "tag" member. this allows
+but with the addition of externalptr's "tag" member. This allows
 type information to be stored on the R side for appropriate conversion to an R class
 
 Uses Rcpp::XPtr. An alternative here would be to use the R API functions
@@ -54,15 +24,15 @@ Uses Rcpp::XPtr. An alternative here would be to use the R API functions
     R_SetExternalPtrProtected
     R_RegisterCFinalizerEx
 See the following
-	http://dirk.eddelbuettel.com/code/rcpp/html/XPtr_8h_source.html
-	http://stackoverflow.com/questions/7032617/storing-c-objects-in-r
+    http://dirk.eddelbuettel.com/code/rcpp/html/XPtr_8h_source.html
+    http://stackoverflow.com/questions/7032617/storing-c-objects-in-r
 However, there appears to be no benefit of dealing with complexity of that,
 so just use Rcpp's constructor
 */
 #define STENCILA_R_USE_RCPP_XPTR 1
 
 #if !STENCILA_R_USE_RCPP_XPTR
-    //! R finaliser for R_RegisterCFinalizerEx
+    // R finaliser for R_RegisterCFinalizerEx
     template<typename Type>
     static void finalizer(SEXP pointer){
         delete static_cast<Type*>(R_ExternalPtrAddr(pointer));
@@ -70,7 +40,7 @@ so just use Rcpp's constructor
     }
 #endif
 
-//! Conversion from C++ to R
+// Conversion from C++ to R
 template<typename Type>
 SEXP to(Type* object, const char* classs){
     #if STENCILA_R_USE_RCPP_XPTR
@@ -85,7 +55,7 @@ SEXP to(Type* object, const char* classs){
     #endif
 }
 
-//! Conversion from R to C++
+// Conversion from R to C++
 template<typename Type>
 Type& from(SEXP self){
     #if STENCILA_R_USE_RCPP_XPTR
@@ -99,5 +69,74 @@ Type& from(SEXP self){
     #endif
 }
 
-//! @}
+/*
+Macros and constants used for brevity and consistency in coding R functions.
+*/
 
+const decltype(R_NilValue) null = R_NilValue;
+
+// Convert from C++ to R
+#define STENCILA_R_TO(TYPE,POINTER) \
+	to<TYPE>(POINTER,#TYPE)
+
+#define STENCILA_R_FUNC extern "C" SEXP
+
+// Define try/catch blocks
+// STENCILA_R_TRY and STENCILA_R_CATCH based on BEGIN_RCPP and END_RCPP macros.
+#define STENCILA_R_TRY \
+    try {
+        
+#define STENCILA_R_CATCH \
+    } catch(std::exception& __ex__ ){ \
+        forward_exception_to_r( __ex__ ); \
+    } catch(...){ \
+        ::Rf_error( "Unknown C++ exception" ); \
+    }
+
+// Define begin and end of a funciton
+#define STENCILA_R_BEGIN \
+    try {
+
+#define STENCILA_R_END \
+    STENCILA_R_CATCH \
+    return null;
+
+// Define a `new` method for a class
+#define STENCILA_R_NEW(CLASS) \
+STENCILA_R_FUNC CLASS##_new(void){ \
+    STENCILA_R_BEGIN \
+        return STENCILA_R_TO(CLASS,new CLASS); \
+    STENCILA_R_END \
+}
+
+// Call a method with 0 arguments and return value
+#define STENCILA_R_GET0(CLASS,NAME) \
+STENCILA_R_FUNC CLASS##_##NAME##_get(SEXP self){ \
+    STENCILA_R_BEGIN \
+        return wrap(from<CLASS>(self).NAME()); \
+    STENCILA_R_END \
+}
+
+// Call a method with 1 arguments and return `null`
+// (`null` is converted to `self` on R side so method chaining can
+// be used)
+#define STENCILA_R_EXEC1(CLASS,NAME,TYPE) \
+STENCILA_R_FUNC CLASS##_##NAME(SEXP self, SEXP arg1){ \
+    STENCILA_R_BEGIN \
+        from<CLASS>(self).NAME(as<TYPE>(arg1)); \
+        return null; \
+    STENCILA_R_END \
+}
+
+// Define a get method for an 
+#define STENCILA_R_GET(CLASS,NAME) \
+    STENCILA_R_GET0(CLASS,NAME,TYPE)
+
+// Define a set method for an attribute
+#define STENCILA_R_SET(CLASS,NAME,TYPE) \
+    STENCILA_R_EXEC1(CLASS,NAME,TYPE)
+
+// Define both get an set for an attribute
+#define STENCILA_R_ATTR(CLASS,NAME,TYPE) \
+    STENCILA_R_GET(CLASS,NAME)\
+    STENCILA_R_SET(CLASS,NAME,TYPE)
