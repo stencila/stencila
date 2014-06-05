3ac0f336cb7c8b1422596ace09afedb7d7a812ad
Nokome Bentley <nokome.bentley@stenci.la>
2014-06-05 13:52:39 +1200

    Tidies up "all" tests file

--- cpp/stencila/tests.cpp
+++ cpp/stencila/tests.cpp
@@ -1,9 +1,10 @@
-//! A C++ file to compile all tests into a single executable
+//Compile all tests into a single executable
+
 #define BOOST_TEST_MODULE tests
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem.hpp>
 
-//! @brief Global fixture for one off setup and teardown at start and end of testing
+// Global fixture for one off setup and teardown at start and end of testing
 struct GlobalFixture {
     GlobalFixture(void){
         
@@ -11,16 +12,25 @@ struct GlobalFixture {
 };
 BOOST_GLOBAL_FIXTURE(GlobalFixture);
 
+#define STENCILA_PYTHON_CONTEXTS 1
+#define STENCILA_R_CONTEXTS 1
+
 #include "component.cpp"
-
 #include "stencil.cpp"
+#include "network.cpp"
 
-#include "map-context.cpp"
-
-#include "dimension.cpp"
-#include "array-dynamic.cpp"
-#include "array-static.cpp"
-
-#include "git.cpp"
-#include "html.cpp"
-#include "xml.cpp"
+// Tests in alphabetical order
+#include "array-dynamic-tests.cpp"
+#include "array-static-tests.cpp"
+#include "component-tests.cpp"
+#include "dimension-tests.cpp"
+#include "git-tests.cpp"
+#include "html-tests.cpp"
+#include "json-tests.cpp"
+#include "map-context-tests.cpp"
+#include "python-context-tests.cpp"
+#include "query-tests.cpp"
+#include "r-context-tests.cpp"
+#include "stencil-tests.cpp"
+#include "traits-tests.cpp"
+#include "xml-tests.cpp"
