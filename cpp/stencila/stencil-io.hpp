ff46cc1077b4727d59dff001ec7016d97161905e
Nokome Bentley <nokome.bentley@stenci.la>
2014-08-14 21:10:44 +1200

    Extracts stencil I/O methods to `stencil-io.cpp`; other tidy-ups

---
+++ cpp/stencila/stencil-io.hpp
@@ -0,0 +1,101 @@
+#include <stecila/stencil.hpp>
+
+namespace Stencila {
+
+/**
+ * Initialise a stencil
+ * 
+ * @param  from A string indicating how the stecnil is initialised
+ */
+Stencil& Stencil::initialise(const std::string& from){
+    std::size_t found = from.find("://");
+    if(found==std::string::npos){
+        // Initialised from a path
+        read(from);
+    } else {
+        // Initialised from some content
+        std::string type = from.substr(0,found);
+        std::string content = from.substr(found+3);
+        if(type=="html") html(content);
+        else if(type=="cila") cila(content);
+        else if(type=="file") import(content);
+        else STENCILA_THROW(Exception,"Unrecognised content type: " + type);
+    }
+}
+
+/**
+ * Import the stencil content from a file
+ * 
+ * @param  path Filesystem path to file
+ */
+Stencil& Stencil::import(const std::string& path=""){
+    std::string ext = boost::filesystem::extension(path);
+    if(ext==".html" or ext==".cila"){
+        std::ifstream file(path);
+        std::stringstream stream;
+        stream<<file.rdbuf();
+        std::string content = stream.str();
+        if(ext==".html") html(content); 
+        else if(ext==".cila") cila(content);
+    }
+    else STENCILA_THROW(Exception,str(boost::format("File extension <%s> not valid for a Stencil")%ext));
+    return *this;
+}
+
+/**
+ * Export the stencil content to a file
+ * 
+ * @param  path Filesystem path to file
+ */
+Stencil& Stencil::export_(const std::string& path=""){
+    std::string ext = boost::filesystem::extension(path);
+    if(ext==".html" or ext==".cila"){
+        std::ofstream file(path);
+        if(ext==".html") file<<html(); 
+        else if(ext==".cila") file<<cila();
+    }
+    else STENCILA_THROW(Exception,str(boost::format("File extension <%s> not valid for a Stencil")%ext));
+    return *this;
+}
+
+/**
+ * Read the stencil from a directory
+ * 
+ * @param  directory Filesystem directory to directory
+ */
+Stencil& Stencil::read(const std::string& directory=""){
+    if(directory.length()){
+        // Check that directory exits and is a directory
+        if(not boost::filesystem::exists(directory)){
+            STENCILA_THROW(Exception,str(boost::format("Path <%s> does not exist")%directory));
+        }
+        if(not boost::filesystem::is_directory(directory)){
+            STENCILA_THROW(Exception,str(boost::format("Path <%s> is not a directory")%directory));
+        }
+        // Set the stencil's path
+        path(directory);
+    }
+    // Currently, set the stencil's content from main.cila
+    boost::filesystem::path cila = boost::filesystem::path(directory) / "main.cila";
+    if(not boost::filesystem::exists(cila)){
+        STENCILA_THROW(Exception,str(boost::format("Directory <%s> does contain a 'main.cila' file")%directory));
+    }
+    import(cila.string());
+    return *this;
+}
+
+/**
+ * Write the stencil to a directory
+ * 
+ * @param  path Filesystem path
+ */
+Stencil& Stencil::write(const std::string& directory=""){
+    // Set `path` if provided
+    if(directory.length()) Component::path(directory);
+    // Write necessary files
+    // @fixme This should use `export_()`
+    Component::write("main.html",html(true));
+    return *this;
+}
+
+}
--- cpp/stencila/stencil.hpp
+++ cpp/stencila/stencil.hpp
@@ -133,132 +133,99 @@ public:
     }
 
     /**
-     * Initialise a stencil
-     * 
-     * @param  from A string indicating how the stecnil is initialised
-     */
-    Stencil& initialise(const std::string& from){
-        std::size_t found = from.find("://");
-        if(found==std::string::npos){
-            // Initialised from a path
-            read(from);
-        } else {
-            // Initialised from some content
-            std::string type = from.substr(0,found);
-            std::string content = from.substr(found+3);
-            if(type=="html") html(content);
-            else if(type=="cila") cila(content);
-            else if(type=="file") import(content);
-            else STENCILA_THROW(Exception,"Unrecognised content type: " + type);
-        }
-    }
-
-    /**
-     * Get the content of the stencil
-     * 
-     * @param  format Format for content
-     */
-    std::string content(const std::string& format="html") const {
-        if(format=="html") return html();
-        else if(format=="cila") return cila();
-        else STENCILA_THROW(Exception,"Format code not recognised: "+format);
-    }
-
-    /**
-     * Set the content of the stencil
+     * @name Input and output
      *
-     * @param  format Format for content
-     */
-    Stencil& content(const std::string& format, const std::string& content){
-        if(format=="html") html(content);
-        else if(format=="cila") cila(content);
-        else STENCILA_THROW(Exception,"Format code not recognised: "+format);
-        return *this;
-    }
-
-    /**
-     * Get stencil content as HTML
-     */
-    std::string html(void) const {
-        // Dump content as html.
-        // Note that this explicitly avoids Stencil::dump
-        // and uses indentation
-        return Xml::Document::dump(true);
-    }
-
-    /**
-     * Set stencil content as HTML
-     *
-     * This method parses the supplied HTML, tidying it up in the process, and appends the resulting node tree
-     * to the stencil's XML tree
-     * 
-     * @param html A HTML string
-     */
-    Stencil& html(const std::string& html){
-        Html::Document doc(html);
-
-        // Being a valid HTML5 document, doc already has a <head> <title> and <body>
-        // so these do not have to be checked for
-        Node head = doc.find("head");
-        Node body = doc.find("body");
-
-        // Title
-        title(head.find("title").text());
-
-        // Keywords
-        if(Node elem = head.find("meta","name","keywords")){
-            std::string content = elem.attr("content");
-            std::vector<std::string> items;
-            boost::split(items,content,boost::is_any_of(","));
-            for(auto& keyword : items) boost::trim(keyword);
-            keywords(items);
-        }
-
-        // Description
-        if(Node elem = head.find("meta","name","description")){
-            std::string content = elem.attr("content");
-            description(content);
-        }       
-
-        // Contexts
-        if(Node elem = body.find("ul","id","contexts")){
-            std::vector<std::string> items;
-            for(auto& item : elem.all("li")){
-                std::string context = item.text();
-                boost::trim(context);
-                if(context.length()) items.push_back(context);
-            }
-            contexts(items);  
-        }
-
-        // Authors
-        if(Node elem = body.find("address","id","authors")){
-            std::vector<std::string> items;
-            for(auto& item : elem.all("a[rel=\"author\"]")){
-                items.push_back(item.text());
-            }
-            authors(items);  
-        }
-
-        // Content
-        // Clear content before appending new content from Html::Document
-        clear();
-        if(Node elem = body.find("main","id","content")){
-            append_children(elem);
-        }
-        append_children(doc.find("body"));
-        return *this;
-    }
-
-    /**
-     * @name Cila parsing and generating
-     *
-     * Method implementation in `stencil-cila.cpp`
+     * Methods implemented in `stencil-io.cpp`
      * 
      * @{
      */
 
-public:
+    /**
+     * Initialise a stencil
+     * 
+     * @param  from A string indicating how the stencil is initialised
+     */
+    Stencil& initialise(const std::string& from);
+
+    /**
+     * Import the stencil content from a file
+     * 
+     * @param  path Filesystem path to file
+     */
+    Stencil& import(const std::string& path="");
+
+    /**
+     * Export the stencil content to a file
+     * 
+     * @param  path Filesystem path to file
+     */
+    Stencil& export_(const std::string& path="");
+
+    /**
+     * Read the stencil from a directory
+     * 
+     * @param  directory Filesystem path to a directory
+     */
+    Stencil& read(const std::string& directory="");
+    
+    /**
+     * Write the stencil to a directory
+     * 
+     * @param  directory Filesystem path to a directory
+     */
+    Stencil& write(const std::string& directory="");
+
+    /**
+     * @}
+     */
+
+    /**
+     * @name HTML parsing and generation
+     *
+     * Methods implemented in `stencil-html.cpp`
+     * 
+     * @{
+     */
+    
+    /**
+     * Get stencil content as HTML
+     */
+    std::string html(bool document = false, bool indent = true) const;
+
+    /**
+     * Set stencil content as HTML
+     *
+     * This method parses the supplied HTML, tidying it up in the process, 
+     * and appends the resulting node tree to the stencil's XML tree
+     * 
+     * @param html A HTML string
+     */
+    Stencil& html(const std::string& html);
+
+    /**
+     * @}
+     */
+    
+
+    /**
+     * @name Cila parsing and generation
+     *
+     * Methods implemented in `stencil-cila.cpp`
+     * 
+     * @{
+     */
+
+    /**
+     * Get stencil content as Cila
+     */
+    std::string cila(void) const;
+
+    /**
+     * Get stencil content as Cila written to an output stream
+     *
+     * @param stream Output stream to write to
+     */
+    std::ostream& cila(std::ostream& stream) const;
 
     /**
      * Set stencil content using Cila
@@ -275,23 +242,11 @@ public:
     Stencil& cila(std::istream& stream);
 
     /**
-     * Get stencil content as Cila
-     */
-    std::string cila(void) const;
-
-    /**
-     * Get stencil content as Cila written to an output stream
-     *
-     * @param stream Output stream to write to
-     */
-    std::ostream& cila(std::ostream& stream) const;
-
-    /**
      * @}
      */
 
     /**
-     * @name Contexts
+     * @name Context
      * @{
      */
     
@@ -339,13 +294,34 @@ public:
     /**
      * @}
      */    
-
+    
     /**
-     * @name Rendering methods
+     * @name Theme
      * @{
      */
+private:
+
+    std::string theme_ = "core/stencils/themes/default";
 
 public:
+
+    const std::string& theme(void) const {
+        return theme_;
+    }
+
+    Stencil& theme(const std::string& theme) {
+        theme_ = theme;
+        return *this;
+    }
+
+    /**
+     * @}
+     */
+
+    /**
+     * @name Rendering
+     * @{
+     */
 
     /**
      * Render this stencil within a context
@@ -371,32 +347,6 @@ public:
      * @}
      */
 
-
-    /**
-     * @name Theme
-     * @{
-     */
-    
-private:
-
-    std::string theme_ = "core/stencils/themes/default";
-
-public:
-
-    const std::string& theme(void) const {
-        return theme_;
-    }
-
-    Stencil& theme(const std::string& theme) {
-        theme_ = theme;
-        return *this;
-    }
-
-    /**
-     * @}
-     */
-
-
     /**
      * @name Sanitize
      * @{
@@ -412,199 +362,6 @@ public:
     /**
      * @}
      */
-
-
-    /**
-     * Import the stencil content from a file
-     * 
-     * @param  path Filesystem path to file
-     */
-    Stencil& import(const std::string& path=""){
-        std::string ext = boost::filesystem::extension(path);
-        if(ext==".html" or ext==".cila"){
-            std::ifstream file(path);
-            std::stringstream stream;
-            stream<<file.rdbuf();
-            std::string content = stream.str();
-            if(ext==".html") html(content); 
-            else if(ext==".cila") cila(content);
-        }
-        else STENCILA_THROW(Exception,str(boost::format("File extension <%s> not valid for a Stencil")%ext));
-        return *this;
-    }
-
-    /**
-     * Export the stencil content to a file
-     * 
-     * @param  path Filesystem path to file
-     */
-    Stencil& export_(const std::string& path=""){
-        std::string ext = boost::filesystem::extension(path);
-        if(ext==".html" or ext==".cila"){
-            std::ofstream file(path);
-            if(ext==".html") file<<html(); 
-            else if(ext==".cila") file<<cila();
-        }
-        else STENCILA_THROW(Exception,str(boost::format("File extension <%s> not valid for a Stencil")%ext));
-        return *this;
-    }
-
-    /**
-     * Read the stencil from a directory
-     * 
-     * @param  directory Filesystem directory to directory
-     */
-    Stencil& read(const std::string& directory=""){
-        if(directory.length()){
-            // Check that directory exits and is a directory
-            if(not boost::filesystem::exists(directory)){
-                STENCILA_THROW(Exception,str(boost::format("Path <%s> does not exist")%directory));
-            }
-            if(not boost::filesystem::is_directory(directory)){
-                STENCILA_THROW(Exception,str(boost::format("Path <%s> is not a directory")%directory));
-            }
-            // Set the stencil's path
-            path(directory);
-        }
-        // Currently, set the stencil's content from main.cila
-        boost::filesystem::path cila = boost::filesystem::path(directory) / "main.cila";
-        if(not boost::filesystem::exists(cila)){
-            STENCILA_THROW(Exception,str(boost::format("Directory <%s> does contain a 'main.cila' file")%directory));
-        }
-        import(cila.string());
-        return *this;
-    }
-    
-    /**
-     * Write the stencil to a directory
-     * 
-     * @param  path Filesystem path
-     */
-    Stencil& write(const std::string& directory=""){
-        // Set `path` if provided
-        if(directory.length()) Component::path(directory);
-        // Write necessary files
-        // @fixme This should use `export_()`
-        Component::write("main.html",dump());
-        return *this;
-    }
-
-    /**
-     * Dump the stencil to a HTML document string
-     */
-    std::string dump(void) const {
-        Html::Document doc;
-
-        // Being a valid HTML5 document, doc already has a <head> <title> and <body>
-        Node head = doc.find("head");
-        Node body = doc.find("body");
-
-        // Title to <title>
-        // Although we are creating an XHTML5 document, if the title tag is empty (i.e <title />)
-        // this can cause browser parsing errors. So always ensure that there is some title content.
-        auto t = title();
-        if(t.length()==0) t = "Untitled";
-        head.find("title").text(t);
-
-        // Keywords to <meta>
-        auto k = keywords();
-        if(k.size()>0){
-            head.append("meta",{
-                {"name","keywords"},
-                {"content",boost::algorithm::join(k,", ")}
-            });
-        }
-
-        // Description to <meta>
-        auto d = description();
-        if(d.length()>0){
-            head.append("meta",{
-                {"name","description"},
-                {"content",d}
-            });
-        }
-
-        // The following tags are added with a space as content so that they do not
-        // get rendered as empty tags (e.g. <script... />). Whilst empty tags should be OK with XHTML
-        // they can cause problems with some browsers.
-
-        /**
-         * <link rel="stylesheet" ...
-         *
-         * Links to CSS stylesheets are [placed in the head](http://developer.yahoo.com/performance/rules.html#css_top) 
-         * Use a site-root relative path (by adding the leading forward slash) so that CSS can be served from 
-         * localhost.
-         */
-        std::string css = "/" + theme() + "/theme.css";
-        head.append("link",{
-            {"rel","stylesheet"},
-            {"type","text/css"},
-            {"href",css}
-        }," ");
-
-        /**
-         * #contexts
-         *
-         * Added as a <ul> in body
-         */
-        auto c = contexts();
-        if(c.size()>0){
-            Node contexts_elem = body.append("ul",{
-                {"id","contexts"}
-            }," ");
-            for(auto context : c){
-                contexts_elem.append("li",{
-                    {"class",context}
-                },context);
-            }
-        }
-
-        /**
-         * #authors
-         *
-         * Use both <address> and <a rel="author" ...> as suggested at http://stackoverflow.com/a/7295013 .
-         * The placement of <address> as a child of <body> should mean that this authors list applies to the whole document.
-         * See:
-            http://html5doctor.com/the-address-element/
-            http://www.w3.org/TR/html5/sections.html#the-address-element
-            http://stackoverflow.com/questions/7290504/which-html5-tag-should-i-use-to-mark-up-an-authors-name
-         */
-        auto a = authors();
-        if(a.size()>0){
-            auto authors_elem = body.append("address",{
-                {"id","authors"}
-            }," ");
-            for(auto author : authors()){
-                authors_elem.append("a",{
-                    {"rel","author"},
-                    {"href","#"}
-                },author);
-            }
-        }
-
-        /**
-         * #content
-         *
-         * Content is placed in a <main> rather than just using the <body> so that extra HTML elements can be added by the 
-         * theme without affecting the stencil's content.
-         */
-        auto content = body.append("main",{
-            {"id","content"}
-        }," ");
-        content.append(*this);
-
-        /**
-         * <script>
-         *
-         * Script elements are [placed at bottom of page](http://developer.yahoo.com/performance/rules.html#js_bottom)
-         * Files are with a fallback to hub.
-         */
-        body.append("script",{{"src","/core/themes/boot.js"}}," ");
-        body.append("script","if(!window.Stencila){window.StencilaHost='http://hub.stenci.la';document.write(unescape('%3Cscript src=\"http://hub.stenci.la/core/themes/boot.js\"%3E%3C/script%3E'))}");
-        body.append("script","window.Stencila.Booter.theme('" + theme() + "');");
-
-        return doc.dump();
-    }
 
     /**
      * Commit changes to this stencil
@@ -748,5 +505,3 @@ BOOST_PP_SEQ_FOR_EACH(STENCILA_LOCAL, ,STENCILA_STENCIL_TAGS)
 #undef STENCILA_LOCAL
 
 }
-
-#include <stencila/stencil-render.hpp>
