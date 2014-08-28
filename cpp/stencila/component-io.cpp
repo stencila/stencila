757d303891ebb431a43e85dc0744e9c7291f2f4c
Nokome Bentley <nokome.bentley@stenci.la>
2014-08-28 14:20:51 +1000

    Extract `Component` input/output methods to `component-io.cpp`

---
+++ cpp/stencila/component-io.cpp
@@ -0,0 +1,141 @@
+#include <stencila/component.hpp>
+
+namespace Stencila {
+
+std::string Component::path(bool ensure) const {
+	if(meta_){
+		if(meta_->path.length()==0 and ensure){
+			// Remove constness so that the setter can be called
+			const_cast<Component&>(*this).path(std::string(""));
+		}
+		return meta_->path;
+	} else {
+		if(ensure){
+			const_cast<Component&>(*this).path(std::string(""));
+			return meta_->path;
+		}
+		else return "";
+	}
+}
+
+Component& Component::path(const std::string& path) {
+	if(not meta_) meta_ = new Meta;
+	std::string current_path = meta_->path;
+	std::string new_path = path;
+	// If the current path is empty...
+	if(current_path.length()==0){
+		// If the new path is empty then...
+		if(new_path.length()==0){
+			// Create a unique one
+			boost::filesystem::path unique_path = stores()[1];
+			unique_path /= boost::filesystem::unique_path("temp/%%%%-%%%%-%%%%-%%%%");
+			boost::filesystem::create_directories(unique_path);
+			meta_->path = unique_path.string();
+		} else {
+			// Create the path if necessary
+			if(not boost::filesystem::exists(new_path)){
+				boost::filesystem::create_directories(new_path);
+			}
+			meta_->path = new_path;
+		}
+	} 
+	// If the current path is not empty...
+	else {
+		/// and the new path is not empty...
+		if(new_path.length()>0){
+			// create necessary directories for the following rename operation
+			boost::filesystem::create_directories(new_path);
+			// move (i.e rename) existing path to the new path.
+			boost::filesystem::rename(current_path,new_path);
+			meta_->path = new_path;
+		}
+	}
+	return *this;
+}
+
+Component& Component::path(const char* path) {
+	return Component::path(std::string(path));
+}
+
+std::string Component::address(bool ensure) {
+	std::string path = this->path(ensure);
+	if(path.length()>0){
+		for(auto store : stores()){
+			if(path.length()>store.length()){
+				if(path.substr(0,store.length())==store){
+					return path.substr(store.length()+1);
+				}
+			}
+		}
+	}
+	return "";
+}
+
+std::vector<Component::File> Component::list(const std::string& subdirectory){
+	using namespace boost::filesystem;
+	std::vector<File> files;
+	std::string dir = boost::filesystem::path(path()).parent_path().string() + subdirectory;
+	if(exists(dir) and is_directory(dir)){
+		directory_iterator end ;
+		for(directory_iterator iter(dir) ; iter != end ; ++iter){
+			File file;
+			file.name = iter->path().filename().string();
+			if(is_regular_file(iter->status())) file.type = "f";
+			else if(is_directory(iter->status())) file.type = "d";
+			else file.type = "o";
+			files.push_back(file);
+		}
+	}
+	// Sort alphabetically
+	std::sort(files.begin(),files.end(),File::by_name);
+	return files;
+}
+
+Component& Component::destroy(void){
+	boost::filesystem::path path_full = Component::path();
+	if(boost::filesystem::exists(path_full)){
+		boost::filesystem::remove_all(path_full);
+	}
+	return *this;
+}
+
+Component& Component::create(const std::string& path,const std::string& content){
+	boost::filesystem::path path_full(Component::path(true));
+	path_full /= path;
+	if(!boost::filesystem::exists(path_full)){
+		std::ofstream file(path_full.string());
+		file<<content;
+		file.close();
+	}
+	return *this;
+}
+
+Component& Component::write(const std::string& path,const std::string& content){
+	boost::filesystem::path path_full(Component::path(true));
+	path_full /= path;
+	std::ofstream file(path_full.string());
+	file<<content;
+	file.close();
+	return *this;
+}
+
+Component& Component::delete_(const std::string& path){
+	boost::filesystem::path path_full(Component::path());
+	path_full /= path;
+	if(boost::filesystem::exists(path_full)){
+		boost::filesystem::remove_all(path_full);
+	}
+	return *this;
+}
+
+Component& Component::read(const std::string& from){
+	path(from);
+	return *this;
+}
+
+Component& Component::write(const std::string& to){
+	path(to);
+	return *this;
+}
+
+}
--- cpp/stencila/component.hpp
+++ cpp/stencila/component.hpp
@@ -572,32 +572,19 @@ public:
 
 
     /**
+     * @name Input and output
+     *
+     * Methods implemented in `component-io.cpp`
+     * 
      * @{
-     * @name Persistence methods
      */
-    
-public:
 
     /**
      * Get the component's path
      *
      * @param ensure Ensure a path is set if component does not yet have one?
      */
-    std::string path(bool ensure = false) const {
-        if(meta_){
-            if(meta_->path.length()==0 and ensure){
-                // Remove constness so that the setter can be called
-                const_cast<Component&>(*this).path(std::string(""));
-            }
-            return meta_->path;
-        } else {
-            if(ensure){
-                const_cast<Component&>(*this).path(std::string(""));
-                return meta_->path;
-            }
-            else return "";
-        }
-    }
+    std::string path(bool ensure = false) const;
 
     /**
      * Set the component's path
@@ -607,66 +594,19 @@ public:
      * 
      * @param path Path to component
      */
-    Component& path(const std::string& path) {
-        if(not meta_) meta_ = new Meta;
-        std::string current_path = meta_->path;
-        std::string new_path = path;
-        // If the current path is empty...
-        if(current_path.length()==0){
-            // If the new path is empty then...
-            if(new_path.length()==0){
-                // Create a unique one
-                boost::filesystem::path unique_path = stores()[1];
-                unique_path /= boost::filesystem::unique_path("temp/%%%%-%%%%-%%%%-%%%%");
-                boost::filesystem::create_directories(unique_path);
-                meta_->path = unique_path.string();
-            } else {
-                // Create the path if necessary
-                if(not boost::filesystem::exists(new_path)){
-                    boost::filesystem::create_directories(new_path);
-                }
-                meta_->path = new_path;
-            }
-        } 
-        // If the current path is not empty...
-        else {
-            /// and the new path is not empty...
-            if(new_path.length()>0){
-                // create necessary directories for the following rename operation
-                boost::filesystem::create_directories(new_path);
-                // move (i.e rename) existing path to the new path.
-                boost::filesystem::rename(current_path,new_path);
-                meta_->path = new_path;
-            }
-        }
-        return *this;
-    }
+    Component& path(const std::string& path);
 
     /**
      * Set the component's path
      * 
      * This overload prevents ambiguities with path(bool) when calling path("")
      */
-    Component& path(const char* path) {
-        return Component::path(std::string(path));
-    }
+    Component& path(const char* path);
 
     /**
      * Get the address of the component
      */
-    std::string address(bool ensure = false) const {
-        std::string path = this->path(ensure);
-        if(path.length()>0){
-            for(auto store : stores()){
-                if(path.length()>store.length()){
-                    if(path.substr(0,store.length())==store){
-                        return path.substr(store.length()+1);
-                    }
-                }
-            }
-        }
-        return "";
-    }
+    std::string address(bool ensure = false);
 
     /**
      * List files and folders in a components directory 
@@ -679,73 +619,26 @@ public:
             return a.name<b.name;
         }
     };
-    std::vector<File> list(const std::string& subdirectory=""){
-        using namespace boost::filesystem;
-        std::vector<File> files;
-        std::string dir = boost::filesystem::path(path()).parent_path().string() + subdirectory;
-        if(exists(dir) and is_directory(dir)){
-            directory_iterator end ;
-            for(directory_iterator iter(dir) ; iter != end ; ++iter){
-                File file;
-                file.name = iter->path().filename().string();
-                if(is_regular_file(iter->status())) file.type = "f";
-                else if(is_directory(iter->status())) file.type = "d";
-                else file.type = "o";
-                files.push_back(file);
-            }
-        }
-        // Sort alphabetically
-        std::sort(files.begin(),files.end(),File::by_name);
-        return files;
-    }
+    std::vector<File> list(const std::string& subdirectory="");
 
     /**
      * Destroy the component's entire working directory
      */
-    Component& destroy(void){
-        boost::filesystem::path path_full = Component::path();
-        if(boost::filesystem::exists(path_full)){
-            boost::filesystem::remove_all(path_full);
-        }
-        return *this;
-    }
+    Component& destroy(void);
 
     /**
      * Create a file within the component's working directory
      * 
      * @param path Filesystem path within the working directory
      */
-    Component& create(const std::string& path,const std::string& content="\n"){
-        boost::filesystem::path path_full(Component::path(true));
-        path_full /= path;
-        if(!boost::filesystem::exists(path_full)){
-            std::ofstream file(path_full.string());
-            file<<content;
-            file.close();
-        }
-        return *this;
-    }
+    Component& create(const std::string& path,const std::string& content="\n");
 
-    Component& write(const std::string& path,const std::string& content){
-        boost::filesystem::path path_full(Component::path(true));
-        path_full /= path;
-        std::ofstream file(path_full.string());
-        file<<content;
-        file.close();
-        return *this;
-    }
+    Component& write(const std::string& path,const std::string& content);
 
     /**
      * Delete a file within the component's working directory
      */
-    Component& delete_(const std::string& path){
-        boost::filesystem::path path_full(Component::path());
-        path_full /= path;
-        if(boost::filesystem::exists(path_full)){
-            boost::filesystem::remove_all(path_full);
-        }
-        return *this;
-    }
+    Component& delete_(const std::string& path);
 
     /**
      * Read the component from a directory
@@ -758,10 +651,7 @@ public:
      * 
      * @param from Filesystem path to component
      */
-    Component& read(const std::string& from=""){
-        path(from);
-        return *this;
-    }
+    Component& read(const std::string& from="");
     
     /**
      * Write the Component to a directory
@@ -771,10 +661,7 @@ public:
      * 
      * @param to Filesystem path to component
      */
-    Component& write(const std::string& to=""){
-        path(to);
-        return *this;
-    }
+    Component& write(const std::string& to="");
     
     /**
      * @}
