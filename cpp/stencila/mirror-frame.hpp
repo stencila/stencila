463b8ac5ed28ffb94ab757df1360d49324dabb77
Nokome Bentley <nokome.bentley@stenci.la>
2014-10-12 20:56:52 +1100

    Adds Structure::write to Frame

--- cpp/stencila/mirror-frame.hpp
+++ cpp/stencila/mirror-frame.hpp
@@ -9,40 +9,26 @@
 namespace Stencila {
 namespace Mirrors {
 
-class FrameReader : public Mirror<FrameReader> {
+template<class Derived>
+class FrameIoer : public Mirror<Derived> {
 public:
 
-	FrameReader(const Frame& frame, const std::vector<std::string>& exclude, const std::string& prefix=""):
+	FrameIoer(const Frame& frame, const std::vector<std::string>& exclude, const std::string& prefix=""):
 		frame_(frame),
 		prefix_(prefix),
 		exclude_(exclude){}
 
+	using Mirror<Derived>::derived;
+
 	template<typename Data,typename... Args>
-	FrameReader& data(Data& data, const std::string& name, Args... args){
-		data_(data,name,IsStructure<Data>(),IsArray<Data>());
+	FrameIoer& data(Data& data, const std::string& name, Args... args){
+		derived().data_(data,name,IsStructure<Data>(),IsArray<Data>());
 		return *this;
 	}
-  
-private:
-	template<typename Data>
-	void data_(Data& data, const std::string& name, const std::true_type& is_structure, const std::false_type& is_array){
-		if(read_(name)) FrameReader(frame_,exclude_,prefix_+name+".").mirror(data);
-	}
 
-	template<typename Data>
-	void data_(Data& data, const std::string& name, const std::false_type& is_structure, const std::true_type& is_array){
-		if(read_(name)) FrameReader(frame_,exclude_,prefix_+name).mirror(data);
-	}
+protected:
 
-	template<typename Data>
-	void data_(Data& data, const std::string& name, const std::false_type& is_structure, const std::false_type& is_array){
-		if(read_(name)){
-			std::string label = prefix_+name;
-			if(frame_.has(label)) data = frame_.value<Data>(label);
-		}
-	}
-
-	bool read_(const std::string& name){
+	bool handle_(const std::string& name){
 		std::string label = prefix_+name;
 		return std::find(exclude_.begin(), exclude_.end(), label)==exclude_.end();
 	}
@@ -50,7 +36,55 @@ private:
 	const Frame& frame_;
 	const std::vector<std::string>& exclude_;
 	std::string prefix_;
+}; // class FrameIoer
+
+
+class FrameReader : public FrameIoer<FrameReader> {
+public:
+	FrameReader(const Frame& frame, const std::vector<std::string>& exclude, const std::string& prefix=""):
+		FrameIoer(frame,exclude,prefix){}
+
+	template<typename Data>
+	void data_(Data& data, const std::string& name, const std::true_type& is_structure, const std::false_type& is_array){
+		if(handle_(name)) FrameReader(frame_,exclude_,prefix_+name+".").mirror(data);
+	}
+
+	template<typename Data>
+	void data_(Data& data, const std::string& name, const std::false_type& is_structure, const std::true_type& is_array){
+		if(handle_(name)) FrameReader(frame_,exclude_,prefix_+name).mirror(data);
+	}
+
+	template<typename Data>
+	void data_(Data& data, const std::string& name, const std::false_type& is_structure, const std::false_type& is_array){
+		if(handle_(name)){
+			std::string label = prefix_+name;
+			if(frame_.has(label)) data = frame_.value<Data>(label);
+		}
+	}
 }; // class FrameReader
+
+class FrameWriter : public FrameIoer<FrameWriter> {
+public:
+	FrameWriter(const Frame& frame, const std::vector<std::string>& exclude, const std::string& prefix=""):
+		FrameIoer(frame,exclude,prefix){}
+
+	template<typename Data>
+	void data_(Data& data, const std::string& name, const std::true_type& is_structure, const std::false_type& is_array){
+		if(handle_(name)) FrameWriter(frame_,exclude_,prefix_+name+".").mirror(data);
+	}
+
+	template<typename Data>
+	void data_(Data& data, const std::string& name, const std::false_type& is_structure, const std::true_type& is_array){
+		if(handle_(name)) FrameWriter(frame_,exclude_,prefix_+name).mirror(data);
+	}
+
+	template<typename Data>
+	void data_(Data& data, const std::string& name, const std::false_type& is_structure, const std::false_type& is_array){
+		if(handle_(name)){
+			frame_.add(prefix_+name,data);
+		}
+	}
+}; // class FrameWriter
 
 }
 }
