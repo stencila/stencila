20a3d0f17656427e6e4a8f337b2b65cc421d7c8d
Nokome Bentley <nokome.bentley@stenci.la>
2015-03-07 07:44:10 +1300

    Adds branching methods and other changes to Git interaces

--- cpp/stencila/git.hpp
+++ cpp/stencila/git.hpp
@@ -67,6 +67,10 @@ public:
 
 /**
  * A [Git](http://git.org) repository.
+ *
+ * The methods exposed here are not always the same as the subcommands in git.
+ * In particular, these methods attempt to simplify a particular workflow and
+ * have been inspired by tools like `legit` (http://git-legit.org/) (e.g. `sprout`, `lop`).
  */
 class Repository {
 public:
@@ -78,8 +82,9 @@ public:
 	 * Create a new repository if it does not exist in the directory
 	 * 
 	 * @param path Filesystem path to the repository
+	 * @param initial_commit Perform an initial commit?
 	 */
-	void init(const std::string& path,bool commit=false);
+	void init(const std::string& path,bool initial_commit=false);
 
 	/**
 	 * Open a repository
@@ -124,6 +129,16 @@ public:
 	Repository& remote(const std::string& name,const std::string& url);
 
 	/**
+	 * Download changes from a remote
+	 */
+	void download(const std::string& name="origin");
+
+	/**
+	 * Upload changes to a remote
+	 */
+	void upload(const std::string& name="origin");
+
+	/**
 	 * Get a commit history for the repository
 	 */
 	std::vector<Commit> commits(void);
@@ -135,7 +150,14 @@ public:
 	 * @param name    Name of the commit author
 	 * @param email   Email of the commit author
 	 */
-	void commit(const std::string& message,const std::string& name,const std::string& email);
+	void commit(const std::string& message="",const std::string& name="",const std::string& email="");
+
+	/**
+	 * Checkout a specific tag 
+	 * 
+	 * @param ref Reference to checkout
+	 */
+	void checkout(const std::string& ref);
 
 	/**
 	 * Get a list of all tags on the repository
@@ -158,19 +180,54 @@ public:
 	void tag(const std::string& tag,const std::string& message,const std::string& name,const std::string& email);
 
 	/**
-	 * Checkout a specific tag 
-	 * 
-	 * @param tag Tag to checkout
+	 * Get a list of all branches
 	 */
-	void checkout(const std::string& tag);
+	std::vector<std::string> branches(void) const;
 
-	void pull(const std::string& name="origin");
+	/**
+	 * Get current branch
+	 */
+	std::string branch(void) const;
 
-	void push(const std::string& name="origin");
+	/**
+	 * Set current branch
+	 *
+	 * @param name Name of branch
+	 */
+	void branch(const std::string& name);
+
+	/**
+	 * Create a new branch
+	 *
+	 * @param new_branch Name of new branch to create
+	 * @param from_branch Name of existing branch to branch off
+	 * 
+	 */
+	void sprout(const std::string& new_branch, const std::string& from_branch = "master");
+
+	/**
+	 * Merge one branch into another branch
+	 *
+	 * @param from_branch Name of the branch to 
+	 * @param into_branch Name of the branch to merge commits into
+	 */
+	void merge(const std::string& from_branch, const std::string& into_branch = "master");
+
+	/**
+	 * Delete a branch
+	 */
+	void lop(const std::string& branch);
+
+	/**
+	 * Archive the repository at a paricular ref to 
+	 * another directory
+	 */
+	void archive(const std::string& ref, const std::string& to) const;
 
 private:
 
 	git_repository* repo_;
+	std::string path_;
 
 }; // end class Repository
 
