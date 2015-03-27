9a520e5ad87d50ff6539c6f7179c9f8e011844ee
Nokome Bentley <nokome.bentley@stenci.la>
2015-03-27 21:09:18 +1300

    Adds method to signin using user token set as an environment variable

--- cpp/stencila/hub.cpp
+++ cpp/stencila/hub.cpp
@@ -1,5 +1,6 @@
 #include <boost/network/utils/base64/encode.hpp>
 
+#include <stencila/host.hpp>
 #include <stencila/hub.hpp>
 #include <stencila/json.hpp>
 #include <stencila/http-client.hpp>
@@ -8,13 +9,13 @@ namespace Stencila {
 
 Hub hub;
 
-const std::string Hub::root_ = "http://127.0.0.1:8000/";
+const std::string Hub::root_ = "https://stenci.la/";
 
 Hub& Hub::signin(const std::string& username, const std::string& password){
-	Http::Request request(root_+"user/permit/");
+	Http::Request request(Http::GET,root_+"user/permit/");
 	request.auth_basic(username,password);
 	
-	auto response = client_.get(request);
+	auto response = client_.request(request);
 	Json::Document doc(response.body());
 	username_ = doc["username"].as<std::string>();
 	permit_ = doc["permit"].as<std::string>();
@@ -23,15 +24,21 @@ Hub& Hub::signin(const std::string& username, const std::string& password){
 }
 
 Hub& Hub::signin(const std::string& token){
-	Http::Request request(root_+"user/permit/");
-	request.header("Authorization","Token "+boost::network::utils::base64::encode<char>(token));
+	Http::Request request(Http::GET,root_+"user/permit/");
+	request.header("Authorization","Token "+token);
 	
-	auto response = client_.get(request);
+	auto response = client_.request(request);
 	Json::Document doc(response.body());
 	username_ = doc["username"].as<std::string>();
 	permit_ = doc["permit"].as<std::string>();
 	
 	return *this;
+}
+
+Hub& Hub::signin(void){
+	std::string token = Host::variable("STENCILA_TOKEN");
+	if(token.length()==0) STENCILA_THROW(Exception,"Environment variable STENCILA_TOKEN is not defined");
+	return signin(token);
 }
 
 std::string Hub::username(void) const {
@@ -44,24 +51,26 @@ Hub& Hub::signout(void){
 	return *this;
 }
 
-Hub::Document Hub::get(const std::string& path){
+Hub::Document Hub::request(Http::Method method, const std::string& path){
 	std::string url = root_ + path;
 	if(path.back()!='/') url += "/";
-	Http::Request request(url);
+	Http::Request request(method,url);
 	request.header("Authorization","Permit "+permit_);
-	auto response = client_.get(request);
+	auto response = client_.request(request);
 	Json::Document doc(response.body());
 	return doc;
 }
 
+Hub::Document Hub::get(const std::string& path){
+	return request(Http::GET,path);
+}
+
 Hub::Document Hub::post(const std::string& path){
-	std::string url = root_ + path;
-	if(path.back()!='/') url += "/";
-	Http::Request request(url);
-	request.header("Authorization","Permit "+permit_);
-	auto response = client_.post(request);
-	Json::Document doc(response.body());
-	return doc;
+	return request(Http::POST,path);
+}
+
+Hub::Document Hub::delete_(const std::string& path){
+	return request(Http::DELETE,path);
 }
 
 }
