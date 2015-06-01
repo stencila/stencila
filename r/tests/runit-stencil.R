eb916f74f03c37422e97d204fdfcc53719ca14fc
Nokome Bentley <nokome.bentley@stenci.la>
2015-06-02 11:04:38 +1200

    Fixes R tests

--- r/tests/runit-stencil.R
+++ r/tests/runit-stencil.R
@@ -34,7 +34,7 @@ render <- function(stencil,context=list()){
 }
 
 test.Stencil.render.exec <- function(){
-  render('<pre data-exec="r">a <- 42</pre><span data-write="a"></span>')
+  render('<pre data-exec="r">a <- 42</pre><span data-text="a"></span>')
 }
 
 test.Stencil.render.exec.1 <- function(){
@@ -49,37 +49,37 @@ test.Stencil.render.exec.image <- function(){
 }
 
 test.Stencil.render.text.numeric <- function(){
-  checkEquals('<span data-write="number">42</span>',
-       render('<span data-write="number">previous</span>',list(number=42)))
-  checkEquals('<span data-write="number">3.14</span>',
-       render('<span data-write="number">previous</span>',list(number=3.14)))
+  checkEquals('<span data-text="number">42</span>',
+       render('<span data-text="number">previous</span>',list(number=42)))
+  checkEquals('<span data-text="number">3.14</span>',
+       render('<span data-text="number">previous</span>',list(number=3.14)))
 }
 
 test.Stencil.render.text.string <- function(){
-  checkEquals('<span data-write="string">Bonjour</span>',
-       render('<span data-write="string">previous</span>',list(string='Bonjour')))
+  checkEquals('<span data-text="string">Bonjour</span>',
+       render('<span data-text="string">previous</span>',list(string='Bonjour')))
 }
 
 test.Stencil.render.text.container <- function(){
-  checkEquals('<span data-write="items">42, 3.14, a, string</span>',
-       render('<span data-write="items">previous</span>',list(items=list(42, 3.14, 'a', 'string'))))
-  checkEquals('<span data-write="items">1, 2, 3, 4, 5</span>',
-       render('<span data-write="items">previous</span>',list(items=1:5)))
+  checkEquals('<span data-text="items">42, 3.14, a, string</span>',
+       render('<span data-text="items">previous</span>',list(items=list(42, 3.14, 'a', 'string'))))
+  checkEquals('<span data-text="items">1, 2, 3, 4, 5</span>',
+       render('<span data-text="items">previous</span>',list(items=1:5)))
 }
 
 btest.Stencil.render.for <- function(){
   checkEquals(
     paste(
       '<ul data-for="item in items">',
-        '<li data-each="true" data-write="item" data-off="true" />',
-        '<li data-write="item" data-index="0">42</li>',
-        '<li data-write="item" data-index="1">3.14</li>',
-        '<li data-write="item" data-index="2">a</li>',
-        '<li data-write="item" data-index="3">string</li>',
+        '<li data-each="true" data-text="item" data-off="true" />',
+        '<li data-text="item" data-index="0">42</li>',
+        '<li data-text="item" data-index="1">3.14</li>',
+        '<li data-text="item" data-index="2">a</li>',
+        '<li data-text="item" data-index="3">string</li>',
       '</ul>',sep=''),
     render(paste(
       '<ul data-for="item in items">',
-        '<li data-each="true" data-write="item"></li>',
+        '<li data-each="true" data-text="item"></li>',
       '</ul>',sep=''),
       list(items=list(42, 3.14, 'a', 'string'))
     )
@@ -87,19 +87,19 @@ btest.Stencil.render.for <- function(){
 }
 
 test.Stencil.render.with.list <- function(){
-  checkEquals('<div data-with="list"><span data-write="string">Bonjour</span></div>',
-       render('<div data-with="list"><span data-write="string">previous</span></div>',list(list=list(string='Bonjour'))))
+  checkEquals('<div data-with="list"><span data-text="string">Bonjour</span></div>',
+       render('<div data-with="list"><span data-text="string">previous</span></div>',list(list=list(string='Bonjour'))))
 }
 
 test.Stencil.render.with.data.frame <- function(){
-  checkEquals('<div data-with="df"><span data-write="a">1, 2, 3</span></div>',
-       render('<div data-with="df"><span data-write="a">previous</span></div>',list(df=data.frame(a=1:3))))
+  checkEquals('<div data-with="df"><span data-text="a">1, 2, 3</span></div>',
+       render('<div data-with="df"><span data-text="a">previous</span></div>',list(df=data.frame(a=1:3))))
 }
 
 test.Stencil.render.if <- function(){
-  stencil <- Stencil('html://<div data-if="test"><span data-write="test">previous</span></div>')
-  checkEquals('<div data-if="test" data-off="true"><span data-write="test">previous</span></div>',render(stencil,list(test=FALSE)))
-  checkEquals('<div data-if="test"><span data-write="test">TRUE</span></div>',render(stencil,list(test=TRUE)))
+  stencil <- Stencil('html://<div data-if="test"><span data-text="test">previous</span></div>')
+  checkEquals('<div data-if="test" data-off="true"><span data-text="test">previous</span></div>',render(stencil,list(test=FALSE)))
+  checkEquals('<div data-if="test"><span data-text="test">TRUE</span></div>',render(stencil,list(test=TRUE)))
 }
 
 test.Stencil.render.if.else <- function(){
