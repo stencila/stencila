c08ccd0455ad94ab39417cd4d91bbcc654855386
Nokome Bentley <nokome.bentley@stenci.la>
2015-08-04 10:24:07 +1200

    Compile stencil to page.html

--- cpp/stencila/stencil-conversion.cpp
+++ cpp/stencila/stencil-conversion.cpp
@@ -162,7 +162,7 @@ Stencil& Stencil::pdf(const std::string& direction, const std::string& path,cons
 Stencil& Stencil::compile(void){
 	render();
 	auto home = boost::filesystem::path(path(true));
-	export_((home/"stencil.html").string());
+	export_((home/"page.html").string());
 	preview((home/"preview.png").string());
 	return *this;
 }
