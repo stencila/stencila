1c40e88431a6973260bd833b2bf6100fa280b73d
Nokome Bentley <nokome.bentley@stenci.la>
2015-10-01 18:17:15 +1300

    Fix in script loader

--- web/stencila/utilities.js
+++ web/stencila/utilities.js
@@ -6,7 +6,7 @@ function load(source,callback){
 	var head = document.getElementsByTagName("head")[0];
 	var script = document.createElement("script");
 	script.type = "text/javascript";
-	script.src  = (StencilaHost || '') + source;
+	script.src  = (window.StencilaHost || '') + source;
 	if(callback) script.onload = callback;
 	head.appendChild(script);
 }
