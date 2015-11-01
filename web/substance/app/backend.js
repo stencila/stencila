ce3af39357feef7c26e95dff3fa2c11075052a06
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-01 17:19:03 +0100

    Experimental: updating generated properties during save.
    But saving is actually not implemented yet.

--- web/substance/app/backend.js
+++ web/substance/app/backend.js
@@ -1,7 +1,8 @@
-var Stencil = require('../model/Stencil');
 var oo = require('substance/util/oo');
 var $ = require('substance/util/jquery');
 var _ = require('substance/util/helpers');
+var Stencil = require('../model/Stencil');
+var StencilNode = require('../model/StencilNode');
 
 var Backend = function() {
   this.apiUrl = "http://10.0.0.12:7373";
@@ -46,7 +47,6 @@ Backend.Prototype = function() {
     this._request('GET', this.apiUrl + docAddress + "@content", null, function(err, resultStr) {
       if (err) { console.error(err); cb(err); }
       var result = JSON.parse(resultStr);
-      debugger;
       var doc = new Stencil();
       doc.loadHtml(result.content);
       doc.id = documentId;
@@ -55,8 +55,38 @@ Backend.Prototype = function() {
     });
   };
 
+  // http://10.0.0.12:7373/core/stencils/examples/kitchensink/@save
+  // http://10.0.0.12:7373/core/stencils/examples/kitchensink/@render
   this.saveDocument = function(doc, cb) {
-    cb('Not supported in dev version');
+    // Save the document, get the rendered html and update
+    // generated properties
+    var docAddress = DOC_ADDRESS;
+
+    // EXPERIMENTAL: using @render endpoint to play around with generated properties
+    this._request('PUT', this.apiUrl + docAddress + "@render", {
+      'format': 'html',
+      'content': doc.toHtml()
+    }, function(err, resultStr) {
+      if (err) return cb(err);
+      var result = JSON.parse(resultStr);
+      // creating a new document instance from the returned html
+      // In future the server could provide a different format
+      // containing only the rendered content as json
+      var tmp = new Stencil();
+      tmp.loadHtml(result.content);
+      _.each(tmp.get('body').nodes, function(nodeId) {
+        var node = doc.get(nodeId);
+        var copy = tmp.get(nodeId);
+        if (!node) {
+          console.warn('Node not present in document', nodeId);
+          return;
+        }
+        if (node instanceof StencilNode) {
+          node.updateGeneratedProperties(copy);
+        }
+      });
+    });
+    console.warn('Not saving yet.');
   };
 
   // Figure related
