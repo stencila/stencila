e4b722823dcb0c4b149856ff1f97d65cda0d6a8a
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-11-01 17:17:51 +0100

    Introduced StencilNode class.
    StencilNodes declare which properties are generated.

---
+++ web/substance/model/StencilNode.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var OO = require('substance/util/oo');
+var DocumentNode = require('substance/model/DocumentNode');
+var _ = require('substance/util/helpers');
+
+function StencilNode() {
+  DocumentNode.apply(this, arguments);
+}
+
+StencilNode.Prototype = function() {
+  this.updateGeneratedProperties = function(props) {
+    var propNames = this.constructor.static.generatedProps;
+    if (propNames) {
+      _.each(propNames, function(propName) {
+        this[propName] = props[propName];
+      }, this);
+      this.emit('properties:changed');
+    }
+  };
+};
+
+OO.inherit(StencilNode, DocumentNode);
+
+StencilNode.static.generatedProps = [];
+
+module.exports = StencilNode;
