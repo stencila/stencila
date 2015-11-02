3509372aeb292a1131e5e33d3eb1167a288c59e3
Michael Aufreiter <michael@substance.io>
2015-11-02 15:09:31 +0100

    Fix package.json and postinstall script.

--- web/postinstall.sh
+++ web/postinstall.sh
@@ -4,7 +4,7 @@ echo "Creating build directories"
 mkdir -p build/fonts
 
 echo "Setting font-awesome 'fa-font-path' variable"
-sed -i 's!\$fa-font-path: .*$!\$fa-font-path: "/web/fonts/";!' node_modules/font-awesome/scss/_variables.scss
+sed -i.back 's!\$fa-font-path: .*$!\$fa-font-path: "/web/fonts/";!' node_modules/font-awesome/scss/_variables.scss
 
 echo "Copying font-awesome fonts"
 cp -f node_modules/font-awesome/fonts/* build/fonts
