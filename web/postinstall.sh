c231b265bf5cb33fcdfa1525969bdb438e115ece
Nokome Bentley <nokome.bentley@stenci.la>
2015-11-02 15:18:58 +0100

    Add ACE editor

--- web/postinstall.sh
+++ web/postinstall.sh
@@ -3,8 +3,13 @@
 echo "Creating build directories"
 mkdir -p build/fonts
 
+
 echo "Setting font-awesome 'fa-font-path' variable"
 sed -i '' 's!\$fa-font-path: .*$!\$fa-font-path: "/web/fonts/";!' node_modules/font-awesome/scss/_variables.scss
 
 echo "Copying font-awesome fonts"
 cp -f node_modules/font-awesome/fonts/* build/fonts
+
+
+echo "Building Ace editor"
+cd node_modules/ace && npm install
