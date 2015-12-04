3947deaa473cdf60136ce2223509dcb1fd234360
Nokome Bentley <nokome.bentley@stenci.la>
2015-12-04 09:08:26 -0800

    Migrate text

---
+++ web/stencil/packages/text/StencilTextHTMLConverter.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+
+  type: 'stencil-text',
+  tagName: 'span',
+
+  matchElement: function(el) {
+    return el.is('[data-text]');
+  },
+
+  import: function(el, text, converter) {
+    text.tagName = el.tagName;
+    text.source = el.attr('data-text');
+    text.error = el.attr('data-error');
+    text.output = el.text();
+  },
+
+  export: function(text, el, converter) {
+    el.tagName = text.tagName;
+    el.attr('data-text',text.source);
+    el.text(text.output);
+    if(text.error) el.attr('data-error',text.error);
+  }
+};
