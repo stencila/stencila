6a8934eb5044849374fb6817d33dcdaf3f3568ab
Nokome Bentley <nokome.bentley@stenci.la>
2016-02-05 18:49:21 +0500

    Adds getting of functions list and definition

--- web/sheet/engine/SheetRemoteEngine.js
+++ web/sheet/engine/SheetRemoteEngine.js
@@ -11,6 +11,26 @@ function SheetRemoteEngine() {
 
 SheetRemoteEngine.Prototype = function() {
 
+  /**
+   * Get a list of function names available in the sheet's context
+   */
+  this.functions = function(cb) {
+    this._request('GET', 'functions', null, function(err, result) {
+      if (err) return cb(err);
+      cb(null, result);
+    });
+  };
+
+  /**
+   * Get a function definition from the sheet's context
+   */
+  this.function = function(name, cb) {
+    this._request('GET', 'function', {name:name}, function(err, result) {
+      if (err) return cb(err);
+      cb(null, result);
+    });
+  };
+
   this.update = function(cells, cb) {
     this._request('PUT', 'update', cells, function(err, result) {
       if (err) return cb(err);
