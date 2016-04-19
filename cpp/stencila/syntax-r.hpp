9356deebe0aa53b56d0a5f49ad0256d339871c5a
Nokome Bentley <nokome.bentley@stenci.la>
2016-04-19 13:31:14 +1200

    Distinguish between two types of R generator

--- cpp/stencila/syntax-r.hpp
+++ cpp/stencila/syntax-r.hpp
@@ -8,20 +8,26 @@
 namespace Stencila {
 namespace Syntax {
 
+/**
+ * This R code generator does not attempt to convert Excel function
+ * names and instead relies on the existence of compatability functions with the
+ * same names in R e.g. `AVERAGE`
+ */
+class ExcelToRSheetGenerator : public CodeGenerator {
+ public:
+    using CodeGenerator::CodeGenerator;
+};
+
+/**
+ * This R code generator converts Excel function calls into their
+ * equivalents in R using only R functions. As such it does not rely on the
+ * compatability functions available in the stencila R package.
+ */
 class ExcelToRGenerator : public CodeGenerator {
  public:
     using CodeGenerator::CodeGenerator;
 
-    /**
-     * Translate an Excel call to an R call
-     */
     void visit_call(const Call* call);
-
-    /**
-     * Map for translating Excel function names to R function names
-     */
-    static const std::map<std::string, std::string> function_map;
-
 };
 
 }
