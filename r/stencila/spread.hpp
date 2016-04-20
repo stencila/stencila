173f830dd86dbadb056a4663d25390f853f84c5d
Nokome Bentley <nokome.bentley@stenci.la>
2016-04-21 07:05:18 +1200

    Fix `call_` for clang + some delinting

--- r/stencila/spread.hpp
+++ r/stencila/spread.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 #include <Rcpp.h>
-
 // Undefine some macros that R defines which clash
 // with those used below
 #undef ERROR
@@ -16,9 +16,8 @@
 namespace Stencila {
 
 class RSpread : public Spread {
-public:
-
-    RSpread(SEXP sexp) {
+ public:
+    explicit RSpread(SEXP sexp) {
         spread_ = Rcpp::Environment(sexp);
     }
 
@@ -32,29 +31,29 @@ public:
      *
      * @{
      */
-    
+
     std::string execute(const std::string& package) {
-        return call_<std::string>(".execute", package);
+        return call_(".execute", package);
     }
 
     std::string evaluate(const std::string& expression) {
-        return call_<std::string>(".evaluate", expression, "eval", true);
+        return call_(".evaluate", expression, "eval", true);
     }
 
     std::string set(const std::string& id, const std::string& expression, const std::string& name = "") {
-        return call_<std::string>(".set", id, expression, name);
+        return call_(".set", id, expression, name);
     }
 
     std::string get(const std::string& name) {
-        return call_<std::string>(".get", name);
+        return call_(".get", name);
     }
 
     std::string clear(const std::string& id = "", const std::string& name = "") {
-        return call_<std::string>(".clear", id, name);
+        return call_(".clear", id, name);
     }
 
     std::string list(void) {
-        return call_<std::string>(".list");
+        return call_(".list");
     }
 
     std::string collect(const std::vector<std::string>& cells) {
@@ -62,26 +61,26 @@ public:
     }
 
     std::string depends(const std::string& expression) {
-        return call_<std::string>(".depends", expression);
+        return call_(".depends", expression);
     }
 
     std::vector<std::string> functions(void) {
-        return split(call_<std::string>(".functions"), ",");
+        return split(call_(".functions"), ",");
     }
 
     Function function(const std::string& name) {
         Rcpp::Function method = spread_.get(".function");
-        Rcpp::Language call(method,name);
+        Rcpp::Language call(method, name);
         Function func = from<Function>(call.eval());
         return func;
     }
 
     void read(const std::string& path) {
-        call_<std::string>(".read", path);
+        call_(".read", path);
     }
 
     void write(const std::string& path) {
-        call_<std::string>(".write", path);
+        call_(".write", path);
     }
 
     /**
@@ -91,14 +90,13 @@ public:
     /**
      * Initialisation of this class
      */
-    static void class_init(void){
-        Class::set(RSpreadType,{
+    static void class_init(void) {
+        Class::set(RSpreadType, {
             "RSpread"
         });
     }
 
-private:
-
+ private:
     /**
      * An Rcpp object which represents this spread on the R side
      */
@@ -106,29 +104,24 @@ private:
 
     /**
      * Call a method on the R side spread
+     * 
+     * Currently, this function only handles strings returned from R and then casts those
+     * using boost::lexical_cast. I got serious errors of the form:
+     *    memory access violation at address: 0x7fff712beff8: no mapping at fault address
+     * when trying to use Rcpp::as<bool> or Rcpp::as<int> even when checking the returned SEXP was
+     * the correct type
      */
-    template<typename... Args>
-    SEXP call_(const char* name, Args... args){
+    template<
+        typename Result = std::string,
+        typename... Args
+    >
+    Result call_(const char* name, Args... args) {
         Rcpp::Function func = spread_.get(name);
-        Rcpp::Language call(func,args...);
-        return call.eval();
-    }
-
-    /**
-     * Call a method on the R side and get the return value
-     */
-    template<typename Result, typename... Args>
-    Result call_(const char* name, Args... args){
-        // Currently, this function only handles strings returned from R and then casts those
-        // using boost::lexical_cast. I got serious errors of the form:
-        //    memory access violation at address: 0x7fff712beff8: no mapping at fault address
-        // when trying to use Rcpp::as<bool> or Rcpp::as<int> even when checking the returned SEXP was
-        // the correct type
-        SEXP result = call_(name,args...);
-        if(TYPEOF(result)!=STRSXP) STENCILA_THROW(Exception,"R-side methods should return a string");
+        Rcpp::Language call(func, args...);
+        SEXP result = call.eval();
+        if (TYPEOF(result) != STRSXP) STENCILA_THROW(Exception,"R-side methods should return a string");
         return unstring<Result>(Rcpp::as<std::string>(result));
     }
-
 };
 
 }
