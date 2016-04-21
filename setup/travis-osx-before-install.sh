42353947979a3608489b2e7c854acabb90199f96
Nokome Bentley <nokome.bentley@stenci.la>
2016-04-22 06:20:45 +1200

    Extracts OS X system setup to separate script; fixes openssl LIB_DIR.

---
+++ setup/travis-osx-before-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# A script for installing system requirements on OS X
+# Avoids the need for lots of "if [[ "$TRAVIS_OS_NAME" == "osx" ]]; ..." statements
+# in the `.travis.yml` and easier to read for others wondering how to setup
+# for a build on their own OS X system
+
+brew update
+
+# GNU sed because OSX sed is old and does not have same behaviour as Linux sed
+# as expected in the Makefile
+brew install --default-names gnu-sed
+
+# Newer version of OpenSSL
+# Env vars need to be set to prevent linker error
+# See https://github.com/rust-lang/cargo/issues/2295#issuecomment-173144879
+brew install openssl
+brew link --force openssl
+export OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+export OPENSSL_LIB_DIR=$(brew --prefix openssl)"/lib"
+export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)"/include"
+
+# R
+brew tap homebrew/science
+brew install R
+
+# Lemon parser (Flex already installed)
+brew install lemon
+
