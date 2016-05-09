b05bb8b8cc2a85cd741796114cfbdba05576a467
Nokome Bentley <nokome.bentley@stenci.la>
2016-05-09 22:31:47 +1200

    Add f option

--- setup/travis-trusty-install.sh
+++ setup/travis-trusty-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get -yq remove r-base r-base-core
-sudo apt-get -yq install r-base=3.2.5-1trusty0 r-base-core=3.2.5-1trusty0 r-recommended=3.2.5-1trusty0 r-base-dev=3.2.5-1trusty0
+sudo apt-get -yqf install r-base=3.2.5-1trusty0 r-base-core=3.2.5-1trusty0 r-recommended=3.2.5-1trusty0 r-base-dev=3.2.5-1trusty0
  
