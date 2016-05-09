9902739dcc760942e8ed886f0f5dc27e80f9b44d
Nokome Bentley <nokome.bentley@stenci.la>
2016-05-09 22:26:45 +1200

    Different install options

--- setup/travis-trusty-install.sh
+++ setup/travis-trusty-install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sudo apt-get -yq remove r-base r-base-core
-sudo apt-get -yq --no-install-suggests --no-install-recommends --force-yes \
-    install r-base=3.2.5-1trusty0 r-base-core=3.2.5-1trusty0 r-recommended=3.2.5-1trusty0 r-base-dev=3.2.5-1trusty0
+sudo apt-get -yq install r-base=3.2.5-1trusty0 r-base-core=3.2.5-1trusty0 r-recommended=3.2.5-1trusty0 r-base-dev=3.2.5-1trusty0
  
