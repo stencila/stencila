2f0a699ac216afbe642a2d6c0eb647e51b4e5e5c
Nokome Bentley <nokome.bentley@stenci.la>
2016-05-22 10:10:13 +0400

    More Mardown tests

--- cpp/tests/markdown.cpp
+++ cpp/tests/markdown.cpp
@@ -28,6 +28,55 @@ BOOST_AUTO_TEST_CASE(html) {
   BOOST_CHECK_EQUAL(doc.md("```\ncode block\n```").html(), "<pre><code>code block\n</code></pre>\n");
 }
 
+BOOST_AUTO_TEST_CASE(html_doc_get) {
+  for(auto pair : std::vector<std::array<std::string,2>>{
+
+    {"> blockquote1\n", "<blockquote>blockquote1</blockquote>"},
+
+    {"  - a\n  - b\n", "<ul><li>a</li><li>b</li></ul>"},
+    {"1.  a\n2.  b\n", "<ol><li>a</li><li>b</li></ol>"},
+
+    {"    x = 42", "<pre><code>x = 42\n</code></pre>"},
+    {"``` r\nx = 42\n```", "<pre><code class=\"r\">x = 42\n</code></pre>"},
+
+    {"Para 1", "<p>Para 1</p>"},
+    {"Para 1\n\nPara 2", "<p>Para 1</p><p>Para 2</p>"},
+
+    {"# Heading 1", "<h1>Heading 1</h1>"},
+    {"## Heading 2", "<h2>Heading 2</h2>"},
+    {"### Heading 3", "<h3>Heading 3</h3>"},
+    {"#### Heading 4", "<h4>Heading 4</h4>"},
+    {"##### Heading 5", "<h5>Heading 5</h5>"},
+    {"###### Heading 6", "<h6>Heading 6</h6>"},
+
+    {"`code`", "<p><code>code</code></p>"},
+    {"Some `inline code`.", "<p>Some <code>inline code</code>.</p>"},
+
+    {"*emphasised*", "<p><em>emphasised</em></p>"},
+    {"**strong**", "<p><strong>strong</strong></p>"},
+
+    {"[link]()", "<p><a>link</a></p>"},
+    {"[link](url)", "<p><a href=\"url\">link</a></p>"},
+    {"[link](url \"title\")", "<p><a href=\"url\" title=\"title\">link</a></p>"},
+
+    {"![]()", "<p><img /></p>"},
+    {"![](url)", "<p><img src=\"url\" /></p>"},
+    {"![](url \"title\")", "<p><img src=\"url\" title=\"title\" /></p>"},
+
+    {"Para 1\n\nPara 2 *emphasised* and **strong**", "<p>Para 1</p><p>Para 2 <em>emphasised</em> and <strong>strong</strong></p>"},
+
+    {"Para\n\n<div>Block HTML</div>", "<p>Para</p><div>Block HTML</div>"},
+
+    // This currently throws a Start-end tags mismatch exception, needs debugging
+    //{"Some <span class=\"foo\">inline</span> HTML.", "Some <span class=\"foo\">inline</span> HTML."}
+
+  }) {
+      Markdown::Document md(pair[0]);
+      Xml::Document html = md.html_doc();
+      BOOST_CHECK_EQUAL(html.dump(),pair[1]);
+  }
+}
+
 BOOST_AUTO_TEST_CASE(html_doc_set) {
   for(auto pair : std::vector<std::array<std::string,2>>{
     
