4ed6bcdb0cb7ea23a8ab4d96131c5afef84eb925
Nokome Bentley <nokome.bentley@stenci.la>
2016-06-02 06:59:27 +1200

    Fixes to boost, cmark and appveyor

--- setup/appveyor-build.sh
+++ setup/appveyor-build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-pacman -S base-devel cmake gcc git mingw-w64-x86_64-python2-pip msys2-devel unzip zip
-pacman -S openssl libssh2
+pacman --needed --noconfirm -Sy pacman-mirrors
+pacman --noconfirm -Sy
+pacman --noconfirm -S base-devel cmake gcc git mingw-w64-x86_64-python2-pip msys2-devel unzip zip
+pacman --noconfirm -S openssl libssh2
