e5e2d21c01bc8b6af93aa5b7511b86385ad617ef
Nokome Bentley <nokome.bentley@stenci.la>
2016-06-02 07:27:37 +1200

    Quiet downloads, install Python

--- setup/appveyor-build.sh
+++ setup/appveyor-build.sh
@@ -2,5 +2,7 @@
 
 pacman --needed --noconfirm -Sy pacman-mirrors
 pacman --noconfirm -Sy
-pacman --noconfirm -S base-devel cmake gcc git mingw-w64-x86_64-python2-pip msys2-devel unzip zip
+pacman --noconfirm -S base-devel cmake gcc git python python2 mingw-w64-x86_64-python2-pip msys2-devel unzip zip
 pacman --noconfirm -S openssl libssh2
+
+ln -sf /usr/bin/python2 /usr/bin/python
