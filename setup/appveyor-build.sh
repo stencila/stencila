708fe18b5f9628bd21092b516b177fb3fa582840
Nokome Bentley <nokome.bentley@stenci.la>
2016-07-04 13:49:22 +1200

    Remove unecessary git package
    Avoids "pactoys-git and repman-git are in conflict"

--- setup/appveyor-build.sh
+++ setup/appveyor-build.sh
@@ -2,6 +2,6 @@
 
 pacman --needed --noconfirm -Sy pacman-mirrors
 pacman --noconfirm -Sy
-pacman --noconfirm -S base-devel git python python2
+pacman --noconfirm -S base-devel python python2
 pacman --noconfirm -S mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-python2-pip mingw-w64-x86_64-python3-pip
 pacman --noconfirm -S mingw-w64-x86_64-openssl mingw-w64-x86_64-libssh2 mingw-w64-x86_64-zlib
