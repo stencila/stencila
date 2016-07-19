004f67db26f9b28b3c9589dbd796642b9ce7338b
Nokome Bentley <nokome.bentley@stenci.la>
2016-07-19 22:16:04 +1200

    Don't reinstall stuff

--- setup/appveyor-build.sh
+++ setup/appveyor-build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Install MSYS2 packages
+# The `base-devel` package is aleady installed on Appveyor but if
+# setting up your own MSYS2 environment you'll need to include that too
 pacman --needed --noconfirm -Sy pacman-mirrors
 pacman --noconfirm -Sy
-pacman --noconfirm -S base-devel python python2
+pacman --noconfirm -S python python2
 pacman --noconfirm -S mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-python2-pip mingw-w64-x86_64-python3-pip
 pacman --noconfirm -S mingw-w64-x86_64-openssl mingw-w64-x86_64-libssh2 mingw-w64-x86_64-zlib
