11961b299da8cec7d886bd3feec9d82f8578cdef
Nokome Bentley <nokome.bentley@stenci.la>
2016-08-02 14:05:01 +1200

    Update `Overlayer` positioning

--- web/document/editors/visual/Overlayer.js
+++ web/document/editors/visual/Overlayer.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var Overlay = require('substance/ui/Overlay');
+var getRelativeBoundingRect = require('substance/util/getRelativeBoundingRect');
 
 var TextToolset = require('./TextToolset');
+var BlockToolset = require('./BlockToolset');
 
 /**
  * Overlay toolsets over the current slection
@@ -35,31 +37,58 @@ Overlayer.Prototype = function() {
       .css('top', this.top + 'px')
       .css('left', this.left + 'px');
     el.append(
-      $$(TextToolset).ref('textToolset')
+      $$(TextToolset).ref('textToolset'),
+      $$(BlockToolset).ref('blockToolset')
     );
     return el;
   }
 
   // Override of `position()` method
   this.position = function(hints) {
-    console.log(hints);
+    // At time of writing `Surface.getBoundingRectangleForSelection` was experimental and
+    // sometimes created a hints.rectangle that was an empty object (e.g. for node selections). This checks for that
+    // and if necessary tries to work out it's own selection rectangle
+    var position= null;
     if (hints) {
-      var contentWidth = this.el.htmlProp('offsetWidth');
-      var selectionMaxWidth = hints.rectangle.width;
+      if (hints.rectangle) {
+        if (hints.rectangle.top) {
+          position = hints.rectangle;
+        }
+      }
+    }
+    if(!position) {
+      console.warn('No selection rectangle provided, attempting to get one');
+      var surface = this.context.surfaceManager.getFocusedSurface();
+      var selection = surface.getSelection();
+      var componentEl = document.querySelector('[data-id=' + selection.getNodeId() + ']');
+      var containerEl = this.context.scrollPane.refs.content.el.el;
+      position = getRelativeBoundingRect(componentEl, containerEl);
+    }
 
-      // By default, Overlay are aligned center/bottom to the selection
-      var top = hints.rectangle.top + hints.rectangle.height;
-      this.el.css('top', top);
-      var leftPos = hints.rectangle.left + selectionMaxWidth/2 - contentWidth/2;
+    if (position) {
+      var overlay = {
+        height: this.el.htmlProp('offsetHeight'),
+        width: this.el.htmlProp('offsetWidth')
+      };
+
+      // By default, aligned top/center to the position
+      var top = position.top - overlay.height;
+      var left = position.left + position.width/2 - overlay.width/2;
       // Must not exceed left bound
-      leftPos = Math.max(leftPos, 0);
+      left = Math.max(left, 0);
       // Must not exceed right bound
-      var maxLeftPos = hints.rectangle.left + selectionMaxWidth + hints.rectangle.right - contentWidth;
-      leftPos = Math.min(leftPos, maxLeftPos);
-      this.el.css('left', leftPos);
+      var maxLeftPos = position.left + position.width + position.right - overlay.width;
+      left = Math.min(left, maxLeftPos);
+      
+      // Change position
+      this.el.css('top', top);
+      this.el.css('left', left);
 
+      // Store position for next rendering
       this.top = top;
-      this.left = leftPos;
+      this.left = left;
+    } else {
+      console.warn('No selection rectangle to position overlay');
     }
   };
 
