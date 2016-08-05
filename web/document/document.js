a0dac8786f7d690c5aac80be30b382d239a368fe
Nokome Bentley <nokome.bentley@stenci.la>
2016-08-05 14:41:13 +1200

    Add URL parameters for static, reveal and edit modes

--- web/document/document.js
+++ web/document/document.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var utilities = require('../shared/utilities');
 var code = require('../shared/code');
 
 /**
@@ -10,32 +11,43 @@ var code = require('../shared/code');
  * content 
  */
 window.onload = function() {
+  // Check parameters for options
+  var params = utilities.location.params();
+  var statico = (params.static || '0') != '0';
+  var reveal = (params.reveal || '0') != '0';
+  var edit = (params.edit || '0') != '0';
 
-  // Get the document content DOM element
-  var content = document.getElementById('content');
+  if (statico) {
 
-  // Get document content as HTML for rerendering by the `DocumentApp`
-  // and then hide content element
-  var html = '';
-  if (content) {
-    html = content.innerHTML;
-    content.style.display = 'none';
+    // Get the document content DOM element
+    var content = document.getElementById('content');
+
+    // Get document content as HTML for rerendering by the `DocumentApp`
+    // and then hide content element
+    var html = '';
+    if (content) {
+      html = content.innerHTML;
+      content.style.display = 'none';
+    }
+
+    // Mount application on page and fallback to 
+    // display orginal content on any error
+    //try {
+      var DocumentApp = require('./DocumentApp');
+      window.app = DocumentApp.mount({
+        html: html,
+        reveal: reveal,
+        edit: edit
+      }, document.body);
+
+      // Load ACE editor
+      code.loadAce();
+
+    //} catch (error) {
+    //  content.style.display = 'block';
+    //  console.error(error);
+    //}
+  
   }
-
-  // Mount application on page and fallback to 
-  // display orginal content on any error
-  //try {
-    var DocumentApp = require('./DocumentApp');
-    window.app = DocumentApp.mount({
-      html: html
-    }, document.body);
-
-    // Load ACE editor
-    code.loadAce();
-
-  //} catch (error) {
-  //  content.style.display = 'block';
-  //  console.error(error);
-  //}
 
 };
--- web/shared/utilities.js
+++ web/shared/utilities.js
@@ -1,13 +1,40 @@
+'use strict';
+
 // Dynamic loading of scripts
 function load(source, callback) {
-    var head = document.getElementsByTagName("head")[0];
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src  = (window.StencilaHost || '') + source;
-    if(callback) script.onload = callback;
-    head.appendChild(script);
-}
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src  = (window.StencilaHost || '') + source;
+  if(callback) script.onload = callback;
+  head.appendChild(script);
+};
+
+// Utilities related to `document.location`
+var location = {
+
+  /**
+   * Get the parameters of the query (`search`) part of the location
+   * 
+   * Based on http://stackoverflow.com/a/1099670/4625911
+   */
+  params : function() {
+    var qs = document.location.search.split('+').join(' ');
+
+    var params = {},
+      tokens,
+      re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+      params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+  }
+
+};
 
 module.exports = {
-	load: load
+  load: load,
+  location: location
 };
