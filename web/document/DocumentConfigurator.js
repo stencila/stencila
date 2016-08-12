4b005065e41e8fe185d872f65a21aae94a572e4e
Nokome Bentley <nokome.bentley@stenci.la>
2016-08-13 07:30:42 +1200

    Make all nodes have a local package (for test setups)

--- web/document/DocumentConfigurator.js
+++ web/document/DocumentConfigurator.js
@@ -21,45 +21,23 @@ function DocumentConfigurator() {
     defaultTextType: 'paragraph'
   });
 
-  // Import Substance packages, in alphabetical order
-  this.import(require('substance/packages/base/BasePackage'));
-  this.import(require('substance/packages/blockquote/BlockquotePackage'));
-  
-  // There is currently (circa beta4) limited supported for lists
-  // in Substance (including missing converters; no ordered support etc) 
-  // so not including lists at this time
-  // this.import(require('substance/packages/list/ListPackage'),{
-  //   enableMacro: true
-  // });
-  
-  this.import(require('substance/packages/paragraph/ParagraphPackage'));
-  this.import(require('substance/packages/subscript/SubscriptPackage'));
-  this.import(require('substance/packages/superscript/SuperscriptPackage'));
-  this.import(require('substance/packages/table/TablePackage'));
-
-
-  // Icons, not defined in Substance packages but used in our `BlockToolset`
-  this.addIcon('heading', { 'fontawesome': 'fa-header' });
-  this.addIcon('paragraph', { 'fontawesome': 'fa-paragraph' });
-  this.addIcon('list', { 'fontawesome': 'fa-list' });
-  this.addIcon('table', { 'fontawesome': 'fa-table' });
-  this.addIcon('blockquote', { 'fontawesome': 'fa-quote-right' });
-  this.addIcon('codeblock', { 'fontawesome': 'fa-code' });
-
-
-  // Import Stencila node packages, in "order of appearance"
+  // Import node packages, in "order of appearance"
   this.import(require('./nodes/title/TitlePackage'));
   this.import(require('./nodes/summary/SummaryPackage'));
   this.import(require('./nodes/heading/HeadingPackage'));
+  this.import(require('./nodes/paragraph/ParagraphPackage'));
 
   this.import(require('./nodes/emphasis/EmphasisPackage'));
   this.import(require('./nodes/strong/StrongPackage'));
+  this.import(require('./nodes/subscript/SubscriptPackage'));
+  this.import(require('./nodes/superscript/SuperscriptPackage')); 
   this.import(require('./nodes/code/CodePackage'));
   this.import(require('./nodes/link/LinkPackage'));
   this.import(require('./nodes/math/MathPackage'));
-  this.import(require('./nodes/emoji/EmojiPackage'));
+  this.import(require('./nodes/emoji/EmojiPackage')); 
 
   this.import(require('./nodes/image/ImagePackage'));
+  this.import(require('./nodes/blockquote/BlockquotePackage'));
   this.import(require('./nodes/codeblock/CodeblockPackage'));
 
   this.import(require('./nodes/execute/ExecutePackage'));
@@ -74,6 +52,17 @@ function DocumentConfigurator() {
   // Import UI packages
   this.import(require('./tools/ToolsPackage'));
   this.import(require('./editors/visual/VisualEditorPackage'));
+
+  // Icons, not defined in Substance packages but used in our `BlockToolset`
+  this.addIcon('heading', { 'fontawesome': 'fa-header' });
+  this.addIcon('paragraph', { 'fontawesome': 'fa-paragraph' });
+  this.addIcon('list', { 'fontawesome': 'fa-list' });
+  this.addIcon('table', { 'fontawesome': 'fa-table' });
+  this.addIcon('blockquote', { 'fontawesome': 'fa-quote-right' });
+  this.addIcon('codeblock', { 'fontawesome': 'fa-code' });
+
+  // CHECK Is this needed?
+  this.import(require('substance/packages/base/BasePackage'));
 
 }
 
