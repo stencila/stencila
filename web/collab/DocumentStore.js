3424246d200d4c2517e3b814160af47202985d4a
Nokome Bentley <nokome.bentley@stenci.la>
2016-08-16 18:37:06 +1200

    Initial redis backed implem of DocumentStore

--- web/collab/DocumentStore.js
+++ web/collab/DocumentStore.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var DocumentStoreBase = require('substance/collab/DocumentStore');
+var Err = require('substance/util/SubstanceError');
+var uuid = require('substance/util/uuid');
+
+var redis = require('redis');
 var extend = require('lodash/object/extend');
 
 /**
@@ -14,35 +18,69 @@ var extend = require('lodash/object/extend');
 function DocumentStore(config) {
   DocumentStore.super.apply(this, arguments);
 
-  this._jams = {
-  };
+  this.client = redis.createClient();
 }
 
 DocumentStore.Prototype = function() {
 
-  this._createDocument = function(props) {
-    console.log('DocumentStore._createDocument' + JSON.stringify(props));
-    this._jams[props.documentId] = props;
+  /**
+   * Create a new document record and return it
+   */
+  this.createDocument = function(args, cb) {
+    if (!args.documentId) {
+      args.documentId = uuid();
+    }
+
+    this.client.exists(args.documentId, function(err, exists) {
+      if (exists) {
+        return cb(new Err('DocumentStore.CreateError', {
+          message: 'Could not create because document already exists.'
+        }));
+      }
+
+      this.client.set(args.documentId, JSON.stringify(args), function(err, result) {
+        cb(err, args);
+      })
+
+    }.bind(this));
   };
 
-  this._deleteDocument = function(documentId) {
-    console.log('DocumentStore._deleteDocument ' + documentId);
-    delete this._jams[documentId];
+  this.documentExists = function(documentId, cb) {
+    this.client.exists(documentId, function(err, exists) {
+      cb(err, Boolean(exists));
+    });
   };
 
-  this._getDocument = function(documentId) {
-    console.log('DocumentStore._getDocument ' + documentId);
-    return this._jams[documentId];
+  this.getDocument = function(documentId, cb) {
+    this.client.get(documentId, function(err, result) {
+      cb(err, JSON.parse(result));
+    });
   };
 
-  this._updateDocument = function(documentId, props) {
-    var doc = this._jams[documentId];
-    extend(doc, props);
+  this.updateDocument = function(documentId, props, cb) {
+    this.client.get(documentId, function(err, result) {
+      if (err) cb(err);
+      var updated = JSON.parse(result);
+      extend(updated, props);
+      this.client.set(documentId, JSON.stringify(updated), function(err, result) {
+        cb(err, updated);
+      });
+    }.bind(this));
   };
 
-  this._documentExists = function(documentId) {
-    return Boolean(this._jams[documentId]);
+  this.deleteDocument = function(documentId, cb) {
+    this.client.multi()
+      // `GET` the document
+      .get(documentId)
+      // `DEL`ete the document
+      .del(documentId)
+      // Return the first reply, the one from `GET`
+      .exec(function(err, replies) {
+        if (err) return cb(err);
+        cb(null, JSON.parse(replies[0]));
+      });
   };
+ 
 };
 
 
