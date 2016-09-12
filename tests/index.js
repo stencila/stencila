171d1903de47e414e14825a3b57f93ce08026ca6
Nokome Bentley <nokome@stenci.la>
2016-09-12 20:09:19 +1200

    Attemp running all tests

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,4 @@
 // Run all tests
 
 require('./unit.js');
-
-// FIXME
-// Temporarily remove functional tests because they are failing to complete on
-// Travis (Xvfb issue?)
-// require('./functional.js');
+require('./functional.js');
