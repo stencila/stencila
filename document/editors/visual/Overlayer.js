b332482caeae60b69e196ca83d6f7bebf95f6ca1
Nokome Bentley <nokome@stenci.la>
2016-09-26 17:50:45 +1300

    New location of 'substance/packages/overlay/OverlayContainer'

--- document/editors/visual/Overlayer.js
+++ document/editors/visual/Overlayer.js
@@ -1,4 +1,4 @@
-import Overlay from 'substance/ui/OverlayContainer'
+import Overlay from 'substance/packages/overlay/OverlayContainer'
 import getRelativeBoundingRect from 'substance/util/getRelativeBoundingRect'
 
 import TextToolset from './TextToolset'
