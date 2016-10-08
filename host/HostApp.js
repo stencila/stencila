410bf0642a8dfa343f99c123eabb061ac3be3f7b
Nokome Bentley <nokome@stenci.la>
2016-10-09 09:00:29 +1300

    Instance -> Host

---
+++ host/HostApp.js
@@ -0,0 +1,99 @@
+import Component from 'substance/ui/Component'
+
+import RemoteHost from 'stencila/src/host/RemoteHost'
+
+class HostApp extends Component {
+
+  getInitialState () {
+    let host = this.props.data
+    return {
+      host: new RemoteHost(host.url)
+    }
+  }
+
+  render ($$) {
+    let host = this.props.data
+
+    let el = $$('div').addClass('ui container')
+
+    el.append(
+      $$('div').addClass('ui segment').text(
+        JSON.stringify(host.manifest)
+      )
+    )
+
+    el.append(
+      $$('form').addClass('ui form').append(
+        $$('div').addClass('ui field').append(
+          $$('input').attr({
+            type: 'text',
+            name: 'address',
+            placeholder: 'Enter a component address'
+          })
+        )
+      ).on('submit', (event) => {
+        let address = event.target.address.value
+        this.state.host.open(address)
+          .then(function (value) {
+            console.log(value)
+          })
+          .catch(function (error) {
+            console.error(error)
+          })
+        event.preventDefault()
+      })
+    )
+
+    let components = $$('div').addClass('ui horizontal list')
+    for (let component of host.components) {
+      components.append(
+        $$('div').addClass('ui item').append(
+          $$('i').addClass('ui tag icon'),
+          $$('div').addClass('ui content').append(
+            $$('div').addClass('ui header').append(
+              $$('a').addClass('ui link').attr({
+                href: component.url,
+                target: '_blank'
+              }).text(
+                component.title || 'Untitled'
+              )
+            ),
+            $$('span').text(component.type || '')
+          )
+        )
+      )
+    }
+    el.append(components)
+
+    let peers = $$('div').addClass('ui horizontal list')
+    for (let peer of host.peers) {
+      peers.append(
+        $$('div').addClass('ui item').append(
+          $$('i').addClass('ui tag icon'),
+          $$('div').addClass('ui content').append(
+            $$('div').addClass('ui header').append(
+              $$('a').addClass('ui link').attr({
+                href: peer.url,
+                target: '_blank'
+              }).text(
+                packageNames[peer.package]
+              )
+            ),
+            $$('span').text(peer.version || '')
+          )
+        )
+      )
+      console.log(peer)
+    }
+    el.append(peers)
+
+    return el
+  }
+
+}
+
+const packageNames = {
+  'py': 'Python'
+}
+
+export default HostApp
--- instance/instance.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-import browser from '../browser'
-import InstanceApp from './InstanceApp'
-
-browser(InstanceApp)
