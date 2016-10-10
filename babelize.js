d44d024d8262a16c0c3165527360e0aed89f6c74
Nokome Bentley <nokome@stenci.la>
2016-10-11 11:33:34 +1300

    Fix babelize

--- babelize.js
+++ babelize.js
@@ -1,11 +1,15 @@
 // Register the Babel require hook
+// Used in various entry points to automatically run Babel transpilation
+// on `require`d modules
 
 require('babel-register')({
+  // es2015 preset for `import` and `export` support
+  presets: [ 'es2015' ],
   // By default, `babel-register` ignores everything in `node-modules`
   // See https://babeljs.io/docs/usage/require/
   // Override that behaviour so that substance is transpiled
   ignore: function (filename) {
-    if (filename.match('.*/node_modules/substance/.+')) {
+    if (filename.match('.*/substance/.+')) {
       return false
     } else if (filename.match('.*/node_modules/.+')) {
       return true
