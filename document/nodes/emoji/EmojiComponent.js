d2bd8b4a600a3238bd5c901074c6ba7b6151b0d6
Nokome Bentley <nokome@stenci.la>
2016-10-12 19:11:08 +1300

    Allow to run out of browser

--- document/nodes/emoji/EmojiComponent.js
+++ document/nodes/emoji/EmojiComponent.js
@@ -2,7 +2,9 @@ import Component from 'substance/ui/Component'
 
 import emojione from 'emojione'
 // Consistent with making everying served locally (for offline use etc)...
-emojione.imagePathPNG = (window.stencila.root || '/web') + '/emojione/png/'
+if (typeof window !== 'undefined') {
+  emojione.imagePathPNG = (window.stencila.root || '/web') + '/emojione/png/'
+}
 
 class EmojiComponent extends Component {
 
