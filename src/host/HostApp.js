a5494196b52f70c6aea08ff79ecd845f4809d628
Nokome Bentley <nokome@stenci.la>
2016-10-26 13:57:34 +1300

    Updates to HostApp

--- src/host/HostApp.js
+++ src/host/HostApp.js
@@ -1,16 +1,21 @@
 import Component from 'substance/ui/Component'
 
+import HostClient from './HostClient'
+
 class HostApp extends Component {
 
   getInitialState () {
-    let host = this.props.data
+    let data = this.props.data
+    let host = new HostClient(data ? data.url : this.props.url)
     return {
-      //host: new RemoteHost(host.url)
+      host: host,
+      data: data
     }
   }
 
   render ($$) {
-    let host = this.props.data
+    let host = this.state.host
+    let data = this.state.data
 
     let el = $$('div').addClass('ui container').css({
       'margin-top': '10%'
@@ -27,67 +32,118 @@ class HostApp extends Component {
         )
       ).on('submit', (event) => {
         let address = event.target.address.value
-        this.state.host.open(address)
-          .then(function (value) {
-            console.log(value)
+        host.open(address)
+          .then(value => {
+            this.refresh()
+            //window.location = value.url
           })
-          .catch(function (error) {
+          .catch(error => {
             console.error(error)
           })
         event.preventDefault()
       })
     )
 
-    let components = $$('div').addClass('ui horizontal list')
-    for (let component of host.components) {
-      components.append(
-        $$('div').addClass('ui item').append(
-          $$('i').addClass('ui tag icon'),
-          $$('div').addClass('ui content').append(
-            $$('div').addClass('ui header').append(
-              $$('a').addClass('ui link').attr({
-                href: component.url,
-                target: '_blank'
-              }).text(
-                component.title || 'Untitled'
-              )
-            ),
-            $$('span').text(component.type || '')
+    if (data) {
+      let components = $$('div').addClass('ui list')
+      for (let component of data.components) {
+        components.append(
+          $$('div').addClass('ui item').append(
+            $$('i').addClass('ui tag icon'),
+            $$('div').addClass('ui content').append(
+              $$('div').addClass('ui header').append(
+                $$('a').addClass('ui link').attr({
+                  href: component.url,
+                  target: '_blank'
+                }).text(
+                  component.title || component.address
+                )
+              ),
+              $$('span').text(component.type || '')
+            )
           )
         )
-      )
-    }
-    el.append(components)
+      }
+      el.append(components)
 
-    let peers = $$('div').addClass('ui horizontal list')
-    for (let peer of host.peers) {
-      peers.append(
-        $$('div').addClass('ui item').append(
-          $$('i').addClass('ui tag icon'),
-          $$('div').addClass('ui content').append(
-            $$('div').addClass('ui header').append(
-              $$('a').addClass('ui link').attr({
-                href: peer.url,
-                target: '_blank'
-              }).text(
-                packageNames[peer.package]
-              )
-            ),
-            $$('span').text(peer.version || '')
+      let schemes = $$('div').addClass('ui horizontal list')
+      for (let scheme in data.schemes) {
+        let details = data.schemes[scheme]
+        schemes.append(
+          $$('div').addClass('ui item').append(
+            $$('div').addClass('ui content').append(
+              $$('span').text(scheme),
+              $$('i').addClass('ui icon ' + (details.enabled ? 'green check circle' : 'red minus circle'))
+            )
           )
         )
+      }
+      el.append(
+        $$('div').addClass('ui segment')
+          .append(schemes)
       )
-      console.log(peer)
+
+      let formats = $$('div').addClass('ui horizontal list')
+      for (let format of data.formats) {
+        formats.append(
+          $$('div').addClass('ui item').append(
+            $$('div').addClass('ui content').append(
+              $$('span').text(format)
+            )
+          )
+        )
+      }
+      el.append(formats)
+
+      let peers = $$('div').addClass('ui horizontal list')
+      for (let peer of data.peers) {
+        peers.append(
+          $$('div').addClass('ui item').append(
+            $$('i').addClass('ui tag icon'),
+            $$('div').addClass('ui content').append(
+              $$('div').addClass('ui header').append(
+                $$('a').addClass('ui link').attr({
+                  href: peer.url,
+                  target: '_blank'
+                }).text(
+                  {
+                    'py': 'Python',
+                    'r': 'R'
+                  }[peer.package] || peer.package
+                )
+              ),
+              $$('span').text(peer.version || '')
+            )
+          )
+        )
+      }
+      el.append(peers)
+      el.append(
+        $$('button').addClass('ui button')
+          .text('Discover')
+          .on('click', () => {
+            host.discover().then(() => {
+              this.refresh()
+            })
+          })
+      )
     }
-    el.append(peers)
 
     return el
   }
 
-}
+  didMount () {
+    if (!this.state.data) this.refresh()
+  }
 
-const packageNames = {
-  'py': 'Python'
+  refresh () {
+    this.state.host.show().then(data => {
+      this.extendState({
+        data: data
+      })
+    })
+  }
+
 }
 
 export default HostApp
