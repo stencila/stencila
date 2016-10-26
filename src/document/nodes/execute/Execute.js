50a3a7f6a49096b2440d5477a166abccc9781e59
Nokome Bentley <nokome@stenci.la>
2016-10-26 13:57:59 +1300

    Preliminary implem of execute directive

--- src/document/nodes/execute/Execute.js
+++ src/document/nodes/execute/Execute.js
@@ -4,14 +4,30 @@ class Execute extends BlockNode {
 
   refresh () {
     if (this.source) {
-      try {
-        this.document.execute(this.source)
-        this.error = false
-      } catch (error) {
-        this.error = true
-        throw error
+      // Check list of sessions and open a new session if necessary
+      if (!this.document.session) {
+        this.document.host.new('session-' + this.language).then(session => {
+          this.document.session = session
+          this.refresh()
+        })
+      } else {
+        try {
+          let timer = window.performance
+          let t0 = timer.now()
+          this.document.session.execute(this.source).then(result => {
+            this.duration = (timer.now() - t0) / 1000
+            this.result = result
+          })
+        } catch (error) {
+          this.result = {
+            errors: {
+              '0': error.toString()
+            }
+          }
+          throw error
+        }
+        this.emit('changed')
       }
-      this.emit('content:changed')
     }
   }
 
@@ -21,9 +37,10 @@ Execute.define({
   type: 'execute',
   language: { type: 'string', default: '' },
   show: { type: 'boolean', default: false },
-  error: { type: 'string', optional: true },
   extra: { type: 'string', optional: true },
-  source: { type: 'string', default: '' }
+  source: { type: 'string', default: '' },
+  result: { type: 'object', default: {} },
+  duration: { type: 'number', default: 0 }
 })
 
 export default Execute
--- src/document/nodes/execute/ExecuteComponent.js
+++ src/document/nodes/execute/ExecuteComponent.js
@@ -1,16 +1,85 @@
+import Component from 'substance/ui/Component'
 import CodeEditorComponent from '../../ui/CodeEditorComponent'
 
-class ExecuteComponent extends CodeEditorComponent {
-
-  constructor (parent, props) {
-    props.codeProperty = 'source'
-    props.languageProperty = 'language'
-    super(parent, props)
-  }
+class ExecuteComponent extends Component {
 
   render ($$) {
-    return super.render.call(this, $$)
+    let node = this.props.node
+    let el = super.render.call(this, $$)
       .addClass('sc-execute')
+      .append(
+        $$('div')
+          .append(
+
+            $$('input')
+              .attr({
+                value: node.language,
+                placeholder: 'Execution language',
+                spellcheck: 'false'
+              })
+              .on('change', (event) => {
+                this.context.documentSession.transaction(function (tx, args) {
+                  tx.set([node.id, 'language'], event.target.value)
+                })
+              }),
+
+            $$('button')
+              .text('refresh')
+              .on('click', (event) => {
+                node.refresh()
+              }),
+
+            $$('span')
+              .text(node.duration.toString())
+          ),
+
+        $$(CodeEditorComponent, {
+          node: node,
+          codeProperty: 'source',
+          languageProperty: 'language'
+        })
+      )
+
+    var errors = node.result.errors
+    if (errors) {
+      el.append(
+        $$('div').text(errors.toString())
+      )
+    }
+
+    var output = node.result.output
+    if (output) {
+      let format = output.format
+      let content = output.content
+      let outputEl
+      if (format === 'png') {
+        outputEl = $$('img').attr({
+          src: content
+        })
+      } else if (format === 'csv') {
+        let table = ''
+        content.split('\n').forEach(function (row) {
+          table += '<tr>'
+          row.split(',').forEach(function (cell) {
+            table += '<td>' + cell + '</td>'
+          })
+          table += '</tr>'
+        })
+        outputEl = $$('table').html(table)
+      } else {
+        outputEl = $$('pre').text(content || '')
+      }
+      el.append(outputEl)
+    }
+
+    return el
+  }
+
+  didMount () {
+    let node = this.props.node
+    node.on('changed', () => {
+      this.rerender()
+    })
   }
 
 }
