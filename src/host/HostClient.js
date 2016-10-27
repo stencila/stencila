f3b7b583a182f0c12334a558eb3b9a5cf86d621a
Nokome Bentley <nokome@stenci.la>
2016-10-27 16:50:28 +1300

    Create base class ComponentClient and derive from that

--- src/host/HostClient.js
+++ src/host/HostClient.js
@@ -1,22 +1,15 @@
+import ComponentClient from '../component/ComponentClient'
 import SessionClient from '../session/SessionClient'
 
-class HostClient {
-
-  constructor (url) {
-    this._url = url
-  }
-
-  show (address) {
-    return this._request('GET', this._url)
-  }
+class HostClient extends ComponentClient {
 
   open (address) {
-    return this._request('GET', this._url + '/' + address)
+    return this.request('GET', this._url + '/' + address)
   }
 
   new (type) {
     return new Promise((resolve, reject) => {
-      this._request('GET', this._url + '/new://' + type)
+      this.request('GET', this._url + '/new://' + type)
         .then((data) => {
           resolve(new SessionClient(data.url))
         })
@@ -26,28 +19,6 @@ class HostClient {
     })
   }
 
-  _request (method, url, data) {
-    return new Promise((resolve, reject) => {
-      var request = new window.XMLHttpRequest()
-      request.open(method, url, true)
-      request.setRequestHeader('Accept', 'application/json')
-      request.setRequestHeader('Content-Type', 'application/json')
-
-      request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-          resolve(JSON.parse(request.responseText))
-        } else {
-          reject(request.status)
-        }
-      }
-
-      if (data) {
-        request.send(JSON.stringify(data))
-      } else {
-        request.send()
-      }
-    })
-  }
 }
 
 export default HostClient
--- src/session/SessionClient.js
+++ src/session/SessionClient.js
@@ -1,37 +1,13 @@
+import ComponentClient from '../component/ComponentClient'
 
-class SessionClient {
-
-  constructor (url) {
-    this._url = url
-  }
+class SessionClient extends ComponentClient {
 
   execute (code) {
-    return this._request('POST', this._url + '!execute', {
+    return this.call('execute', {
       code: code
     })
   }
 
-  _request (method, url, data) {
-    return new Promise((resolve, reject) => {
-      var request = new window.XMLHttpRequest()
-      request.open(method, url, true)
-      request.setRequestHeader('Content-Type', 'application/json')
-
-      request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-          resolve(JSON.parse(request.responseText))
-        } else {
-          reject(request.status)
-        }
-      }
-
-      if (data) {
-        request.send(JSON.stringify(data))
-      } else {
-        request.send()
-      }
-    })
-  }
 }
 
 export default SessionClient
