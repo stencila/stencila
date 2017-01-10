57f48438a6384f4213546268fd274908d894d112
Nokome Bentley <nokome@stenci.la>
2017-01-11 09:43:12 +1300

    Increase test coverage

--- tests/need.test.js
+++ tests/need.test.js
@@ -8,5 +8,10 @@ test('need() can fetch a NPM module', t => {
   t.equal(isNumber(1), true)
   t.equal(isNumber('foo'), false)
 
+  let isNumberCached = need('is-number')
+  t.deepEqual(isNumber, isNumberCached)
+
+  t.throws(() => need('a-non-existent-npm-module'))
+
   t.end()
 })
