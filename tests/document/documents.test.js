63e894c670f09c0be678c930d7561fe9a1236e23
Nokome Bentley <nokome@stenci.la>
2017-02-16 11:33:14 +1300

    Make documents test synchronous see #4

--- tests/document/documents.test.js
+++ tests/document/documents.test.js
@@ -10,69 +10,69 @@ const Document = require('../../src/document/Document')
  * documents.
  */
 test('Document test documents', t => {
-  fs.readdir('tests/document/documents', (err, files) => {
-    if (err) {
-      t.skip('Folder tests/document/documents not found. Do you need to run `make test-documents`?')
-      t.end()
-      return
-    }
+  // Get list of test document directories
+  try {
+    var dirs = fs.readdirSync('tests/document/documents')
+  } catch (error) {
+    t.skip('Folder tests/document/documents not found. Do you need to run `make test-documents`?')
+    t.end()
+    return
+  }
 
-    // For each test document...
-    for (let dir of files) {
+  // For each test document...
+  for (let dir of dirs) {
+    // Start a subtest...
+    t.test(dir, st => {
+      // Failing tests that we will skip at present
+      if (['input', 'output', 'execute', 'simple'].indexOf(dir) >= 0) {
+        st.skip('TODO')
+        st.end()
+        return
+      }
+
       // Get list of files
-      fs.readdir(path.join('tests/document/documents', dir), (err, files) => {
-        // Start a subtest...
-        t.test(dir, t => {
-          // Failing tests that we will skip at present
-          if (['input', 'output', 'execute', 'simple'].indexOf(dir) >= 0) {
-            t.skip('TODO')
-            t.end()
-            return
-          }
+      let files = fs.readdirSync(path.join('tests/document/documents', dir))
 
-          t.notOk(err)
+      // Read in files
+      let docs = {}
+      for (let file of files) {
+        let canon = file.substring(0, 7) === 'default'
+        let format = path.extname(file).substring(1)
+        let content = fs.readFileSync(path.join('tests/document/documents', dir, file), {encoding: 'utf8'})
+        content = content.trim()
+        // Load the document from the content
+        let doc = new Document()
+        doc.load(content, format)
+        docs[file] = {
+          canon: canon,
+          format: format,
+          content: content,
+          doc: doc
+        }
+      }
 
-          // Read in files
-          let docs = {}
-          for (let file of files) {
-            let canon = file.substring(0, 7) === 'default'
-            let format = path.extname(file).substring(1)
-            let content = fs.readFileSync(path.join('tests/document/documents', dir, file), {encoding: 'utf8'})
-            content = content.trim()
-            // Load the document from the content
-            let doc = new Document()
-            doc.load(content, format)
-            docs[file] = {
-              canon: canon,
-              format: format,
-              content: content,
-              doc: doc
-            }
-          }
+      // All documents should dump HTML equal to default.html
+      let htmlDoc = docs['default.html']
+      if (htmlDoc) {
+        let expected = htmlDoc.content
+        for (let file in docs) {
+          let obtained = docs[file].doc.dump('html')
+          st.equal(obtained, expected, dir + '/' + file + ' should be equivalent to ' + dir + '/default.html')
+        }
+      }
 
-          // All documents should dump HTML equal to default.html
-          let htmlDoc = docs['default.html']
-          if (htmlDoc) {
-            let expected = htmlDoc.content
-            for (let file in docs) {
-              let obtained = docs[file].doc.dump('html')
-              t.equal(obtained, expected, dir + '/' + file + ' should be equivalent to ' + dir + '/default.html')
-            }
-          }
+      // All documents should dump Markdown equal to default.md
+      let mdDoc = docs['default.md']
+      if (mdDoc) {
+        let expected = mdDoc.content
+        for (let file in docs) {
+          let obtained = docs[file].doc.dump('md')
+          st.equal(obtained, expected, dir + '/' + file + ' should be equivalent to ' + dir + '/default.md')
+        }
+      }
 
-          // All documents should dump Markdown equal to default.md
-          let mdDoc = docs['default.md']
-          if (mdDoc) {
-            let expected = mdDoc.content
-            for (let file in docs) {
-              let obtained = docs[file].doc.dump('md')
-              t.equal(obtained, expected, dir + '/' + file + ' should be equivalent to ' + dir + '/default.md')
-            }
-          }
-
-          t.end()
-        })
-      })
-    }
-  })
+      st.end()
+    })
+  }
+  t.end()
 })
