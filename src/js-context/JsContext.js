7d0993697135b34877f2d8469ac9fdf387b36c0c
Nokome Bentley <nokome@stenci.la>
2017-02-21 16:43:56 +1300

    Rename JsSession -> JsContext

--- src/js-context/JsContext.js
+++ src/js-context/JsContext.js
@@ -4,15 +4,15 @@ const {pack, unpack} = require('../packing')
 const require_ = typeof window !== 'undefined' ? require('../need') : require
 
 /**
- * A Javascript session
+ * A Javascript context
  *
- * Implements the Stencila `Session` API for executing chunks
+ * Implements the Stencila `Context` API for executing chunks
  * of Javascript code. The main (and currently, only!) method is `execute`.
  * It takes a chunck of code and, optionally, any inputs (i.e. arguments).
  * It returns any errors and an output value (i.e. return value).
  * See the examples given below.
  */
-class JsSession {
+class JsContext {
 
   constructor (options) {
     this.options = options || {}
@@ -36,21 +36,21 @@ class JsSession {
    * @example
    *
    * // Evaluate an expression...
-   * session.execute('6*7') // { errors: {}, output: { type: 'int', format: 'text', value: '42' } }
+   * context.execute('6*7') // { errors: {}, output: { type: 'int', format: 'text', value: '42' } }
    *
    * // Output is the value of the last line,
-   * session.execute('let x = 6\nx*7') // { errors: {}, output: { type: 'int', format: 'text', value: '42' } }
+   * context.execute('let x = 6\nx*7') // { errors: {}, output: { type: 'int', format: 'text', value: '42' } }
    *
    * // If the last line is blank there is no output (this is intended for code chunks that have side effects e.g. set up data),
-   * session.execute('let x = 6\nx*7\n\n') // { errors: {}, output: null }
+   * context.execute('let x = 6\nx*7\n\n') // { errors: {}, output: null }
    *
    * // You can specify input variables (that are local to that call),
-   * session.execute('Math.PI*radius', {radius:{type:'flt', format:'text', value: '21.4'}}) // { errors: {}, output: { type: 'flt', format: 'text', value: '67.23008278682157' } }
-   * session.execute('radius') // { errors: { '1': 'ReferenceError: radius is not defined' }, output: null }
+   * context.execute('Math.PI*radius', {radius:{type:'flt', format:'text', value: '21.4'}}) // { errors: {}, output: { type: 'flt', format: 'text', value: '67.23008278682157' } }
+   * context.execute('radius') // { errors: { '1': 'ReferenceError: radius is not defined' }, output: null }
    *
    * // You can also assign global variables which are available in subsequent calls,
-   * session.execute('globals.foo = "bar"\n\n') // { errors: {}, output: null }
-   * session.execute('foo') // { errors: {}, output: { type: 'str', format: 'text', value: 'bar' } }
+   * context.execute('globals.foo = "bar"\n\n') // { errors: {}, output: null }
+   * context.execute('foo') // { errors: {}, output: { type: 'str', format: 'text', value: 'bar' } }
    */
   execute (code, inputs) {
     code = code || ''
@@ -132,4 +132,4 @@ class JsSession {
 
 }
 
-module.exports = JsSession
+module.exports = JsContext
