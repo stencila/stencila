4dcc488ce580d9de69a393256d732e400e54853c
Nokome Bentley <nokome@stenci.la>
2017-03-06 13:17:49 +1300

    Refactor language code conversion and add a test

---
+++ tests/language.test.js
@@ -0,0 +1,20 @@
+import test from 'tape'
+
+import {shortname, longname} from '../src/language'
+
+function check (t, lang, short, long) {
+  t.equal(shortname(lang), short)
+  t.equal(longname(shortname(lang)), long)
+  t.equal(shortname(short), short)
+  t.equal(longname(long), long)
+}
+
+test('pack works for primitive types', function (t) {
+  check(t, 'Javascript', 'js', 'JavaScript')
+  check(t, 'Julia', 'jl', 'Julia')
+  check(t, 'Python', 'py', 'Python')
+  check(t, 'R', 'r', 'R')
+  check(t, 'SQL', 'sql', 'SQL')
+
+  t.end()
+})
