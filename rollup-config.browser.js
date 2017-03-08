079422f2c9259f48c993d261f2b10d171d825fba
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-08 16:14:19 +1300

    Added rollup configuration for creating browser bundle.

---
+++ rollup-config.browser.js
@@ -0,0 +1,57 @@
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import json from 'rollup-plugin-json'
+import hypothetical from 'rollup-plugin-hypothetical'
+
+const STUBS = {
+  './src/document/Document': 'export default function() {}',
+  './src/vega-context/VegaContext': 'export default function() {}',
+  './src/vega-lite-context/VegaLiteContext': 'export default function() {}'
+}
+
+export default {
+  entry: './index.js',
+  dest: './build/stencila-js.js',
+  format: 'umd',
+  moduleName: 'stencilaJs',
+  sourceMap: true,
+  plugins: [
+    json({
+      include: 'node_modules/**'
+    }),
+    hypothetical({
+      allowRealFiles: true,
+      files: STUBS
+    }),
+    nodeResolve({
+      jsnext: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: { 'acorn/dist/walk.js': [ 'simple', 'base' ] }
+    }),
+    babel({
+      // overriding babelrc
+      babelrc: false,
+      presets: [
+        [ 'es2015', { 'modules': false } ]
+      ],
+      plugins: [
+        'external-helpers',
+        ['babel-plugin-transform-builtin-extend', {
+          globals: ['Error']
+        }]
+      ]
+    })
+  ],
+  external: [
+  ],
+  globals: {
+  },
+  acorn: {
+    // Avoid error when bundling d3: "The keyword 'await' is reserved"
+    // See https://github.com/rollup/rollup/issues/564#issuecomment-225302878
+    allowReserved: true
+  }
+}
