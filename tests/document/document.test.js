4f589fe57d3e8de6e7ce5f9e388f89d82b127c9c
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-15 06:09:12 +1300

    Introduced dashboard.test

--- tests/document/document.test.js
+++ tests/document/document.test.js
@@ -1,10 +1,10 @@
-// Integration tests for src/document/document
-
 import test from 'tape'
 
 import getSandbox from '../getSandbox'
 import { DocumentPage } from '../../index.es'
 import TestBackend from '../backend/TestBackend'
+
+// Integration tests for src/document
 
 test('Document: mounting a DocumentPage', (t) => {
   const sandbox = getSandbox(t)
@@ -12,6 +12,6 @@ test('Document: mounting a DocumentPage', (t) => {
     backend: new TestBackend(),
     archiveURL: '/tests/documents/simple/default.html'
   }, sandbox)
-  t.ok(page.isMounted(), 'Page should be mounted')
+  t.ok(page.isMounted(), 'DocumentPage should be mounted')
   t.end()
 })
