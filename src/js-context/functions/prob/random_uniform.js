42fcaa82cbee4a3c1fa3114dc44243eddfcebeb3
Nokome Bentley <nokome.bentley@stenci.la>
2017-03-18 04:50:56 +1300

    Introduce random number generation

---
+++ src/js-context/functions/prob/random_uniform.js
@@ -0,0 +1,6 @@
+import random_generate_ from './random_generate_'
+
+export default function random_uniform(n = 1, min = 0, max = 1) {
+  let diff = max - min
+  return random_generate_(n, () => min + Math.random() * diff)
+}
