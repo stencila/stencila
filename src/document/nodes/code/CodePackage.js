dc6a2394fb2707acbe0e87101a3c28777a3d5a6e
Michael Aufreiter <michael@substance.io>
2017-03-23 20:00:17 +1300

    Disable Macros.

--- src/document/nodes/code/CodePackage.js
+++ src/document/nodes/code/CodePackage.js
@@ -1,10 +1,8 @@
 import { CodePackage } from 'substance'
-import CodeMacro from './CodeMacro'
 
 export default {
   name: 'code',
   configure: function (config) {
     config.import(CodePackage, { disableCollapsedCursor: true })
-    config.addMacro(new CodeMacro())
   }
 }
--- src/document/nodes/strong/StrongPackage.js
+++ src/document/nodes/strong/StrongPackage.js
@@ -1,10 +1,8 @@
 import { StrongPackage } from 'substance'
-import StrongMacro from './StrongMacro'
 
 export default {
   name: 'strong',
   configure: function (config) {
     config.import(StrongPackage, { disableCollapsedCursor: true })
-    config.addMacro(new StrongMacro())
   }
 }
