cd4e87eeb48cdebd1133feee6f82e8281b9f1014
Oliver Buchtala <oliver.buchtala@gmail.com>
2017-03-26 06:59:10 +0200

    Allow to add NL and TAB into mini-lang editor.

--- src/document/nodes/cell/MiniLangEditor.js
+++ src/document/nodes/cell/MiniLangEditor.js
@@ -1,11 +1,12 @@
-import { Component, TextPropertyEditor } from 'substance'
+import {
+  Component, TextPropertyEditor, isArrayEqual
+} from 'substance'
 
 export default
 class MiniLangEditor extends Component {
 
   render($$) {
     let el = $$('div').addClass('sc-mini-lang-editor')
-
     // the source code
     const path = this.props.path
     const commands = this.props.commands
@@ -13,8 +14,14 @@ class MiniLangEditor extends Component {
     let content = $$(TextPropertyEditor, {
       path,
       commands,
-      markers
+      markers,
+      handleTab: false
     }).ref('contentEditor')
+      // NOTE: disable these if these are causing troubles
+      // EXPERIMENTAL: adding "\n" plus indent of current line
+      .on('enter', this._onEnterKey)
+      // EXPERIMENTAL: adding 2 spaces if at begin of line
+      .on('tab', this._onTabKey)
     content.addClass('se-content')
     el.append(content)
     return el
@@ -39,4 +46,44 @@ class MiniLangEditor extends Component {
     }
   }
 
+  _onEnterKey() {
+    // find the indent of the current line
+    let indent = this._getCurrentIndent() || ''
+    const editorSession = this.context.editorSession
+    editorSession.transaction((tx) => {
+      tx.insertText('\n' + indent)
+    })
+  }
+
+  _onTabKey() {
+    const editorSession = this.context.editorSession
+    const head = this._getCurrentLineHead()
+    if (/^\s*$/.exec(head)) {
+      editorSession.transaction((tx) => {
+        tx.insertText('  ')
+      })
+    }
+  }
+
+  _getCurrentIndent() {
+    const line = this._getCurrentLineHead()
+    const match = /^(\s+)/.exec(line)
+    if (match) {
+      return match[1]
+    }
+  }
+
+  _getCurrentLineHead() {
+    const editorSession = this.context.editorSession
+    const doc = editorSession.getDocument()
+    const sel = editorSession.getSelection()
+    if (!sel || !sel.isPropertySelection() || !isArrayEqual(sel.path, this.props.path)) {
+      return
+    }
+    const offset = sel.start.offset
+    const exprStr = doc.get(this.props.path)
+    const head = exprStr.slice(0, offset)
+    const lastNL = Math.max(0, head.lastIndexOf('\n'))
+    return exprStr.slice(lastNL+1)
+  }
 }
