8f3d4b0823dc25a14620dfff07c9ab95fb1a09fe
Nokome Bentley <nokome@stenci.la>
2017-07-17 11:07:43 +1200

    Record and exit with the number of errors

--- tests/integration.sh
+++ tests/integration.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Integration tests
+
+# Record errors
+errors=0
+error_handler () {
+  (( errors++ ))
+}
+trap error_handler ERR
+
 
 # Run a Stencila Docker container which provides all external language contexts
 docker run --detach --publish 2100:2000 stencila/alpha
@@ -11,3 +19,7 @@ STENCILA_PEERS=http://localhost:2100 node tools/runner.js tests/documents/extern
 
 # Configured using peer dicovery
 STENCILA_DISCOVER=30 node tools/runner.js tests/documents/external-language-cells.html
+
+
+# Exit with number of errors
+exit $errors
