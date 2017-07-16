be5ff5772deaa4520234ef0d08a05aca5a2c892b
Nokome Bentley <nokome@stenci.la>
2017-07-17 11:39:00 +1200

    Make tests more like Desktop setup (multiple peers)

--- tests/integration.sh
+++ tests/integration.sh
@@ -10,14 +10,17 @@ error_handler () {
 trap error_handler ERR
 
 
-# Run a Stencila Docker container which provides all external language contexts
+# Run a Stencila Docker container which provides a Node Host (as have in Desktop)
+docker run --detach --publish 2000:2000 stencila/iota
+# Run a Stencila Docker container which provides several language Hosts
 docker run --detach --publish 2100:2000 stencila/alpha
+
 sleep 5
 
-# Configured using Docker container as only peer
+# Configured using one of the containers as only peer
 STENCILA_PEERS=http://localhost:2100 node tools/runner.js tests/documents/external-language-cells.html
 
-# Configured using peer dicovery
+# Configured using peer dicovery (this is current configuration for Desktop)
 STENCILA_DISCOVER=30 node tools/runner.js tests/documents/external-language-cells.html
 
 
