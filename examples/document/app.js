f2f0b8a23de18ee613668e4fcdc5b77dc860e716
Nokome Bentley <nokome@stenci.la>
2017-08-18 08:06:09 +1200

    Use address instead of documentId; remove backend

--- examples/document/app.js
+++ examples/document/app.js
@@ -3,18 +3,17 @@
   using a stub backend.
 */
 
-import { DocumentPage, Host, MemoryBackend, getQueryStringParam } from 'stencila'
+import { DocumentPage, Host, getQueryStringParam } from 'stencila'
 
 window.addEventListener('load', () => {
   window.documentPage = DocumentPage.mount({
+    address: getQueryStringParam('address'),
     host: new Host({
       // Initial peers can be set in an environment variable
       peers: window.STENCILA_PEERS ? window.STENCILA_PEERS.split(' ') : [],
       // Peer discovery defaults to false but its frequency (in seconds) can be set in an environment variable
       discover: window.STENCILA_DISCOVER ? parseFloat(window.STENCILA_DISCOVER) : false,
-    }),
-    backend: new MemoryBackend(window.GUIDES),
-    documentId: getQueryStringParam('documentId') || '01-welcome-to-stencila'
+    })
   }, window.document.body)
 
   function onKeyDown(e) {
@@ -22,6 +21,7 @@ window.addEventListener('load', () => {
     if (e.ctrlKey && e.keyCode === 83) {
       console.info('saving')
       window.documentPage.save()
+      e.preventDefault()
     }
   }
   document.addEventListener('keydown', onKeyDown, false)
