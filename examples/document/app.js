dcd7316c8c6c5b8ad08db130c547f78b69aa424a
Nokome Bentley <nokome@stenci.la>
2017-08-28 15:51:52 +1200

    Allow peers to be specified as a query parameter

--- examples/document/app.js
+++ examples/document/app.js
@@ -7,9 +7,12 @@ import { DocumentPage, Host, getQueryStringParam } from 'stencila'
 
 window.addEventListener('load', () => {
 
+  let peers = (getQueryStringParam('peers') || window.STENCILA_PEERS)
+  if (peers) peers = peers.split(',')
+
   let host = new Host({
     // Initial peers can be set in an environment variable
-    peers: window.STENCILA_PEERS ? window.STENCILA_PEERS.split(' ') : [],
+    peers: peers,
     // Peer discovery defaults to false but its frequency (in seconds) can be set in an environment variable
     discover: window.STENCILA_DISCOVER ? parseFloat(window.STENCILA_DISCOVER) : false,
   })
