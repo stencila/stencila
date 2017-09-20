f5e352ad1e14bf04b57ffaf2f43fe77a60e933d8
Michael Aufreiter <michael@substance.io>
2017-09-20 21:09:04 +0200

    Bring back some styles.

--- src/document/DocumentEditorPackage.js
+++ src/document/DocumentEditorPackage.js
@@ -6,6 +6,7 @@ import ReproFigComponent from './ReproFigComponent'
 import CellComponent from './CellComponent'
 import IntegerValueComponent from '../shared/IntegerValueComponent'
 import FloatValueComponent from '../shared/FloatValueComponent'
+import CodeHighlightComponent from '../shared/CodeHighlightComponent'
 import StringValueComponent from '../shared/StringValueComponent'
 import ArrayValueComponent from '../shared/ArrayValueComponent'
 import TableValueComponent from '../shared/TableValueComponent'
@@ -17,6 +18,7 @@ export default {
     config.import(TextureEditorPackage)
     config.addComponent('repro-fig', ReproFigComponent)
     config.addComponent('cell', CellComponent)
+    config.addComponent('code-highlight', CodeHighlightComponent)
     config.addComponent('value:integer', IntegerValueComponent)
     config.addComponent('value:float', FloatValueComponent)
     config.addComponent('value:string', StringValueComponent)
