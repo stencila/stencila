389a51a194f607259e66db5901cd2d8ba8924c8c
Nokome Bentley <nokome@stenci.la>
2017-10-17 15:14:04 +0200

    Add ObjectValueComponent

--- src/document/DocumentEditorPackage.js
+++ src/document/DocumentEditorPackage.js
@@ -12,6 +12,7 @@ import NumberValueComponent from '../shared/NumberValueComponent'
 import IntegerValueComponent from '../shared/IntegerValueComponent'
 import StringValueComponent from '../shared/StringValueComponent'
 import ArrayValueComponent from '../shared/ArrayValueComponent'
+import ObjectValueComponent from '../shared/ObjectValueComponent'
 import TableValueComponent from '../shared/TableValueComponent'
 import TestValueComponent from '../shared/TestValueComponent'
 import ImageValueComponent from '../shared/ImageValueComponent'
@@ -29,6 +30,7 @@ export default {
     config.addComponent('value:number', NumberValueComponent)
     config.addComponent('value:string', StringValueComponent)
     config.addComponent('value:array', ArrayValueComponent)
+    config.addComponent('value:object', ObjectValueComponent)
     config.addComponent('value:table', TableValueComponent)
     config.addComponent('value:test', TestValueComponent)
     config.addComponent('value:image', ImageValueComponent)
