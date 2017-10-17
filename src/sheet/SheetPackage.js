14d929d911f0b638639647a22408be164f9de35d
Nokome Bentley <nokome@stenci.la>
2017-10-17 15:16:49 +0200

    Add cell value components to SheetPackage

--- src/sheet/SheetPackage.js
+++ src/sheet/SheetPackage.js
@@ -14,6 +14,16 @@ import ColumnSettingsDialog from './ColumnSettingsDialog'
 import SheetIssuesComponent from './SheetIssuesComponent'
 import SheetIssuesOverlay from './SheetIssuesOverlay'
 
+import BooleanValueComponent from '../shared/BooleanValueComponent'
+import NumberValueComponent from '../shared/NumberValueComponent'
+import IntegerValueComponent from '../shared/IntegerValueComponent'
+import StringValueComponent from '../shared/StringValueComponent'
+import ArrayValueComponent from '../shared/ArrayValueComponent'
+import ObjectValueComponent from '../shared/ObjectValueComponent'
+import TableValueComponent from '../shared/TableValueComponent'
+import TestValueComponent from '../shared/TestValueComponent'
+import ImageValueComponent from '../shared/ImageValueComponent'
+
 export default {
   name: 'Sheet',
 
@@ -105,6 +115,18 @@ export default {
     config.addLabel('set-js', 'Javascript')
     config.addLabel('set-py', 'Python')
     config.addLabel('set-r', 'R')
+
+    // Cell values
+    config.addComponent('value:boolean', BooleanValueComponent)
+    config.addComponent('value:integer', IntegerValueComponent)
+    config.addComponent('value:number', NumberValueComponent)
+    config.addComponent('value:string', StringValueComponent)
+    config.addComponent('value:array', ArrayValueComponent)
+    config.addComponent('value:object', ObjectValueComponent)
+    config.addComponent('value:table', TableValueComponent)
+    config.addComponent('value:test', TestValueComponent)
+    config.addComponent('value:image', ImageValueComponent)
+
 
     config.addComponent('sheet', SheetComponent)
 
