31a1d54438e8aa4c31d42af32a2e0010a03ec05a
Michael Aufreiter <michael@substance.io>
2017-10-19 13:54:53 +0200

    Integrating FunctionUsage command/tool.

---
+++ src/shared/FunctionUsageTool.js
@@ -0,0 +1,18 @@
+import { ToggleTool } from 'substance'
+import FunctionUsageComponent from '../shared/FunctionUsageComponent'
+
+export default class FunctionUsageTool extends ToggleTool {
+  render($$) {
+    let functionManager = this.context.functionManager
+    let func = functionManager.getFunction(this.props.commandState.functionName)
+    let el = $$('div').addClass('sc-function-usage-tool')
+    if (func) {
+      el.append(
+        $$(FunctionUsageComponent, {
+          spec: func.getSpec()
+        })
+      )
+    }
+    return el
+  }
+}
