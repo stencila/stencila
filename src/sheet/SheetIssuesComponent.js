280079edda79bfaf3452ece08fb83c1d39b5fcf7
Daniel Beilinson <beilinson@gmail.com>
2017-10-21 14:29:03 +0200

    Close issue list after issue resolving.

--- src/sheet/SheetIssuesComponent.js
+++ src/sheet/SheetIssuesComponent.js
@@ -4,7 +4,7 @@ export default class SheetIssuesComponent extends Component {
 
   didMount() {
     const issueManager = this.context.issueManager
-    issueManager.on('issues:changed', this.rerender, this)
+    issueManager.on('issues:changed', this._onIssuesChnage, this)
     const cellId = this.props.cellId
     if(cellId) {
       this.refs.scrollPane.scrollTo('[data-key="' + cellId + '"]')
@@ -33,6 +33,23 @@ export default class SheetIssuesComponent extends Component {
 
   _renderIssue($$, issue, highlighted) {
     return $$(CellIssueComponent, { issue, highlighted: highlighted })
+  }
+
+  _onIssuesChnage() {
+    const issueManager = this.context.issueManager
+    const hasIssues = issueManager.hasAnyIssues()
+    if(hasIssues) {
+      this.rerender()
+    } else {
+      this._close()
+    }
+  }
+
+  _close() {
+    let sheetEditor = this.context.app.getSheetEditor()
+    if (sheetEditor) {
+      sheetEditor.toggleConsole('sheet-issues')
+    }
   }
 }
 
