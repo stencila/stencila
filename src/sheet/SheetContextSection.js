184a065dd955c84a6d94dd6d9bcb49a63c0f4f0b
Daniel Beilinson <beilinson@gmail.com>
2017-10-22 14:42:31 +0200

    Use context panel instead of console.

--- src/sheet/SheetContextSection.js
+++ src/sheet/SheetContextSection.js
@@ -1,8 +1,22 @@
 import { Component } from 'substance'
 
 export default class SheetContextSection extends Component {
+
   render($$) {
+    const contextId = this.props.contextId
+
     let el = $$('div').addClass('sc-context-section')
+
+    if (!contextId) {
+      console.error('FIXME: could not find contextId')
+    } else {
+      const ComponentClass = this.getComponent(contextId)
+
+      el.append(
+        $$(ComponentClass, { cellId: this.props.cellId })
+      )
+    }
+
     return el
   }
 }
