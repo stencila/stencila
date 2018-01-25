121c48d8cd220fec318a59cc04a395aac07a810f
Michael Aufreiter <michael@substance.io>
2018-01-25 10:28:20 +0100

    Fix ice cream data.

--- examples/util/ice-cream-sales.js
+++ examples/util/ice-cream-sales.js
@@ -15,16 +15,16 @@ const COLS = {
 
 // These columns are rearranged in the sheet, see below
 const DATA = [
-  [18,'no',50],
-  [20,'yes',126],
-  [24,'yes',118],
-  [23,'yes',126],
-  [26,'yes',280],
-  [25,'no',102],
-  [20,'no',93],
-  [17,'no',32],
-  [18,'yes',103],
-  [28,'yes',246],
+  [18,50,'no'],
+  [20,126,'yes'],
+  [24,118,'yes'],
+  [23,126,'yes'],
+  [26,280,'yes'],
+  [25,102,'no'],
+  [20,93,'no'],
+  [17,32,'no'],
+  [18,103,'yes'],
+  [28,246,'yes'],
 ]
 
 let cells = {}
