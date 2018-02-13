33617a421ad2868444b718c59c1945f1771663c2
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-02-13 15:28:52 +0100

    Several fixes.

--- tests/engine/CellGraph.test.js
+++ tests/engine/CellGraph.test.js
@@ -2,7 +2,7 @@ import test from 'tape'
 
 import CellGraph from '../../src/engine/CellGraph'
 import Cell from '../../src/engine/Cell'
-import { READY, WAITING, OK } from '../../src/engine/CellStates'
+import { READY, WAITING, OK, toString } from '../../src/engine/CellStates'
 
 /*
 
@@ -41,16 +41,76 @@ test('CellGraph: single cell with no deps', t => {
 */
 test('CellGraph: two linked cells', t => {
   let g = new CellGraph()
-  let cell1 = new Cell({ id: 'cell1', output: 'x' })
-  let cell2 = new Cell({ id: 'cell2', inputs: ['x'] })
-  g.addCell(cell1)
-  g.addCell(cell2)
-  g.update()
-  t.equal(cell1.state, READY, 'cell1 should be ready')
-  t.equal(cell2.state, WAITING, 'cell2 should be waiting')
+  let cells = [
+    new Cell({ id: 'cell1', output: 'x' }),
+    new Cell({ id: 'cell2', inputs: ['x'] })
+  ]
+  cells.forEach(c => g.addCell(c))
+
+  let updates = g.update()
+  _checkUpdates(t, updates, ['cell1', 'cell2'])
+  _checkStates(t, cells, [READY, WAITING])
+
   g.setResult('cell1', 1)
-  g.update()
-  t.equal(cell1.state, OK, 'cell1 should be ok')
-  t.equal(cell2.state, READY, 'cell2 should be ready')
+  updates = g.update()
+  _checkUpdates(t, updates, ['cell1', 'cell2'])
+  _checkStates(t, cells, [OK, READY])
+
+  g.setResult('cell2', 2)
+  updates = g.update()
+  _checkUpdates(t, updates, ['cell2'])
+  _checkStates(t, cells, [OK, OK])
+
   t.end()
 })
+
+test('CellGraph: Y shaped graph', t => {
+  let g = new CellGraph()
+  let cells = [
+    new Cell({ id: 'cell1', output: 'x' }),
+    new Cell({ id: 'cell2', output: 'y' }),
+    new Cell({ id: 'cell3', inputs: ['x', 'y'], output: 'z' }),
+    new Cell({ id: 'cell4', inputs: ['z'] }),
+  ]
+  cells.forEach(c => g.addCell(c))
+
+  let updates = g.update()
+  _checkUpdates(t, updates, ['cell1', 'cell2', 'cell3', 'cell4'])
+  _checkStates(t, cells, [READY, READY, WAITING, WAITING])
+
+  g.setResult('cell1', 1)
+  updates = g.update()
+  _checkUpdates(t, updates, ['cell1'])
+  _checkStates(t, cells, [OK, READY, WAITING, WAITING])
+
+  g.setResult('cell2', 2)
+  updates = g.update()
+  _checkUpdates(t, updates, ['cell2', 'cell3'])
+  _checkStates(t, cells, [OK, OK, READY, WAITING])
+
+  g.setResult('cell3', 3)
+  updates = g.update()
+  _checkUpdates(t, updates, ['cell3', 'cell4'])
+  _checkStates(t, cells, [OK, OK, OK, READY])
+
+  g.setResult('cell4', 6)
+  updates = g.update()
+  _checkUpdates(t, updates, ['cell4'])
+  _checkStates(t, cells, [OK, OK, OK, OK])
+
+  t.end()
+})
+
+function _checkStates(t, cells, states) {
+  for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i]
+    const state = states[i]
+    t.equal(cell.state, state, `${cell.id} should be ${toString(state)}`)
+  }
+}
+
+function _checkUpdates(t, updates, expected) {
+  let actual = Array.from(updates)
+  actual.sort()
+  t.deepEqual(actual, expected, 'Updates should be correct')
+}
