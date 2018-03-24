e6101ee9b1f46ae4b4dff18bcb04331d8c679f58
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-03-24 02:44:07 +0100

    Add readable names for CellErrors.

--- src/engine/CellErrors.js
+++ src/engine/CellErrors.js
@@ -7,30 +7,39 @@ export class CellError extends Error {
 
 export class SyntaxError extends CellError {
   get type() { return 'engine' }
+  get name() { return 'syntax' }
 }
 
 export class ContextError extends CellError {
   get type() { return 'engine' }
+  get name() { return 'context' }
 }
 
 export class GraphError extends CellError {
   get type() { return 'graph' }
 }
 
-export class UnresolvedInputError extends GraphError {}
+export class UnresolvedInputError extends GraphError {
+  get name() { return 'unresolved' }
+}
 
 export class CyclicDependencyError extends GraphError {
   get trace() {
     return this.details.trace
   }
+  get name() { return 'cyclic'}
 }
 
-export class OutputCollisionError extends GraphError {}
+export class OutputCollisionError extends GraphError {
+  get name() { return 'collision'}
+}
 
 export class RuntimeError extends CellError {
   get type() { return 'runtime' }
+  get name() { return 'runtime' }
 }
 
 export class ValidationError extends CellError {
   get type() { return 'runtime' }
+  get name() { return 'validation' }
 }
