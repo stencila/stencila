de423cbd6f84814bb51df7e5b2724590e4f95a89
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-04-22 22:44:23 +0700

    Starting tests for sheets and articles.

---
+++ tests/sheet/Sheet.test.js
@@ -0,0 +1,11 @@
+import test from 'tape'
+
+/*
+  This test is using an archive with a sheet attached to the Engine
+  making sure all Sheet interactions and manipulations are working.
+
+  All tests are done on a model level, i.e. the UI is not tested.
+*/
+test('Sheet: TODO', (t) => {
+  t.end()
+})
