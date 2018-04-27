519e7f432a9070896bbc5804b9e68dcf91870582
Oliver Buchtala <oliver.buchtala@gmail.com>
2018-04-27 21:53:55 +0700

    Add test revealing a bug in Sheet.insertCols()

--- tests/engine/Engine.test.js
+++ tests/engine/Engine.test.js
@@ -741,6 +741,35 @@ test('Engine: delete last row of a cell range', t => {
   })
 })
 
+test('Engine: insert a column', t => {
+  t.plan(3)
+  let { engine } = _setup()
+  let sheet = engine.addSheet({
+    id: 'sheet1',
+    lang: 'mini',
+    cells: [
+      ['1', '2'],
+      ['3', '4'],
+      ['5', '6'],
+      ['7', '8'],
+      ['=sum(A1:A4)', '=sum(A1:B4)'],
+    ]
+  })
+  let cells = sheet.cells[4]
+  play(engine)
+  .then(() => {
+    t.deepEqual(getValues(cells), [16,36], 'cells should have correct values')
+  })
+  .then(() => {
+    sheet.insertCols(1, [['2'], ['3'], ['4'], ['5'], ['=sum(B1:B4)']])
+    t.deepEqual(getSources(sheet.queryCells('A5:C5')), ['=sum(A1:A4)','=sum(B1:B4)', '=sum(A1:C4)'], 'sources should have been updated')
+  })
+  .then(() => play(engine))
+  .then(() => {
+    t.deepEqual(getValues(sheet.queryCells('A5:C5')), [16,14,50], 'cells should have correct values')
+  })
+})
+
 function _checkActions(t, engine, cells, expected) {
   let nextActions = engine.getNextActions()
   let actual = []
