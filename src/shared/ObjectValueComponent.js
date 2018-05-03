f41c0dfa0f83f0c619d21e32e9bbd416d0559c57
Nokome Bentley <nokome@stenci.la>
2018-05-03 13:35:08 +1200

    A more useful ObjectValueComponent

--- src/shared/ObjectValueComponent.js
+++ src/shared/ObjectValueComponent.js
@@ -4,16 +4,10 @@ export default
 class ObjectValueComponent extends Component {
   render($$) {
     let value = this.props.value
-    let el = $$('div').addClass('sc-object-value')
-    let ul = $$('ul')
-    Object.keys(value.data).forEach((key) => {
-      let json = JSON.stringify(value.data[key])
-      if (json && json.length > 1000) json = json.slice(0, 1000)+'...'
-      ul.append(
-        $$('li').text(json)
-      )
-    })
-    el.append(ul)
+    let el = $$('pre').addClass('sc-object-value')
+    let json = JSON.stringify(value.data, null, ' ')
+    if (json && json.length > 1000) json = json.slice(0, 1000) + '...'
+    el.append(json)
     return el
   }
 }
