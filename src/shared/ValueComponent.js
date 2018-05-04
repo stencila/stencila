9ff5186107a2672ea281f2cf87eb1eb9999cd536
Nokome Bentley <nokome@stenci.la>
2018-05-04 19:44:58 +1200

    Use ObjectValueComponent as default

--- src/shared/ValueComponent.js
+++ src/shared/ValueComponent.js
@@ -9,20 +9,23 @@ class ValueComponent extends Component {
     
     let valueType = this.props.type
     let ValueDisplay = registry.get('value:'+valueType)
-    if (ValueDisplay) {
-      let value = this.props
-      let pointer = false
-      if (!value.data && value.preview) {
-        pointer = true
-        value = {
-          type: value.type,
-          data: value.preview
-        }
-      }
+    // Use the `ObjectValueComponent` by default since most of the time
+    // types without a registered component will be 'extended' types (i.e objects with a type property)
+    if (!ValueDisplay) ValueDisplay = registry.get('value:object')
 
-      let valueEl = $$(ValueDisplay, {value, pointer}).ref('value')
-      el.append(valueEl)
+    let value = this.props
+    let pointer = false
+    if (!value.data && value.preview) {
+      pointer = true
+      value = {
+        type: value.type,
+        data: value.preview
+      }
     }
+
+    let valueEl = $$(ValueDisplay, {value, pointer}).ref('value')
+    el.append(valueEl)
+
     return el
   }
 
