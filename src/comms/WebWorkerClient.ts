923b6296a1395dc2fab9eb4da16693cb43549465
Nokome Bentley <nokome@stenci.la>
2018-11-30 09:48:04 +1300

    refactor(WebWorkerClient): refactor contructor

--- src/comms/WebWorkerClient.ts
+++ src/comms/WebWorkerClient.ts
@@ -7,15 +7,21 @@ import JsonRpcRequest from './JsonRpcRequest'
 export default class WebWorkerClient extends Client {
 
   /**
-   * A [`Worker`](https://developer.mozilla.org/en-US/docs/Web/API/Worker) instance,
-   * usually the `worker` property of a `WebWorkerServer`.
+   * A [`Worker`](https://developer.mozilla.org/en-US/docs/Web/API/Worker) instance.
    */
   worker: Worker
 
-  constructor (worker: Worker) {
+  /**
+   * Constructor
+   * 
+   * @param server A URL to a Javascript file or a `Worker` instance
+   */
+  constructor (server: string | Worker) {
     super()
 
-    this.worker = worker
+    if (typeof server === 'string') server = new Worker(server)
+
+    this.worker = server
     this.worker.onmessage = (event: MessageEvent) => {
       this.recieve(event.data)
     }
