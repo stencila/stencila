285946b382a5d8da6b8afe6d867bd9275ae6aba0
Nokome Bentley <nokome@stenci.la>
2018-12-08 11:39:57 +1300

    tests: unit tests for Stdio Client/Server

--- tests/comms/StdioClientServerTest.py
+++ tests/comms/StdioClientServerTest.py
@@ -3,39 +3,17 @@ import pytest
 from stencilaschema.comms.StdioClient import StdioClient
 from stencilaschema.comms.StdioServer import StdioServer
 
+from helpers.TestProcessor import TestProcessor
+
+# It is difficult to test StdIO client/servers under pytest
+# because if captures stdin and stdout. Even turning it of
+# using the `capsys` fixture still causes issues
+# So see integration tests for tests that connect the client and server
+
 def test_client():
     client = StdioClient([])
     assert client.url == 'pipe://stdio'
 
-@pytest.mark.asyncio
-async def test_server(capsys):
-    server = StdioServer()
+def test_server():
+    server = StdioServer(TestProcessor())
     assert server.url == 'pipe://stdio'
-        
-    # Disable stdout/stderr/stdin capturing
-    with capsys.disabled():
-        pass
-
-        # TODO This occaisonally fails under pytests and
-        # is probably related to capuring / asyn behaviour
-        # await server.start()
-        
-        # TODO This fails:
-        # await server.stop()
-
-@pytest.mark.asyncio
-async def test_client_server(capsys):
-    # Test that the client can start a peer server
-    client = StdioClient(['python3', 'tests/comms/stdioServer.py'])
-
-    # Disable stdout/stderr/stdin capturing
-    with capsys.disabled():
-        pass
-        
-        # TODO This hangs:
-        # await client.start()
-        
-        #await client.execute({"type": "Thing"})
-        
-        # TODO This fails:
-        # await client.stop()
