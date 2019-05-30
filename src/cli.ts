f7c5a814731ee8cf5db28d3857d232eeaeef79be
Ben Shaw <ben@beneboy.co.nz>
2019-05-30 19:43:53 +1200

    chore: Move encoda interface and CLI options to its own file

--- src/cli.ts
+++ src/cli.ts
@@ -1,42 +1,11 @@
 import yargs from 'yargs'
-import * as encoda from '@stencila/encoda'
+import { addCliCommands as addEncodaCliCommands } from './encoda'
 
 const VERSION = require('../package').version
 
-yargs
-  .scriptName('stencila')
+const yargsDefinition = yargs.scriptName('stencila')
 
-  .command(
-    'convert [input] [output]',
-    'Convert between file formats',
-    (args: yargs.Argv<{}>): yargs.Argv<any> => {
-      return args
-        .positional('input', {
-          describe: 'The input file path. Defaults to standard input.',
-          type: 'string',
-          default: '-'
-        })
-        .positional('output', {
-          describe: 'The output file path. Defaults to standard output.',
-          type: 'string',
-          default: '-'
-        })
-        .option('from', {
-          describe: 'The format to convert the input from.',
-          type: 'string'
-        })
-        .option('to', {
-          describe: 'The format to convert the output to.',
-          type: 'string'
-        })
-    },
-    async (argv: any): Promise<void> => {
-      const {input, output, ...options} = argv
-      await encoda.convert(input, output, options)
-      cleanup()
-    }
-  )
-
+addEncodaCliCommands(yargsDefinition, cleanup)
   // Any command-line argument given that is not demanded, or does not have a corresponding description, will be reported as an error.
   // Unrecognized commands will also be reported as errors.
   .strict()
@@ -55,11 +24,10 @@ yargs
 
   .parse()
 
-
-  function cleanup() {
-    // Trigger a clean up
-    //   "The 'beforeExit' event is not emitted for conditions causing
-    //   explicit termination, such as calling process.exit() or uncaught
-    //   exceptions."
-    process.emit('beforeExit', 0)
-  }
+function cleanup() {
+  // Trigger a clean up
+  //   "The 'beforeExit' event is not emitted for conditions causing
+  //   explicit termination, such as calling process.exit() or uncaught
+  //   exceptions."
+  process.emit('beforeExit', 0)
+}
