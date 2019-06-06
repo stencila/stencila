cb93e9b863c414f57ee94c6e88e78990abd24ee1
Alex Ketch <alex@ketch.me>
2019-06-06 16:32:13 -0400

    feat(Themes): Add initial versions of Stencila and eLife inspired themes

---
+++ src/common/js/index.ts
@@ -0,0 +1,5 @@
+/**
+ * This file is imported by all themes, allowing for shared configurations among themes.
+ */
+
+import './syntaxHighlight'
---
+++ src/common/js/syntaxHighlight.ts
@@ -0,0 +1,5 @@
+import Prism from 'prismjs'
+import 'prismjs/plugins/line-highlight/prism-line-highlight'
+import 'prismjs/plugins/line-numbers/prism-line-numbers'
+
+Prism.highlightAll()
