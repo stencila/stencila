c2f25f500bb1cab33befda9d5470ded9487f680f
Nokome Bentley <nokome@stenci.la>
2019-07-30 12:33:20 +1200

    chore(Schema): Add checking of schemas before generation of JSON

---
+++ src/log.ts
@@ -0,0 +1,24 @@
+/**
+ * Log configuration for development scripts in this repo.
+ *
+ * To get `DEBUG` level log entries, set the `DEBUG` env var. e.g
+ *
+ * ```bash
+ * DEBUG=1 npx ts-node src/schema.ts
+ * ```
+ */
+
+import * as logga from '@stencila/logga'
+
+logga.replaceHandlers(
+  (data: logga.LogData): void => {
+    if (data.level <= (process.env.DEBUG !== undefined ? 3 : 2)) {
+      // Don't print noisy stack traces
+      data.stack = ''
+      logga.defaultHandler(data)
+    }
+  }
+)
+
+const log = logga.getLogger('schema')
+export default log
