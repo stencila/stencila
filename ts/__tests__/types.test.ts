64872fa5f4b26ba1913e82e0791b20fec29585d5
Alex Ketch <alex@ketch.me>
2019-07-31 23:03:50 -0400

    feat(Factory Functions): Filter properties if their value is undefined

---
+++ ts/__tests__/types.test.ts
@@ -0,0 +1,39 @@
+import { article, organization, person } from '../types'
+
+describe('Schema factory functions', () => {
+  test('it creates a valid Person node', () => {
+    expect(person()).toMatchObject({ type: 'Person' })
+  })
+
+  test('it creates a valid Article node', () => {
+    expect(
+      article([organization({ name: 'Stencila' })], 'Testing')
+    ).toMatchObject({
+      type: 'Article',
+      title: 'Testing',
+      authors: [
+        {
+          name: 'Stencila',
+          type: 'Organization'
+        }
+      ]
+    })
+  })
+
+  test('it filters undefined values from properties', () => {
+    const actual = person({
+      honorificPrefix: 'Sir',
+      honorificSuffix: undefined,
+      givenNames: ['Isaac'],
+      familyNames: ['Newton'],
+      memberOf: undefined
+    })
+
+    expect(actual).toMatchObject({
+      type: 'Person',
+      familyNames: ['Newton'],
+      givenNames: ['Isaac'],
+      honorificPrefix: 'Sir'
+    })
+  })
+})
