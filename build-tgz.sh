e62758880a648d4e48889c3502fbc16275b24283
Nokome Bentley <nokome@stenci.la>
2019-09-03 21:15:34 +1200

    build(*): Use rsync on Mac

--- build-tgz.sh
+++ build-tgz.sh
@@ -6,10 +6,16 @@ ARCHIVE_NAME=stencila-deps.tgz
 
 mkdir -p stencila-deps
 
-cp --parents --recursive node_modules/@stencila/encoda/dist/codecs/pandoc/templates stencila-deps
-cp --parents --recursive node_modules/@stencila/encoda/vendor stencila-deps
-cp --parents --recursive node_modules/puppeteer/.local-chromium stencila-deps
-cp --parents --recursive node_modules/opn/xdg-open stencila-deps
+if [[ "$(uname)" == "Darwin" ]]; then
+  CP="rsync -R"
+else
+  CP="cp --parent --recursive"
+fi
+
+$CP node_modules/@stencila/encoda/dist/codecs/pandoc/templates stencila-deps
+$CP node_modules/@stencila/encoda/vendor stencila-deps
+$CP node_modules/puppeteer/.local-chromium stencila-deps
+$CP node_modules/opn/xdg-open stencila-deps
 
 tar czf ${ARCHIVE_NAME} stencila-deps
 
