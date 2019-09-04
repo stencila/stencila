658e2bc97d29baaf9188fa14ff92d07da62f9645
Nokome Bentley <nokome@stenci.la>
2019-09-04 17:16:59 +1200

    fix(Encoda): Update encoda to 0.71.2

--- build-tgz.sh
+++ build-tgz.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-ARCHIVE_NAME=stencila-deps.tgz
-
 mkdir -p stencila-deps
 
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -12,11 +10,10 @@ else
   CP="cp --parent --recursive"
 fi
 
+$CP node_modules/@stencila/encoda/dist/codecs/pandoc/binary stencila-deps
 $CP node_modules/@stencila/encoda/dist/codecs/pandoc/templates stencila-deps
-$CP node_modules/@stencila/encoda/vendor stencila-deps
 $CP node_modules/puppeteer/.local-chromium stencila-deps
 $CP node_modules/opn/xdg-open stencila-deps
 
-tar czf ${ARCHIVE_NAME} stencila-deps
-
+tar czf stencila-deps.tgz stencila-deps
 rm -r stencila-deps
