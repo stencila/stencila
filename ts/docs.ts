05c02aade45b4529923047fb4f3c2b47e8be9482
alex-ketch <alex@ketch.me>
2020-01-22 14:27:19 -0500

    docs(Index): Improve category presentation and sorting based on feedback

--- ts/docs.ts
+++ ts/docs.ts
@@ -11,7 +11,7 @@
 import * as encoda from '@stencila/encoda'
 import fs from 'fs-extra'
 import globby from 'globby'
-import { groupBy, orderBy, startCase } from 'lodash'
+import { flow, groupBy, sortBy, startCase, uniq } from 'lodash'
 import flatten from 'lodash.flatten'
 import path from 'path'
 import { readSchemas } from './helpers'
@@ -31,6 +31,7 @@ import {
   listItem,
   paragraph,
   strong,
+  superscript,
   table,
   tableCell,
   tableRow
@@ -51,6 +52,12 @@ const DOCS_SOURCE_DIR = path.join(__dirname, '..', 'schema')
  * The destination directory for docs e.g. `*.html` and `*.md` files
  */
 const DOCS_DEST_DIR = path.join(__dirname, '..', 'public')
+
+/**
+ * String marker appended to Schema links in the documentation to indicate
+ * Unstable status, and that the underlying schema may change at any time.
+ */
+const unstableMarker = superscript({ content: ['U'] })
 
 /**
  * Generate docs for each `public/*.schema.json` file and
@@ -101,24 +108,52 @@ async function build(): Promise<void> {
     })
   )
 
+  // This determines the order in which Schema categories are listed in the Table of Contents
+  const orderedCategories = uniq([
+    'Prose',
+    'Code',
+    'Data',
+    'Validation',
+    'Metadata',
+    'Miscellaneous',
+    // Any other categories should be listed at the end
+    ...Object.values(schemas).map(schema => startCase(schema.category))
+  ])
+
+  // Group schemas by category, and within each group sort schemas by `status`, and then `name`.
+  const groupedSchemas: { [category: string]: Schema[] } = flow([
+    (_schemas: Schema[]) =>
+      groupBy(_schemas, schema =>
+        startCase(schema.category ?? 'Miscellaneous')
+      ),
+    (_schemas: typeof groupedSchemas) =>
+      orderedCategories.reduce(
+        (categories: typeof groupedSchemas, category) =>
+          _schemas[category] !== undefined
+            ? {
+                ...categories,
+                [category]: sortBy(_schemas[category], ['status', 'name'])
+              }
+            : categories,
+        {}
+      )
+  ])(schemas)
+
   // Generate the index page list of links
   const schemaList = list({
-    items: Object.entries(
-      groupBy(schemas, schema => schema.category || 'Miscellaneous')
-    ).map(([group, items]) => {
-      const sortedItems = orderBy(items, ['status', 'name'])
-      return listItem({
+    items: Object.entries(groupedSchemas).map(([group, items]) =>
+      listItem({
         content: [
           strong({
-            content: [startCase(group)]
+            content: [group]
           }),
           list({
-            items: sortedItems.map(schema2ListItem),
+            items: items.map(schema2ListItem),
             order: 'unordered'
           })
         ]
       })
-    }),
+    ),
     order: 'unordered'
   })
 
@@ -133,6 +168,17 @@ async function build(): Promise<void> {
     content: [
       ...readmeContent,
       heading({ content: ['Available types'], depth: 2 }),
+      paragraph({
+        content: [
+          emphasis({
+            content: [
+              'Schemas marked with “',
+              unstableMarker,
+              '” are considered unstable and have a higher likelihood of changes.'
+            ]
+          })
+        ]
+      }),
       schemaList
     ]
   })
@@ -373,9 +419,7 @@ function schema2SummaryArticle(schema: Schema): Article {
  */
 function schema2ListItem(schema: Schema): ListItem {
   const { title = 'Untitled' } = schema
-  const status = schema.status
-    ? [' (', codeFragment({ text: schema.status || '' }), ')']
-    : []
+  const status = schema.status === 'unstable' ? [unstableMarker] : []
 
   return listItem({
     content: [
