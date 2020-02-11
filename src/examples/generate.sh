8258923ab48e73ecc7d6857c1329fcc80612457c
Nokome Bentley <nokome@stenci.la>
2020-02-11 17:08:51 +1300

    test(Examples): Add new kitchen sink example; remove simple example

--- src/examples/generate.sh
+++ src/examples/generate.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+# Generate article-kitchen-sink.html
+npx encoda convert article-kitchen-sink.json article-kitchen-sink.html
+
+# Generate article-drosophila.html
 npx encoda convert https://elifesciences.org/articles/49574v2 article-drosophila.html
 
+# Generate article-antibodies.html
 npx encoda convert 'https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1007207' article-antibodies.html
 
-curl -o rmarkdown.nb.html https://raw.githubusercontent.com/stencila/examples/master/rmarkdown/rmarkdown.nb.html
-npx encoda convert  rmarkdown.nb.html --from rnb article-rmarkdown.html
-rm rmarkdown.nb.html
+# Generate article-rmarkdown.html
+# Temporarily excluded becuase, with removal of `coerce()` call from `xmd` codec
+# the metadata is causing this to fail.
+# curl -o rmarkdown.nb.html https://raw.githubusercontent.com/stencila/examples/master/rmarkdown/rmarkdown.nb.html
+# npx encoda convert  rmarkdown.nb.html --from rnb article-rmarkdown.html
+# rm rmarkdown.nb.html
 
 # Replace unpkg Thema packages with local versions for development. Swapping out JavaScript files for TypeScript
 perl -pi -w -e 's/https:\/\/unpkg.com\/\@stencila\/thema\@1\/dist\/(.+\/index)\.js/\.\.\/$1\.ts/g;' *.html
