7543d0e2e30f8f2e0af77b23cef3123bb987cdaf
Nokome Bentley <nokome@stenci.la>
2020-02-11 20:31:17 +1300

    refactor(Mathjax CSS): Remove unused options and regenerate.

--- src/generate/mathjax.ts
+++ src/generate/mathjax.ts
@@ -12,8 +12,6 @@ import path from 'path'
 const dest = path.join(__dirname, '..', 'shared', 'styles', 'mathjax.css')
 
 MathJax.typeset({
-  format: "TeX",
-  html: true,
   css: true
 }, (result: any) => {
   const {errors, css} = result
