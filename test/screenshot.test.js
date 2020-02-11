b271d8fba4dad6d49ea17382658a18db7527efc0
Nokome Bentley <nokome@stenci.la>
2020-02-12 06:49:04 +1300

    test(Visual regressions): Test that file can be got
    See discussion in #33: https://github.com/stencila/thema/issues/33#issuecomment-584593727

--- test/screenshot.test.js
+++ test/screenshot.test.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
-const path = require('path')
-const { env, normalizeName } = require('./config.js')
+const http = require('http')
+const { env: {baseUrl, examples, diff} } = require('./config.js')
 const assert = require('assert')
 
-const isHTML = file => file.endsWith('.html')
+// Get hostname and port for doing connectivity tests
+const {hostname, port} = new URL(baseUrl)
 
 // Find HTML files stored the examples folder for testing
-const files = fs.readdirSync(env.examples).filter(isHTML)
+const files = fs.readdirSync(examples).filter(file => file.endsWith('.html'))
 
 /**
  * Takes an array of visual regression comparison results, and checks if all
@@ -21,15 +22,29 @@ const allScreenshotsPass = results =>
     true
   )
 
-describe('visual regression: ', () => {
-  files.forEach(function(file) {
-    it(`compare screenshot of ${file}`, async () => {
+describe('visual regressions: ', () => {
+  files.forEach(file => {
+    it(`${file}: can be got`, () => {
+      const req = http.get({
+        hostname,
+        port,
+        path: '/' + file
+      }, res => {
+        assert.equal(res.statusCode, '200')
+      })
+      req.on('error', error => {
+        assert.fail(error)
+      })
+      req.end()
+    })
+
+    it.skip(`${file}: screenshots have not changed`, async () => {
       await browser.url('/' + file)
       const results = await browser.checkDocument()
 
       assert.ok(
         allScreenshotsPass(results),
-        `Styles differ from current references. Please see ${env.diff} for differences`
+        `Styles differ from current references. Please see ${diff} for differences`
       )
     })
   })
