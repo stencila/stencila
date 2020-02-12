186c1acbf49e4983d3391555318e0069f1aebd9c
Nokome Bentley <nokome@stenci.la>
2020-02-13 11:04:30 +1300

    chore(Python formatting): Do not run black on types.py

--- py/setup.py
+++ py/setup.py
@@ -11,17 +11,21 @@ from shutil import rmtree
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
-VERSION_PATH = os.path.join(HERE, 'version')
+VERSION_PATH = os.path.join(HERE, "version")
 
 
 def get_tag_version() -> str:
     """Get the version by parsing git tag."""
-    result = subprocess.run(['git', 'describe', '--tags', '--abbrev=0'], stdout=subprocess.PIPE, encoding='ascii')
+    result = subprocess.run(
+        ["git", "describe", "--tags", "--abbrev=0"],
+        stdout=subprocess.PIPE,
+        encoding="ascii",
+    )
     version = result.stdout
-    return version[1:] if version.startswith('v') else version
+    return version[1:] if version.startswith("v") else version
 
 
 def get_version():
@@ -31,7 +35,7 @@ def get_version():
             return vf.read().strip()
 
     tag_version = get_tag_version().strip()
-    with open(VERSION_PATH, 'w') as vf:
+    with open(VERSION_PATH, "w") as vf:
         vf.write(tag_version)
     return tag_version
 
@@ -43,13 +47,13 @@ class UploadCommand(Command):
     Based on, and thanks to, https://github.com/kennethreitz/setup.py/blob/master/setup.py
     """
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -59,46 +63,48 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(HERE, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(HERE, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
+        self.status("Uploading the package to PyPI via Twine…")
 
-        repo_arg = '--repository-url https://test.pypi.org/legacy/' if os.environ.get('PYPI_ENV') == 'test' else ''
+        repo_arg = (
+            "--repository-url https://test.pypi.org/legacy/"
+            if os.environ.get("PYPI_ENV") == "test"
+            else ""
+        )
 
-        os.system('twine upload {} dist/*'.format(repo_arg))
+        os.system("twine upload {} dist/*".format(repo_arg))
 
         sys.exit()
 
 
 setup(
-    name='stencila-schema',
+    name="stencila-schema",
     version=get_version(),
-    description='',
+    description="",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Stencila and contributors',
-    author_email='hello@stenci.la',
-    python_requires='>=3.6.0',
-    url='https://github.com/stencila/schema',
-    packages=['stencila.schema'],
+    long_description_content_type="text/markdown",
+    author="Stencila and contributors",
+    author_email="hello@stenci.la",
+    python_requires=">=3.6.0",
+    url="https://github.com/stencila/schema",
+    packages=["stencila.schema"],
     extras_require={},
     include_package_data=True,
-    license='Apache-2.0',
+    license="Apache-2.0",
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand,},
 )
