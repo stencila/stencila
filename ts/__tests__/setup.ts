083d693bcd4e6a048992cfa88ca0b1960c79e516
Nokome Bentley <nokome@stenci.la>
2020-02-13 13:58:43 +1300

    chore(*): Linting

--- ts/__tests__/setup.ts
+++ ts/__tests__/setup.ts
@@ -3,6 +3,6 @@ import { build } from '../schema'
 /**
  * Global setup run before any tests
  */
-export default async function setup(): void {
+export default async function setup(): Promise<void> {
   await build()
 }
