d1d941ec70b9bbd068cea5e34d801f8a9362bf0b
Nokome Bentley <nokome@stenci.la>
2020-02-17 14:36:51 +1300

    fix(Skeleton): Ensure index.ts is a module

--- src/themes/skeleton/index.ts
+++ src/themes/skeleton/index.ts
@@ -1,3 +1,6 @@
 // If your theme needs Javascript (e.g. to reorder elements in HTML)
 // put it in here. See the `src/shared/js` folder for useful, reusable functions.
 export {}
+
+// This export is necessary to make this a module for importing into themes.ts.
+export {}
