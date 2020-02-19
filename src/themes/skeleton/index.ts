e73d8530d8dddb2523444d35d04c1be367bbdaeb
Nokome Bentley <nokome@stenci.la>
2020-02-19 19:32:22 +1300

    feat(Skeleton theme): Add all existing addons

--- src/themes/skeleton/index.ts
+++ src/themes/skeleton/index.ts
@@ -1,6 +1,3 @@
-// If your theme needs Javascript (e.g. to reorder elements in HTML)
-// put it in here. See the `src/shared/js` folder for useful, reusable functions.
-export {}
-
-// This export is necessary to make this a module for importing into themes.ts.
-export {}
+import '../../addons/mathjax'
+import '../../addons/prism'
+import '../../addons/stencilaComponents'
