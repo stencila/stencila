70f69c348980979da3ab78c63230d7e318f0e5ed
Nokome Bentley <nokome@stenci.la>
2020-02-27 15:10:59 +1300

    test(APA citations extension): Add a test

---
+++ src/extensions/cite-apa/index.test.ts
@@ -0,0 +1,19 @@
+import { select, text, whenReady } from '../../util'
+
+test('DOM manipulations', async () => {
+  document.body.innerHTML = `
+  <section data-itemprop="references">
+    <li itemscope="" itemtype="http://schema.org/Person" itemprop="author">
+       <span itemprop="name" content="Sarel J. Fleishman">
+         <span itemprop="givenName">Sarel J.</span>
+         <span itemprop="familyName">Fleishman</span>
+       </span>
+    </li>
+  </section>
+  `
+
+  await import('.')
+  whenReady()
+
+  expect(select(':--givenName').map(elem => text(elem))).toEqual(['S', 'J'])
+})
