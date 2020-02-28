b21b8c8b11af46db44f3fe5c77581dfdc96d0e75
alex-ketch <alex@ketch.me>
2020-02-27 23:38:33 -0500

    wip Add foundations of theme customization UI

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -17,8 +17,17 @@
 
 import { examples, resolveExample } from '../examples'
 import { themes } from '../themes'
+import { create, append, ready } from '../util'
+import { styleEntry } from '../browser'
+import { getCssVariables } from './parseCss'
 
 const url = new URL(window.location.href)
+let preview: HTMLIFrameElement | null | undefined
+const getPreviewDoc = (): Document | null =>
+  preview != null ? preview.contentDocument : null
+
+const themeName = document.getElementById('themeName')
+const themeVariables = document.getElementById('themeVariables')
 
 /**
  * The keys used to refer to which example and
@@ -43,8 +52,43 @@ const defaults: {
   HEADER: 'true'
 }
 
-// The `init` function of the current theme
-let themeInit: () => void
+const getThemeCSS = (theme: string): string => {
+  const req = new XMLHttpRequest()
+  req.open('GET', `themes/${theme}/styles.css`, false)
+  req.send(null)
+  return req.responseText
+}
+
+// Theme customizer UI
+const parseTheme = (theme: string): void => {
+  // Find CSS variables from a stylesheet
+  const css = getThemeCSS(theme)
+  const vars = getCssVariables(css)
+
+  // Build up a form label/input pairs for each variable
+  const formEls = Object.entries(vars).reduce(
+    (els: Element[], [name, value]) => {
+      const label = create('label')
+      label.textContent = name
+
+      const input = create('input') as HTMLInputElement
+      input.value = value
+
+      return [...els, label, input]
+    },
+    []
+  )
+
+  if (themeName !== null) {
+    themeName.textContent = theme
+  }
+
+  // Insert input fields into sidebar
+  if (themeVariables !== null) {
+    themeVariables.innerHTML = ''
+    formEls.map(el => append(themeVariables, el))
+  }
+}
 
 // Set an example
 const exampleSet = (example: string): void => {
@@ -54,19 +98,10 @@ const exampleSet = (example: string): void => {
   sessionStorage.setItem(keys.EXAMPLE, example)
   if (exampleSelect !== null) exampleSelect.value = example
 
-  // Load the HTML content
-  const req = new XMLHttpRequest()
-  req.open('GET', `examples/${resolveExample(example)}.html`, false)
-  req.send(null)
-  const html = req.responseText
-
-  // Set the content of `<main>` to the content of the example `<body>`
-  const dom = new DOMParser().parseFromString(html, 'text/html')
-  const main = document.getElementsByTagName('main')[0]
-  main.innerHTML = dom.body.innerHTML
-
-  // Run the `init` function of the current theme
-  if (themeInit !== undefined) themeInit()
+  // Change the preview document contents to the chosen example
+  if (preview != null) {
+    preview.setAttribute('src', `examples/${resolveExample(example)}.html`)
+  }
 }
 
 // Initialize the example selector
@@ -104,34 +139,56 @@ const themeSet = (theme: string): void => {
   sessionStorage.setItem(keys.THEME, theme)
   if (themeSelect !== null) themeSelect.value = theme
 
-  // Enable the theme's CSS
-  document
-    .querySelectorAll('link.theme[rel="stylesheet"]')
-    .forEach(node => ((node as HTMLInputElement).disabled = node.id !== theme))
+  parseTheme(theme)
 
-  // Append remove all theme scripts, and re-append chosen theme’s script
+  const themeStyles = create('link')
+  themeStyles.setAttribute('rel', 'stylesheet')
+  themeStyles.setAttribute('href', `/themes/${theme}/${styleEntry}`)
+  themeStyles.setAttribute('id', 'themaStyles')
+
+  const previewDoc = getPreviewDoc()
+  const previewHead = previewDoc?.getElementsByTagName('head')[0]
+
+  if (preview != null && previewHead != null) {
+    const injectedStyle = previewDoc?.getElementById('themaStyles')
+
+    if (injectedStyle != null) {
+      injectedStyle.remove()
+    }
+
+    append(previewHead, themeStyles)
+  }
+
+  // Remove all appended theme scripts, and re-append chosen theme’s script
   // This causes the browser to re-evaluate the script
-  document.querySelectorAll('script.themeScript').forEach(node => {
-    document.body.removeChild(node)
-  })
+  // TODO: Check if this is still needed now that content is in an iframe
+  if (previewDoc != null) {
+    previewDoc.querySelectorAll('script.themeScript').forEach(node => {
+      if (previewDoc != null) {
+        previewDoc.body.removeChild(node)
+      }
+    })
 
-  const themeScript = document.createElement('script')
-  themeScript.type = 'text/javascript'
-  themeScript.src = `themes/${theme}/index.js`
-  themeScript.classList.add('themeScript')
+    const themeScript = previewDoc.createElement('script')
+    themeScript.type = 'text/javascript'
+    themeScript.src = `themes/${theme}/index.js`
+    themeScript.classList.add('themeScript')
 
-  document.body.appendChild(themeScript)
+    previewDoc.body.appendChild(themeScript)
 
-  // Add delay before dispatching ready event, giving newly added theme script time to be parsed
-  // This causes the dom/ready() helper function to perform any scheduled functions.
-  setTimeout(() => {
-    document.dispatchEvent(
-      new Event('DOMContentLoaded', {
-        bubbles: true,
-        cancelable: true
-      })
-    )
-  }, 300)
+    // Add delay before dispatching ready event, giving newly added theme script time to be parsed
+    // This causes the dom/ready() helper function to perform any scheduled functions.
+    setTimeout(() => {
+      if (previewDoc != null) {
+        previewDoc.dispatchEvent(
+          new Event('DOMContentLoaded', {
+            bubbles: true,
+            cancelable: true
+          })
+        )
+      }
+    }, 300)
+  }
 }
 
 // Set a theme when it is selected from the theme selector
@@ -153,16 +210,50 @@ if (themeSelect !== null) {
   })
 }
 
-// Set the initial theme
-themeSet(
-  url.searchParams.get(keys.THEME) ??
-    sessionStorage.getItem(keys.THEME) ??
-    defaults.THEME
-)
-
-// Set display of header
-const header = document.querySelector<HTMLInputElement>('#header')
-if (header !== null) {
-  header.style.display =
-    url.searchParams.get(keys.HEADER) === 'false' ? 'none' : 'block'
+// Change preview iframe's size to simulate a mobile view
+const mobileView = (e: MouseEvent): void => {
+  e.preventDefault()
+  if (preview != null) {
+    preview.classList.add('mobile')
+  }
 }
+
+// Make preview iframe full width
+const desktopView = (e: MouseEvent): void => {
+  e.preventDefault()
+  if (preview != null) {
+    preview.classList.remove('mobile')
+  }
+}
+
+ready(() => {
+  // TOOD: Check to see if this is triggered each time theme/example is changed
+  preview = document.getElementsByTagName('iframe')[0] ?? null
+
+  // Set display of header
+  const header = document.querySelector<HTMLInputElement>('#header')
+  if (header !== null) {
+    header.style.display =
+      url.searchParams.get(keys.HEADER) === 'false' ? 'none' : 'block'
+  }
+
+  // Attach event handler to theme editor sidebar
+  const mobileButton = document.getElementById('mobileView')
+  if (mobileButton !== null) {
+    mobileButton.addEventListener('mouseup', mobileView)
+  }
+
+  const desktopButton = document.getElementById('desktopView')
+  if (desktopButton !== null) {
+    desktopButton.addEventListener('mouseup', desktopView)
+  }
+
+  preview.addEventListener('load', function() {
+    // Set the initial theme
+    themeSet(
+      url.searchParams.get(keys.THEME) ??
+        sessionStorage.getItem(keys.THEME) ??
+        defaults.THEME
+    )
+  })
+})
