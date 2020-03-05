d0603a2f7ac0e7a66e779ba5dea09d3b81f760a3
David Moulton <d.moulton@elifesciences.org>
2020-03-05 17:26:17 +0000

    feat(Date): Format eLife's publication date correctly

--- src/themes/elife/index.ts
+++ src/themes/elife/index.ts
@@ -1,1 +1,19 @@
-export {}
+import { first, ready } from '../../util'
+import DateTimeFormat = Intl.DateTimeFormat
+
+function elifeFormatDate(date: Date): string {
+  const formatter: DateTimeFormat = new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric'
+  })
+  return formatter.format(date)
+}
+
+ready(() => {
+  const dateEl: Element | null = first(':--Date')
+  if (!(dateEl instanceof Element)) return
+  const date = new Date(dateEl.innerHTML)
+  const formattedDate: string = elifeFormatDate(date)
+  dateEl.innerHTML = formattedDate
+})
