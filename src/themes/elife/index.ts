5dc178217c3c58d197934b5dffbda65b50ce85fa
David Moulton <d.moulton@elifesciences.org>
2020-03-09 11:46:32 +0000

    refactor(Elife): Extract function

--- src/themes/elife/index.ts
+++ src/themes/elife/index.ts
@@ -1,18 +1,19 @@
 import { first, ready } from '../../util'
 import DateTimeFormat = Intl.DateTimeFormat
 
-function elifeFormatDate(date: Date): string {
-  const formatter = new DateTimeFormat('en-US', {
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric'
-  })
-  return formatter.format(date)
+const dateFormatter = new DateTimeFormat('en-US', {
+  month: 'short',
+  day: 'numeric',
+  year: 'numeric'
+})
+
+const formatDate = (date: Date): string => {
+  return dateFormatter.format(date)
 }
 
 ready((): void => {
   const dateEl = first(':--Date')
   if (!(dateEl instanceof Element)) return
   const date = new Date(dateEl.innerHTML)
-  dateEl.innerHTML = elifeFormatDate(date)
+  dateEl.innerHTML = formatDate(date)
 })
