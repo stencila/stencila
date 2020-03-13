7e540e3a62535a76860142d2d359bb17610d0b14
David Moulton <d.moulton@elifesciences.org>
2020-03-13 14:03:17 +0000

    feat(Elife): Build more of the downloads list

---
+++ src/themes/elife/downloads.ts
@@ -0,0 +1,81 @@
+import { append, create, select } from '../../util';
+
+const getUrl = (type: string, id: string, title = ''): string => {
+  switch (type) {
+    case 'bibtex':
+      return `https://elifesciences.org/articles/${id}.bib`
+    case 'ris':
+      return `https://elifesciences.org/articles/${id}.ris`
+    case 'mendeley':
+      return `https://www.mendeley.com/import?doi=10.7554/eLife.${id}`
+    case 'readcube':
+      return `https://www.readcube.com/articles/10.7554/eLife.${id}`
+    case 'papers':
+      return `papers2://url/https%3A%2F%2Felifesciences.org%2Farticles%2F46206?title=${encodeURIComponent(
+        title
+      )}`
+    case 'citeulike':
+      return `http://www.citeulike.org/posturl?url=https%3A%2F%2Felifesciences.org%2Farticles%2F${id}&title=${encodeURIComponent(
+        title
+      )}&doi=10.7554/eLife.${id}`
+  }
+  return ''
+}
+
+export const build = (articleId: string, articleTitle: string): void => {
+  append(
+    select(':--references')[0],
+    create('h2', null, 'Download links'),
+    create('h3', null, 'Downloads'),
+    create(
+      'ul',
+      null,
+      create('li', null, create('a', { href: '#' }, 'Article PDF')),
+      create('li', null, create('a', { href: '#' }, 'Figures PDF'))
+    ),
+    create('h3', null, 'Download citations'),
+    create(
+      'ul',
+      null,
+      create(
+        'li',
+        null,
+        create('a', { href: `${getUrl('bibtex', articleId)}` }, 'BibTeX')
+      ),
+      create(
+        'li',
+        null,
+        create('a', { href: `${getUrl('ris', articleId)}` }, 'RIS')
+      )
+    ),
+    create('h3', null, 'Open citations'),
+    create(
+      'ul',
+      null,
+      create(
+        'li',
+        null,
+        create('a', { href: `${getUrl('mendeley', articleId)}` }, 'Mendeley')
+      ),
+      create(
+        'li',
+        null,
+        create('a', { href: `${getUrl('readcube', articleId)}` }, 'ReadCube')
+      ),
+      create(
+        'li',
+        null,
+        create('a', { href: `${getUrl('papers', articleId)}` }, 'Papers')
+      ),
+      create(
+        'li',
+        null,
+        create(
+          'a',
+          { href: `${getUrl('citeulike', articleId, articleTitle)}` },
+          'CiteULike'
+        )
+      )
+    )
+  )
+}
--- src/themes/elife/index.ts
+++ src/themes/elife/index.ts
@@ -1,7 +1,7 @@
 import '../../extensions/headings'
 import '../../extensions/person'
-// import { first, ready } from '../../util'
-import * as util from '../../util'
+import { first, ready } from '../../util'
+import * as downloads from './downloads'
 import DateTimeFormat = Intl.DateTimeFormat
 
 const dateFormatter = new DateTimeFormat('en-US', {
@@ -14,41 +14,11 @@ const formatDate = (date: Date): string => {
   return dateFormatter.format(date)
 }
 
-const buildDownloads = (): void => {
-  util.append(
-    util.select(':--references')[0],
-    util.create('h2', null, 'Download links'),
-    util.create('h3', null, 'Downloads'),
-    util.create(
-      'ul',
-      null,
-      util.create('li', null, util.create('a', { href: '#' }, 'Article PDF')),
-      util.create('li', null, util.create('a', { href: '#' }, 'Figures PDF'))
-    ),
-    util.create('h3', null, 'Download citations'),
-    util.create(
-      'ul',
-      null,
-      util.create('li', null, util.create('a', { href: '#' }, 'BibTeX')),
-      util.create('li', null, util.create('a', { href: '#' }, 'RIS'))
-    ),
-    util.create('h3', null, 'Open citations'),
-    util.create(
-      'ul',
-      null,
-      util.create('li', null, util.create('a', { href: '#' }, 'Mendeley')),
-      util.create('li', null, util.create('a', { href: '#' }, 'ReadCube')),
-      util.create('li', null, util.create('a', { href: '#' }, 'Papers')),
-      util.create('li', null, util.create('a', { href: '#' }, 'CiteULike'))
-    )
-  )
-}
-
-util.ready((): void => {
-  const dateEl = util.first(':--Date')
+ready((): void => {
+  const dateEl = first(':--Date')
   if (!(dateEl instanceof Element)) return
   const date = new Date(dateEl.innerHTML)
   dateEl.innerHTML = formatDate(date)
 
-  buildDownloads()
+  downloads.build('TheArticleId', 'The Article Title')
 })
