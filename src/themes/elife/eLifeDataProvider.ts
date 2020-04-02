2aa68987add787ec4e1186279fa0dba76902806d
David Moulton <d.moulton@elifesciences.org>
2020-04-02 14:01:53 +0100

    refactor(Elife): Use async / await

--- src/themes/elife/eLifeDataProvider.ts
+++ src/themes/elife/eLifeDataProvider.ts
@@ -3,14 +3,11 @@ interface ArticleData {
 }
 
 export default {
-  query: (id: string, fetcher: Function): Promise<ArticleData> => {
-    return fetcher(`https://api.elifesciences.org/articles/${id}`).then(
-      (data: ArticleData) => {
-        if (data.status === 404) {
-          throw new ReferenceError(`Invalid eLife article id: ${id}`)
-        }
-        return { status: data.status }
-      }
-    )
+  query: async (id: string, fetcher: Function): Promise<ArticleData> => {
+    const data = await fetcher(`https://api.elifesciences.org/articles/${id}`)
+    if (data.status === 404) {
+      throw new ReferenceError(`Invalid eLife article id: ${id}`)
+    }
+    return Promise.resolve({ status: data.status })
   }
 }
