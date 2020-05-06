2081f0063b48b2e5d7621c0cce964116e20ba05e
David Moulton <d.moulton@elifesciences.org>
2020-05-06 17:21:27 +0100

    feat(Elife): Catch errors from date formatting

--- src/themes/elife/index.ts
+++ src/themes/elife/index.ts
@@ -6,12 +6,11 @@ import * as socialSharers from './lib/socialSharers'
 import * as references from './lib/references'
 
 ready((): void => {
-  dateFormatter.format(first(':--datePublished'))
-
   const articleTitle = dataProvider.getArticleTitle()
   downloads.build(articleTitle, dataProvider.getArticleId())
 
   try {
+    dateFormatter.format(first(':--datePublished'))
     socialSharers.build(articleTitle, dataProvider.getArticleDoi())
   } catch (e) {
     console.error(e)
