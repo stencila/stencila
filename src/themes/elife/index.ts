8feadf2f50df188ec563c080ec3334fbc0da20b6
nlisgo <nathan@aeopus.com>
2020-07-22 14:38:00 +0100

    refactor(Elife): introduce generic pre-header-wrapper

--- src/themes/elife/index.ts
+++ src/themes/elife/index.ts
@@ -1,13 +1,21 @@
-import { first, ready, select } from '../../util'
+import { before, create, first, ready, select } from '../../util'
 import * as dateFormatter from './lib/dateFormatter'
 import * as dataProvider from './lib/dataProvider'
 import * as downloads from './lib/downloads'
 import * as socialSharers from './lib/socialSharers'
 import * as referenceFormatter from './lib/referencesFormatter'
 
-ready((): void => {
+ready((): void | unknown => {
+  const articleTitleElement = first(':--Article > :--title')
+  if (articleTitleElement === null) {
+    return Promise.reject(
+      new Error("Can't find element to bolt the pre-header-wrapper on top of")
+    )
+  }
+  const preHeaderWrapper = create('div', { class: 'pre-header-wrapper' })
+  before(articleTitleElement, preHeaderWrapper)
   const articleTitle = dataProvider.getArticleTitle()
-  downloads.build(articleTitle, dataProvider.getArticleId())
+  downloads.build(preHeaderWrapper, articleTitle, dataProvider.getArticleId())
 
   try {
     dateFormatter.format(first(':--datePublished'))
