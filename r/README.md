7aed77e91712843f44f232a40b51832253041cbb
Nokome Bentley <nokome@stenci.la>
2021-01-17 16:36:08 +1300

    feat(R): Very preliminary version of Stencila for R package

---
+++ r/README.md
@@ -0,0 +1,5 @@
+# Stencila for R
+
+This is a very preliminary version of a `stencila` package for R. It uses the [`extendr`](https://github.com/extendr/extendr) to expose functions implemented in Rust to R.
+
+See this repository's roadmap for when further work on this package is anticipated.
---
+++ r/src/lib.rs
@@ -0,0 +1,41 @@
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
+
+use extendr_api::*;
+use std::str::FromStr;
+
+#[extendr]
+fn init(rfunc: &str) -> Result<()> {
+    let _robj = call!(rfunc)?;
+    Ok(())
+}
+
+#[extendr]
+fn serve(
+    protocol: Option<String>,
+    address: Option<String>,
+    port: u16,
+    background: Option<bool>,
+) -> Result<()> {
+    let protocol = protocol.unwrap_or_else(|| "stdio".to_string());
+    let protocol = match stencila::protocols::Protocol::from_str(protocol.as_str()) {
+        Ok(value) => Some(value),
+        Err(error) => return Err(Error::Other("Invalid protocol".to_string())),
+    };
+
+    let background = background.unwrap_or(false);
+
+    match if background {
+        stencila::serve::serve_background(protocol, address, Some(port))
+    } else {
+        stencila::serve::serve_blocking(protocol, address, Some(port))
+    } {
+        Ok(_) => Ok(()),
+        Err(error) => Err(Error::Other(error.to_string())),
+    }
+}
+
+extendr_module! {
+    mod stencila;
+    fn init;
+    fn serve;
+}
