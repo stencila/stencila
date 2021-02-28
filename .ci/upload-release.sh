8ac528cc9b87808576a564ce100214d82a00d3f3
Nokome Bentley <nokome@stenci.la>
2021-03-01 08:33:32 +1300

    fix(Release): Drop use of upload label which was causing issues for Windows
    Using `%20` in the label appears to have caused issues on Windows.
    Dropping label also has advantage of not obscuring actual file name in release asset list.

--- .ci/upload-release.sh
+++ .ci/upload-release.sh
@@ -2,13 +2,14 @@
 
 # Upload files to a GitHub release
 #
-#    upload-release.sh <path> <asset> <triple> <label>
+#    upload-release.sh <path> <asset> <triple>
 #
 # <path> is the local path to the binary, <asset> is (usually) the binary name, <triple> is the target triple
 # (see https://github.com/japaric/trust#supported-targets) and <label> is the label for the
-# file on the GitHub release page.
+# file on the GitHub release page. See https://github.com/japaric/trust/releases for an example
+# of the download names we are targetting for compatability with `self_update` crate.
 #
-#    upload-release.sh ./target/release/stencila stencila x86_64-unknown-linux-gnu Linux%20CLI
+#    upload-release.sh ./target/release/stencila stencila x86_64-unknown-linux-gnu
 #
 # Expects the $GITHUB_TOKEN to be set and for the release for the current tag to have already
 # been created.
@@ -18,7 +19,6 @@ set -e
 FILE_PATH=$1
 ASSET_NAME=$2
 TARGET_TRIPLE=$3
-DOWNLOAD_LABEL=$4
 
 AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 TAG=$(git describe --tags --abbrev=0)
@@ -26,7 +26,7 @@ echo "Will upload for tag $TAG"
 RELEASE_ID=$(curl -s -H "$AUTH_HEADER" "https://api.github.com/repos/stencila/stencila/releases/tags/$TAG" | grep -m 1 "id.:" | cut -c9-16)
 echo "Will upload to release $RELEASE_ID"
 DOWNLOAD_NAME="$ASSET_NAME-$TAG-$TARGET_TRIPLE"
-UPLOAD_URL="https://uploads.github.com/repos/stencila/stencila/releases/$RELEASE_ID/assets?name=$DOWNLOAD_NAME&label=$DOWNLOAD_LABEL"
+UPLOAD_URL="https://uploads.github.com/repos/stencila/stencila/releases/$RELEASE_ID/assets?name=$DOWNLOAD_NAME"
 echo "Will upload $FILE_PATH to $UPLOAD_URL"
 curl -H "$AUTH_HEADER" -H "Content-Type: application/octet-stream" -o /dev/null --data-binary @"$FILE_PATH" "$UPLOAD_URL"
 echo "Upload complete"
