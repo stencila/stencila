e8cdf4477e7472e3c85fb27dff9c0a1b7359d606
Nokome Bentley <nokome@stenci.la>
2021-03-02 18:44:50 +1300

    perf(Upgrade): Release and fetch compressed binaries

--- .ci/upload-release.sh
+++ .ci/upload-release.sh
@@ -2,14 +2,15 @@
 
 # Upload files to a GitHub release
 #
-#    upload-release.sh <path> <asset> <triple>
+#    upload-release.sh <path> <asset> <triple> <format>
 #
-# <path> is the local path to the binary, <asset> is (usually) the binary name, <triple> is the target triple
-# (see https://github.com/japaric/trust#supported-targets) and <label> is the label for the
-# file on the GitHub release page. See https://github.com/japaric/trust/releases for an example
+# <path> is the local path to the binary, <asset> is the asset name, <target> is
+# the targe triple and <format> is the archive format (i.e. `zip` or `tar.gz`)
+# See https://github.com/japaric/trust/releases for an example
 # of the download names we are targetting for compatability with `self_update` crate.
 #
-#    upload-release.sh ./target/release/stencila stencila x86_64-unknown-linux-gnu
+#    upload-release.sh target/release/stencila stencila x86_64-unknown-linux-gnu.tar.gz
+#    upload-release.sh target/release/stencila.exe stencila x86_64-pc-windows-msvc.zip
 #
 # Expects the $GITHUB_TOKEN to be set and for the release for the current tag to have already
 # been created.
@@ -19,14 +20,25 @@ set -e
 FILE_PATH=$1
 ASSET_NAME=$2
 TARGET_TRIPLE=$3
+ARCHIVE_FORMAT=$4
 
-AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+ARCHIVE_PATH="$FILE_PATH.$ARCHIVE_FORMAT"
+if [ $ARCHIVE_FORMAT == "zip" ]; then
+    (cd $(dirname $FILE_PATH) && zip -r - $(basename $FILE_PATH)) > $ARCHIVE_PATH
+elif [ $ARCHIVE_FORMAT == "tar.gz" ]; then
+    tar -C $(dirname $FILE_PATH) -czvf $ARCHIVE_PATH $(basename $FILE_PATH)
+fi
+
 TAG=$(git describe --tags --abbrev=0)
 echo "Will upload for tag $TAG"
+
+AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
 RELEASE_ID=$(curl -s -H "$AUTH_HEADER" "https://api.github.com/repos/stencila/stencila/releases/tags/$TAG" | grep -m 1 "id.:" | cut -c9-16)
 echo "Will upload to release $RELEASE_ID"
-DOWNLOAD_NAME="$ASSET_NAME-$TAG-$TARGET_TRIPLE"
+
+DOWNLOAD_NAME="$ASSET_NAME-$TAG-$TARGET_TRIPLE.$ARCHIVE_FORMAT"
 UPLOAD_URL="https://uploads.github.com/repos/stencila/stencila/releases/$RELEASE_ID/assets?name=$DOWNLOAD_NAME"
-echo "Will upload $FILE_PATH to $UPLOAD_URL"
-curl -H "$AUTH_HEADER" -H "Content-Type: application/octet-stream" -o /dev/null --data-binary @"$FILE_PATH" "$UPLOAD_URL"
+echo "Will upload $ARCHIVE_PATH to $UPLOAD_URL"
+
+curl -H "$AUTH_HEADER" -H "Content-Type: application/octet-stream" -o /dev/null --data-binary @"$ARCHIVE_PATH" "$UPLOAD_URL"
 echo "Upload complete"
