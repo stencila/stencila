b5fe31d0b1edb2f6ffacb8a8ec052c60cf9a5a01
Nokome Bentley <nokome@stenci.la>
2021-03-03 20:41:43 +1300

    fix(Deps): Use dirs-next instead of dirs
    See https://rustsec.org/advisories/RUSTSEC-2020-0053

--- rust/src/util/dirs.rs
+++ rust/src/util/dirs.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 
 /// Get the directory where configuration data is stored
 pub fn config(ensure: bool) -> Result<PathBuf> {
-    let config_base = dirs::config_dir().unwrap_or_else(|| env::current_dir().unwrap());
+    let config_base = dirs_next::config_dir().unwrap_or_else(|| env::current_dir().unwrap());
     let dir = match env::consts::OS {
         "macos" => config_base.join("Stencila"),
         "windows" => config_base.join("Stencila").join("Config"),
