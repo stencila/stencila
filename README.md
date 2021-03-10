ca2c15accb60aa0c7ac742cab0d3b1ce55b7d6ee
Nokome Bentley <nokome@stenci.la>
2021-03-11 10:27:35 +1300

    feat(Install): Add install script and instructions for CLI

--- README.md
+++ README.md
@@ -27,6 +27,40 @@ If you want to use Stencila on your own machine, then the `stencila` command lin
 
 🦄 The CLI is is early stages of development (again, all contributions welcome!). We don't recommend installing it yet, but if you are an early adopter 💖, we'd also appreciate any feedback. You can download standalone binaries for MacOS, Windows or Linux from the [latest release](https://github.com/stencila/stencila/releases/latest).
 
+### Windows
+
+To install the latest release download `stencila-<version>-x86_64-pc-windows-msvc.zip` from the [latest release](https://github.com/stencila/stencila/releases/latest) and place it somewhere on your `PATH`.
+
+### MacOS
+
+To install the latest release in `/usr/local/bin` just use,
+
+```bash
+curl -L https://raw.githubusercontent.com/stencila/stencila/master/install.sh | bash
+```
+
+To install a specific version, append `-s vX.X.X`. Or, if you'd prefer to do it manually, download `stencila-<version>-x86_64-apple-darwin.tar.gz` from the one of the [releases](https://github.com/stencila/stencila/releases) and then,
+
+```bash
+tar xvf stencila-*.tar.gz
+sudo mv -f stencila /usr/local/bin # or wherever you prefer
+```
+
+### Linux
+
+To install the latest release in `~/.local/bin/` just use,
+
+```bash
+curl -L https://raw.githubusercontent.com/stencila/stencila/master/install.sh | bash
+```
+
+To install a specific version, append `-s vX.X.X`. Or, if you'd prefer to do it manually, download `stencila-<version>-x86_64-unknown-linux-gnu.tar.gz` from the one of the [releases](https://github.com/stencila/stencila/releases) and then,
+
+```bash
+tar xvf stencila-*.tar.gz
+mv -f stencila ~/.local/bin/ # or wherever you prefer
+```
+
 <br>
 
 ## 🔌 Plugins
---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# A script to download and install the latest (or specified) version
+# of the Stencila CLI on MacOS or Linux
+#  
+# First arg:  desired version e.g. v0.42.1
+#             defaults to latest
+# Second arg: desired install location, e.g. /bin
+#             defaults to /usr/local/bin on MacOD, to ~/.local/bin on Linux
+
+set -e
+
+OS=$(uname)
+if [[ "${OS}" == "Linux" || "${OS}" == "Darwin" ]]; then
+    case "${OS}" in
+        'Linux')
+            if [ -z "$1" ]; then
+                VERSION=$(curl --silent "https://api.github.com/repos/stencila/stencila/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+            else
+                VERSION=$1
+            fi
+            TARGET_TRIPLE="x86_64-unknown-linux-gnu"
+            INSTALL_PATH="${2:-$HOME/.local/bin}"
+            ;;
+        'Darwin')
+            if [ -z "$1" ]; then
+                VERSION=$(curl --silent "https://api.github.com/repos/stencila/stencila/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+            else
+                VERSION=$1
+            fi
+            TARGET_TRIPLE="x86_64-apple-darwin"
+            INSTALL_PATH="${2:-/usr/local/bin}"
+            ;;
+    esac
+    
+    echo "Downloading stencila $VERSION for platform $TARGET_TRIPLE"
+    curl -sL https://github.com/stencila/stencila/releases/download/$VERSION/stencila-$VERSION-$TARGET_TRIPLE.tar.gz | tar xvz
+    
+    echo "Installing stencila in $INSTALL_PATH"
+    mv stencila* $INSTALL_PATH
+    
+    echo "Successfully installed stencila CLI"
+else
+    echo "Sorry, I don't know how to install on this operating system, please see https://github.com/stencila/stencila#readme"
+fi
