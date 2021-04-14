2544b6ea755239cef67cbaaf236a92ebcce1f6c6
Nokome Bentley <nokome@stenci.la>
2021-04-14 14:08:29 +1200

    feat(VSCode bindings): Add maapings between file types and schemas
    Also simplifies package.json scripts and other updates there.

---
+++ ts/bindings/vscode.ts
@@ -0,0 +1,31 @@
+/**
+ * A script to update the `json.schemas` property of the `./vscode/settings.json` file.
+ *
+ * This enabled intellisense for files following the naming conversion of `.<type>.json`.
+ * For example, `a.cite.json` file will have `Cite.schema.json` applied to it within
+ * the editor.
+ *
+ * @see https://code.visualstudio.com/docs/languages/json#_json-schemas-and-settings
+ *
+ * Run using `npx ts-node ts/bindings/vscode.ts`
+ */
+
+import fs from 'fs-extra'
+import path from 'path'
+import { filterInterfaceSchemas, readSchemas } from '../util/helpers'
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+;(async () => {
+  const mappings = filterInterfaceSchemas(await readSchemas()).map(
+    ({ title }) => ({
+      fileMatch: [`*.${title?.toLowerCase() ?? ''}.json`],
+      url: `./public/${title ?? ''}.schema.json`,
+    })
+  )
+  const file = path.join(__dirname, '..', '..', '.vscode', 'settings.json')
+  fs.writeJsonSync(
+    file,
+    { ...fs.readJsonSync(file), 'json.schemas': mappings },
+    { spaces: 2 }
+  )
+})()
