245d701ca948b2880b846e7b4174451e3578b0f4
Nokome Bentley <nokome@stenci.la>
2021-06-07 13:30:02 +1200

    refactor(*): Remove viewer folder
    Functionality is now in `encode_html` module in Rust.

--- viewer/src/Viewer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from 'solid-js/web'
-import { Viewer } from './Viewer'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  const dispose = render(Viewer, div)
-  div.textContent = ''
-  dispose()
-})
