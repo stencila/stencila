020e53af0cc5b224ddf6e2826134c94e81745040
Nokome Bentley <nokome@stenci.la>
2021-06-18 07:22:59 +1200

    test(TypeScript bindings): Remove unecessary snapshots tests
    Unecessary as the other test (which compiles TypeScript) covers that code

--- ts/bindings/__file_snapshots__/BlockContent.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Union type for valid block content.
- */
-export type BlockContent = Claim | CodeBlock | CodeChunk | Collection | Figure | Heading | List | MathBlock | Paragraph | QuoteBlock | Table | ThematicBreak
-
--- ts/bindings/typescript.test.ts
+++ ts/bindings/typescript.test.ts
@@ -1,34 +1,8 @@
-/**
- * Tests of the generation of Typescript language bindings
- *
- * This test suite uses file snapshots. During development
- * it can be useful to update the snapshots on the fly for manual inspection:
- *
- * ```bash
- * npx jest typescript.test.ts --watch --updateSnapshot
- * ```
- */
-
-import * as typescript from 'typescript'
-import { schema, snapshot } from '../__tests__/helpers'
-import {
-  generateTypeDefinitions,
-  interfaceGenerator,
-  unionGenerator,
-} from './typescript'
-
-test('generators', async () => {
-  expect(interfaceGenerator(await schema('Person.schema.json'))).toMatchFile(
-    snapshot(__dirname, 'Person.ts')
-  )
-
-  expect(unionGenerator(await schema('BlockContent.schema.json'))).toMatchFile(
-    snapshot(__dirname, 'BlockContent.ts')
-  )
-})
+import typescript from 'typescript'
+import { generateTypeDefinitions } from './typescript'
 
 /**
- * Build `dist/types.ts` and compile it using Typescript to
+ * Build `dist/types.ts` and compile it using TypeScript to
  * check that there are no errors.
  */
 test('build', async () => {
