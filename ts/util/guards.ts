d348d174be660384369d6fe9887bf82745d1b7aa
Nokome Bentley <nokome@stenci.la>
2021-06-18 11:23:07 +1200

    fix(TypeScript): Reduce number, and simplify, type guards
    We where exporting quite a lot of type guard function, several of which
    were effectively aliases  (e.g. `isListItem`) some of which effiectively duplicated
    the functionality of others.
    
    This commit attempts to simplify the guards and reduce them
    to a mimimal, but still useful set. It also aims to reduce the confusion
    between a guard for a specific type `isA` and a guard for a family of types
    e.g. `isTypeOf`. The previous `isCreativeWork` was actually testing for waht is now
    `isTypeOf('CreativeWork')(node)` but could be confused for `isA('CreativeWork', node)`.

--- ts/util/guards.ts
+++ ts/util/guards.ts
@@ -1,78 +1,56 @@
 import {
-  Article,
   blockContentTypes,
-  codeTypes,
-  creativeWorkTypes,
   Entity,
   InlineContent,
   inlineContentTypes,
-  ListItem,
   Node,
-  Paragraph,
   TypeMap,
   TypeMapGeneric,
   Types,
 } from '../types'
 
-type ExtractGeneric<Type> = Type extends TypeMap<infer X>
-  ? X
-  : Type extends TypeMapGeneric<infer Y>
-  ? Y
-  : never
-
 /**
- * Returns a function which returns true is the type is a member
- * of the type map.
- *
- * @param {TypeMap} typeMap An object containing schema type values
+ * Type guard to determine whether a node is a primitive type
+ * (i.e. not an `Entity`).
  */
-export const typeIs =
-  <T extends Partial<TypeMap | TypeMapGeneric>>(typeMap: T) =>
-  (type: string): boolean =>
-    Object.keys(typeMap).includes(type)
+export const isPrimitive = (
+  node?: Node
+): node is null | boolean | number | string => {
+  const type = typeof node
+  if (node === null) return true
+  if (type === 'boolean') return true
+  if (type === 'number') return true
+  if (type === 'string') return true
+  if (Array.isArray(node)) return true
+  if (type === 'object' && !Object.prototype.hasOwnProperty.call(node, 'type'))
+    return true
 
-/**
- * Returns a type guard to determine whether a node belongs to a set
- * of types.
- * Returns a boolean value and narrows the TypeScript inferred type to
- * the type.
- *
- * @template {TypeMap} T
- * @param {T} typeMap
- * @param {Node} node A Stencila schema node object
- */
-export const nodeIs =
-  <T extends Partial<TypeMap | TypeMapGeneric>>(typeMap: T) =>
-  (node?: Node): node is ExtractGeneric<T> => {
-    return isEntity(node) ? typeIs(typeMap)(node.type) : false
-  }
-
-/**
- * Returns a type guard to determine whether a node is of a particular type.
- * Returns a boolean value and narrows the TypeScript inferred type to
- * the type.
- *
- * @param type The type to test for
- */
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/explicit-module-boundary-types
-export const is = <Ts extends Entity>(type: keyof TypeMap<Ts>) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const typeMap: TypeMap<Ts> = {
-    [type]: type,
-  }
-  return nodeIs(typeMap)
+  return false
 }
 
 /**
+ * Type guard to determine whether a node is an `Entity`
+ */
+export const isEntity = (node?: Node): node is Entity => {
+  if (node === null || node === undefined) return false
+  return Object.prototype.hasOwnProperty.call(node, 'type')
+}
+
+/**
+ * Returns a type guard to determine whether a node has a types
+ * that is a member of the type map.
+ *
+ * e.g. isTypeOf('CreativeWork')(node)
+ */
+export const isTypeOf =
+  <T extends Partial<TypeMap | TypeMapGeneric>>(typeMap: T) =>
+  (node?: Node): boolean =>
+    isEntity(node) && Object.keys(typeMap).includes(node.type)
+
+/**
  * A type guard to determine whether a node is of a specific type.
- * Returns a boolean value and narrows the TypeScript inferred type to
- * the type.
  *
  * e.g. `isA('Paragraph', node)`
- *
- * @param type The name of the type to test for
- * @param node The node being tested
  */
 export const isA = <K extends keyof Types>(
   type: K,
@@ -83,114 +61,28 @@ export const isA = <K extends keyof Types>(
 
 /**
  * Returns a type guard to determine whether a node is of a specific type.
- * Returns a boolean value and narrows the TypeScript inferred type to
- * the type.
  *
+ * e.g. isType('Article')(node)
  * e.g. `article.content.filter(isType('Paragraph'))`
- *
- * @param type The type to test for
  */
 export const isType =
   <K extends keyof Types>(type: K) =>
   (node?: Node): node is Types[K] => {
-    return node !== undefined && isA(type, node)
+    return isA(type, node)
   }
 
 /**
- * Type guard to determine whether a node is a primitive type.
- * Returns a boolean value and narrows the TypeScript inferred type.
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is null | boolean | number | string)} Returns true if node is one of `null`, `boolean`, `string`, or `number`
- */
-export const isPrimitive = (
-  node?: Node
-): node is null | boolean | number | string => {
-  const type = typeof node
-  if (node === null) return true
-  if (type === 'boolean') return true
-  if (type === 'number') return true
-  if (type === 'string') return true
-  return false
-}
-
-/**
- * Type guard to determine whether a node is an `Entity`
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is Entity)} Returns true if node is an `Entity` or derived type
- */
-export const isEntity = (node?: Node): node is Entity => {
-  if (node === null || node === undefined) return false
-  return Object.prototype.hasOwnProperty.call(node, 'type')
-}
-
-/**
- * Type guard to determine whether a node is both `InlineContent` and
- * and an `Entity`.
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is InlineContent)}
- */
-export const isInlineEntity = (node?: Node): node is InlineContent => {
-  return typeof node === 'object' && isEntity(node)
-    ? typeIs(inlineContentTypes)(node.type)
-    : false
-}
-/**
  * Type guard to determine whether a node is `InlineContent`.
  *
- * @param {Node} node The node to get the type for
- * @returns {(node is InlineContent)}
+ * e.g. `nodes.filter(isInlineContent)`
  */
 export const isInlineContent = (node?: Node): node is InlineContent => {
-  return isPrimitive(node) || isInlineEntity(node)
+  return isPrimitive(node) || isTypeOf(inlineContentTypes)(node)
 }
 
 /**
  * Type guard to determine whether a node is `BlockContent`.
  *
- * @param {Node} node The node to get the type for
- * @returns {(node is BlockContent)}
+ * e.g. `nodes.filter(isBlockContent)`
  */
-export const isBlockContent = nodeIs(blockContentTypes)
-
-/**
- * Type guard to determine whether a node is a `CreativeWork`.
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is CreativeWork)}
- */
-export const isCreativeWork = nodeIs(creativeWorkTypes)
-
-/**
- * Type guard to determine whether a node is a `Code`.
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is Code)}
- */
-export const isCode = nodeIs(codeTypes)
-
-/**
- * Type guard to determine whether a node is an `Article`.
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is Article)}
- */
-export const isArticle = is<Article>('Article')
-
-/**
- * Type guard to determine whether a node is an `Paragraph`.
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is Paragraph)}
- */
-export const isParagraph = is<Paragraph>('Paragraph')
-
-/**
- * Type guard to determine whether a node is an `ListItem`.
- *
- * @param {Node} node The node to get the type for
- * @returns {(node is ListItem)}
- */
-export const isListItem = is<ListItem>('ListItem')
+export const isBlockContent = isTypeOf(blockContentTypes)
