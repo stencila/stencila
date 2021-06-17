be8a07091248bcb767d2f76ae94aeb8ebd45e04d
Nokome Bentley <nokome@stenci.la>
2021-06-17 17:59:42 +1200

    chore(Logging): Use debug when in development

--- node/src/logging.ts
+++ node/src/logging.ts
@@ -1,10 +1,10 @@
 /**
  * Node.js bindings for ../../rust/src/logging.rs, see there for more documentation.
- * 
+ *
  * The events published on the "logging" topic by Rust are serializations
  * of `tracing_subscriber`'s events. At the time of writing they looked
  * like this
- * 
+ *
  *  {
  *    message: 'An info event',
  *    metadata: {
@@ -21,43 +21,46 @@
  *  }
  */
 
-import { subscribe, unsubscribe } from "./pubsub"
+import { subscribe, unsubscribe } from './pubsub'
 
 const addon = require('../index.node')
 
 // Initialize this module (sets up logging handlers on the Rust side)
-addon.loggingInit()
+if (process.env.NODE_ENV === 'development') {
+  const json = JSON.stringify({ logging: { desktop: { level: 'debug' } } })
+  addon.loggingInit(json)
+} else {
+  addon.loggingInit()
+}
 
 /**
  * Send pubsub events on the "logging" topic to Node's `console` object.
- *  
+ *
  * Call `toConsole(false)` to turn this off (e.g. if you want to send
  * to desktop notifications)
  */
 export function toConsole(on = true): void {
   if (on) {
-    subscribe("logging", (_topic: string, event: any) => {
-      let message = event.message
-      switch (event.metadata.level) {
+    subscribe('logging', (_topic: string, event: any) => {
+      const {message, metadata: {level, target}} = event
+      const line = `${level} ${target} ${message}`
+      switch (level) {
         case 'TRACE':
-          return console.trace(message)
+          return console.trace(line)
         case 'DEBUG':
-          return console.debug(message)
+          return console.debug(line)
         case 'INFO':
-          return console.info(message)
+          return console.info(line)
         case 'WARN':
-          return console.warn(message)
+          return console.warn(line)
         case 'ERROR':
-          return console.error(message)
+          return console.error(line)
       }
     })
   } else {
-    unsubscribe("logging")
+    unsubscribe('logging')
   }
 }
-
-// By default log to console
-toConsole(true)
 
 /**
  * Test that Rust to Node.js logging is working by emitting some log events from Rust.
