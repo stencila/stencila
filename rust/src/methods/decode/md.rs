78ba8521912ea961d9fdcdb41ca721e5f434fbec
Nokome Bentley <nokome@stenci.la>
2021-06-25 08:53:20 +1200

    feat(Decode Markdown): Handle inline content extensions e.g. math, citations

--- rust/src/methods/decode/md.rs
+++ rust/src/methods/decode/md.rs
@@ -1,13 +1,46 @@
 use eyre::Result;
-use pulldown_cmark::{CodeBlockKind, Event, Options, Parser, Tag};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take, take_until, take_while1},
+    character::complete::{char, digit1, multispace0, multispace1},
+    combinator::{map_res, not, peek},
+    multi::{fold_many0, separated_list0},
+    sequence::{delimited, preceded, tuple},
+    IResult,
+};
 use stencila_schema::{
-    Article, BlockContent, CodeBlock, CodeFragment, Delete, Emphasis, Heading, InlineContent, Link,
-    Node, Paragraph, Strong, ThematicBreak,
+    Article, BlockContent, Cite, CiteGroup, CodeBlock, CodeFragment, Delete, Emphasis, Heading,
+    InlineContent, Link, MathFragment, Node, Paragraph, Strong, Subscript, Superscript,
+    ThematicBreak,
 };
 
-/// Decode a `Node` from Markdown
-pub fn decode(markdown: &str) -> Result<Node> {
-    let parser = Parser::new_ext(markdown, Options::all());
+/// Decode a Markdown document to a `Node`
+///
+/// Intended for decoding an entire document, this function extracts
+/// YAML front matter, parses the Markdown, and returns a `Node::Article` variant.
+pub fn decode(md: &str) -> Result<Node> {
+    let content = decode_fragment(md);
+
+    let article = Article {
+        content: Some(content),
+        ..Default::default()
+    };
+
+    Ok(Node::Article(article))
+}
+
+/// Decode a Markdown fragment to a vector of `BlockContent`
+///
+/// Intended for decoding a fragment of Markdown (e.g. a Markdown cell in
+/// a Jupyter Notebook) and inserting it into a larger document.
+///
+/// Uses the `pulldown_cmark` and transforms its `Event`s into
+/// `Vec<BlockContent>`. Text is further parsed using `nom` based parsers
+/// to handle the elements that `pulldown_cmark` does not handle (e.g. math).
+pub fn decode_fragment(md: &str) -> Vec<BlockContent> {
+    use pulldown_cmark::{CodeBlockKind, Event, Options, Parser, Tag};
+
+    let parser = Parser::new_ext(md, Options::all());
 
     let mut inline_content: Vec<InlineContent> = Vec::new();
     let mut inline_content_marks: Vec<usize> = Vec::new();
@@ -15,7 +48,6 @@ pub fn decode(markdown: &str) -> Result<Node> {
     let mut text = String::new();
 
     let mut block_content: Vec<BlockContent> = Vec::new();
-
     for event in parser {
         match event {
             Event::Start(tag) => match tag {
@@ -94,7 +126,8 @@ pub fn decode(markdown: &str) -> Result<Node> {
                 ),
             },
             Event::Text(value) => {
-                inline_content.push(InlineContent::String(value.to_string()));
+                let mut content = decode_inline_content(&value);
+                inline_content.append(&mut content);
                 text = value.to_string()
             }
             Event::Code(value) => {
@@ -104,29 +137,193 @@ pub fn decode(markdown: &str) -> Result<Node> {
                 }));
             }
             Event::Html(value) => {
-                println!("Html {}", value);
+                tracing::warn!("Unhandled Markdown element Html {}", value);
             }
             Event::FootnoteReference(value) => {
-                println!("FootnoteReference {}", value);
+                tracing::warn!("Unhandled Markdown element FootnoteReference {}", value);
             }
             Event::SoftBreak => {
-                println!("SoftBreak");
+                tracing::warn!("Unhandled Markdown element SoftBreak");
             }
             Event::HardBreak => {
-                println!("HardBreak");
+                tracing::warn!("Unhandled Markdown element HardBreak");
             }
             Event::Rule => block_content.push(BlockContent::ThematicBreak(ThematicBreak {
                 ..Default::default()
             })),
             Event::TaskListMarker(value) => {
-                println!("TaskListMarker {}", value);
+                tracing::warn!("Unhandled Markdown element TaskListMarker {}", value);
             }
         };
     }
 
-    let article = Article {
-        content: Some(block_content),
-        ..Default::default()
-    };
-    Ok(Node::Article(article))
+    block_content
+}
+
+/// Decode a string into a vector of `InlineContent` nodes
+///
+/// This is the entry point into `nom` Markdown parsing functions.
+/// It is infallible in that if there is a parse error,
+/// the original input string is returned as the only item
+/// in the vector (with a warning).
+fn decode_inline_content(input: &str) -> Vec<InlineContent> {
+    match inline_content(input) {
+        Ok((_, content)) => content,
+        Err(error) => {
+            tracing::warn!("While parsing inline content: {}", error);
+            vec![InlineContent::String(String::from(input))]
+        }
+    }
+}
+
+/// Parse a string into a vector of `InlineContent` nodes
+///
+/// Whilst accumulating, will combine adjacent `String` nodes.
+/// This is necessary because of the catch all `character` parser.
+fn inline_content(input: &str) -> IResult<&str, Vec<InlineContent>> {
+    fold_many0(
+        alt((
+            cite_group,
+            cite,
+            math,
+            subscript,
+            superscript,
+            string,
+            character,
+        )),
+        Vec::new(),
+        |mut vec: Vec<InlineContent>, node| {
+            if let InlineContent::String(string) = &node {
+                match vec.last_mut() {
+                    Some(InlineContent::String(last)) => last.push_str(&string),
+                    _ => vec.push(node),
+                }
+            } else {
+                vec.push(node)
+            }
+            vec
+        },
+    )(input)
+}
+
+/// Parse a string into a `CiteGroup` node
+///
+/// Simply collects several `Cite` notes into a `CiteGroup`.
+pub fn cite_group(input: &str) -> IResult<&str, InlineContent> {
+    map_res(
+        delimited(
+            char('['),
+            separated_list0(delimited(multispace0, tag(";"), multispace0), cite),
+            char(']'),
+        ),
+        |items: Vec<InlineContent>| -> Result<InlineContent> {
+            Ok(InlineContent::CiteGroup(CiteGroup {
+                items: items
+                    .iter()
+                    .filter_map(|item| match item {
+                        InlineContent::Cite(cite) => Some(cite),
+                        _ => None,
+                    })
+                    .cloned()
+                    .collect(),
+                ..Default::default()
+            }))
+        },
+    )(input)
+}
+
+/// Parse a string into a `Cite` node
+///
+/// This attempts to follow Pandoc's citation handling as closely as possible
+/// (see https://pandoc.org/MANUAL.html#citations).
+///
+/// The following properties of a `Cite` are parsed:
+///   - [x] target
+///   - [ ] citation_mode
+///   - [ ] page_start
+///   - [ ] page_end
+///   - [ ] pagination
+///   - [ ] citation_prefix
+///   - [ ] citation_suffix
+///   - [ ] citation_intent
+pub fn cite(input: &str) -> IResult<&str, InlineContent> {
+    map_res(
+        preceded(char('@'), take_while1(|chr: char| chr.is_alphanumeric())),
+        |res: &str| -> Result<InlineContent> {
+            let target = res.into();
+            Ok(InlineContent::Cite(Cite {
+                target,
+                ..Default::default()
+            }))
+        },
+    )(input)
+}
+
+/// Parse a string into an `InlineContent` node
+///
+/// This attempts to follow Pandoc's match parsing as closely as possible
+/// (see https://pandoc.org/MANUAL.html#math).
+pub fn math(input: &str) -> IResult<&str, InlineContent> {
+    map_res(
+        delimited(
+            // Pandoc: "opening $ must have a non-space character immediately to its right"
+            tuple((char('$'), peek(not(multispace1)))),
+            take_until("$"),
+            // Pandoc: "the closing $ must have a non-space character immediately to its left,
+            // and must not be followed immediately by a digit"
+            tuple((peek(not(multispace1)), char('$'), peek(not(digit1)))),
+        ),
+        |res: &str| -> Result<InlineContent> {
+            Ok(InlineContent::MathFragment(MathFragment {
+                text: res.into(),
+                ..Default::default()
+            }))
+        },
+    )(input)
+}
+
+/// Parse a string into a `Subscript` node
+pub fn subscript(input: &str) -> IResult<&str, InlineContent> {
+    map_res(
+        delimited(char('~'), take_until("~"), char('~')),
+        |res: &str| -> Result<InlineContent> {
+            Ok(InlineContent::Subscript(Subscript {
+                content: vec![InlineContent::String(res.into())],
+                ..Default::default()
+            }))
+        },
+    )(input)
+}
+
+/// Parse a string into a `Superscript` node
+pub fn superscript(input: &str) -> IResult<&str, InlineContent> {
+    map_res(
+        delimited(char('^'), take_until("^"), char('^')),
+        |res: &str| -> Result<InlineContent> {
+            Ok(InlineContent::Superscript(Superscript {
+                content: vec![InlineContent::String(res.into())],
+                ..Default::default()
+            }))
+        },
+    )(input)
+}
+
+/// Accumulate characters into a `String` node
+///
+/// Will greedily take as many characters as possible, excluding those that appear at the
+/// start of other inline parsers e.g. '$', '['
+fn string(input: &str) -> IResult<&str, InlineContent> {
+    map_res(
+        take_while1(|chr: char| chr != '^' && chr != '~' && chr != '$' && chr != '['),
+        |res: &str| -> Result<InlineContent> { Ok(InlineContent::String(String::from(res))) },
+    )(input)
+}
+
+/// Take a single character into a `String` node
+///
+/// Necessary so that the characters no consumed by `string` are not lost.
+fn character(input: &str) -> IResult<&str, InlineContent> {
+    map_res(take(1usize), |res: &str| -> Result<InlineContent> {
+        Ok(InlineContent::String(String::from(res)))
+    })(input)
 }
