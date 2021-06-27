680eff4e28c43da31f84f2b165c7efe1d05615e7
Nokome Bentley <nokome@stenci.la>
2021-06-28 08:58:20 +1200

    docs(Tests): Details on snapshot_content function

--- rust/src/utils/tests.rs
+++ rust/src/utils/tests.rs
@@ -16,6 +16,17 @@ pub fn fixtures() -> PathBuf {
 
 /// Generate snapshots from the string content of fixtures matching
 /// a glob pattern.
+///
+/// # Arguments
+///
+/// - `pattern`: glob pattern _within_ the fixtures folder
+/// - `func`: the test function to run on the string content of each
+///           file matching the `pattern`.
+///
+/// `insta`'s `glob` macro seems to have difficulties with our Rust module
+/// layout (workspaces and nested modules). This function deals with that
+/// by making the pattern relative to the fixtures and adding some other
+/// conveniences.
 pub fn snapshot_content<F: FnMut(&str)>(pattern: &str, mut func: F) {
     let mut settings = insta::Settings::clone_current();
     settings.set_prepend_module_to_snapshot(false);
