0e64a5f283ec77f3e2223b3f419aebdb7203a7e4
Nokome Bentley <nokome@stenci.la>
2021-06-30 09:54:52 +1200

    ci(Release): Code signing
    Closes #931.
    
    * ci(MacOS & Windows): Turn on/off sections for testing code-signing on each push
    
    * chore(Release): Add configuration for OSX code signing
    
    * ci(Desktop): Split off forge.config.js
    
    * ci(Release): Download and use code signing cert for Windows
    
    * ci(Release): Install Apple cert; refactor Windows build
    
    * ci(Windows): Download signtool.exe
    
    * ci(Windows): Try alternative means to pass env vars
    
    * ci(MacOS): Use p12 file with password variable
    
    * ci(Desktop): Set NODE_ENV is cross-platform compatible way
    
    * ci(Desktop): Build on Linux
    
    * chore(Desktop): Update package lock
    
    * ci(*): Reinstate conditions and publishing

---
+++ desktop/forge.config.js
@@ -0,0 +1,99 @@
+/**
+ * Electron Forge Configuration
+ * 
+ * See the following examples for guidance:
+ * 
+ * - https://github.com/felixrieseberg/windows95/blob/master/forge.config.js
+ * - https://github.com/electron/fiddle/blob/master/forge.config.js
+ */
+
+
+module.exports = {
+  packagerConfig: {
+    name: 'Stencila',
+    // The executableName seems to needs to be the same as the various `exe` and `bin`
+    // properties in the makers below
+    executableName: 'stencila-desktop',
+    appCategoryType: 'public.app-category.productivity',
+    icon: './src/assets/icon/stencila',
+    osxSign: {
+      identity: 'Developer ID Application: Stencila Ltd. (K3PWLCZ5R6)',
+      'hardened-runtime': true,
+      'gatekeeper-assess': false,
+      'entitlements': 'entitlements.plist',
+      'entitlements-inherit': 'entitlements.plist',
+      'signature-flags': 'library',
+    },
+  },
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'stencila',
+          name: 'stencila',
+        },
+      },
+    },
+  ],
+  makers: [
+    {
+      // For more settings see
+      // https://js.electronforge.io/maker/squirrel/interfaces/makersquirrelconfig
+      name: '@electron-forge/maker-squirrel',
+      platforms: ['win32'],
+      config: {
+        certificateFile: process.env.WINDOWS_CODESIGN_FILE,
+        certificatePassword: process.env.WINDOWS_CODESIGN_PASSWORD,
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      // For more settings see
+      // https://js.electronforge.io/maker/deb/interfaces/makerdebconfigoptions
+      name: '@electron-forge/maker-deb',
+      platforms: ['linux'],
+      config: {
+        productName: 'Stencila',
+        name: 'stencila-desktop',
+        icon: './src/assets/icon/stencila.png',
+        homepage: 'https://stenci.la',
+      },
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      platforms: ['linux'],
+    },
+  ],
+  plugins: [
+    [
+      '@electron-forge/plugin-webpack',
+      {
+        mainConfig: './webpack.main.config.js',
+        renderer: {
+          config: './webpack.renderer.config.js',
+          entryPoints: [
+            {
+              html: './src/renderer/index.html',
+              js: './src/renderer/renderer.ts',
+              name: 'main_window',
+              preload: {
+                js: './src/preload/index.ts',
+              },
+            },
+          ],
+        },
+      },
+    ],
+    [
+      '@electron-forge/plugin-electronegativity',
+      {
+        isSarif: true,
+        parserPlugins: [],
+      },
+    ],
+  ],
+}
