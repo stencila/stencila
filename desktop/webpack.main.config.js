f06b73dc3d2b66314523c7287bd32c8fb7100d22
alex-ketch <alex@ketch.me>
2021-06-30 17:18:31 -0400

    fix(Desktop): Inline env variables when building Renderer files

--- desktop/webpack.main.config.js
+++ desktop/webpack.main.config.js
@@ -10,7 +10,9 @@ module.exports = {
   plugins: [
     ...plugins,
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV ?? 'development'
+      ),
       'process.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
       'process.type': '"browser"',
     }),
--- desktop/webpack.renderer.config.js
+++ desktop/webpack.renderer.config.js
@@ -18,6 +18,10 @@ module.exports = {
   plugins: [
     ...plugins,
     new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV ?? 'development'
+      ),
+      'process.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
       'process.type': '"renderer"',
     }),
     new HtmlInsertTagWebpackPlugin([
