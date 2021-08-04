ef9cefcaec891e13bc3db4c87a762cdf85ef4cdd
Nokome Bentley <nokome@stenci.la>
2021-08-05 10:03:38 +1200

    test(Benches): Fix bench

--- rust/benches/compile_code.rs
+++ rust/benches/compile_code.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use stencila::{graphs::resources, methods::compile::code::compile};
 
@@ -5,8 +7,8 @@ fn criterion_benchmark(criterion: &mut Criterion) {
     criterion.bench_function("compile r", |bencher| {
         bencher.iter(|| {
             compile(
-                black_box("path"),
-                &resources::file("path"),
+                black_box(&PathBuf::from("path")),
+                &resources::file(&PathBuf::from("path")),
                 black_box("library(pkg)"),
                 "r",
             )
