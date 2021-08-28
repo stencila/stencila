5ad37af9dd883d99bf05410480e52918e9c5bace
Nokome <nokome@stenci.la>
2021-08-28 13:21:55 +1200

    feat(Encoding JSON): Add option for compact or indented

--- rust/src/methods/encode/json.rs
+++ rust/src/methods/encode/json.rs
@@ -1,7 +1,15 @@
+use super::Options;
 use eyre::Result;
 use stencila_schema::Node;
 
 /// Encode a `Node` to a JSON document
-pub fn encode(node: &Node) -> Result<String> {
-    Ok(serde_json::to_string_pretty::<Node>(node)?)
+///
+/// Defaults to pretty (indented). Use "compact" theme for non-indented JSON.
+pub fn encode(node: &Node, options: Option<Options>) -> Result<String> {
+    let Options { theme, .. } = options.unwrap_or_default();
+    let json = match theme.as_str() {
+        "compact" => serde_json::to_string::<Node>(node)?,
+        _ => serde_json::to_string_pretty::<Node>(node)?,
+    };
+    Ok(json)
 }
