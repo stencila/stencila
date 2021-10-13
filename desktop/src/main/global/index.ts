e95b5e22ac512e990fb67edee9a54445e763c529
alex-ketch <alex@ketch.me>
2021-10-12 23:41:09 -0400

    fix(Desktop): Fix capturing of unhandled promise rejection errors

--- desktop/src/main/global/index.ts
+++ desktop/src/main/global/index.ts
@@ -1,7 +1,6 @@
 import { app, shell } from 'electron'
 import { CHANNEL } from '../../preload/channels'
-import { captureError } from '../../preload/errors'
-import { CaptureError, GetAppVersion, OpenLink } from '../../preload/types'
+import { GetAppVersion, OpenLink } from '../../preload/types'
 import { makeHandlers, removeChannelHandlers } from '../utils/handler'
 import { handle, valueToSuccessResult } from '../utils/ipc'
 import { GLOBAL_CHANNEL } from './channels'
@@ -10,10 +9,6 @@ const registerGlobalHandlers = () => {
   handle<OpenLink>(CHANNEL.OPEN_LINK_IN_DEFAULT_BROWSER, (_event, link) =>
     shell.openExternal(link).then(() => valueToSuccessResult())
   )
-
-  handle<CaptureError>(CHANNEL.CAPTURE_ERROR, async (_event, payload) => {
-    return valueToSuccessResult(captureError(payload))
-  })
 
   handle<GetAppVersion>(CHANNEL.GET_APP_VERSION, async (_event) => {
     return valueToSuccessResult(app.getVersion())
