98667b9223dec006f1d3e0bfbee5db5072ad9a01
alex-ketch <alex@ketch.me>
2021-10-26 13:43:34 -0400

    chore(PostCSS): Downgrade PostCSS dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -9,28 +9,26 @@ module.exports = {
     require('postcss-url')({
       url: 'rebase',
     }),
-    require('postcss-custom-selectors')({
-      importFrom: ['src/selectors.css', 'src/extensions/code/styles.css'],
-    }),
     require('postcss-custom-media'),
     require('postcss-custom-properties')({ preserve: true }),
     require('postcss-nested'),
     require('postcss-nested-import'),
+    require('postcss-custom-selectors')({
+      importFrom: ['src/selectors.css', 'src/extensions/code/styles.css'],
+    }),
+    require('postcss-extend'),
     // Many browsers don’t support compound `:not()` selectors, this splits it
-    require('postcss-selector-not').default,
     // We remove the PrismJS specific modifier when used in `:not()` selectors
     // see ./src/scripts/selectors.ts:61
     require('postcss-selector-replace')({
       before: [/\[class\*=language-\]\)/gm],
       after: [')'],
     }),
-    require('autoprefixer'),
-    require('postcss-extend'),
     require('postcss-mixins'),
+    require('postcss-selector-not'),
+    require('autoprefixer'),
     require('cssnano')({ preset: 'default' }),
-    // TODO: Fix compatability with PostCSS v8. Currently this plugin drops several selectors
-    // in the generated stylesheets.
-    // require('postcss-combine-media-query'),
+    require('postcss-combine-media-query'),
     require('postcss-sort-media-queries'),
   ],
 }
