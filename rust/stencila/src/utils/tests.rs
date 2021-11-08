792f2efb322b86f19a00f53fd0feaca57709d897
Nokome <nokome@stenci.la>
2021-11-09 10:12:22 +1300

    chore(Tests): Formatting

--- rust/stencila/src/utils/tests.rs
+++ rust/stencila/src/utils/tests.rs
@@ -4,8 +4,10 @@ use std::{fs::read_to_string, path::PathBuf};
 /// Get the path of the home directory of this repository
 pub fn home() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR")) // the crate
-        .parent().expect("Should have parent") // the `rust` dir
-        .parent().expect("Should have parent") // the `stencila` dir
+        .parent()
+        .expect("Should have parent") // the `rust` dir
+        .parent()
+        .expect("Should have parent") // the `stencila` dir
         .canonicalize()
         .expect("Unable to get repository home directory")
 }
