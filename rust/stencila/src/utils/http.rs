e41a666708fc40010de026909d78ac9c95779648
Nokome <nokome@stenci.la>
2021-11-10 08:37:31 +1300

    fix(Downloads): Add user agent header to avoid 403s

--- rust/stencila/src/utils/http.rs
+++ rust/stencila/src/utils/http.rs
@@ -3,7 +3,13 @@
 pub async fn download<P: AsRef<std::path::Path>>(url: &str, path: P) -> eyre::Result<()> {
     use std::{fs::File, io};
 
-    let response = reqwest::get(url).await?;
+    // TODO: Allow an existing client to be passed into this function
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .header(reqwest::header::USER_AGENT, "Stencila")
+        .send()
+        .await?;
     let bytes = response.bytes().await?;
     let mut file = File::create(&path)?;
     io::copy(&mut bytes.as_ref(), &mut file)?;
