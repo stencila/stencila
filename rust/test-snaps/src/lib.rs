c9401d1b4cb25ed7c5e331922cffaa0994a3b78d
Nokome <nokome@stenci.la>
2021-11-09 13:26:27 +1300

    test(Snapshots): Provide function to easily read in node fixtures

--- rust/test-snaps/src/lib.rs
+++ rust/test-snaps/src/lib.rs
@@ -36,3 +36,17 @@ pub fn snapshot_fixtures_content<F: FnMut(&str)>(pattern: &str, mut func: F) {
         func(&content)
     })
 }
+
+/// Generate snapshots from JSON node fixtures matching a glob pattern.
+///
+/// # Arguments
+///
+/// - `pattern`: glob pattern _within_ the fixtures folder
+/// - `func`: the test function to run on the content of each file matching the `pattern`.
+pub fn snapshot_fixtures_nodes<F: FnMut(stencila_schema::Node)>(pattern: &str, mut func: F) {
+    snapshot_fixtures(pattern, |path| {
+        let json = std::fs::read_to_string(path).expect("Unable to read file");
+        let node = serde_json::from_str(&json).expect("Unable to deserialize from JSON");
+        func(node)
+    })
+}
