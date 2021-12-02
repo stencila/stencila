b5f0d5f1b075cfead4d9e53f390c10927769efff
Nokome Bentley <nokome@stenci.la>
2021-12-03 09:07:56 +1300

    test(Bash kernel): Add basic tests

--- rust/kernel-bash/src/lib.rs
+++ rust/kernel-bash/src/lib.rs
@@ -16,18 +16,54 @@ pub async fn new() -> Result<MicroKernel> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use kernel::KernelTrait;
-    use test_utils::{assert_json_eq, print_logs};
+    use kernel::{stencila_schema::Node, KernelTrait};
+    use test_utils::{assert_json_eq, serde_json::json};
 
+    /// Tests of basic functionality
+    /// This test is replicated in all the microkernels.
+    /// Other test should be written for language specific quirks and regressions.
     #[tokio::test]
-    async fn basic() -> Result<()> {
-        print_logs();
-
+    async fn basics() -> Result<()> {
         let mut kernel = new().await?;
         kernel.start().await?;
 
-        let result = kernel.exec("echo foo").await?;
-        assert_json_eq!(result.0, ["foo\n"]);
+        // Assign a variable and output it
+        let (outputs, messages) = kernel.exec("a=2\necho $a").await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [2]);
+
+        // Print the variable twice and then output it
+        // For bash, we need to separate outputs manually using the Unicode separator character
+        let (outputs, messages) = kernel
+            .exec("echo $a\u{10ABBA}\necho $a\u{10ABBA}\necho $a")
+            .await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [2, 2, 2]);
+
+        // Syntax error
+        let (outputs, messages) = kernel.exec("if").await?;
+        messages[0]
+            .error_message
+            .ends_with("syntax error: unexpected end of file\n");
+
+        assert_json_eq!(outputs, json!([]));
+
+        // Runtime error
+        let (outputs, messages) = kernel.exec("foo").await?;
+        messages[0]
+            .error_message
+            .ends_with("foo: command not found\n");
+        assert_json_eq!(outputs, json!([]));
+
+        // Set and get another variable
+        kernel.set("b", Node::Integer(3)).await?;
+        let b = kernel.get("b").await?;
+        assert_json_eq!(b, 3);
+
+        // Use both variables
+        let (outputs, messages) = kernel.exec("echo $a$b").await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [23]);
 
         Ok(())
     }
