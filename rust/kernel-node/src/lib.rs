9967d38f839e08bc3bde9b95c1f019cd6991b03f
Nokome Bentley <nokome@stenci.la>
2021-12-03 08:48:17 +1300

    fix(Node kernel): Fix line unescaping, override console methods

--- rust/kernel-node/src/lib.rs
+++ rust/kernel-node/src/lib.rs
@@ -17,45 +17,115 @@ pub async fn new() -> Result<MicroKernel> {
 mod tests {
     use super::*;
     use kernel::{stencila_schema::Node, KernelTrait};
-    use test_utils::{assert_json_eq, print_logs};
+    use test_utils::{assert_json_eq, serde_json::json};
 
+    /// Tests of basic functionality
+    /// This test is replicated in all the microkernels.
+    /// Other test should be written for language specific quirks and regressions.
     #[tokio::test]
-    async fn all() -> Result<()> {
-        print_logs();
-
+    async fn basics() -> Result<()> {
         let mut kernel = new().await?;
         kernel.start().await?;
 
-        // exec
+        // Assign a variable and output it
+        let (outputs, messages) = kernel.exec("const a = 2\na").await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [2]);
 
-        let (outputs, messages) = kernel.exec("const a = 1\na").await?;
-        assert_json_eq!(outputs, [1]);
-        assert!(messages.is_empty());
+        // Print the variable twice and then output it
+        let (outputs, messages) = kernel.exec("console.log(a)\nconsole.log(a)\na").await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [2, 2, 2]);
 
-        let (outputs, messages) = kernel.exec("console.log(a*2)\na*3").await?;
-        assert_json_eq!(outputs, [2, 3]);
-        assert!(messages.is_empty());
-
+        // Syntax error
         let (outputs, messages) = kernel.exec("bad ^ # syntax").await?;
-        assert!(outputs.is_empty());
         assert_json_eq!(messages[0].error_type, "SyntaxError");
         assert_json_eq!(messages[0].error_message, "Invalid or unexpected token");
+        assert_json_eq!(outputs, json!([]));
 
+        // Runtime error
         let (outputs, messages) = kernel.exec("foo").await?;
-        assert!(outputs.is_empty());
         assert_json_eq!(messages[0].error_type, "ReferenceError");
         assert_json_eq!(messages[0].error_message, "foo is not defined");
+        assert_json_eq!(outputs, json!([]));
 
-        // set & get
-
-        kernel.set("b", Node::Integer(2)).await?;
-
+        // Set and get another variable
+        kernel.set("b", Node::Integer(3)).await?;
         let b = kernel.get("b").await?;
-        assert_json_eq!(b, 2);
+        assert_json_eq!(b, 3);
 
+        // Use both variables
         let (outputs, messages) = kernel.exec("a*b").await?;
-        assert_json_eq!(outputs, [2]);
-        assert!(messages.is_empty());
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [6]);
+
+        Ok(())
+    }
+
+    // Test that `console.log` arguments are treated as separate outputs
+    #[tokio::test]
+    async fn console_log() -> Result<()> {
+        let mut kernel = new().await?;
+        kernel.start().await?;
+
+        let (outputs, messages) = kernel.exec("console.log(1)").await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [1]);
+
+        let (outputs, messages) = kernel.exec("console.log(1, 2, 3, 4)").await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, [1, 2, 3, 4]);
+
+        let (outputs, messages) = kernel.exec("console.log([1, 2, 3], 4, 'str')").await?;
+        assert_json_eq!(messages, json!([]));
+        assert_json_eq!(outputs, json!([[1, 2, 3], 4, "str"]));
+
+        Ok(())
+    }
+
+    // Test that `console.debug`, `console.warn` etc are treated as separate messages
+    #[tokio::test]
+    async fn console_messages() -> Result<()> {
+        let mut kernel = new().await?;
+        kernel.start().await?;
+
+        let (outputs, messages) = kernel
+            .exec(
+                r#"
+console.log(1)
+console.debug("Debug message")
+console.log(2)
+console.info("Info message")
+console.log(3)
+console.warn("Warn message")
+console.log(4)
+console.error("Error message")
+5
+"#,
+            )
+            .await?;
+
+        assert_json_eq!(
+            messages,
+            json!([{
+                "type": "CodeError",
+                "errorType": "CodeDebug",
+                "errorMessage": "Debug message",
+            }, {
+                "type": "CodeError",
+                "errorType": "CodeInfo",
+                "errorMessage": "Info message",
+            }, {
+                "type": "CodeError",
+                "errorType": "CodeWarn",
+                "errorMessage": "Warn message",
+            } , {
+                "type": "CodeError",
+                "errorType": "CodeError",
+                "errorMessage": "Error message",
+            }])
+        );
+        assert_json_eq!(outputs, json!([1, 2, 3, 4, 5]));
 
         Ok(())
     }
--- rust/kernel-node/src/node-kernel.js
+++ rust/kernel-node/src/node-kernel.js
@@ -10,14 +10,31 @@ const transSep = '\u{10ACDC}\n'
 
 const { stdin, stdout, stderr } = process
 
-// TODO: Write as console normally does but with `resSep` and newline at end
-/*
-console.log = () => stdout.write()
-console.debug = () => stderr.write()
-console.info = () => stderr.write()
-console.warn = () => stderr.write()
-console.error = () => stderr.write()
-*/
+console.log = function (...args) {
+  for (const arg of args) {
+    stdout.write(encodeValue(arg) + resSep)
+  }
+}
+
+console.debug = (message) =>
+  stderr.write(
+    `{"type":"CodeError","errorType":"CodeDebug","errorMessage":"${message}"}${resSep}`
+  )
+
+console.info = (message) =>
+  stderr.write(
+    `{"type":"CodeError","errorType":"CodeInfo","errorMessage":"${message}"}${resSep}`
+  )
+
+console.warn = (message) =>
+  stderr.write(
+    `{"type":"CodeError","errorType":"CodeWarn","errorMessage":"${message}"}${resSep}`
+  )
+
+console.error = (message) =>
+  stderr.write(
+    `{"type":"CodeError","errorType":"CodeError","errorMessage":"${message}"}${resSep}`
+  )
 
 const rl = readline.createInterface({
   input: stdin,
@@ -34,18 +51,16 @@ const context = {
 vm.createContext(context)
 
 rl.on('line', (code) => {
-  const unescaped = code.replace('\\n', '\n')
+  const unescaped = code.replace(/\\n/g, '\n')
   try {
     const output = vm.runInContext(unescaped, context)
     if (output !== undefined) {
       const json = encodeValue(output)
-      stdout.write(resSep)
-      stdout.write(json)
+      stdout.write(json + resSep)
     }
   } catch (error) {
     const json = encodeError(error)
-    stdout.write(resSep)
-    stderr.write(json)
+    stderr.write(json + resSep)
   }
   stdout.write(transSep)
   stderr.write(transSep)
