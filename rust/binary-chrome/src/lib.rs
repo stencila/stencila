89cbb96e9e02e27096178c56d04c6374435082a2
Nokome Bentley <nokome@stenci.la>
2021-12-06 08:01:41 +1300

    chore(Chrome binary): Update installable version

--- rust/binary-chrome/src/lib.rs
+++ rust/binary-chrome/src/lib.rs
@@ -12,10 +12,12 @@ impl BinaryTrait for ChromeBinary {
         Binary::new(
             "chrome",
             &["chromium"],
-            // Version history at https://en.wikipedia.org/wiki/Google_Chrome_version_history
-            // but only use triples ending in `.0` here and make sure there is a mapping in the
+            // Version history at https://en.wikipedia.org/wiki/Google_Chrome_version_history.
+            // Rather than support installing multiple versions, we normally only support the
+            // most recent version in the stable channel.
+            // Note: Use triples ending in `.0` here and make sure there is a mapping in the
             // `install_version` method.
-            &["91.0.0"],
+            &["96.0.0"],
         )
     }
 
@@ -26,12 +28,13 @@ impl BinaryTrait for ChromeBinary {
         let minor_version = version.split('.').take(2).collect::<Vec<&str>>().join(".");
 
         // Map the minor_version to a "position" number which can be obtained from
-        // https://vikyd.github.io/download-chromium-history-version
+        // https://vikyd.github.io/download-chromium-history-version.
+        // Note: the position number may be different for each os/arch
         let suffix = match minor_version.as_ref() {
-            "91.0" => match os {
-                "macos" => "Mac/869727/chrome-mac.zip",
-                "windows" => "Win_x64/867878/chrome-win.zip",
-                "linux" => "Linux_x64/860960/chrome-linux.zip",
+            "96.0" => match os {
+                "macos" => "Mac/925110/chrome-mac.zip",
+                "windows" => "Win_x64/925110/chrome-win.zip",
+                "linux" => "Linux_x64/926934/chrome-linux.zip",
                 _ => bail!("Unmapped OS '{}'", os),
             },
             _ => bail!("Unmapped version number '{}'", version),
