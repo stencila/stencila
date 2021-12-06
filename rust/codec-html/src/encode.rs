513a53a3619afafb7b63e33a7fa3e2c139d9b59e
Nokome Bentley <nokome@stenci.la>
2021-12-06 15:34:55 +1300

    test(HTML Encoding): Skip validation on Windows

--- rust/codec-html/src/encode.rs
+++ rust/codec-html/src/encode.rs
@@ -353,7 +353,7 @@ mod tests {
     use test_snaps::{
         insta::assert_display_snapshot, snapshot_fixtures_content, snapshot_fixtures_nodes,
     };
-    use test_utils::{assert_json_eq, home, skip_slow};
+    use test_utils::{assert_json_eq, home, skip_ci_os, skip_slow};
 
     /// Encode the node fixtures
     #[test]
@@ -399,7 +399,7 @@ mod tests {
     /// for more on the API.
     #[tokio::test]
     async fn nu_validate() -> Result<()> {
-        if skip_slow() {
+        if skip_slow() || skip_ci_os("windows", "Failed with error: The filename, directory name, or volume label syntax is incorrect. (os error 123)") {
             return Ok(());
         }
 
