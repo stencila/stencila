e4a778238803f9ddcbcc039174cede44bc8c44a0
Nokome Bentley <nokome@stenci.la>
2021-12-24 10:43:56 +1300

    fix(R microkernel): Handle interrupts more gracefully

--- rust/kernel-r/src/r-kernel.r
+++ rust/kernel-r/src/r-kernel.r
@@ -67,80 +67,97 @@ unbox <- jsonlite::unbox
 write(READY, stdout)
 write(READY, stderr)
 
+saved_task <<- NULL
+
 while (!is.null(stdin)) {
-  task <- readLines(stdin, n=1)
+  tryCatch(
+    {
+      # A SIGINT does not interrupt `readLines` but instead gets fired just after it when the next
+      # line is read. So, we have to save the task in case interrupt was inadvertantly called during
+      # readline and then "replay" it on the next loop.
+      if (is.null(saved_task)) {
+        task <<- readLines(stdin, n=1)
+      } else {
+        task <<- saved_task
+      }
+      saved_task <<- NULL
 
-  # If there is no task from `readLines` it means `stdin` was closed, so exit gracefully
-  if (length(task) == 0) quit(save = "no")
+      # If there is no task from `readLines` it means `stdin` was closed, so exit gracefully
+      if (length(task) == 0) quit(save = "no")
 
-  lines <- strsplit(task, "\\n", fixed = TRUE)[[1]]
+      lines <- strsplit(task, "\\n", fixed = TRUE)[[1]]
 
-  if (lines[1] == FORK) {
-    # The `eval_safe` function of https://github.com/jeroen/unix provides an alternative 
-    # implementation of fork-exec for R. We might use it in the future.
-  
-    # The Rust process will kill the child so use `estranged` to avoid zombie processes
-    # (because this process still has an entry for the child)
-    process <- parallel:::mcfork(estranged = TRUE)
-    if (!inherits(process, "masterProcess")) {
-      # Parent process, so return the pid of the fork and then wait for the next task
-      write(paste0(process$pid, TASK), stdout)
+      if (lines[1] == FORK) {
+        # The `eval_safe` function of https://github.com/jeroen/unix provides an alternative 
+        # implementation of fork-exec for R. We might use it in the future.
+      
+        # The Rust process will kill the child so use `estranged` to avoid zombie processes
+        # (because this process still has an entry for the child)
+        process <- parallel:::mcfork(estranged = TRUE)
+        if (!inherits(process, "masterProcess")) {
+          # Parent process, so return the pid of the fork and then wait for the next task
+          write(paste0(process$pid, TASK), stdout)
+          write(TASK, stderr)
+          next
+        }
+
+        # Child process, so...
+
+        # Remove the FORK flag and the pipe paths from the front of lines
+        new_stdout <- lines[2]
+        new_stderr <- lines[3]
+        lines <- tail(lines, -3)
+
+        # Set stdin to /dev/null to end loop
+        stdin <- NULL
+
+        # Replace stdout and stderr with pipes
+        stdout <- file(new_stdout, open = "w", raw = TRUE)
+        stderr <- file(new_stderr, open = "w", raw = TRUE)
+      }
+
+      code <- paste0(lines, collapse = "\n")
+      compiled <- tryCatch(parse(text=code), error=identity)
+      if (inherits(compiled, "simpleError")) {
+        error(compiled, "SyntaxError")
+      } else {  
+        # Default graphics device to avoid window popping up or `Rplot.pdf` polluting
+        # local directory. The tempdir check is needed when forking.
+        png(tempfile(tmpdir = tempdir(check=TRUE)))
+        # Recording must be enabled for recordPlot() to work
+        dev.control("enable")
+
+        value <- tryCatch(
+          eval(compiled, envir, .GlobalEnv),
+          message=info,
+          warning=warning,
+          error=error,
+          interrupt=interrupt
+        )
+        
+        if (!withVisible(value)$visible) {
+          value <- NULL
+        }
+
+        rec_plot <- recordPlot()
+        if (!is.null(rec_plot[[1]])) {
+          value <- rec_plot
+        }
+        
+        # Clear the existing device
+        dev.off()  
+
+        if (!is.null(value)) {
+          assignment <- grepl("^\\s*\\w+\\s*(<-|=)\\s*", tail(lines, 1))
+          if (!assignment) write(paste0(encode_value(value), RESULT), stdout)
+        }
+      }
+
+      write(TASK, stdout)
       write(TASK, stderr)
-      next
+    },
+    interrupt=function(condition){
+      saved_task <<- task
     }
-
-    # Child process, so...
-
-    # Remove the FORK flag and the pipe paths from the front of lines
-    new_stdout <- lines[2]
-    new_stderr <- lines[3]
-    lines <- tail(lines, -3)
-
-    # Set stdin to /dev/null to end loop
-    stdin <- NULL
-
-    # Replace stdout and stderr with pipes
-    stdout <- file(new_stdout, open = "w", raw = TRUE)
-    stderr <- file(new_stderr, open = "w", raw = TRUE)
-  }
-
-  code <- paste0(lines, collapse = "\n")
-  compiled <- tryCatch(parse(text=code), error=identity)
-  if (inherits(compiled, "simpleError")) {
-    error(compiled, "SyntaxError")
-  } else {  
-    # Default graphics device to avoid window popping up or `Rplot.pdf` polluting
-    # local directory. The tempdir check is needed when forking.
-    png(tempfile(tmpdir = tempdir(check=TRUE)))
-    # Recording must be enabled for recordPlot() to work
-    dev.control("enable")
-
-    value <- tryCatch(
-      eval(compiled, envir, .GlobalEnv),
-      message=info,
-      warning=warning,
-      error=error,
-      interrupt=interrupt
-    )
-    
-    if (!withVisible(value)$visible) {
-      value <- NULL
-    }
-
-    rec_plot <- recordPlot()
-    if (!is.null(rec_plot[[1]])) {
-      value <- rec_plot
-    }
-    
-    # Clear the existing device
-    dev.off()  
-
-    if (!is.null(value)) {
-      assignment <- grepl("^\\s*\\w+\\s*(<-|=)\\s*", tail(lines, 1))
-      if (!assignment) write(paste0(encode_value(value), RESULT), stdout)
-    }
-  }
-
-  write(TASK, stdout)
-  write(TASK, stderr)
+  )
 }
