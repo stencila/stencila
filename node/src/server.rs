fec9925598a0ccd4531c0fba1f9315eb937a6cd6
Nokome Bentley <nokome@stenci.la>
2022-01-07 17:08:31 +1300

    fix(Node bindings): Correct module name

--- node/src/server.rs
+++ node/src/server.rs
@@ -1,11 +1,11 @@
 use crate::prelude::*;
 use neon::prelude::*;
-use stencila::serve;
+use stencila::server;
 
 /// Serve a path
 pub fn serve(mut cx: FunctionContext) -> JsResult<JsString> {
     let path = &cx.argument::<JsString>(0)?.value(&mut cx);
 
-    let result = RUNTIME.block_on(async { serve::serve(path, None, false).await });
+    let result = RUNTIME.block_on(async { server::serve(path, None, false).await });
     to_string_or_throw(cx, result)
 }
