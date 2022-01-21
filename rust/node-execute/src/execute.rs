843861e6cebbfd577c21076be791e51d7f1b76c3
Nokome <nokome@stenci.la>
2022-01-21 17:30:01 +1300

    feat(Node execution): Cancel execution tasks where possible

--- rust/node-execute/src/execute.rs
+++ rust/node-execute/src/execute.rs
@@ -1,9 +1,9 @@
 use std::{
-    collections::{BTreeMap, BTreeSet},
+    collections::{BTreeMap, BTreeSet, HashMap},
     sync::Arc,
 };
 
-use eyre::{bail, Result};
+use eyre::{Report, Result};
 use futures::stream::{FuturesUnordered, StreamExt};
 use graph::Plan;
 use graph_triples::{Resource, ResourceInfo};
@@ -13,7 +13,7 @@ use node_patch::{diff, mutate, Patch};
 use stencila_schema::{CodeChunk, CodeExecutableExecuteStatus, CodeExpression, Node};
 use tokio::sync::{
     mpsc::{Receiver, UnboundedSender},
-    RwLock,
+    oneshot, RwLock,
 };
 
 use crate::{
@@ -147,19 +147,21 @@ pub async fn execute(
         // Create a kernel task for each step in this stage
         let step_count = stage.steps.len();
         let mut patches = Vec::with_capacity(step_count);
-        let mut futures = Vec::with_capacity(step_count);
+        let mut cancellers = HashMap::new();
+        let mut futures = Vec::new();
         for (step_index, step) in stage.steps.iter().enumerate() {
             // Get the node info for the step
             let mut node_info = node_infos
                 .get(&step.resource_info.resource)
                 .cloned()
                 .expect("Node info for resource should be available");
+            let node_id = node_info.node_id.clone();
 
             // Has the step been cancelled?
-            if cancelled.contains(&node_info.node_id) {
+            if cancelled.contains(&node_id) {
                 tracing::trace!(
                     "Step for node `{}` was cancelled before it was run",
-                    node_info.node_id
+                    node_id
                 );
                 // Send a patch to revert `execute_status` to previous status
                 // (the `Cancelled` state is reserved for nodes that have started and are cancelled)
@@ -170,6 +172,9 @@ pub async fn execute(
             // Set the `execute_status` of the node to `Running` or `RunningPreviouslyFailed`
             // and send the resulting patch
             patches.push(node_info.set_execute_status_running());
+
+            // Create a channel to send cancel requests to task
+            let (cancel_sender, cancel_receiver) = oneshot::channel::<()>();
 
             // Create clones of variables needed to execute the task
             let kernel_space = kernel_space.clone();
@@ -189,48 +194,87 @@ pub async fn execute(
 
                 // Create a mutable draft of the node and execute it in the kernel space
                 let mut executed = node_info.node.clone();
-                match executed
-                    .execute(&kernel_space, &kernel_selector, &resource_info, is_fork)
+
+                // Start execution of the node
+                let task_info = match executed
+                    .execute_begin(&kernel_space, &kernel_selector, &resource_info, is_fork)
                     .await
                 {
-                    Ok(_) => {
-                        // Update the resource to indicate that the resource was executed
-                        let execute_status = match &executed {
-                            Node::CodeChunk(CodeChunk { execute_status, .. })
-                            | Node::CodeExpression(CodeExpression { execute_status, .. }) => {
-                                execute_status.clone()
-                            }
-                            _ => None,
-                        };
-                        resource_info.did_execute(execute_status);
-
-                        // Generate a patch for the differences resulting from execution
-                        let mut patch = diff(&node_info.node, &executed);
-                        patch.address = Some(node_info.node_address.clone());
-                        patch.target = Some(node_info.node_id.clone());
-
-                        // Having generated the patch, update the node_info.node (which may be used
-                        // for assesing execution status etc)
-                        node_info.node = executed;
-
-                        Ok((step_index, resource_info, node_info, patch))
+                    Ok(task_info) => task_info,
+                    Err(error) => {
+                        tracing::error!(
+                            "While beginning task for step {}/{}: {}",
+                            step_index + 1,
+                            step_count,
+                            error
+                        );
+                        None
                     }
-                    Err(error) => bail!(error),
+                };
+
+                // If a `TaskInfo` was returned, the execution is async, can potentially be
+                // cancelled, and need to be waited for...
+                if let Some(mut task_info) = task_info {
+                    // Hook the `cancel_receiver` to the task's `canceller`, if it has one
+                    // (i.e. if it is cancellable)
+                    if let Some(task_cancel_sender) = task_info.task.canceller.clone() {
+                        tracing::trace!("Task is `{}` cancellable", task_info.task.id);
+                        let task_id = task_info.task.id.clone();
+                        tokio::spawn(async move {
+                            if let Ok(..) = cancel_receiver.await {
+                                tracing::trace!(
+                                    "Attempting to cancel task `{}` in stage {}/{}",
+                                    task_id,
+                                    stage_index + 1,
+                                    stage_count
+                                );
+                                if let Err(error) = task_cancel_sender.send(()).await {
+                                    tracing::error!(
+                                        "While attempting to cancel task `{}`: {}",
+                                        task_id,
+                                        error
+                                    );
+                                }
+                            }
+                        });
+                    } else {
+                        tracing::trace!("Task `{}` is not cancellable", task_info.task.id);
+                    };
+
+                    // Wait for the task to finish (or be cancelled and update the executed node when it has
+                    let task_result = task_info.result().await?;
+                    executed.execute_end(task_info, task_result)?;
                 }
+
+                // Update the resource to indicate that the resource was executed
+                let execute_status = match &executed {
+                    Node::CodeChunk(CodeChunk { execute_status, .. })
+                    | Node::CodeExpression(CodeExpression { execute_status, .. }) => {
+                        execute_status.clone()
+                    }
+                    _ => None,
+                };
+                resource_info.did_execute(execute_status);
+
+                // Generate a patch for the differences resulting from execution
+                let mut patch = diff(&node_info.node, &executed);
+                patch.address = Some(node_info.node_address.clone());
+                patch.target = Some(node_info.node_id.clone());
+
+                // Having generated the patch, update the node_info.node (which may be used
+                // for assesing execution status etc)
+                node_info.node = executed;
+
+                Ok::<_, Report>((step_index, resource_info, node_info, patch))
             };
+            cancellers.insert(node_id, cancel_sender);
             futures.push(future);
         }
+
+        // Send patches updated execution status
         send_patches(patch_request_sender, patches, true);
 
-        // Spawn all tasks in the stage and wait for each to finish, sending on the resultant `Patch`
-        // for application and publishing (if it is not empty)
-        // TODO: Replace `FuturesUnordered` with `TaskSet`. See https://news.ycombinator.com/item?id=29912386
-        let mut results = futures
-            .into_iter()
-            .map(tokio::spawn)
-            .collect::<FuturesUnordered<_>>();
-
-        if results.is_empty() {
+        if futures.is_empty() {
             tracing::debug!(
                 "Skipping stage {}/{}, all steps cancelled",
                 stage_index + 1,
@@ -239,11 +283,19 @@ pub async fn execute(
             continue;
         }
 
+        // Spawn all task futures
+        // TODO: Replace `FuturesUnordered` with `TaskSet`. See https://news.ycombinator.com/item?id=29912386
+        let mut futures_unordered = futures
+            .into_iter()
+            .map(tokio::spawn)
+            .collect::<FuturesUnordered<_>>();
+
         // Wait for both execution results and any cancellation requests and act
         // accordingly
         loop {
             tokio::select! {
-                result = results.next() => {
+                // Handle tasks that have finished
+                result = futures_unordered.next() => {
                     let result = match result {
                         Some(result) => result,
                         // If next() is none, we've reached the end of the tasks so break
@@ -296,11 +348,38 @@ pub async fn execute(
                             .and_modify(|current_node_info| *current_node_info = node_info);
                     }
                 }
+
+                // Handle cancellation requests
                 Some(request) = cancel_request_receiver.recv() => {
-                    if let Some(start) = request.start {
+                    if let Some(node_id) = request.start {
+                        // Try to find matching cancel channel sender
+                        match cancellers.remove(&node_id) {
+                            Some(cancel_task_sender) => {
+                                tracing::debug!("Cancelling node `{}` in stage {}/{}",
+                                    node_id,
+                                    stage_index + 1,
+                                    stage_count
+                                );
+                                if let Err(..) = cancel_task_sender.send(()) {
+                                    tracing::error!(
+                                        "While attempting to cancel node `{}` in stage {}/{}: channel closed",
+                                        node_id,
+                                        stage_index + 1,
+                                        stage_count
+                                    );
+                                }
+                            },
+                            None => {
+                                tracing::debug!(
+                                    "No canceller for node `{}` in stage {}/{}: already cancelled?",
+                                    node_id,
+                                    stage_index + 1,
+                                    stage_count
+                                );
+                            }
+                        }
                         // Add to list of cancelled nodes
-                        // TODO cancel the actual task if possible
-                        cancelled.push(start);
+                        cancelled.push(node_id);
                     }
                 }
             }
