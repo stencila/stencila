7059d2e9d1996b0f4a34e2fbb4ba85dabe7fb474
alex-ketch <alex@ketch.me>
2022-02-24 17:07:27 -0500

    refactor(Web): Simplify node ID generator helper

--- web/src/utils/uid.test.ts
+++ web/src/utils/uid.test.ts
@@ -1,61 +1,16 @@
-import lolex from 'lolex'
-import { epoch, generate, parse } from './uid'
-
-const clock = lolex.install()
-
-beforeEach(() => {
-  clock.setSystemTime(epoch)
-})
-
-afterEach(() => {
-  clock.reset()
-})
+import { generate } from './uid'
 
 describe('generate', () => {
   test('structure', () => {
-    expect(generate('ab').family).toBe('ab')
-    expect(generate('abc').value.length).toBe(34)
-    expect(generate('a').value).toMatch(
-      /^aa\.([0-9a-f]{10})\.([0-9A-Za-z]{20})$/
-    )
+    expect(generate('abc').length).toBe(23)
+    expect(generate('a')).toMatch(/^aa-([0-9A-Za-z]{20})$/)
+    expect(generate('cc')).toMatch(/^cc-([0-9A-Za-z]{20})$/)
+    expect(generate('code')).toMatch(/^co-([0-9A-Za-z]{20})$/)
   })
 
-  test('ordered by time', () => {
-    const gen = (): string => generate('ab').value
-
-    const id0 = gen()
-    expect(id0).toMatch(/^ab\.0000000000\.([0-9A-Za-z]{20})$/)
-
-    clock.tick(1)
-    const id1 = gen()
-    expect(id1).toMatch(/^ab\.0000000001\.([0-9A-Za-z]{20})$/)
-    expect(id1 > id0).toBe(true)
-
-    clock.tick(9)
-    const id2 = gen()
-    expect(id2).toMatch(/^ab\.000000000a\.([0-9A-Za-z]{20})$/)
-    expect(id2 > id1).toBe(true)
-
-    clock.tick(16)
-    const id3 = gen()
-    expect(id3).toMatch(/^ab\.000000001a\.([0-9A-Za-z]{20})$/)
-    expect(id3 > id1).toBe(true)
-  })
-})
-
-describe('parse', () => {
-  test('ok', () => {
-    expect(parse('aa.00000000.11111111111111111111')).toEqual({
-      family: 'aa',
-      time: new Date(epoch),
-      rand: '11111111111111111111',
-    })
-  })
-
-  test('bad', () => {
-    expect(parse('')).toBeUndefined()
-    expect(parse('a')).toBeUndefined()
-    expect(parse('aa')).toBeUndefined()
-    expect(parse('aa01')).toBeUndefined()
+  test('size', () => {
+    expect(generate('ab').length).toBe(23)
+    expect(generate('ab', 30).length).toBe(33)
+    expect(generate('code', 50)).toMatch(/^co-([0-9A-Za-z]{50})$/)
   })
 })
--- web/src/utils/uid.ts
+++ web/src/utils/uid.ts
@@ -1,20 +1,27 @@
 import { customAlphabet } from 'nanoid'
 
-/**
- * The epoch used for calculating
- * the time stamp. Chosen as a recent time
- * that was easily remembered.
- * Happens to correspond to 2017-07-14T02:40:00.000Z.
- */
-export const epoch = 1500000000000
+const idGenerator: Record<number, () => string> = {}
 
 /**
  * A random id generator using custom alphabet and length.
+ * Because instantiating the function can be expensive, we cache the generators
+ * based on the size of the ID required.
  */
-const nanoid = customAlphabet(
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  20
-)
+const nanoid = (size = 20): string => {
+  let uidGenerator = idGenerator[size]
+
+  if (uidGenerator) {
+    return uidGenerator()
+  } else {
+    uidGenerator = customAlphabet(
+      '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+      size
+    )
+
+    idGenerator[size] = uidGenerator
+    return uidGenerator()
+  }
+}
 
 /**
  * Generate a unique identifier.
@@ -24,25 +31,34 @@ const nanoid = customAlphabet(
  * - are URL safe
  * - contain information on the type of object that they
  *   are identifying
- * - are roughly sortable by time of generation
  * - have an extremely low probability of collision
  *
  * Generated identifiers have a fixed length of 32 characters made up
  * of three parts separated by dots:
  *
  * - 2 characters in the range `[a-z]` that identifying the "family" of
- *   identifiers, usually the type of object
- *   the identifier is for e.g. `rq` = request
+ *   identifiers, usually the type of object the identifier is for:
+ *   - do: document
+ *   - cb: code block
+ *   - cc: code chunk
+ *   - ce: code expression
+ *   - cf: code fragment
+ *   - mb: math block
+ *   - mf: math fragment
+ *   - re: request
+ *   - cl: client
+ *   - su: subscription
+ *   - ta: task
+ *   - ke: kernel
+ *   - se: session
  *
- * - 10 characters in the range `[0-9a-f]` that are the hexadecimal encoding of the
- *   seconds since `2017-07-14T02:40:00.000Z`the hexadecimal
- *
- * - 20 characters in the range `[0-9A-Za-z]` that are randomly generated
+ * - 20 characters in the range `[0-9A-Za-z]` that are randomly generated.
+ *   The length can be adjusted by the `size` parameter.
  *
  * @see {@link https://segment.com/blog/a-brief-history-of-the-uuid/|A brief history of the UUID}
- * @see {@link https://zelark.github.io/nano-id-cc/|Nano ID Collision Calculator}
+ * @see {@link https://zelark.github.io/nano-id-cc/ |Nano ID Collision Calculator}
  */
-export function generate(code: string): { family: string; value: string } {
+export const generate = (code: string, size?: number): string => {
   const family =
     code.length === 2
       ? code
@@ -51,38 +67,7 @@ export function generate(code: string): { family: string; value: string } {
       : code.length === 1
       ? code.repeat(2)
       : code.slice(0, 2)
-  const time = (Date.now() - epoch).toString(16).padStart(10, '0')
-  const rand = nanoid()
-  const value = `${family}.${time}.${rand}`
-  return { family, value }
-}
 
-/**
- * Parse a unique identifier.
- *
- * Extracts the parts from the identifier: `family`, `time` and `rand`.
- */
-export function parse(id: string):
-  | {
-      family: string
-      time: Date
-      rand: string
-    }
-  | undefined {
-  const [_, family, t, rand] =
-    /^([a-z]{2})\.([0-9a-f]{8})\.([0-9A-Za-z]{20})$/.exec(id) ?? []
-
-  if (family === undefined || t === undefined || rand === undefined) {
-    return
-  }
-
-  let seconds
-  try {
-    seconds = parseInt(t ?? '', 16)
-  } catch (error) {
-    return undefined
-  }
-
-  const time = new Date(seconds + epoch)
-  return { family, time, rand }
+  const rand = nanoid(size)
+  return `${family}-${rand}`
 }
