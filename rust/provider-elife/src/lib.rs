218afd0bf02a10c1c79f63e6865749eef1878681
Nokome Bentley <nokome@stenci.la>
2022-03-01 17:57:52 +1300

    chore(ELife provider): Update to meet `ProviderTrait` API

--- rust/provider-elife/src/lib.rs
+++ rust/provider-elife/src/lib.rs
@@ -1,10 +1,9 @@
 use provider::{Provider, ProviderTrait};
 
-/// A provider for eLife
-struct ElifeProvider {}
+pub struct ElifeProvider {}
 
 impl ProviderTrait for ElifeProvider {
     fn spec() -> Provider {
-        Provider {}
+        Provider::new("elife")
     }
 }
