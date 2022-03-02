b795d2dfdbad7262cb4a212067960cb661516ff5
Nokome Bentley <nokome@stenci.la>
2022-03-03 12:26:30 +1300

    feat(GitHub Provider): Initial iteration of `import`

--- rust/provider-github/src/lib.rs
+++ rust/provider-github/src/lib.rs
@@ -1,5 +1,8 @@
-use octocrab::{models::User, OctocrabBuilder};
+use archive_utils::extract_tar;
+use octocrab::{models::User, params::repos::Commitish, Octocrab, OctocrabBuilder};
 use provider::{
+    async_trait::async_trait,
+    codecs,
     eyre::Result,
     once_cell::sync::Lazy,
     regex::Regex,
@@ -7,11 +10,57 @@ use provider::{
         CreativeWorkContent, CreativeWorkPublisher, CreativeWorkVersion, Node, Organization,
         SoftwareSourceCode,
     },
-    Provider, ProviderParsing, ProviderTrait,
+    tracing, Provider, ProviderParsing, ProviderTrait,
+};
+use std::{
+    fs::{create_dir_all, File},
+    io::{self, Write},
+    path::{Path, PathBuf},
 };
 
 pub struct GithubProvider;
 
+impl GithubProvider {
+    /// Extract the GitHub repository owner and name from a [`SoftwareSourceCode`] node (if any)
+    fn owner_name(ssc: &SoftwareSourceCode) -> Option<(&str, &str)> {
+        if let Some(repo) = &ssc.code_repository {
+            if let Some(repo) = repo.strip_prefix("https://github.com/") {
+                let parts: Vec<&str> = repo.split('/').collect();
+                if parts.len() >= 2 {
+                    Some((parts[0], parts[1]))
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    /// Extract the sub-path from a [`SoftwareSourceCode`] node (if any)
+    fn path(ssc: &SoftwareSourceCode) -> Option<&str> {
+        ssc.content
+            .as_ref()
+            .and_then(|content| match content.as_ref() {
+                CreativeWorkContent::String(path) => Some(path.as_str()),
+                _ => None,
+            })
+    }
+
+    /// Extract the version from a [`SoftwareSourceCode`] node (if any)
+    fn version(ssc: &SoftwareSourceCode) -> Option<&str> {
+        ssc.version
+            .as_ref()
+            .and_then(|version| match version.as_ref() {
+                CreativeWorkVersion::String(version) => Some(version.as_str()),
+                _ => None,
+            })
+    }
+}
+
+#[async_trait]
 impl ProviderTrait for GithubProvider {
     fn spec() -> Provider {
         Provider::new("github")
@@ -24,7 +73,7 @@ impl ProviderTrait for GithubProvider {
                 .expect("Unable to create regex")
         });
 
-        // Regex targeting URL copies from th browser address bar
+        // Regex targeting URL copied from the browser address bar
         static URL_REGEX: Lazy<Regex> = Lazy::new(|| {
             Regex::new(r"^(?:https?://)?github\.com/([a-z0-9\-]+)/([a-z0-9\-_]+)/?(?:(?:tree|blob))?/?([^/]+)?/?(.+)?$")
                 .expect("Unable to create regex")
@@ -79,6 +128,95 @@ impl ProviderTrait for GithubProvider {
                 },
             )
             .collect()
+    }
+
+    async fn import(node: &Node, destination: &Path, token: Option<String>) -> Result<bool> {
+        let ssc = match node {
+            Node::SoftwareSourceCode(ssc) => ssc,
+            _ => return Ok(false),
+        };
+        let (owner, name) = match GithubProvider::owner_name(ssc) {
+            Some(owner_name) => owner_name,
+            None => return Ok(false),
+        };
+        let subpath = GithubProvider::path(ssc);
+        let version = GithubProvider::version(ssc);
+
+        let client = Octocrab::builder().build()?;
+        let repo = client.repos(owner, name);
+
+        // Fetch the contents at the path / version
+        let mut builder = repo.get_content();
+        if let Some(path) = subpath {
+            builder = builder.path(path);
+        }
+        if let Some(version) = version {
+            builder = builder.r#ref(version)
+        }
+        let content_items = builder.send().await?;
+        let count = content_items.items.len();
+
+        // No content (in directory)
+        if count == 0 {
+            tracing::warn!("No content at GitHub repository path/ref");
+            return Ok(true);
+        }
+
+        // Content is a single file with content so write to destination
+        if count == 1 {
+            let content = &content_items.items[0];
+            if content.r#type == "file" && content.content.is_some() {
+                let name = PathBuf::from(&content.name);
+                let string = content.decoded_content().unwrap_or_default();
+
+                if let Some(dest_ext) = destination.extension() {
+                    let dest_ext = dest_ext.to_string_lossy().to_string();
+                    let source_ext = name.extension().map_or_else(
+                        || content.name.to_string(),
+                        |os_str| os_str.to_string_lossy().to_string(),
+                    );
+                    if source_ext == dest_ext {
+                        // Destination format is same as content so just write it
+                        let mut file = File::create(destination)?;
+                        file.write_all(string.as_bytes())?;
+                    } else {
+                        // Destination has a different format so convert it first
+                        codecs::str_to_path(&string, &source_ext, destination, None).await?;
+                    }
+                } else {
+                    // Destination has no extension so treat it as a directory
+                    // and write the file there
+                    create_dir_all(destination)?;
+                    let mut file = File::create(destination.join(name))?;
+                    file.write_all(string.as_bytes())?;
+                }
+                return Ok(true);
+            }
+        }
+
+        // Content is a directory so fetch the whole repo as a tarball and extract the directory
+        // (getting the whole rpo as a tarball is more efficient than making lots of small requests
+        // for each file's content; for most repos)
+        let version = version.unwrap_or("HEAD").to_string();
+        let commitish = Commitish::from(version);
+        let response = repo.download_tarball(commitish).await?.error_for_status()?;
+
+        let mut file = tempfile::NamedTempFile::new()?;
+        let bytes = response.bytes().await?;
+        io::copy(&mut bytes.as_ref(), &mut file)?;
+        file.flush()?;
+        let archive = file.path();
+
+        create_dir_all(destination)?;
+        extract_tar(
+            "gz",
+            archive,
+            destination,
+            1,
+            subpath,
+        )?;
+
+        Ok(true)
     }
 }
 
