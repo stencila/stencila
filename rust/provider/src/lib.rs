1e72306e17fb8fb5e45dd60729bb70f611764b85
Nokome <nokome@stenci.la>
2022-03-07 20:54:13 +1300

    refactor(Provider): Use `cron-utils` crate

--- rust/provider/src/lib.rs
+++ rust/provider/src/lib.rs
@@ -229,133 +229,16 @@ impl Visitor for Detector {
     }
 }
 
-#[derive(Debug, Clone)]
-enum Schedule {
-    Cron(cron::Schedule),
-    Lingo(cron_lingo::Schedule),
-}
-
-impl Schedule {
-    fn from_str(schedule: &str) -> Result<Self> {
-        // Allow for "hourly" and "every hour" etc
-        static PERIOD_REGEX: Lazy<Regex> = Lazy::new(|| {
-            Regex::new(r"(?i)^\s*(yearly|annually|monthly|weekly|daily|hourly|minutely)|((every\s+)(year|month|week|day|hr|hour|min|minute))$").expect("Unable to create regex")
-        });
-        let schedule = PERIOD_REGEX
-            .replace_all(schedule, |captures: &Captures| {
-                let period = captures
-                    .get(1)
-                    .or_else(|| captures.get(4))
-                    .expect("Should have one or other")
-                    .as_str();
-                println!("{}", period);
-                match period {
-                    "year" | "yearly" | "annually" => "@yearly",
-                    "month" | "monthly" => "@monthly",
-                    "week" | "weekly" => "@weekly",
-                    "day" | "daily" => "@daily",
-                    "hr" | "hour" | "hourly" => "@hourly",
-                    "min" | "minute" | "minutely" => "0 * * * * *",
-                    _ => &captures[0],
-                }
-                .to_string()
-            })
-            .to_string();
-
-        match cron::Schedule::from_str(&schedule) {
-            Ok(schedule) => Ok(Schedule::Cron(schedule)),
-            Err(_) => {
-                // Allow for title case starting `At` or no `at` at all
-                let schedule = if let Some(rest) = schedule.strip_prefix("At") {
-                    ["at", rest].concat()
-                } else if !schedule.starts_with("at") {
-                    ["at ", &schedule].concat()
-                } else {
-                    schedule.to_string()
-                };
-
-                // Allow for lowercase am/pm and next to number e.g 9am or 8PM
-                static AMPM_REGEX: Lazy<Regex> =
-                    Lazy::new(|| Regex::new(r"(?i)\s*(am|pm)").expect("Unable to create regex"));
-                let schedule = AMPM_REGEX.replace_all(&schedule, |captures: &Captures| {
-                    let day = captures[1].to_lowercase();
-                    match day.as_str() {
-                        "am" => " AM",
-                        "pm" => " PM",
-                        _ => &captures[0],
-                    }
-                    .to_string()
-                });
-
-                // Allow for short and non-pluralized days of week
-                static DAY_REGEX: Lazy<Regex> = Lazy::new(|| {
-                    Regex::new(
-                        r"(?i)\b(mon|tue|tues|wed|wednes|thu|thur|thurs|fri|sat|satur|sun)(day)?\b",
-                    )
-                    .expect("Unable to create regex")
-                });
-                let schedule = DAY_REGEX.replace_all(&schedule, |captures: &Captures| {
-                    let day = captures[1].to_lowercase();
-                    match day.as_str() {
-                        "mon" => "Mondays",
-                        "tue" => "Tuesdays",
-                        "wed" => "Wednesdays",
-                        "thu" | "thur" | "thurs" => "Thursdays",
-                        "fri" => "Fridays",
-                        "sat" | "satur" => "Saturdays",
-                        "sun" => "Sundays",
-                        _ => &captures[0],
-                    }
-                    .to_string()
-                });
-
-                match cron_lingo::Schedule::from_str(&schedule) {
-                    Ok(schedule) => Ok(Schedule::Lingo(schedule)),
-                    Err(error) => {
-                        bail!("Unable to parse schedule: {}", error);
-                    }
-                }
-            }
-        }
-    }
-
-    fn next(&self) -> Option<DateTime<Utc>> {
-        match self {
-            Self::Cron(schedule) => schedule.upcoming(Utc).next(),
-            Self::Lingo(schedule) => match schedule.iter() {
-                Ok(mut iter) => iter.next().and_then(|result| match result {
-                    Ok(offset_date_time) => {
-                        let timestamp = offset_date_time.unix_timestamp();
-                        let naive_datetime = NaiveDateTime::from_timestamp(timestamp, 0);
-                        Some(DateTime::from_utc(naive_datetime, Utc))
-                    }
-                    Err(error) => {
-                        tracing::error!("While getting next scheduled time: {:?}", error);
-                        None
-                    }
-                }),
-                Err(error) => {
-                    // The "The system's UTC offset could not be determined" error is relation to this
-                    // https://github.com/time-rs/time/issues/296. Note sure there is a work around right now
-                    // so we might need to wait for a fix in that upstream crate.
-                    tracing::error!("While getting schedule: {}", error);
-                    None
-                }
-            },
-        }
-    }
-}
-
 /// Schedule import and/or export to/from a remove [`Node`] and a local path
 pub async fn run_schedule(schedule: &str, sender: mpsc::Sender<()>) -> Result<()> {
-    let schedule = Schedule::from_str(schedule)?;
+    let (schedules, timezone) = cron_utils::parse(schedule)?;
 
     let (interrupt_sender, mut interrupt_receiver) = mpsc::unbounded_channel();
     subscribe("interrupt", Subscriber::Sender(interrupt_sender))?;
 
     tokio::spawn(async move {
         let interval = Duration::from_secs(1);
-        let mut next = schedule.next();
+        let mut next = cron_utils::next(&schedules, &timezone);
         if let Some(time) = next {
             tracing::debug!("First action scheduled for {}", time);
         }
@@ -367,7 +250,7 @@ pub async fn run_schedule(schedule: &str, sender: mpsc::Sender<()>) -> Result<()
                             if let Err(error) = sender.send(()).await {
                                 tracing::error!("When sending schedule message: {}", error);
                             }
-                            next = schedule.next();
+                            next = cron_utils::next(&schedules, &timezone);
                             if let Some(time) = next {
                                 tracing::debug!("Next action scheduled for {}", time);
                             }
