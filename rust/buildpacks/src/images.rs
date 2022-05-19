a8b617917f97f364052a9fb6e935b53e6e65466c
Nokome Bentley <nokome@stenci.la>
2022-05-20 10:48:04 +1200

    refactor(Build snapshots): Store directory for more convenient repeats and change calculation

--- rust/buildpacks/src/images.rs
+++ rust/buildpacks/src/images.rs
@@ -1,7 +1,7 @@
 use std::{
     collections::HashMap,
-    ffi::{OsStr, OsString},
-    fs::{self, DirEntry, File, FileType, Metadata},
+    ffi::OsString,
+    fs::{self, File, FileType, Metadata},
     hash::Hasher,
     io,
     os::unix::prelude::MetadataExt,
@@ -9,20 +9,21 @@ use std::{
     time::{SystemTime, UNIX_EPOCH},
 };
 
-use derive_more::{Constructor, Deref, DerefMut};
+use derive_more::{Deref, DerefMut};
 use jwalk::WalkDirGeneric;
 use seahash::SeaHasher;
 
 use archive_utils::tar;
 use buildpack::{
-    eyre::{bail, eyre, Context, Result},
+    eyre::{eyre, Result},
     serde::{Deserialize, Serialize},
     serde_json, tracing,
 };
 
 /// The set of changes between two snapshots
 ///
-/// This is a https://github.com/opencontainers/image-spec/blob/main/layer.md
+/// This `struct` represents a set of changes between two snapshots and thus represents
+/// an OCI [Layer](https://github.com/opencontainers/image-spec/blob/main/layer.md)
 #[derive(Deref, DerefMut)]
 struct SnapshotChanges(Vec<SnapshotChange>);
 
@@ -35,7 +36,7 @@ impl SnapshotChanges {
     /// Create a tar archive from the snapshot changes
     ///
     /// This implements the [Representing Changes](https://github.com/opencontainers/image-spec/blob/main/layer.md#representing-changes)
-    /// section of the spec. `Added` and `Modified` paths are added to the tar archive.
+    /// section of the OCI image spec. `Added` and `Modified` paths are added to the tar archive.
     /// `Removed` paths are represented as "whiteout" files.
     fn write(self, src: &Path, dest: &Path) -> Result<()> {
         let file = File::create(dest).unwrap();
@@ -71,7 +72,7 @@ impl SnapshotChanges {
 /// A change in a path between two snapshots
 ///
 /// This enum represents the [Change Types](https://github.com/opencontainers/image-spec/blob/main/layer.md#change-types)
-/// described in the spec.
+/// described in the OCI spec.
 #[derive(Debug, PartialEq)]
 enum SnapshotChange {
     Added(PathBuf),
@@ -79,15 +80,30 @@ enum SnapshotChange {
     Removed(PathBuf),
 }
 
-/// Map of file or directory paths and snapshot entries
-#[derive(Debug, Deref, Default, Serialize, Deserialize)]
+/// A snapshot of the files and directories in a directory
+///
+/// A snapshot is created at the start of a session and stored to disk. Another snapshot
+/// is taken at the end of session. The changes between the snapshots are used to create
+/// an image layer.
+///
+/// Currently this uses `serde_json` for serializing to/from disk. An alternative
+/// serialization such as `rkyv` would be a lot more efficient but, at the time of writing,
+/// does not support `HashMap` with `PathBuf` as the key.
+#[derive(Debug, Default, Serialize, Deserialize)]
 #[serde(crate = "buildpack::serde")]
-struct Snapshot(HashMap<PathBuf, SnapshotEntry>);
+struct Snapshot {
+    /// The directory to snapshot
+    dir: PathBuf,
+
+    /// Entries in the snapshot
+    entries: HashMap<PathBuf, SnapshotEntry>,
+}
 
 impl Snapshot {
     /// Create a new snapshot of a directory
-    fn new(dir: &Path) -> Self {
-        let entries = WalkDirGeneric::<((), SnapshotEntry)>::new(dir)
+    fn new<P: AsRef<Path>>(dir: P) -> Self {
+        let dir = dir.as_ref().to_path_buf();
+        let entries = WalkDirGeneric::<((), SnapshotEntry)>::new(&dir)
             .skip_hidden(false)
             .process_read_dir(|_depth, _path, _read_dir_state, children| {
                 children.iter_mut().flatten().for_each(|dir_entry| {
@@ -105,7 +121,7 @@ impl Snapshot {
                 Ok(entry) => {
                     let relative_path = entry
                         .path()
-                        .strip_prefix(dir)
+                        .strip_prefix(&dir)
                         .expect("Should always be able to strip the root dir")
                         .to_path_buf();
                     match relative_path == PathBuf::from("") {
@@ -119,8 +135,12 @@ impl Snapshot {
                 }
             })
             .collect();
+        Self { dir, entries }
+    }
 
-        Self(entries)
+    /// Create a new snapshot by repeating the current one
+    fn repeat(&self) -> Self {
+        Self::new(&self.dir)
     }
 
     /// Write a snapshot to a file
@@ -137,11 +157,11 @@ impl Snapshot {
         Ok(snapshot)
     }
 
-    /// Create a set of changes from the snapshot to another
-    fn changes(&self, other: &Snapshot) -> SnapshotChanges {
+    /// Create a set of changes by calculating the difference between two snapshots
+    fn diff(&self, other: &Snapshot) -> SnapshotChanges {
         let mut changes = SnapshotChanges::new();
-        for (path, entry) in self.iter() {
-            match other.get(path) {
+        for (path, entry) in self.entries.iter() {
+            match other.entries.get(path) {
                 Some(other_entry) => {
                     if entry != other_entry {
                         changes.push(SnapshotChange::Modified(path.into()))
@@ -150,15 +170,21 @@ impl Snapshot {
                 None => changes.push(SnapshotChange::Removed(path.into())),
             }
         }
-        for path in other.keys() {
-            if !self.contains_key(path) {
+        for path in other.entries.keys() {
+            if !self.entries.contains_key(path) {
                 changes.push(SnapshotChange::Added(path.into()))
             }
         }
         changes
     }
+
+    /// Create a set of changes by repeating the current snapshot
+    fn changes(&self) -> SnapshotChanges {
+        self.diff(&self.repeat())
+    }
 }
 
+/// An entry for a file or directory in a snapshot
 #[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
 #[serde(crate = "buildpack::serde")]
 struct SnapshotEntry {
@@ -174,6 +200,7 @@ struct SnapshotEntry {
     fingerprint: Option<u64>,
 }
 
+/// Filesystem metadata for a snapshot entry
 #[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
 #[serde(crate = "buildpack::serde")]
 struct SnapshotEntryMetadata {
@@ -185,6 +212,7 @@ struct SnapshotEntryMetadata {
 }
 
 impl SnapshotEntry {
+    /// Create a new snapshot entry
     fn new(path: &Path, file_type: &FileType, metadata: Option<Metadata>) -> Self {
         let metadata = metadata.map(|metadata| SnapshotEntryMetadata {
             created: file_timestamp(metadata.created()),
@@ -198,7 +226,7 @@ impl SnapshotEntry {
             match file_hash::<SeaHasher>(path) {
                 Ok(fingerprint) => Some(fingerprint),
                 Err(error) => {
-                    tracing::error!("While fingerprinting file: {}", error);
+                    tracing::error!("While fingerprinting file `{}`: {}", path.display(), error);
                     None
                 }
             }
@@ -251,8 +279,7 @@ fn file_timestamp(time: Result<SystemTime, io::Error>) -> Option<u64> {
 
 #[cfg(test)]
 mod tests {
-    use test_snaps::insta::assert_json_snapshot;
-    use test_utils::{fixtures, print_logs, tempfile::tempdir};
+    use test_utils::{print_logs, tempfile::tempdir};
 
     use super::*;
 
@@ -269,9 +296,9 @@ mod tests {
         // Create an initial snapshot which should be empty and has not changes with self
 
         let snap1 = Snapshot::new(dir.path());
-        assert_eq!(snap1.len(), 0);
+        assert_eq!(snap1.entries.len(), 0);
 
-        let changes = snap1.changes(&snap1);
+        let changes = snap1.diff(&snap1);
         assert_eq!(changes.len(), 0);
 
         // Add a file, create a new snapshot and check it has one entry and produces a change set
@@ -280,11 +307,11 @@ mod tests {
         let a_txt = PathBuf::from("a.txt");
         fs::write(dir.path().join(&a_txt), "Hello from a.txt")?;
 
-        let snap2 = Snapshot::new(dir.path());
-        assert_eq!(snap2.len(), 1);
-        assert_eq!(snap2[&a_txt].fingerprint, Some(3958791156379554752));
+        let snap2 = snap1.repeat();
+        assert_eq!(snap2.entries.len(), 1);
+        assert_eq!(snap2.entries[&a_txt].fingerprint, Some(3958791156379554752));
 
-        let changes = snap1.changes(&snap2);
+        let changes = snap1.diff(&snap2);
         assert_eq!(changes.len(), 1);
         assert_eq!(changes[0], SnapshotChange::Added(a_txt.clone()));
 
@@ -300,12 +327,15 @@ mod tests {
         let b_txt = PathBuf::from("b.txt");
         fs::write(dir.path().join(&b_txt), "Hello from b.txt")?;
 
-        let snap3 = Snapshot::new(dir.path());
-        assert_eq!(snap3.len(), 2);
-        assert_eq!(snap2[&a_txt].fingerprint, Some(3958791156379554752));
-        assert_eq!(snap3[&b_txt].fingerprint, Some(15548480638800185371));
+        let snap3 = snap1.repeat();
+        assert_eq!(snap3.entries.len(), 2);
+        assert_eq!(snap2.entries[&a_txt].fingerprint, Some(3958791156379554752));
+        assert_eq!(
+            snap3.entries[&b_txt].fingerprint,
+            Some(15548480638800185371)
+        );
 
-        let changes = snap2.changes(&snap3);
+        let changes = snap2.diff(&snap3);
         assert_eq!(changes.len(), 1);
         assert_eq!(changes[0], SnapshotChange::Added(b_txt.clone()));
 
@@ -314,11 +344,14 @@ mod tests {
 
         fs::remove_file(dir.path().join(&a_txt))?;
 
-        let snap4 = Snapshot::new(dir.path());
-        assert_eq!(snap4.len(), 1);
-        assert_eq!(snap4[&b_txt].fingerprint, Some(15548480638800185371));
+        let snap4 = snap1.repeat();
+        assert_eq!(snap4.entries.len(), 1);
+        assert_eq!(
+            snap4.entries[&b_txt].fingerprint,
+            Some(15548480638800185371)
+        );
 
-        let changes = snap3.changes(&snap4);
+        let changes = snap3.diff(&snap4);
         assert_eq!(changes.len(), 1);
         assert_eq!(changes[0], SnapshotChange::Removed(a_txt));
 
@@ -334,11 +367,11 @@ mod tests {
 
         fs::write(dir.path().join(&b_txt), "Hello")?;
 
-        let snap5 = Snapshot::new(dir.path());
-        assert_eq!(snap5.len(), 1);
-        assert_eq!(snap5[&b_txt].fingerprint, Some(3297469917561599766));
+        let snap5 = snap1.repeat();
+        assert_eq!(snap5.entries.len(), 1);
+        assert_eq!(snap5.entries[&b_txt].fingerprint, Some(3297469917561599766));
 
-        let changes = snap4.changes(&snap5);
+        let changes = snap4.diff(&snap5);
         assert_eq!(changes.len(), 1);
         assert_eq!(changes[0], SnapshotChange::Modified(b_txt.clone()));
 
