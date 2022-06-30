f151f27fdd8f3012ccac3a085a6dd6747965a3c5
Nokome Bentley <nokome@stenci.la>
2022-06-30 13:23:54 +1200

    feat(Google Docs codec): Create a new Google Doc when encoding

--- rust/codec-gdoc/src/encode.rs
+++ rust/codec-gdoc/src/encode.rs
@@ -1,15 +1,52 @@
 use std::path::Path;
 
-use codec::{common::eyre::Result, stencila_schema::Node, EncodeOptions};
+use codec::{
+    common::{eyre::Result, tempfile::tempdir, tracing},
+    stencila_schema::{Article, Node},
+    EncodeOptions,
+};
+use provider_gdrive::{GoogleDriveProvider, ProviderTrait};
 
-/// Encode a Stencila `Node` as a Microsoft Word `docx` that can then be uploaded
-/// as a Google Doc.
+/// Encode a Stencila `Node` as a Google Doc
+///
+/// The Google Docs API v1 [`documents.create` method](https://developers.google.com/docs/api/reference/rest/v1/documents/create)
+/// ignores any provided content (i.e. only empty documents can be created). However, the Google Drive API has a
+/// [`files.create` method](https://developers.google.com/drive/api/v3/reference/files/create) which allows a new Google Doc
+/// to be created by uploading a Microsoft Word file.
+///
+/// Therefore, this function 'encodes' a new `.gdoc` file by:
+///
+/// 1. creating a temporary `.docx` file
+/// 2. creating a new Google Doc by uploading the `.docx` file
+/// 3. pulling the new Google Doc to a `.gdoc` file at `path`
+///
+/// Step three is not entirely necessary (since the Google Doc is available online) but provides
+/// consistent UX when users are using Stencila to convert between file formats.
 pub(crate) async fn encode(node: &Node, path: &Path, options: Option<EncodeOptions>) -> Result<()> {
-    let mut options = options.unwrap_or_default();
-    options.rpng_content = true;
-    options.rpng_link = true;
+    // Encode to docx
+    let tempdir = tempdir()?;
+    let docx = tempdir.path().join("temp.docx");
+    codec_pandoc::encode(
+        node,
+        Some(&docx),
+        "docx",
+        &[],
+        Some(EncodeOptions {
+            rpng_content: true,
+            rpng_link: true,
+            ..options.unwrap_or_default()
+        }),
+    )
+    .await?;
 
-    codec_pandoc::encode(node, Some(path), "docx", &[], Some(options)).await?;
+    // Create new Google Doc from docx
+    let gdoc = GoogleDriveProvider::export(&Node::Article(Article::default()), &docx, None).await?;
+    if let Node::Article(Article { url: Some(url), .. }) = &gdoc {
+        tracing::info!("Successfully created Google Doc: {}", url);
+    }
+
+    // Pull the new Google Doc
+    GoogleDriveProvider::import(&gdoc, path, None).await?;
 
     Ok(())
 }
