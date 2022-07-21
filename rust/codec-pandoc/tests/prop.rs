cb10828bd7741e311ee4c0bfa38af28fcdff08e2
Nokome Bentley <nokome@stenci.la>
2022-07-21 18:04:02 +1200

    test(Pandoc codec): Use RPNG `rpng_content: true` in tests

--- rust/codec-pandoc/tests/prop.rs
+++ rust/codec-pandoc/tests/prop.rs
@@ -1,4 +1,4 @@
-use codec::CodecTrait;
+use codec::{CodecTrait, EncodeOptions};
 use codec_pandoc::{decode_pandoc, encode_node, PandocCodec};
 use test_props::{article, proptest::prelude::*, Freedom};
 use test_utils::assert_json_eq;
@@ -10,7 +10,10 @@ proptest! {
         PandocCodec::spec().unsupported_types,
         PandocCodec::spec().unsupported_properties,
     )) {
-        let pandoc = encode_node(&input, None).unwrap();
+        let pandoc = encode_node(&input, Some(EncodeOptions{
+            rpng_content: true,
+            ..Default::default()
+        })).unwrap();
         let output = decode_pandoc(pandoc).unwrap();
         assert_json_eq!(input, output);
     }
