c1727b7ecaca22f27d1902a8524c578e6d85df55
Nokome Bentley <nokome@stenci.la>
2022-09-21 09:21:25 +1200

    fix(Icon component): Use `bundle-text` to reduce number of files and requests

--- web/src/components/base/icon.ts
+++ web/src/components/base/icon.ts
@@ -5,25 +5,30 @@ import '@shoelace-style/shoelace/dist/components/icon/icon'
 
 import StencilaElement from './element'
 
+// prettier-ignore
 const icons: Record<string, string> = {
-  'arrow-repeat': require('@shoelace-style/shoelace/dist/assets/icons/arrow-repeat.svg'),
-  'check-circle': require('@shoelace-style/shoelace/dist/assets/icons/check-circle.svg'),
-  'dash-circle': require('@shoelace-style/shoelace/dist/assets/icons/dash-circle.svg'),
-  'eye-slash': require('@shoelace-style/shoelace/dist/assets/icons/eye-slash.svg'),
-  'lightning-fill': require('@shoelace-style/shoelace/dist/assets/icons/lightning-fill.svg'),
-  clock: require('@shoelace-style/shoelace/dist/assets/icons/clock.svg'),
-  code: require('@shoelace-style/shoelace/dist/assets/icons/code.svg'),
-  dash: require('@shoelace-style/shoelace/dist/assets/icons/dash.svg'),
-  eye: require('@shoelace-style/shoelace/dist/assets/icons/eye.svg'),
-  hourglass: require('@shoelace-style/shoelace/dist/assets/icons/hourglass.svg'),
+  'arrow-repeat': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/arrow-repeat.svg'),
+  'check-circle': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/check-circle.svg'),
+  'circle': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/circle.svg'),
+  'clock': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/clock.svg'),
+  'code': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/code.svg'),
+  'dash-circle': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/dash-circle.svg'),
+  'dash': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/dash.svg'),
+  'eye-slash': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/eye-slash.svg'),
+  'eye': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/eye.svg'),
+  'hourglass': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/hourglass.svg'),
+  'lightning-fill': require('bundle-text:@shoelace-style/shoelace/dist/assets/icons/lightning-fill.svg'),
 }
+
+export type IconName = keyof typeof icons
 
 /**
  * An icon
  *
  * This is a thin wrapper around the Shoelace `<sl-icon>` which, instead of downloading
- * icon SVG files on the fly, bundles them. This avoids having to serve the icons SVGs
- * independently which in turn simplifies embedding components in Stencila binaries.
+ * icon SVG files on the fly, bundles them as SVG strings. This avoids having to serve the icons SVGs
+ * independently which in turn simplifies embedding components in Stencila binaries and
+ * reduces the number distributed files and requests.
  */
 @customElement('stencila-icon')
 export default class StencilaIcon extends StencilaElement {
@@ -31,7 +36,7 @@ export default class StencilaIcon extends StencilaElement {
    * The name of the icon to render
    */
   @property()
-  name: keyof typeof icons
+  name: IconName
 
   /**
    * An alternate description to use for accessibility.
@@ -41,9 +46,9 @@ export default class StencilaIcon extends StencilaElement {
   label?: string
 
   render() {
-    const svg = icons[this.name] ?? icons.circle
+    const svg = encodeURIComponent(icons[this.name] ?? icons.circle)
     return html`<sl-icon
-      src="${svg}"
+      src="data:image/svg+xml,${svg}"
       ${this.label ? `label=${this.label}` : ''}
     ></sl-icon>`
   }
