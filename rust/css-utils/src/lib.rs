d67506acd8891b2429cbae478513da3a76b29474
Nokome Bentley <nokome@stenci.la>
2022-10-04 17:23:17 +1300

    refactor(CSS utils): Use `tailwind-css` for parsing group syntax
    I did not realize that `tailwind-css` parses `twind` style groups
    and so implemented this myself using `nom`. However, I realized
    that it is actually duplicating (less well), code already in `tailwind-ast` (which `tailwind-css`
    depends upon).

--- rust/css-utils/src/lib.rs
+++ rust/css-utils/src/lib.rs
@@ -3,18 +3,6 @@ use lightningcss::{
     targets::Browsers,
     traits::ToCss,
 };
-use nom::{
-    branch::alt,
-    bytes::complete::{is_not, take_while1},
-    character::{
-        complete::{multispace0, multispace1}, is_alphanumeric,
-        streaming::char,
-    },
-    combinator::{all_consuming, map, peek, recognize},
-    multi::{separated_list0, separated_list1},
-    sequence::{delimited, terminated, tuple},
-    IResult,
-};
 use tailwind_css::TailwindBuilder;
 
 use common::{
@@ -31,134 +19,12 @@ pub fn parse_string(string: &str) -> Result<Vec<String>> {
 
 /// Parse Tailwind directives
 fn parse_tailwind(tw: &str) -> Result<Vec<String>> {
-    // Expand any Tailwind groups e.g. `text(lg red-100)` into `text-lg text-red-100`
-    let result = expand_tailwind_groups(tw.trim());
-    let directives = result
-        .map_err(|error| eyre!("While parsing Tailwind groups: {}", error.to_string()))?
-        .1;
-
-    // Transform each Tailwind directive into CSS, wrapping the result in
-    // `@media` queries if necessary
+    // Transform Tailwind to CSS
     let mut tailwind = TailwindBuilder::default();
-    let mut all = String::new();
-    for directive in directives {
-        let (.., css) = tailwind.inline(&directive).unwrap();
-        let css = if let Some((breakpoint, ..)) =
-            directive.splitn(2, ':').collect_tuple::<(&str, &str)>()
-        {
-            let px = match breakpoint {
-                "sm" => 640,
-                "md" => 768,
-                "lg" => 1024,
-                "xl" => 1280,
-                "2xl" => 1536,
-                _ => 768,
-            };
-            [
-                "@media (min-width: ",
-                &px.to_string(),
-                "px) {\n",
-                &css,
-                "\n}\n",
-            ]
-            .concat()
-        } else {
-            css
-        };
-        all.push_str(&css);
-    }
+    let (.., css) = tailwind.inline(tw)?;
 
     // Parse the CSS
-    parse_css_wrapped(&all)
-}
-
-/// Expand any Tailwind directive
-fn expand_tailwind_groups(input: &str) -> IResult<&str, Vec<String>> {
-    all_consuming(separated_list0(
-        multispace1,
-        alt((
-            tailwind_directive_group,
-            tailwind_variant_group,
-            tailwind_directive,
-        )),
-    ))(input)
-}
-
-/// Recognize a Tailwind directive
-///
-/// Should parse all directives e.g.
-///   border-2 border-red-100 sm:border-opacity-50 sm:hover:border-dashed
-fn tailwind_directive(input: &str) -> IResult<&str, String> {
-    map(
-        recognize(take_while1(|c: char| {
-            is_alphanumeric(c as u8) || c == '-' || c == ':'
-        })),
-        |dir: &str| -> String { dir.to_string() },
-    )(input)
-}
-
-/// Expand a Tailwind directive group
-///
-/// Implements the directive grouping of https://twind.dev e.g.
-///    border(2 red-100)
-fn tailwind_directive_group(input: &str) -> IResult<&str, String> {
-    map(
-        tuple((
-            recognize(terminated(
-                take_while1(|c: char| is_alphanumeric(c as u8) || c == '-' || c == ':'),
-                peek(is_not(":")),
-            )),
-            delimited(
-                tuple((char('('), multispace0)),
-                separated_list1(
-                    multispace1,
-                    alt((
-                        tailwind_directive_group,
-                        tailwind_variant_group,
-                        tailwind_directive,
-                    )),
-                ),
-                tuple((multispace0, char(')'))),
-            ),
-        )),
-        |(prefix, items)| -> String {
-            items
-                .iter()
-                .map(|item| {
-                    if prefix.ends_with('-') || item.starts_with('-') {
-                        [prefix, item].concat()
-                    } else {
-                        [prefix, "-", item].concat()
-                    }
-                })
-                .join(" ")
-        },
-    )(input)
-}
-
-/// Expand a Tailwind variant group
-///
-/// Implements the variant grouping whcih allows for nested directive
-/// groups but not nested variant groups e.g.
-///    sm:(border(2 red-100))
-fn tailwind_variant_group(input: &str) -> IResult<&str, String> {
-    map(
-        tuple((
-            recognize(tuple((
-                take_while1(|c: char| is_alphanumeric(c as u8)),
-                char(':'),
-            ))),
-            delimited(
-                tuple((char('('), multispace0)),
-                separated_list1(
-                    multispace1,
-                    alt((tailwind_directive_group, tailwind_directive)),
-                ),
-                tuple((multispace0, char(')'))),
-            ),
-        )),
-        |(prefix, items)| -> String { items.iter().map(|item| [prefix, item].concat()).join(" ") },
-    )(input)
+    parse_css_wrapped(&css)
 }
 
 /// Wrap CSS in a `root` selector before parsing
@@ -254,6 +120,16 @@ mod tests {
         // Basic
         assert_snapshot!(parse_tailwind(r"text-md text-red-200 bg-red-100")?.join("\n\n"));
 
+        Ok(())
+    }
+
+    /// Test Tailwind variants e.g. sm, md, lg and hover
+    ///
+    /// Currently, handling of variants is not implemented by `tailwind-css`. These tests
+    /// therefore only produce the CSS for the last variant. The snapshots are expected
+    /// to break when this is implemented.
+    #[test]
+    fn test_parse_tailwind_variants() -> Result<()> {
         // Four ways to do same thing with breakpoints
         assert_snapshot!(
             parse_tailwind(r"sm:text-md sm:text(md) sm:(text(md)) sm:(text-md)")?.join("\n\n")
