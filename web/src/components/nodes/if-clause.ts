e81b576bbca6ecf5a12c45bdf331270117f27481
Nokome Bentley <nokome@stenci.la>
2022-10-18 21:41:47 +1300

    feat(Web components): More fully develop `If` and `IfClause`

--- web/src/components/nodes/if-clause.ts
+++ web/src/components/nodes/if-clause.ts
@@ -1,10 +1,17 @@
-import { html } from 'lit'
+import { html, PropertyValueMap } from 'lit'
 import { customElement, property, state } from 'lit/decorators'
+import { currentMode, Mode } from '../../mode'
 
 import { Patch } from '../../types'
+import '../base/icon-button'
 import '../editors/code-editor'
+import StencilaCodeEditor from '../editors/code-editor'
 import { twSheet } from '../utils/css'
-import StencilaExecutable from './executable'
+import './code-error'
+import './code-executable'
+import StencilaCodeExecutable, {
+  StencilaExecutableLanguage,
+} from './code-executable'
 
 const { tw, sheet } = twSheet()
 
@@ -12,26 +19,84 @@ const { tw, sheet } = twSheet()
  * A component representing a Stencila `IfClause` document node
  */
 @customElement('stencila-if-clause')
-export default class StencilaIfClause extends StencilaExecutable {
+export default class StencilaIfClause extends StencilaCodeExecutable {
   static styles = sheet.target
 
   /**
-   * An expression, in the programming language, that evaluates to a truthy/falsy value
+   * The JSON value sent in a patch when creating a new clause
    */
-  @property()
-  text: string
+  static json = {
+    type: 'IfClause',
+    guessLanguage: true,
+  }
 
   /**
-   * The programming language of the expression
+   * The HTML fragment added to the DOM when creating a new clause
    */
-  @property({ attribute: 'programming-language' })
-  programmingLanguage: string
+  static html = `<stencila-if-clause guess-language="true">
+    <div data-prop="errors" slot="errors"></div>
+  </stencila-if-clause>`
 
   /**
-   * Whether the clause is active (the first amongst the clauses to be truthy)
+   * The `IfClause.isActive` property
+   *
+   * As for `guessLanguage`, needs to be a string.
    */
-  @property({ type: Boolean, attribute: 'is-active' })
-  private isActive: boolean
+  @property({ attribute: 'is-active', reflect: true })
+  isActive: string = 'false'
+
+  /**
+   * The index of this clause within an `If` node
+   */
+  @state()
+  private index: number
+
+  /**
+   * Whether this is the first clause in an `If` node
+   */
+  @state()
+  private isFirst: boolean
+
+  /**
+   * Whether this is the last clause in an `If` node
+   */
+  @state()
+  private isLast: boolean
+
+  /**
+   * Whether this is an else clause
+   */
+  @state()
+  private isElse: boolean
+
+  /**
+   * Whether the clause has any content
+   */
+  @state()
+  private hasContent: boolean = false
+
+  /**
+   * An observer to update `hasContent`
+   */
+  private contentObserver: MutationObserver
+
+  /**
+   * Handle a change, including on initial load, of the content slot
+   */
+  onContentSlotChange(event: Event) {
+    const contentElem = (event.target as HTMLSlotElement).assignedElements({
+      flatten: true,
+    })[0]
+
+    this.hasContent = contentElem.childElementCount > 0
+
+    this.contentObserver = new MutationObserver(() => {
+      this.hasContent = contentElem.childElementCount > 0
+    })
+    this.contentObserver.observe(contentElem, {
+      childList: true,
+    })
+  }
 
   /**
    * Whether the content of the clause is visible to the user
@@ -54,23 +119,38 @@ export default class StencilaIfClause extends StencilaExecutable {
   }
 
   /**
-   * Override of `Entity.sendPatch` to use targe the parent `If` node by using
-   * the id of the containing <stencila-if> node and prepending the address
-   * for this clause
+   * Request an update of all clauses in the parent `If` node
    */
-  protected async sendPatch(patch: Patch) {
+  private requestUpdateAll() {
+    this.getIfClauses().forEach((clause: StencilaIfClause) =>
+      clause.requestUpdate()
+    )
+  }
+
+  /**
+   * Override of `Element.emitPatch` to make the parent `If` node the `target` of
+   * the patch (by using the id of the containing <stencila-if>) prepending the address
+   * with the relative address of this `IfClause`
+   */
+  protected async emitPatch(patch: Patch) {
     const index = this.getIfClauses().indexOf(this)
 
     const ops = patch.ops.map((op) => {
-      if (op.type === 'Move')
-        throw new Error('Unable to adjust a Move operation')
-      return {
-        ...op,
-        address: ['clauses', index, ...op.address],
+      if (op.type === 'Move') {
+        return {
+          ...op,
+          from: ['clauses', ...op.from],
+          to: ['clauses', ...op.to],
+        }
+      } else {
+        return {
+          ...op,
+          address: ['clauses', index, ...op.address],
+        }
       }
     })
 
-    return super.sendPatch({
+    return super.emitPatch({
       target: this.getIf().id,
       ops,
     })
@@ -87,68 +167,245 @@ export default class StencilaIfClause extends StencilaExecutable {
     })
   }
 
-  protected render() {
+  /**
+   * Override to set `isExpanded` based on the changes in `isActive`. This allows expansion/contraction
+   * based on changes to which clause is active as well as based on user interaction
+   */
+  protected update(
+    changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+  ): void {
+    super.update(changedProperties)
+
     const clauses = this.getIfClauses()
     const index = clauses.indexOf(this)
-    const isLast = index == clauses.length - 1
-    const label =
-      index == 0 ? 'if' : isLast && this.text.length == 0 ? 'else' : 'elif'
-    const icon =
+    this.index = index
+    this.isFirst = index == 0
+    this.isLast = index == clauses.length - 1
+    this.isElse = this.isLast && this.text.trim().length == 0
+
+    if (changedProperties.has('isActive')) {
+      this.isExpanded = this.isActive == 'true'
+    }
+  }
+
+  protected render() {
+    const label = this.index == 0 ? 'if' : this.isElse ? 'else' : 'elif'
+    const iconName =
       label == 'if' || label == 'elif' ? 'arrow-right' : 'arrow-return-right'
+    const isActive = this.isActive == 'true'
 
-    const text =
-      label !== 'else'
-        ? html`<stencila-code-editor
-            class=${tw`min-w-0 w-full rounded overflow-hidden border(& violet-200) focus:ring(1 violet-200) bg-violet-50`}
-            language=${this.programmingLanguage}
-            single-line
-            line-wrapping
-            no-controls
-            @stencila-patch=${(event: CustomEvent) =>
-              this.sendPatch(event.detail)}
-            @stencila-ctrl-enter=${() => this.execute()}
-          >
-            <code slot="code">${this.text}</code>
-          </stencila-code-editor>`
-        : html`<span class=${tw`min-w-0 w-full`}></span>`
+    const mode = currentMode()
+    const readOnly = mode < Mode.Alter || mode === Mode.Edit
 
-    return html`<div part="base">
-      <div part="header" class=${tw`flex justify-between items-center ${
-        isLast && !this.isExpanded ? '' : 'border(b violet-200)'
-      } bg-violet-50 p-1 font(mono bold) text(sm violet-800)`}>
-        <span class=${tw`flex items-center text-base mx-2`}>
-        <stencila-icon name=${icon}></stencila-icon>
-        </span>
-        <span class=${tw`mr-1 w-12`}>${label}</span>
-        ${text}
-        <span>
-            <sl-tooltip style="--show-delay: 500ms">
-                <span class=${tw`text-xs`} slot="content">
-                Click to ${
-                  this.isExpanded ? 'hide' : 'show'
-                } content for this clause.<br>
-                <kbd>Shift</kbd>+click to ${
-                  this.isExpanded ? 'hide' : 'show'
-                } content for all clauses
-                </span>
-                <stencila-icon-button class=${tw`ml-1 rotate-${
-                  this.isExpanded ? '90' : '0'
-                } transition-transform`} name=${`chevron-right`} @click=${(
-      event: KeyboardEvent
-    ) => {
-      this.isExpanded = !this.isExpanded
-      if (event.shiftKey) {
-        clauses.forEach((sibling) => (sibling.isExpanded = this.isExpanded))
+    const iconElem = html`<span
+      class=${tw`flex items-center text-base mx-2 p-1 ${
+        isActive ? 'rounded-full border(& violet-300) bg-violet-100' : ''
+      }`}
+    >
+      <stencila-icon name=${iconName}></stencila-icon>
+    </span>`
+
+    const labelElem = html`<span class=${tw`mr-1 w-12`}>${label}</span>`
+
+    const textEditor = html`<stencila-code-editor
+      class=${tw`min-w-0 w-full rounded overflow-hidden border(& violet-200) focus:border(& violet-400) focus:ring(2 violet-100) bg-violet-50 font-normal`}
+      language=${this.programmingLanguage}
+      single-line
+      line-wrapping
+      no-controls
+      ?read-only=${readOnly}
+      @stencila-document-patch=${(event: CustomEvent) => {
+        const patch = event.detail as Patch
+
+        // If the `text` is currently empty and this is the last clause (i.e. and "else")
+        // then request a rerender to make it an elif
+        if (this.isLast) {
+          // Set `text` to trigger update and recalculation of `isElse`
+          const editor = event.target as StencilaCodeEditor
+          this.text = editor.getCode()
+        }
+
+        // Emit patch using override above
+        event.stopPropagation()
+        this.emitPatch(patch)
+      }}
+      @stencila-ctrl-enter=${() => this.execute()}
+    >
+      <code slot="code">${this.text}</code>
+    </stencila-code-editor>`
+
+    const programmingLanguageMenu = html`<stencila-executable-language
+      class=${tw`ml-2 text(base gray-500)`}
+      programming-language=${this.programmingLanguage}
+      guess-language=${this.guessLanguage == 'true'}
+      exclude='["tailwind"]'
+      color="violet"
+      ?disabled=${readOnly}
+      @stencila-document-patch=${(event: CustomEvent) => {
+        // Update `this.programmingLanguage` (and `guessLanguage` for completeness)
+        // so that the code editor language updates
+        const elem = event.target as StencilaExecutableLanguage
+        this.programmingLanguage = elem.programmingLanguage
+        this.guessLanguage = elem.guessLanguage.toString()
+        // Emit patch using override above
+        event.stopPropagation()
+        this.emitPatch(event.detail)
+      }}
+    ></stencila-executable-language>`
+
+    const moveUp = (event: KeyboardEvent) => {
+      if (this.previousElementSibling) {
+        this.parentElement?.insertBefore(this, this.previousElementSibling)
       }
-    }}></stencila-icon></sl-tooltip>
-        </span>
-      </div>
+      this.requestUpdateAll()
 
-      <div part="content" class=${tw`p-2 ${
-        isLast ? '' : 'border(b violet-200)'
-      } ${this.isActive || this.isExpanded || 'hidden'}`}>
-        <slot name="content"></slot>
+      this.emitOperations({
+        type: 'Move',
+        from: [this.index],
+        to: [this.index - 1],
+        items: 1,
+      })
+    }
+
+    const moveDown = (event: KeyboardEvent) => {
+      if (this.nextElementSibling) {
+        this.parentElement?.insertBefore(this.nextElementSibling, this)
+      }
+      this.requestUpdateAll()
+
+      this.emitOperations({
+        type: 'Move',
+        from: [this.index],
+        to: [this.index + 1],
+        items: 1,
+      })
+    }
+
+    const moveButton = !readOnly
+      ? html`<span
+          class=${tw`flex justify-between items-center h-6 ml-2 rounded-full outline-none bg-violet-200(hover:& focus:&) focus:ring(1 violet-300)`}
+          tabindex="0"
+          @keydown=${(event: KeyboardEvent) => {
+            const retainFocus = () => (event.target as HTMLElement).focus()
+            if (event.key == 'ArrowUp' && !this.isFirst) {
+              event.preventDefault()
+              moveUp(event)
+              retainFocus()
+            } else if (event.key == 'ArrowDown' && !this.isLast) {
+              event.preventDefault()
+              moveDown(event)
+              retainFocus()
+            }
+          }}
+        >
+          <stencila-icon
+            name="arrow-up"
+            class=${tw`w-3 ${
+              this.isFirst ? 'text-gray-300' : 'cursor-n-resize'
+            }`}
+            ?aria-disabled=${this.isFirst}
+            @click=${(event: KeyboardEvent) => !this.isFirst && moveUp(event)}
+          ></stencila-icon>
+          <stencila-icon
+            name="arrow-down"
+            class=${tw`w-3 ${
+              this.isLast ? 'text-gray-300' : 'cursor-s-resize'
+            }`}
+            ?aria-disabled=${this.isLast}
+            @click=${(event: KeyboardEvent) => !this.isLast && moveDown(event)}
+          ></stencila-icon>
+        </span>`
+      : ''
+
+    const remove = () => {
+      this.emitOperations({
+        type: 'Remove',
+        address: [],
+        items: 1,
+      })
+
+      const parent = this.parentElement!
+
+      this.remove()
+      ;[...parent.children].forEach((clause: StencilaIfClause) =>
+        clause.requestUpdate()
+      )
+    }
+
+    const removeButton = !readOnly
+      ? html`<stencila-icon-button
+          name="x-circle"
+          color="violet"
+          adjust="ml-2"
+          @keydown=${(event: KeyboardEvent) =>
+            event.key == 'Enter' && event.shiftKey && remove()}
+          @click=${() => remove()}
+        >
+        </stencila-icon-button>`
+      : ''
+
+    const expandButton = html`<stencila-icon-button
+      name="chevron-right"
+      color="violet"
+      adjust=${`ml-2 rotate-${
+        this.isExpanded ? '90' : '0'
+      } transition-transform`}
+      @click=${(event: KeyboardEvent) => {
+        this.isExpanded = !this.isExpanded
+        if (event.shiftKey) {
+          const clauses = this.getIfClauses()
+          clauses.forEach((clause) => (clause.isExpanded = this.isExpanded))
+        }
+      }}
+      @keydown=${(event: KeyboardEvent) => {
+        if (
+          event.key == 'Enter' ||
+          (event.key == 'ArrowUp' && this.isExpanded) ||
+          (event.key == 'ArrowDown' && !this.isExpanded)
+        ) {
+          event.preventDefault()
+          this.isExpanded = !this.isExpanded
+        }
+        if (event.shiftKey) {
+          const clauses = this.getIfClauses()
+          clauses.forEach((clause) => (clause.isExpanded = this.isExpanded))
+        }
+      }}
+    >
+    </stencila-icon-button>`
+
+    const errorsContainer = html`<div
+      part="errors"
+      class=${tw`border(t violet-200) ${this.hasErrors || 'hidden'}`}
+    >
+      <slot
+        name="errors"
+        @slotchange=${(event: Event) => this.onErrorsSlotChange(event)}
+      ></slot>
+    </div>`
+
+    const contentContainer = html`<div
+      part="content"
+      class=${tw`border(t violet-200) p-2 ${this.isExpanded || 'hidden'}`}
+    >
+      ${!this.hasContent
+        ? html`<p class=${tw`text(center gray-300)`}>No content</p>`
+        : ''}
+      <slot
+        name="content"
+        @slotchange=${(event: Event) => this.onContentSlotChange(event)}
+      ></slot>
+    </div>`
+
+    return html`<div part="base" class=${tw`border(b violet-200)`}>
+      <div
+        part="header"
+        class=${tw`flex justify-between items-center bg-violet-50 p-1 font(mono bold) text(sm violet-800)`}
+      >
+        ${iconElem} ${labelElem} ${textEditor} ${programmingLanguageMenu}
+        ${moveButton} ${removeButton} ${expandButton}
       </div>
+      ${errorsContainer} ${contentContainer}
     </div>`
   }
 }
--- web/src/components/nodes/if.ts
+++ web/src/components/nodes/if.ts
@@ -1,9 +1,12 @@
+import HtmlFragment from 'html-fragment'
 import { html } from 'lit'
 import { customElement } from 'lit/decorators'
+import { currentMode, Mode } from '../../mode'
 
 import { twSheet } from '../utils/css'
 import StencilaExecutable from './executable'
 import './if-clause'
+import StencilaIfClause from './if-clause'
 
 const { tw, sheet } = twSheet()
 
@@ -15,12 +18,53 @@ export default class StencilaIf extends StencilaExecutable {
   static styles = sheet.target
 
   protected render() {
+    const add = () => {
+      const clauses = (
+        this.renderRoot.querySelector('slot[name=clauses]') as HTMLSlotElement
+      ).assignedElements({
+        flatten: true,
+      })[0]
+
+      this.emitOperations({
+        type: 'Add',
+        address: ['clauses', clauses.childElementCount],
+        length: 1,
+        value: StencilaIfClause.json,
+      })
+
+      clauses.appendChild(HtmlFragment(StencilaIfClause.html))
+      ;[...clauses.children].forEach((clause: StencilaIfClause) =>
+        clause.requestUpdate()
+      )
+    }
+
+    const mode = currentMode()
+    const readOnly = mode < Mode.Alter || mode === Mode.Edit
+
+    const addButton = !readOnly
+      ? html`<stencila-icon-button
+          name="plus-lg"
+          color="violet"
+          adjust="ml-3"
+          @keydown=${(event: KeyboardEvent) => event.key == 'Enter' && add()}
+          @click=${() => add()}
+        >
+        </stencila-icon-button>`
+      : ''
+
     return html`<div
       part="base"
       class=${tw`my-4 rounded border(& violet-200) overflow-hidden`}
     >
       <div part="clauses">
         <slot name="clauses"></slot>
+      </div>
+
+      <div
+        part="footer"
+        class=${tw`flex justify-between items-center bg-violet-50 p-1 font(mono bold) text(sm violet-800)`}
+      >
+        ${addButton}
       </div>
     </div>`
   }
