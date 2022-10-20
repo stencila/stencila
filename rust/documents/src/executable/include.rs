db7424bf60922a11f0de38b3ad007a7ba1b4437e
Nokome Bentley <nokome@stenci.la>
2022-10-20 13:25:41 +1300

    chore(Include nodes): Add web component and update execution

--- rust/documents/src/executable/include.rs
+++ rust/documents/src/executable/include.rs
@@ -1,5 +1,3 @@
-use std::path::Path;
-
 use common::{async_trait::async_trait, eyre::Result};
 use graph_triples::{
     resources::{self, ResourceDigest},
@@ -7,10 +5,10 @@ use graph_triples::{
 };
 
 use node_address::Address;
-use node_patch::diff_address;
+use node_patch::diff_id;
 use node_transform::Transform;
 use path_utils::merge;
-use stencila_schema::{BlockContent, Include, InlineContent, Paragraph};
+use stencila_schema::{CodeError, Include};
 
 use crate::{
     assert_id,
@@ -18,72 +16,75 @@ use crate::{
     register_id,
 };
 
-fn include_digest(include: &Include, path: &Path) -> ResourceDigest {
-    let mut content_string = include.source.clone();
-    if let Some(media_type) = include.media_type.as_deref() {
-        content_string.push_str(media_type);
-    }
-    if let Some(select) = include.select.as_deref() {
-        content_string.push_str(select);
-    }
-
-    let mut digest = ResourceDigest::from_strings(&content_string, None);
-    let dependency_digest =
-        ResourceDigest::from_path(path, include.media_type.as_ref().map(|str| str.as_str()));
-    digest.dependencies_digest = dependency_digest.content_digest;
-
-    digest
-}
-
 #[async_trait]
 impl Executable for Include {
+    /// Assemble an `Include` node
+    ///
+    /// Registers the node `id`, and decodes the content of `source` into `content`.
+    /// The `compile_digest` is used to ensure that this is not done unnecessarily.
+    ///
+    /// Generates a patch for the change in content (unlike for `compile()` and `execute()`
+    /// this is not done automatically for `assemble()` for efficiency reasons).
+    ///
+    /// Because reading the `source` from disk and calculating a hash is relatively
+    /// resource intensive, this is only done during `assemble`, not during `compile`.
     async fn assemble(
         &mut self,
         address: &mut Address,
         context: &mut AssembleContext,
     ) -> Result<()> {
-        register_id!("in", self, address, context);
+        let id = register_id!("in", self, address, context);
 
+        // Calculate a resource digest using the `source`, `media_type` and `select` properties
+        let mut resource_digest = digest_from_properties(self);
+
+        // Add the digest of the content of the source file as the dependencies digest
         let path = merge(&context.path, &self.source);
-        let digest = include_digest(self, &path);
+        let dependency_digest =
+            ResourceDigest::from_path(&path, self.media_type.as_ref().map(|str| str.as_str()));
+        resource_digest.dependencies_digest = dependency_digest.content_digest;
 
+        // Determine if it is necessary to update content (i.e. has the `compile_digest` changed since last time)
         let should_decode = self.content.is_none()
             || match self.compile_digest.as_deref() {
-                Some(compile_digest) => digest.to_cord() != *compile_digest,
+                Some(compile_digest) => resource_digest.to_cord() != *compile_digest,
                 None => true,
             };
         if should_decode {
+            // Clone self for patch
+            let before = self.clone();
+
             // Decode block content from the source
-            let mut content = match codecs::from_path(
+            let result = codecs::from_path(
                 &path,
                 self.media_type
                     .as_ref()
                     .map(|media_type| media_type.as_str()),
                 None,
             )
-            .await
-            {
-                Ok(content) => Some(content.to_blocks()),
-                Err(error) => Some(vec![BlockContent::Paragraph(Paragraph {
-                    content: vec![InlineContent::String(error.to_string())],
-                    ..Default::default()
-                })]),
+            .await;
+            match result {
+                Ok(content) => {
+                    // Decoding was OK so assemble to content and clear errors
+                    let mut content = content.to_blocks();
+                    content
+                        .assemble(&mut address.add_name("content"), context)
+                        .await?;
+                    self.content = Some(content);
+                    self.errors = None;
+                }
+                Err(error) => {
+                    // Decoding failed so clear content and populate errors
+                    self.content = None;
+                    self.errors = Some(vec![CodeError {
+                        error_message: error.to_string(),
+                        ..Default::default()
+                    }]);
+                }
             };
 
-            // Assemble the content as well
-            content
-                .assemble(&mut address.add_name("content"), context)
-                .await?;
-
             // Generate a patch for changes to self
-            let mut patch = diff_address(
-                address.clone(),
-                self,
-                &Include {
-                    content,
-                    ..self.to_owned()
-                },
-            );
+            let mut patch = diff_id(&id, &before, self);
             patch.remove_overwrite_derived();
             context.patches.push(patch)
         }
@@ -91,32 +92,51 @@ impl Executable for Include {
         Ok(())
     }
 
+    /// Compile an `Include` node
+    ///
+    /// Declares this node as a resource with a relation to the source file.
+    /// Note that there is no need to compile the `content` since any executable
+    /// nodes within it should have been registered in `assemble`.
+    ///
+    /// TODO: If there is a change in the compile digest (ie. `source`, `media_type`
+    /// or `select` have changed) then trigger an `assemble` of the document.
     async fn compile(&mut self, context: &mut CompileContext) -> Result<()> {
         let id = assert_id!(self)?;
 
-        // Add resource relations to the context
         let path = merge(&context.path, &self.source);
-        let digest = include_digest(self, &path);
         let resource = resources::node(&context.path, id, "Include");
         let object = resources::file(&path);
         let relations = vec![(Relation::Includes, object)];
+        let execute_pure = Some(true); // Never has side effects
+        let compile_digest = self
+            .compile_digest
+            .as_ref()
+            .map(|cord| ResourceDigest::from_cord(cord));
         let resource_info = ResourceInfo::new(
             resource,
             Some(relations),
             self.execute_auto.clone(),
-            Some(true), // Never has side effects
+            execute_pure,
             None,
-            Some(digest),
+            compile_digest,
             None,
             None,
         );
         context.resource_infos.push(resource_info);
 
-        // Compile any included content
-        if let Some(content) = &mut self.content {
-            content.compile(context).await?
-        }
-
         Ok(())
     }
+}
+
+/// Calculate a resource digest based on the `source`, `media_type` and `select` properties
+/// of an `Include` node
+fn digest_from_properties(include: &Include) {
+    let mut content_str = include.source.clone();
+    if let Some(media_type) = include.media_type.as_deref() {
+        content_str.push_str(media_type);
+    }
+    if let Some(select) = include.select.as_deref() {
+        content_str.push_str(select);
+    }
+    ResourceDigest::from_strings(&content_str, None)
 }
--- web/src/components/nodes/include.ts
+++ web/src/components/nodes/include.ts
@@ -1,18 +1,220 @@
-import { html } from 'lit'
-import { customElement } from 'lit/decorators'
+import { css, html } from 'lit'
+import { customElement, property, state } from 'lit/decorators'
 
+import '@shoelace-style/shoelace/dist/components/input/input'
+
+import '../base/tag'
 import { twSheet } from '../utils/css'
 import StencilaExecutable from './executable'
+import SlInput from '@shoelace-style/shoelace/dist/components/input/input'
+import { TW } from 'twind'
 
 const { tw, sheet } = twSheet()
 
+/**
+ * A component representing a Stencila `Include` node and the base
+ * for the derived `Call` node
+ *
+ * @slot content The `Include.content` property
+ * @slot errors The `Executable.errors` property
+ */
 @customElement('stencila-include')
 export default class StencilaInclude extends StencilaExecutable {
-  static styles = [sheet.target]
+  static styles = [
+    sheet.target,
+    css`
+      sl-input::part(base) {
+        font-family: monospace;
+        border-color: rgba(221, 214, 254, var(--tw-border-opacity));
+      }
+    `,
+  ]
 
-  render() {
-    return html`<span
-      ><stencila-tag color="indigo">${this.id}</stencila-tag></span
-    >`
+  /**
+   * The `Include.source` property
+   */
+  @property({ reflect: true })
+  source: string
+
+  /**
+   * The `Include.mediaType` property
+   */
+  @property({ reflect: true })
+  mediaType: string
+
+  /**
+   * The `Include.select` property
+   */
+  @property({ reflect: true })
+  select: string
+
+  /**
+   * Whether the `Include.content` property is visible
+   */
+  @state()
+  protected isExpanded: boolean = true
+
+  /**
+   * Whether the `Include.content` property has content
+   */
+  @state()
+  private hasContent: boolean = false
+
+  /**
+   * An observer to update `hasContent`
+   */
+  private contentObserver: MutationObserver
+
+  /**
+   * Handle a change to the `content` slot
+   *
+   * Initializes `hasContent` and a `MutationObserver` to watch for changes
+   * to the number of elements in the slot and change `hasContent` accordingly.
+   */
+  private onContentSlotChange(event: Event) {
+    const contentElem = (event.target as HTMLSlotElement).assignedElements({
+      flatten: true,
+    })[0]
+
+    this.hasContent = contentElem.childElementCount > 0
+
+    this.contentObserver = new MutationObserver(() => {
+      this.hasContent = contentElem.childElementCount > 0
+    })
+    this.contentObserver.observe(contentElem, {
+      childList: true,
+    })
+  }
+
+  protected renderSourceInput(tw: TW) {
+    const replace = (event: Event): boolean => {
+      const input = event.target as SlInput
+      if (input.reportValidity()) {
+        this.source = (event.target as HTMLInputElement).value
+        this.emitReplaceOperations('source')
+        return true
+      }
+      return false
+    }
+
+    return html`<sl-input
+      class=${tw`min-w-0 w-full`}
+      size="small"
+      placeholder="path/to/file.ext"
+      required="true"
+      value=${this.source}
+      ?disabled=${this.isReadOnly()}
+      @sl-change=${replace}
+      @sl-blur=${replace}
+      @keypress=${(event: KeyboardEvent) => {
+        if (event.key == 'Enter' && event.ctrlKey) {
+          event.preventDefault()
+          if (replace(event)) {
+            this.compile()
+          }
+        }
+      }}
+    ></sl-input>`
+  }
+
+  protected renderSelectInput(tw: TW) {
+    const replace = (event: Event): boolean => {
+      const input = event.target as SlInput
+      if (input.reportValidity()) {
+        this.select = (event.target as HTMLInputElement).value
+        this.emitReplaceOperations('select')
+        return true
+      }
+      return false
+    }
+
+    return html`<sl-input
+      class=${tw`min-w-0 w-full`}
+      size="small"
+      value=${this.select}
+      ?disabled=${this.isReadOnly()}
+      @sl-change=${replace}
+      @sl-blur=${replace}
+      @keypress=${(event: KeyboardEvent) => {
+        if (event.key == 'Enter' && event.ctrlKey) {
+          event.preventDefault()
+          if (replace(event)) {
+            this.compile()
+          }
+        }
+      }}
+    ></sl-input>`
+  }
+
+  protected renderExpandButton(tw: TW, color: string) {
+    return html`<stencila-icon-button
+      name="chevron-right"
+      color=${color}
+      adjust=${`ml-2 rotate-${this.isExpanded ? 90 : 0} transition-transform`}
+      @click=${() => {
+        this.isExpanded = !this.isExpanded
+      }}
+      @keydown=${(event: KeyboardEvent) => {
+        if (
+          event.key == 'Enter' ||
+          (event.key == 'ArrowUp' && this.isExpanded) ||
+          (event.key == 'ArrowDown' && !this.isExpanded)
+        ) {
+          event.preventDefault()
+          this.isExpanded = !this.isExpanded
+        }
+      }}
+    >
+    </stencila-icon-button>`
+  }
+
+  protected renderErrorsContainer(tw: TW, color: string) {
+    return html`<div
+      part="errors"
+      class=${tw`border(t ${color}-200) ${this.hasErrors || 'hidden'}`}
+    >
+      <slot
+        name="errors"
+        @slotchange=${(event: Event) => this.onErrorsSlotChange(event)}
+      ></slot>
+    </div>`
+  }
+
+  protected renderContentContainer(tw: TW, color: string) {
+    return html`<div
+      part="content"
+      class=${tw`border(t ${color}-200) p-2 ${this.isExpanded || 'hidden'}`}
+    >
+      ${!this.hasContent
+        ? html`<p class=${tw`text(center gray-300)`}>No content</p>`
+        : ''}
+      <slot
+        name="content"
+        @slotchange=${(event: Event) => this.onContentSlotChange(event)}
+      ></slot>
+    </div>`
+  }
+
+  protected render() {
+    const color = 'purple'
+    return html`<div
+      part="base"
+      class=${tw`my-4 rounded border(& ${color}-200) overflow-hidden`}
+    >
+      <div
+        part="header"
+        class=${tw`flex items-center bg-${color}-100 p-1 font(mono bold) text(sm ${color}-800)`}
+      >
+        <span class=${tw`flex items-center text-base mr-2`}>
+          <stencila-icon name="box-arrow-in-right"></stencila-icon>
+        </span>
+        <span class=${tw`mr-2`}>include</span>
+        ${this.renderSourceInput(tw)}
+        <span class=${tw`mx-2`}>select</span>
+        ${this.renderSelectInput(tw)} ${this.renderExpandButton(tw, color)}
+      </div>
+      ${this.renderErrorsContainer(tw, color)}
+      ${this.renderContentContainer(tw, color)}
+    </div>`
   }
 }
