c603010ac2a65cd9bf4e9834c8b0c5dedad076b2
Nokome Bentley <nokome@stenci.la>
2022-11-10 14:51:06 +1300

    fix(Math utils): Avoid panic caused by backslashes

--- rust/math-utils/src/lib.rs
+++ rust/math-utils/src/lib.rs
@@ -8,12 +8,18 @@ use common::eyre::Result;
  */
 pub fn to_mathml(language: &str, text: &str, block: bool) -> Result<String> {
     Ok(match language {
+        "asciimath" => {
+            // A backslashes causes a panic within the `charred` crate
+            // used by `asciimath_rs`. This removes all backslashes. The user can use
+            // the "backslash" TeX alternative if they need a backslash in th math output.
+            let text = text.replace('\\', "");
+            format!(
+                r#"<math xmlns="http://www.w3.org/1998/Math/MathML" display="{}">{}</math>"#,
+                if block { "block" } else { "inline" },
+                &asciimath_rs::parse(text).to_mathml()
+            )
+        }
         "mathml" => text.to_string(),
-        "asciimath" => format!(
-            r#"<math xmlns="http://www.w3.org/1998/Math/MathML" display="{}">{}</math>"#,
-            if block { "block" } else { "inline" },
-            &asciimath_rs::parse(text).to_mathml()
-        ),
         _ => latex_to_mathml(
             text,
             if block {
