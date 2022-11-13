357321f37fef95578c7113b2ba04efa22102f54c
Nokome Bentley <nokome@stenci.la>
2022-11-14 11:21:31 +1300

    refactor(Interact mode): Remove unimplemented  filter node

--- web/src/components/nodes/filter.ts
+++
@@ -1,6 +0,0 @@
-import { customElement } from 'lit/decorators'
-
-import StencilaExecutable from './executable'
-
-@customElement('stencila-filter')
-export default class StencilaFilter extends StencilaExecutable {}
--- web/src/modes/interact.ts
+++ web/src/modes/interact.ts
@@ -1,9 +1,8 @@
 import '../components/nodes/button'
-import '../components/nodes/filter'
 import '../components/nodes/form'
 import '../components/nodes/parameter'
 
 import { waitForElems } from '../utils/curtain'
 
 import('./dynamic.ts')
-waitForElems(['button', 'filter', 'form', 'parameter'])
+waitForElems(['button', 'form', 'parameter'])
