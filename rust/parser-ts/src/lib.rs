87b5e79a96e5c89a6b8484b60e7765e7e8e8574a
Nokome Bentley <nokome@stenci.la>
2022-11-19 14:21:36 +1300

    refactor(JavaScript and TypeScript parsers): Move to using `ParseInfo`

--- rust/parser-ts/src/lib.rs
+++ rust/parser-ts/src/lib.rs
@@ -1,10 +1,14 @@
-use std::path::Path;
+use std::{
+    env::current_dir,
+    path::{Path, PathBuf},
+};
 
 use parser_treesitter::{
     common::{eyre::Result, once_cell::sync::Lazy},
     formats::Format,
-    graph_triples::{relations, resources, Resource, ResourceInfo},
-    resource_info, Parser, ParserTrait, TreesitterParser,
+    parse_info,
+    utils::declares_variable,
+    ParseInfo, Parser, ParserTrait, TreesitterParser,
 };
 
 /// Tree-sitter based parser for JavaScript
@@ -35,57 +39,61 @@ impl ParserTrait for TsParser {
         }
     }
 
-    fn parse(resource: Resource, path: &Path, code: &str) -> Result<ResourceInfo> {
+    fn parse(code: &str, path: Option<&Path>) -> Result<ParseInfo> {
         let code = code.as_bytes();
         let tree = PARSER_TS.parse(code);
 
+        let mut dependencies = Vec::new();
+        let mut dependents = Vec::new();
+
         // Query the tree for typed patterns defined in this module
         let matches = PARSER_TS.query(code, &tree);
-        let relations_typed = matches
-            .iter()
-            .filter_map(|(pattern, captures)| match pattern {
-                0 => {
-                    // Assigns a symbol at the top level of the module
-                    let range = captures[0].range;
-                    let name = captures[0].text.clone();
-                    let type_annotation = captures[1].node;
-                    let type_string = type_annotation
-                        .named_child(0)
-                        .and_then(|node| node.utf8_text(code).ok())
-                        .unwrap_or_default();
-                    let kind = match type_string {
-                        "boolean" => "Boolean",
-                        "number" => "Number",
-                        "string" => "String",
-                        "object" => "Object",
-                        _ => {
-                            if type_string.starts_with("Array<") {
-                                "Array"
-                            } else if type_string.starts_with("Record<string") {
-                                "Object"
-                            } else {
-                                ""
-                            }
+        for (pattern, captures) in matches.iter() {
+            if *pattern == 0 {
+                // Assigns a symbol at the top level of the module
+                let range = captures[0].range;
+                let name = captures[0].text.clone();
+                let type_annotation = captures[1].node;
+                let type_string = type_annotation
+                    .named_child(0)
+                    .and_then(|node| node.utf8_text(code).ok())
+                    .unwrap_or_default();
+                let kind = match type_string {
+                    "boolean" => Some("Boolean".to_string()),
+                    "number" => Some("Number".to_string()),
+                    "string" => Some("String".to_string()),
+                    "object" => Some("Object".to_string()),
+                    _ => {
+                        if type_string.starts_with("Array<") {
+                            Some("Array".to_string())
+                        } else if type_string.starts_with("Record<string") {
+                            Some("Object".to_string())
+                        } else {
+                            None
                         }
-                    };
-                    Some((
-                        relations::declares(range),
-                        resources::symbol(path, &name, kind),
-                    ))
-                }
-                _ => None,
-            });
+                    }
+                };
+                dependents.push(declares_variable(&name, path, kind, Some(range)))
+            }
+        }
 
         // Query the tree for untyped patterns defined in the JavaScript module
         let matches = PARSER_JS.query(code, &tree);
-        let relations_untyped = matches.iter().filter_map(|(pattern, capture)| {
-            parser_js::handle_patterns(path, code, pattern, capture)
-        });
+        let path_buf = path
+            .map(PathBuf::from)
+            .unwrap_or_else(|| current_dir().expect("Should be able to get pwd"));
+        for (pattern, captures) in matches.iter() {
+            parser_js::handle_pattern(
+                code,
+                &path_buf,
+                pattern,
+                captures,
+                &mut dependencies,
+                &mut dependents,
+            )
+        }
 
-        let relations = relations_typed.chain(relations_untyped).collect();
-
-        let resource_info = resource_info(
-            resource,
+        let parse_info = parse_info(
             path,
             Self::spec().language,
             code,
@@ -93,9 +101,10 @@ impl ParserTrait for TsParser {
             &["comment"],
             matches,
             0,
-            relations,
+            dependencies,
+            dependents,
         );
-        Ok(resource_info)
+        Ok(parse_info)
     }
 }
 
@@ -110,9 +119,8 @@ mod tests {
         snapshot_fixtures("fragments/ts/*.ts", |path| {
             let code = std::fs::read_to_string(path).expect("Unable to read");
             let path = path.strip_prefix(fixtures()).expect("Unable to strip");
-            let resource = resources::code(path, "", "SoftwareSourceCode", Format::TypeScript);
-            let resource_info = TsParser::parse(resource, path, &code).expect("Unable to parse");
-            assert_json_snapshot!(resource_info);
+            let parse_info = TsParser::parse(&code, Some(path)).expect("Unable to parse");
+            assert_json_snapshot!(parse_info);
         })
     }
 
@@ -121,9 +129,8 @@ mod tests {
         snapshot_fixtures("fragments/js/*.js", |path| {
             let code = std::fs::read_to_string(path).expect("Unable to read");
             let path = path.strip_prefix(fixtures()).expect("Unable to strip");
-            let resource = resources::code(path, "", "SoftwareSourceCode", Format::JavaScript);
-            let resource_info = TsParser::parse(resource, path, &code).expect("Unable to parse");
-            assert_json_snapshot!(resource_info);
+            let parse_info = TsParser::parse(&code, Some(path)).expect("Unable to parse");
+            assert_json_snapshot!(parse_info);
         })
     }
 }
