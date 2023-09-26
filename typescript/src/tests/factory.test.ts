eb05c05a3faba8d44363d74c63cb91a6cb72ff53
Nokome Bentley <nokome@stenci.la>
2023-09-26 20:06:46 +1300

    test(TypeScript): Add test for union factory

--- typescript/src/tests/factory.test.ts
+++ typescript/src/tests/factory.test.ts
@@ -4,9 +4,11 @@ import {
   Paragraph,
   Person,
   article,
+  block,
   organization,
   paragraph,
   person,
+  subscript,
   text,
 } from "../index.js";
 
@@ -33,4 +35,19 @@ test("new Article with options", () => {
 
   // @ts-expect-error type of author unknown
   expect(n.authors?.[0].affiliations[0]).toBeInstanceOf(Organization);
+});
+
+test("new block", () => {
+  const p1 = block({
+    type: "Paragraph",
+    content: [],
+  });
+  expect(p1).toBeInstanceOf(Paragraph);
+
+  const p2 = block(paragraph([]));
+  expect(p2).toBeInstanceOf(Paragraph);
+
+  expect(() => block(subscript([]))).toThrowError(
+    "Unexpected type for Block: Subscript"
+  );
 });
