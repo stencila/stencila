f9dddc912ce0f0662b47c5129b9e625911d4ff08
Nokome Bentley <nokome@stenci.la>
2023-10-09 15:54:36 +1300

    feat(Schema): Implement `Text::from` as a convenience

--- rust/schema/src/implem/text.rs
+++ rust/schema/src/implem/text.rs
@@ -1,9 +1,18 @@
-use crate::{prelude::*, Text};
+use crate::{prelude::*, Cord, Text};
 
 impl Text {
     pub fn to_jats_special(&self) -> (String, Losses) {
         use codec_jats_trait::encode::escape;
 
         (escape(&self.value.0).to_string(), Losses::none())
+    }
+}
+
+impl<S> From<S> for Text
+where
+    S: AsRef<str>,
+{
+    fn from(value: S) -> Self {
+        Self::new(Cord::new(value))
     }
 }
