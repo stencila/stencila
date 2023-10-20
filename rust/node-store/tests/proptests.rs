a2359bf5e22522d29e9d06628f9738f1024f9e26
Nokome Bentley <nokome@stenci.la>
2023-10-20 14:09:51 +1300

    test(Node store): Add proptests

---
+++ rust/node-store/tests/proptests.rs
@@ -0,0 +1,30 @@
+//! Property-based tests of roundtrip dump/load of nodes to/from Automerge store
+
+#![allow(unused_imports)]
+
+use common::eyre::Result;
+use common_dev::{
+    pretty_assertions::assert_eq,
+    proptest::prelude::{proptest, ProptestConfig},
+};
+use node_store::{ReadNode, WriteNode, WriteStore};
+use schema::Article;
+
+#[cfg(any(
+    feature = "proptest-min",
+    feature = "proptest-low",
+    feature = "proptest-high",
+    feature = "proptest-max"
+))]
+proptest! {
+    #![proptest_config(ProptestConfig::with_cases(100))]
+
+    #[test]
+    fn article(node: Article) {
+        let mut store = WriteStore::default();
+        node.dump(&mut store).unwrap();
+        let roundtrip = Article::load_without_ids(&store).unwrap();
+
+        assert_eq!(roundtrip, node);
+    }
+}
