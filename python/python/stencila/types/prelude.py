9586e75245852b7c2945d4ef667ff6e431ac78e3
Nokome Bentley <nokome@stenci.la>
2023-10-26 17:38:56 +1300

    fix(Python): Provide `StrEnum` for Python less than 3.11

--- python/python/stencila/types/prelude.py
+++ python/python/stencila/types/prelude.py
@@ -1,4 +1,10 @@
-from enum import StrEnum
+import sys
+
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+    from strenum import StrEnum
+
 from typing import Any, ForwardRef, Literal, List, Optional, Union
 
 from dataclasses import dataclass, field
