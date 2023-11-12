462a694e161d038df74306b03716c180de101b2e
Nokome Bentley <nokome@stenci.la>
2023-11-12 19:51:56 +1300

    fix(CLI): Add default for `--log-filter`

--- rust/cli/src/cli.rs
+++ rust/cli/src/cli.rs
@@ -33,8 +33,8 @@ pub struct Cli {
     ///
     /// Allows more fine-grained control over which log entries are shown.
     /// To additionally see lower level entries for a specific crates use
-    /// syntax such as `tokio_postgres=debug`.
-    #[arg(long, default_value = "", global = true)]
+    /// syntax such as `tokio=debug`.
+    #[arg(long, default_value = "hyper=info,mio=info,tokio=info,tungstenite=info", global = true)]
     pub log_filter: String,
 
     /// The log format to use
