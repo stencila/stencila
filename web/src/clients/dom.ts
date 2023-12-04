e9179d1747ec3249183d2d66054c9dfc1b414b6f
Nokome Bentley <nokome@stenci.la>
2023-12-04 22:41:51 +1300

    chore(Web DOM client): Log node being morphed

--- web/src/clients/dom.ts
+++ web/src/clients/dom.ts
@@ -7,20 +7,25 @@ import { FormatClient } from "./format";
 /**
  * A read-only client that keeps a DOM element synchronized with the HTML
  * representation of a document on the server
- * 
+ *
  * This class simply extends `FormatClient` and uses `morphdom`
  * to update the DOM element whenever the HTML changes.
  */
 export class DomClient extends FormatClient {
   /**
    * Construct a new `DomClient`
-   * 
+   *
    * @param id The id of the document
    * @param elem The DOM element that will be updated
    */
   constructor(id: DocumentId, elem: HTMLElement) {
     super(id, "read", "html");
 
-    this.subscribe((html) => morphdom(elem, html));
+    this.subscribe((html) => {
+      if (process.env.NODE_ENV === "development") {
+        console.log(`📝 DomClient morphing element`, elem);
+      }
+      morphdom(elem, html);
+    });
   }
 }
