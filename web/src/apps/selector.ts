c4c655cdf96f91c9f957dec2e7c13e6598bc76d6
Simon Winter <simon@dragonfly.co.nz>
2023-12-11 23:30:47 +1300

    fix(selector): selctor component now closes when clicking outside of component

--- web/src/apps/selector.ts
+++ web/src/apps/selector.ts
@@ -1,11 +1,17 @@
-import { css, tw, tx } from "@twind/core";
+import { apply, css, tw } from "@twind/core";
 import { LitElement, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
+import { Ref, createRef, ref } from 'lit/directives/ref.js'
 
 import { withTwind } from "../twind";
 import type { DocumentView } from "../types";
 
-// import "./selector-styles.css";
+/**
+ * This class extends LitElement to include a tw instance.
+ */
+class TWLitElement extends LitElement {
+  protected tw: typeof tw
+}
 
 /**
  * UI selector
@@ -14,11 +20,17 @@ import type { DocumentView } from "../types";
  */
 @customElement("stencila-ui-selector")
 @withTwind()
-export class UISelector extends LitElement {
+export class UISelector extends TWLitElement {
+  /**
+   * Ref to allow us to close the details element when needed.
+   */
+  detailsRef: Ref<HTMLDetailsElement> = createRef();
+
   /**
    * Manages the open state of the open listbox
    */
-  open: boolean = false;
+  @state()
+  private open: boolean = false;
 
   /**
    * Label displayed when listbox is not open
@@ -46,24 +58,33 @@ export class UISelector extends LitElement {
  
 
   override render() {
-    // const x = tx(`border-b-brand-blue`)
     const open = css`
+      &[open] {
+        z-index: 100;
+      }
+
       &[open] summary {
-        border-bottom-color: ${this.tw.theme`colors.brand.blue`};
+        color: ${this.tw.theme('colors.brand.blue')};
+        border-bottom-color: ${this.tw.theme('colors.brand.blue')};
       }
     `
 
     return html`
-    <details role="list" class="p-0 relative block flex-grow ${open}">
+    ${this.renderOverlay()}
+    <details role="list" class="p-0 relative block flex-grow ${open}" ${ref(this.detailsRef)}>
       ${this.renderSummary()}
       <ul role="listbox" class="absolute top-8 bg-gray-aluminium flex flex-col">
         ${this.list.map(
           ([value, desc]) =>
             html`<li class="min-w-fit block whitespace-nowrap">
-              <button data-value="${value}" class="${this.target === value ? 'text-brand-red' : ''}" @click=${(e: Event) => {
-      this.open = !this.open;
-      this.clickEvent && this.clickEvent(e);
-    }}>
+              <button 
+                data-value="${value}"
+                class="${this.target === value ? 'text-brand-red' : ''}"
+                @click=${(e: Event) => {
+                  this.setOpen();
+                  this.clickEvent && this.clickEvent(e);
+                }}
+              >
               ${desc}
               </button>
             </li>`,
@@ -74,6 +95,20 @@ export class UISelector extends LitElement {
   }
 
   private renderSummary() {
+    const styles = apply([
+      "text-sm",
+      "text-gray-aluminum",
+      "leading-none",
+      "select-none",
+      "appearance-none ",
+      "min-w-fit",
+      "py-2 px-4",
+      "bg-white",
+      "border-b-2 border-b-transparent ",
+      "transition-all ease-in-out ",
+      "hover:text-brand-blue hover:border-b-brand-blue",
+    ])
+
     const hideMarker = css`
       &::marker {
         display: none;
@@ -81,6 +116,18 @@ export class UISelector extends LitElement {
       }
     `
 
-    return html`<summary aria-haspopup="listbox" role="button" class="min-w-fit select-none bg-white leading-none text-sm py-2 px-4 text-gray-aluminum appearance-none ${hideMarker} border-b-2 border-b-transparent transition-all ease-in-out hover:text-brand-blue hover:border-b-brand-blue" @click=${() => { this.open = !this.open; console.log(this.open) }}>${this.label}</summary>`
+    return html`<summary aria-haspopup="listbox" role="button" class="${styles} ${hideMarker}" @click=${this.setOpen}>${this.label}</summary>`
+  }
+
+  private renderOverlay() {
+    return this.open ? html`<div class="w-screen h-screen fixed z-50 top-0 left-0 bg-transparent" @click=${this.setOpen}></div>` : null
+  }
+
+  private setOpen() {
+    this.open = !this.open
+
+    if (!this.open) {
+      this.detailsRef.value?.removeAttribute('open')
+    }
   }
 }
