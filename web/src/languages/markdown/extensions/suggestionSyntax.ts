7932d036bbcd8ea6c02be4664ba4c597ccef1015
mike-parkin <mike@dragonfly.co.nz>
2023-12-21 11:18:11 +1300

    feat(syntax): add modify block highlights

--- web/src/languages/markdown/extensions/suggestionSyntax.ts
+++ web/src/languages/markdown/extensions/suggestionSyntax.ts
@@ -9,13 +9,13 @@ import {
   Line,
   BlockParser,
   BlockContext,
+  LeafBlockParser,
+  LeafBlock,
 } from '@lezer/markdown'
 
 const customTags = {
-  insertBase: Tag.define(),
-  insertMark: Tag.define(),
-  modifyBase: Tag.define(),
-  modifyMark: Tag.define(),
+  suggestMark: Tag.define(),
+  suggestBase: Tag.define(),
 }
 
 // TODO ->
@@ -25,22 +25,22 @@ const customTags = {
 
 // Insert --------------------------------------------
 
-const insertBlockDelim = /^\+\+$/
+const insertBlockRe = /^\+\+$/
 
 // insert `NodeSpecs`
 const insertBlock = {
   name: 'InsertBlock',
   block: true,
-  style: customTags.insertBase,
+  style: customTags.suggestBase,
 }
-const insertInline = { name: 'InsertInline', style: customTags.insertBase }
+const insertInline = { name: 'InsertInline', style: customTags.suggestBase }
 const insertBlockMark = {
   name: 'InsertBlockMark',
-  style: customTags.insertMark,
+  style: customTags.suggestMark,
 }
 const insertInlineMark = {
   name: 'InsertInlineMark',
-  style: customTags.insertMark,
+  style: customTags.suggestMark,
 }
 
 const BLOCK_MARK_LENGTH = 2
@@ -56,7 +56,7 @@ const delimiter = { resolve: insertInline.name, mark: insertInlineMark.name }
 class InsertBlockParser implements BlockParser {
   name = insertBlock.name
   parse(cx: BlockContext, line: Line): boolean {
-    if (!insertBlockDelim.test(line.text)) {
+    if (!insertBlockRe.test(line.text)) {
       return false
     }
     const start = cx.lineStart
@@ -67,9 +67,10 @@ class InsertBlockParser implements BlockParser {
         cx.lineStart + BLOCK_MARK_LENGTH
       ),
     ]
+
     // iterate over lines until closing delim found
     while (cx.nextLine()) {
-      if (insertBlockDelim.test(line.text)) {
+      if (insertBlockRe.test(line.text)) {
         elements.push(
           cx.elt(
             insertBlockMark.name,
@@ -120,6 +121,71 @@ class InsertInlineParser implements InlineParser {
 
 // Modify --------------------------------------------
 
+const modOuterRe = /^!!$/
+const modInnerRe = /^!>$/
+
+const modBlockOuter = { name: 'ModifyBlockDelimiter' }
+const modBlockInner = { name: 'ModifyBlockInner' }
+const modInlineOuter = { name: 'ModifyInlineDelimiter' }
+const modInlineInner = { name: 'ModifyInlineInner' }
+const modMark = { name: 'ModifyMark', style: customTags.suggestMark }
+// const modInlineMark = { n}
+
+const MOD_BLOCK_DELIM = '!!'
+const MOD_INNER_DELIM = '!>'
+// const MOD_ININE_OPEN = '{!!'
+// const MOD_ININE_CLOSE = '!!}'
+
+const parseModifyLeaf = (
+  cx: BlockContext,
+  leaf: LeafBlock,
+  nodeType: string,
+  mark: string
+): boolean => {
+  const start = leaf.start
+  const end = start + mark.length
+  if (start >= end) {
+    return false
+  }
+  console.log('hi')
+  cx.addElement(
+    cx.elt(nodeType, start, end, [cx.elt(modMark.name, start, end)])
+  )
+  console.log('hello')
+  return true
+}
+
+/**
+ * `LeafBlockParser` for the opening and closing delimiter
+ * of a modify block.
+ * eg: `!!`
+ */
+class ModifyBlockParser implements LeafBlockParser {
+  nextLine = () => false
+  finish(cx: BlockContext, leaf: LeafBlock): boolean {
+    try {
+      return parseModifyLeaf(cx, leaf, modBlockOuter.name, MOD_BLOCK_DELIM)
+    } catch {
+      return false
+    }
+  }
+}
+
+class ModifyInnerParser implements LeafBlockParser {
+  nextLine = () => false
+  finish = (cx: BlockContext, leaf: LeafBlock): boolean => {
+    try {
+      return parseModifyLeaf(cx, leaf, modBlockInner.name, MOD_INNER_DELIM)
+    } catch {
+      return false
+    }
+  }
+}
+
+// class ModifyBlockCloseParser implements LeafBlockParser {}
+
+// class ModifyInlineParser implements InlineParser {}
+
 // Replace -------------------------------------------
 
 // Delete --------------------------------------------
@@ -139,8 +205,33 @@ class InsertInlineParser implements InlineParser {
  *  - delete inline `{-- --}`
  */
 const StencilaSuggestionSyntax: MarkdownConfig = {
-  defineNodes: [insertBlock, insertInline, insertBlockMark, insertInlineMark],
-  parseBlock: [new InsertBlockParser()],
+  defineNodes: [
+    insertBlock,
+    insertInline,
+    insertBlockMark,
+    insertInlineMark,
+    modBlockInner,
+    modBlockOuter,
+    modInlineOuter,
+    modInlineInner,
+    modMark,
+  ],
+  parseBlock: [
+    new InsertBlockParser(),
+    // modify block parsers
+    {
+      name: modBlockOuter.name,
+      leaf: (_, leaf) =>
+        modOuterRe.test(leaf.content) ? new ModifyBlockParser() : null,
+      endLeaf: (_, line) => !modOuterRe.test(line.text),
+    },
+    {
+      name: modBlockInner.name,
+      leaf: (_, leaf) =>
+        modInnerRe.test(leaf.content) ? new ModifyInnerParser() : null,
+      endLeaf: (_, line) => !modInnerRe.test(line.text),
+    },
+  ],
   parseInline: [new InsertInlineParser()],
   wrap: parseMixed((node) => {
     if (node.type.name === insertBlock.name) {
@@ -167,7 +258,7 @@ const StencilaSuggestionSyntax: MarkdownConfig = {
 
 const highlightStyles: TagStyle[] = [
   {
-    tag: customTags.insertMark,
+    tag: customTags.suggestMark,
     color: 'red',
   },
 ]
