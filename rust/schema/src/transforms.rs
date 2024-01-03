ac386fb4b081f9e27b39742b508b93104dd04c71
Nokome Bentley <nokome@stenci.la>
2024-01-04 09:11:52 +1300

    feat(schema): add more transforms

--- rust/schema/src/transforms.rs
+++ rust/schema/src/transforms.rs
@@ -5,7 +5,7 @@
 //! trait. If possible, prefer to implement `From`, or `Into` in the `implem/*.rs`
 //! file for the type, rather than add another function here.
 
-use crate::{Block, Inline, Node};
+use crate::{Block, CodeBlock, CodeChunk, CodeExpression, CodeInline, Inline, Node, NodeType};
 
 /// Transform a vector of [`Block`]s into a vector of [`Inline`]s
 pub fn blocks_to_inlines(blocks: Vec<Block>) -> Vec<Inline> {
@@ -13,6 +13,11 @@ pub fn blocks_to_inlines(blocks: Vec<Block>) -> Vec<Inline> {
         .into_iter()
         .flat_map(|block| -> Vec<Inline> { block.into() })
         .collect()
+}
+
+/// Transform a vector of [`Inline`]s into a vector of [`Block`]s
+pub fn inlines_to_blocks(inlines: Vec<Inline>) -> Vec<Block> {
+    vec![inlines.into()]
 }
 
 /// Transform a vector of [`Inline`]s into a vector of [`Node`]s
@@ -29,4 +34,50 @@ pub fn blocks_to_nodes(blocks: Vec<Block>) -> Vec<Node> {
         .into_iter()
         .map(|block| -> Node { block.into() })
         .collect()
+}
+
+/// Transform between inline types if possible
+pub fn transform_inline(inline: Inline, node_type: NodeType) -> Inline {
+    match inline {
+        Inline::CodeInline(inner) => match node_type {
+            NodeType::CodeExpression => Inline::CodeExpression(CodeExpression {
+                code: inner.code,
+                programming_language: inner.programming_language,
+                ..Default::default()
+            }),
+            _ => Inline::CodeInline(inner),
+        },
+        Inline::CodeExpression(inner) => match node_type {
+            NodeType::CodeInline => Inline::CodeInline(CodeInline {
+                code: inner.code,
+                programming_language: inner.programming_language,
+                ..Default::default()
+            }),
+            _ => Inline::CodeExpression(inner),
+        },
+        _ => inline,
+    }
+}
+
+/// Transform between block types if possible
+pub fn transform_block(block: Block, node_type: NodeType) -> Block {
+    match block {
+        Block::CodeBlock(inner) => match node_type {
+            NodeType::CodeChunk => Block::CodeChunk(CodeChunk {
+                code: inner.code,
+                programming_language: inner.programming_language,
+                ..Default::default()
+            }),
+            _ => Block::CodeBlock(inner),
+        },
+        Block::CodeChunk(inner) => match node_type {
+            NodeType::CodeBlock => Block::CodeBlock(CodeBlock {
+                code: inner.code,
+                programming_language: inner.programming_language,
+                ..Default::default()
+            }),
+            _ => Block::CodeChunk(inner),
+        },
+        _ => block,
+    }
 }
