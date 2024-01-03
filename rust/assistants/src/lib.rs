7e1f9ca23fbd4990eb397a89c1adb73bed7e1f9e
Nokome Bentley <nokome@stenci.la>
2024-01-04 09:28:41 +1300

    feat(assistants): recursively perform inner instructions

--- rust/assistants/src/lib.rs
+++ rust/assistants/src/lib.rs
@@ -1,13 +1,20 @@
-use std::{cmp::Ordering, sync::Arc};
+use std::{cmp::Ordering, collections::HashMap, sync::Arc};
 
 use assistant::{
     common::{
-        eyre::{bail, Result},
+        async_recursion::async_recursion,
+        eyre::{bail, eyre, Result},
         futures::future::join_all,
         itertools::Itertools,
         tracing,
+        uuid::Uuid,
     },
-    Assistant, GenerateDetails, GenerateOptions, GenerateOutput, GenerateTask,
+    schema::{
+        walk::{VisitorMut, WalkControl, WalkNode},
+        Block, ExecutionError, Inline, InsertBlock, InsertInline, Node, SuggestionBlockType,
+        SuggestionInlineType,
+    },
+    Assistant, GenerateDetails, GenerateOptions, GenerateOutput, GenerateTask, Instruction, Nodes,
 };
 
 pub use assistant;
@@ -17,14 +24,14 @@ mod testing_db;
 
 /// Get a list of available assistants in descending preference rank
 pub async fn list() -> Vec<Arc<dyn Assistant>> {
-    let futures = (0..=5).into_iter().map(|provider| async move {
+    let futures = (0..=5).map(|provider| async move {
         let (provider, result) = match provider {
             0 => ("Anthropic", assistant_anthropic::list().await),
             1 => ("Google", assistant_google::list().await),
             2 => ("Mistral", assistant_mistral::list().await),
             3 => ("Ollama", assistant_ollama::list().await),
             4 => ("OpenAI", assistant_openai::list().await),
-            5 => ("Stencila", assistant_custom::list().await),
+            5 => ("Stencila", assistant_custom::list()),
             _ => return vec![],
         };
 
@@ -50,33 +57,198 @@ pub async fn list() -> Vec<Arc<dyn Assistant>> {
         .collect_vec()
 }
 
-/// Perform a `GenerateTask` and return the generated content as a string
-pub async fn perform_task(
-    mut task: GenerateTask,
+/// Perform an instruction and return the generated content as a string
+#[tracing::instrument(skip_all)]
+#[async_recursion]
+pub async fn perform_instruction(
+    instruction: Instruction,
+    document: Option<Node>,
     options: &GenerateOptions,
 ) -> Result<(GenerateOutput, GenerateDetails)> {
+    let mut task = GenerateTask::new(instruction, document.clone());
+
     let assistants = list().await;
 
-    // It is tempting to use the position_max iterator method here but, in the case of
-    // ties, that returns the assistant with the higher index (ie. lower preference), whereas
-    // we want the one with the lowest index.
-    let mut best = (0., 0);
-    for (index, assistant) in assistants.iter().enumerate() {
-        let score = assistant.suitability_score(&mut task)?;
-        if score > best.0 {
-            best = (score, index);
+    let assistant = if let Some(assignee) = task.instruction.assignee() {
+        // Get the assistant assigned to the task
+
+        let id = if assignee.contains('/') {
+            assignee.to_string()
+        } else {
+            ["stencila/", assignee].concat()
+        };
+
+        assistants
+            .iter()
+            .find(|assistant| assistant.id() == id)
+            .ok_or_else(|| eyre!("No assistant with id `{id}`"))?
+    } else {
+        // Get the assistant with the highest suitability score for the task
+
+        // It is tempting to use the position_max iterator method here but, in the case of
+        // ties, that returns the assistant with the higher index (ie. lower preference), whereas
+        // we want the one with the lowest index.
+        let mut best = (0., 0);
+        for (index, assistant) in assistants.iter().enumerate() {
+            let score = assistant.suitability_score(&mut task)?;
+            if score > best.0 {
+                best = (score, index);
+            }
+        }
+
+        let (max, index) = best;
+        if max == 0. {
+            bail!("Unable to delegate the task, no assistants with suitable capabilities")
+        }
+
+        assistants
+            .get(index)
+            .ok_or_else(|| eyre!("Best assistant not in list of assistants!?"))?
+    };
+
+    // Perform the task
+    let (mut output, details) = assistant.perform_task(task, options).await?;
+
+    // Walk over any generated nodes and recursively perform any instructions within them
+    if let Some(nodes) = &mut output.nodes {
+        // Collect instructions within the nodes
+        let mut collector = InstructionCollector::default();
+        match nodes {
+            Nodes::Blocks(nodes) => nodes.walk_mut(&mut collector),
+            Nodes::Inlines(nodes) => nodes.walk_mut(&mut collector),
+        }
+
+        // Perform inner instructions in parallel and put results into a hash map
+        // so they can be applied to the instructions
+        let futures = collector.instructions.into_iter().map(|(id, instruction)| {
+            let document = document.clone();
+            async move {
+                (
+                    id,
+                    perform_instruction(instruction, document, options).await,
+                )
+            }
+        });
+        let results = join_all(futures).await.into_iter().collect();
+
+        // Apply the results to the instructions
+        let mut applier = ResultApplier { results };
+        match nodes {
+            Nodes::Blocks(nodes) => nodes.walk_mut(&mut applier),
+            Nodes::Inlines(nodes) => nodes.walk_mut(&mut applier),
         }
     }
 
-    let (max, index) = best;
+    Ok((output, details))
+}
 
-    if max == 0. {
-        bail!("Unable to delegate the task, no assistants with suitable capabilities")
+/// A node visitor which collects instructions
+#[derive(Default)]
+struct InstructionCollector {
+    /// A map of instructions by their id
+    instructions: HashMap<String, Instruction>,
+}
+
+impl VisitorMut for InstructionCollector {
+    fn visit_inline_mut(&mut self, inline: &mut Inline) -> WalkControl {
+        if let Inline::InstructionInline(instruction) = inline {
+            let id = instruction
+                .id
+                .get_or_insert_with(|| Uuid::new_v4().to_string())
+                .clone();
+            let instruction = Instruction::from(instruction.clone());
+            self.instructions.insert(id, instruction);
+        }
+        WalkControl::Continue
     }
 
-    let Some(assistant) = assistants.get(index) else {
-        bail!("Best assistant not in list of assistants!?")
-    };
+    fn visit_block_mut(&mut self, block: &mut Block) -> WalkControl {
+        if let Block::InstructionBlock(instruction) = block {
+            let id = instruction
+                .id
+                .get_or_insert_with(|| Uuid::new_v4().to_string())
+                .clone();
+            let instruction = Instruction::from(instruction.clone());
+            self.instructions.insert(id, instruction);
+        }
+        WalkControl::Continue
+    }
+}
 
-    assistant.perform_task(task, options).await
+/// A node visitor which applies generation results to instructions
+struct ResultApplier {
+    /// A map of generation results by instruction id
+    results: HashMap<String, Result<(GenerateOutput, GenerateDetails)>>,
+}
+
+impl VisitorMut for ResultApplier {
+    fn visit_inline_mut(&mut self, inline: &mut Inline) -> WalkControl {
+        if let Inline::InstructionInline(instruction) = inline {
+            if let Some(result) = instruction
+                .id
+                .as_ref()
+                .and_then(|id| self.results.remove(id))
+            {
+                match result {
+                    Ok((
+                        GenerateOutput {
+                            nodes: Some(nodes), ..
+                        },
+                        ..,
+                    )) => {
+                        instruction.suggestion = Some(SuggestionInlineType::InsertInline(
+                            InsertInline::new(nodes.into_inlines()),
+                        ))
+                    }
+                    Ok(..) => {
+                        instruction.options.execution_errors = Some(vec![ExecutionError::new(
+                            "No nodes in generation result".to_string(),
+                        )]);
+                    }
+                    Err(error) => {
+                        instruction.options.execution_errors =
+                            Some(vec![ExecutionError::new(error.to_string())]);
+                    }
+                }
+            } else {
+                tracing::debug!("Instruction has no id and/or result: {instruction:?}")
+            }
+        }
+        WalkControl::Continue
+    }
+
+    fn visit_block_mut(&mut self, block: &mut Block) -> WalkControl {
+        if let Block::InstructionBlock(instruction) = block {
+            if let Some(result) = instruction
+                .id
+                .as_ref()
+                .and_then(|id| self.results.remove(id))
+            {
+                match result {
+                    Ok((
+                        GenerateOutput {
+                            nodes: Some(nodes), ..
+                        },
+                        ..,
+                    )) => {
+                        instruction.suggestion = Some(SuggestionBlockType::InsertBlock(
+                            InsertBlock::new(nodes.into_blocks()),
+                        ))
+                    }
+                    Ok(..) => {
+                        instruction.options.execution_errors = Some(vec![ExecutionError::new(
+                            "No nodes in generation result".to_string(),
+                        )]);
+                    }
+                    Err(error) => {
+                        instruction.options.execution_errors =
+                            Some(vec![ExecutionError::new(error.to_string())]);
+                    }
+                }
+            } else {
+                tracing::debug!("Instruction has no id and/or result: {instruction:?}")
+            }
+        }
+        WalkControl::Continue
+    }
 }
