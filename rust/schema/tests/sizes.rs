2a871df216c3f637c7a281e6188b5e18e5311edb
Nokome Bentley <nokome@stenci.la>
2024-01-06 09:52:38 +1300

    test(schema): add module for inspecting size of enums and their variants

---
+++ rust/schema/tests/sizes.rs
@@ -0,0 +1,105 @@
+//! Inspect sizes of enums and their variants
+//!
+//! The `sizes` function is not actually a test but rather a way to check that
+//! the size of an enum is not being excessively bloated by
+//! the largest variants.
+//!
+//! Where appropriate properties of large variants should be made
+//! non-core so that they are heap allocated, rather than stack
+//! allocated (which means that they do no inflate the size of the enum).
+//!
+//! Run like this:
+//!
+//!   cargo test -p schema --test sizes -- --nocapture
+
+use schema::*;
+
+macro_rules! sizes {
+    ($($t:ty),*) => {
+        {
+            let mut sizes: Vec<(&str, usize)> = Vec::new();
+            $(
+                sizes.push((stringify!($t), std::mem::size_of::<$t>()));
+            )*
+            sizes.sort_by(|a, b| b.1.cmp(&a.1));
+
+            for (name, size) in sizes {
+                println!("{:<20} {:>6}", name, size);
+            }
+        }
+    };
+}
+
+#[test]
+fn sizes() {
+    sizes!(
+        Block,
+        Admonition,
+        CallBlock,
+        Claim,
+        CodeBlock,
+        CodeChunk,
+        DeleteBlock,
+        Figure,
+        ForBlock,
+        Form,
+        Heading,
+        IfBlock,
+        IncludeBlock,
+        InsertBlock,
+        InstructionBlock,
+        List,
+        MathBlock,
+        ModifyBlock,
+        Paragraph,
+        QuoteBlock,
+        ReplaceBlock,
+        Section,
+        StyledBlock,
+        Table,
+        ThematicBreak
+    );
+
+    println!("\n");
+
+    sizes!(
+        Inline,
+        AudioObject,
+        Button,
+        Cite,
+        CiteGroup,
+        CodeExpression,
+        CodeInline,
+        Date,
+        DateTime,
+        DeleteInline,
+        Duration,
+        Emphasis,
+        ImageObject,
+        InsertInline,
+        InstructionInline,
+        Link,
+        MathInline,
+        MediaObject,
+        ModifyInline,
+        Note,
+        Parameter,
+        QuoteInline,
+        ReplaceInline,
+        StyledInline,
+        Strikeout,
+        Strong,
+        Subscript,
+        Superscript,
+        Text,
+        Time,
+        Timestamp,
+        Underline,
+        VideoObject,
+        Null,
+        Boolean,
+        Integer,
+        UnsignedInteger,
+        Number
+    );
+}
