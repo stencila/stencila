2276dd451ab10a767832da67b18db5610277df59
mike-parkin <mike@dragonfly.co.nz>
2024-01-25 12:10:02 +1300

    fix(tooltip): clone node for source hover view

--- web/src/codemirror/tooltip.ts
+++ web/src/codemirror/tooltip.ts
@@ -36,12 +36,18 @@ const tooltipOnHover = (sourceView: SourceView) =>
         pos,
         above: true,
         create: () => {
+          // fetch id of hovered node, filter out 'Text' nodes
           const { nodeId } = sourceView
             .getNodesAt(pos)
             .filter((node) => node.nodeType !== 'Text')[0]
-          const dom = sourceView.domElement.value.querySelector(
-            `#${nodeId}`
-          ) as HTMLElement
+
+          // create clone of node from the `DomClient`
+          const dom = sourceView.domElement.value
+            .querySelector(`#${nodeId}`)
+            .cloneNode(true) as HTMLElement
+
+          // change id to avoid duplicates
+          dom.setAttribute('id', `tooltip-${nodeId}`)
           return { dom, offset: { x: 10, y: 10 } }
         },
       }
