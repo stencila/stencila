5daf51b06fc21a14a43c3335c5e44b359bb19701
Nokome Bentley <nokome@stenci.la>
2024-02-01 17:09:06 +1300

    fix(web): switch `DirectoryView` to use `ObjectClient`

--- web/src/views/directory.ts
+++ web/src/views/directory.ts
@@ -1,17 +1,16 @@
-import { LitElement, html } from 'lit'
+import type { File, Directory } from '@stencila/types'
+import { LitElement, TemplateResult, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
 import { DirectoryClient } from '../clients/directory'
-import { DomClient } from '../clients/dom'
-import '../nodes/directory'
-import '../nodes/file'
+import { ObjectClient } from '../clients/object'
 import type { DocumentId } from '../types'
 
 /**
  * Tree view of a directory
  *
- * Updates as the directory changes on the files system (i.e. when
- * files or subdirectories are created, deleted or renamed).
+ * Uses the `ObjectClient` to update the view and the directory changes on the
+ * file system (i.e. when files or subdirectories are created, deleted or renamed).
  *
  * Uses the `DirectoryClient` to provide buttons to create, rename,
  * and delete files and subdirectories.
@@ -19,47 +18,64 @@ import type { DocumentId } from '../types'
 @customElement('stencila-directory-view')
 export class DirectoryView extends LitElement {
   /**
-   * The id of the document
+   * The id of the document for the directory
    */
   @property()
   doc: DocumentId
 
   /**
+   * The directory as a JavaScript object
+   *
+   * Synced from the server by the `objectClient`
+   */
+  private directory: Directory
+
+  /**
    * A read-only client which updates the DOM representation of
    * the directory when it changes
    */
-  // @ts-expect-error "is declared but its value is never read"
-  private domClient: DomClient
+  private objectClient: ObjectClient
 
   /**
    * A write-only client which captures custom events for creating,
    * renaming and deleting files and subdirectories and sends them
-   * to the server for handling
+   * to the server for application
    */
   // @ts-expect-error "is declared but its value is never read"
   private directoryClient: DirectoryClient
 
-  /**
-   * Override so that the `DomClient` is instantiated _after_ this
-   * element has a document `[root]` element in its `renderRoot`.
-   */
-  override firstUpdated(changedProperties: Map<string, string | boolean>) {
-    super.firstUpdated(changedProperties)
+  override connectedCallback() {
+    super.connectedCallback()
 
-    this.domClient = new DomClient(
-      this.doc,
-      this.renderRoot.querySelector('[root]')
-    )
+    this.objectClient = new ObjectClient(this.doc)
+    this.objectClient.subscribe((_, { node }) => {
+      this.directory = node as Directory
+      this.requestUpdate()
+    })
 
-    this.directoryClient = new DirectoryClient(
-      this.doc,
-      this.renderRoot.querySelector('[root]')
-    )
+    this.directoryClient = new DirectoryClient(this.doc, this)
   }
 
   override render() {
     return html`<sl-tree class="tree-with-lines tree-with-icons">
-      <stencila-directory root></stencila-directory>
+      ${this.directory ? this.renderDirectory(this.directory) : ''}
     </sl-tree>`
+  }
+
+  private renderDirectory(directory: Directory): TemplateResult {
+    return html`<sl-tree-item>
+      <sl-icon name="folder"></sl-icon> ${directory.name}
+      ${directory.parts.map((part: Directory | File) =>
+        part.type === 'Directory'
+          ? this.renderDirectory(part as Directory)
+          : this.renderFile(part)
+      )}
+    </sl-tree-item>`
+  }
+
+  private renderFile(file: File) {
+    return html`<sl-tree-item
+      ><sl-icon name="file"></sl-icon> ${file.name}</sl-tree-item
+    >`
   }
 }
