a9268b870936172aa51ea8c3aeb7509e9db7408c
Nokome Bentley <nokome@stenci.la>
2024-02-07 10:17:42 +1300

    feat(kernels): show terminate in cli table

--- rust/kernels/src/cli.rs
+++ rust/kernels/src/cli.rs
@@ -5,7 +5,7 @@ use kernel::{
         eyre::Result,
         itertools::Itertools,
     },
-    KernelAvailability, KernelForks, KernelInterrupt, KernelKill,
+    KernelAvailability, KernelForks, KernelInterrupt, KernelKill, KernelTerminate,
 };
 
 use crate::list;
@@ -35,9 +35,10 @@ impl Cli {
                     "Id",
                     "Availability",
                     "Languages",
-                    "Interrupt",
-                    "Kill",
                     "Fork",
+                    "Interrupt",
+                    "Terminate",
+                    "Kill",
                 ]);
                 for kernel in list().await {
                     use KernelAvailability::*;
@@ -48,9 +49,10 @@ impl Cli {
                         .iter()
                         .map(|format| format.name())
                         .join(", ");
-                    let interrupt = kernel.supports_interrupt();
-                    let kill = kernel.supports_kill();
                     let forks = kernel.supports_forks();
+                    let interrupt = kernel.supports_interrupt();
+                    let terminate = kernel.supports_terminate();
+                    let kill = kernel.supports_kill();
 
                     table.add_row([
                         Cell::new(kernel.id()).add_attribute(Attribute::Bold),
@@ -60,17 +62,21 @@ impl Cli {
                             Unavailable => Cell::new(availability).fg(Color::Grey),
                         },
                         Cell::new(langs),
+                        match forks {
+                            KernelForks::Yes => Cell::new(forks).fg(Color::Green),
+                            KernelForks::No => Cell::new(forks).fg(Color::DarkGrey),
+                        },
                         match interrupt {
                             KernelInterrupt::Yes => Cell::new(interrupt).fg(Color::Green),
                             KernelInterrupt::No => Cell::new(interrupt).fg(Color::DarkGrey),
                         },
+                        match terminate {
+                            KernelTerminate::Yes => Cell::new(terminate).fg(Color::Green),
+                            KernelTerminate::No => Cell::new(terminate).fg(Color::DarkGrey),
+                        },
                         match kill {
                             KernelKill::Yes => Cell::new(kill).fg(Color::Green),
                             KernelKill::No => Cell::new(kill).fg(Color::DarkGrey),
-                        },
-                        match forks {
-                            KernelForks::Yes => Cell::new(forks).fg(Color::Green),
-                            KernelForks::No => Cell::new(forks).fg(Color::DarkGrey),
                         },
                     ]);
                 }
