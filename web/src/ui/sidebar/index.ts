714c2d107db1041c8ccd9af02d3edcbb9eb7d3b1
Nokome Bentley <nokome@stenci.la>
2024-02-08 06:48:34 +1300

    refactor(web ui): rename to `emitEvent`

--- web/src/ui/sidebar/index.ts
+++ web/src/ui/sidebar/index.ts
@@ -6,6 +6,8 @@ import { SidebarContext, sidebarContext } from '../../contexts/sidebar-context'
 import { withTwind } from '../../twind'
 import { MainContextEvent } from '../../types'
 
+import '../buttons/icon'
+
 /**
  * UI sidebar
  *
@@ -24,7 +26,7 @@ export class UISidebar extends LitElement {
       <stencila-ui-icon-button
         icon="sidebar"
         .clickEvent=${() => {
-          this.createEvent('stencila-directory-toggle', {
+          this.emitEvent('stencila-directory-toggle', {
             directoryOpen: !this.context.directoryOpen,
           })
         }}
@@ -34,7 +36,7 @@ export class UISidebar extends LitElement {
         <stencila-ui-icon-button
           icon="visual-view"
           .clickEvent=${() => {
-            this.createEvent('stencila-view-change', {
+            this.emitEvent('stencila-view-change', {
               currentView: 'visual',
             })
           }}
@@ -44,7 +46,7 @@ export class UISidebar extends LitElement {
         <stencila-ui-icon-button
           icon="code-view"
           .clickEvent=${() => {
-            this.createEvent('stencila-view-change', {
+            this.emitEvent('stencila-view-change', {
               currentView: 'source',
             })
           }}
@@ -53,7 +55,7 @@ export class UISidebar extends LitElement {
         <stencila-ui-icon-button
           icon="split-view"
           .clickEvent=${() => {
-            this.createEvent('stencila-view-change', {
+            this.emitEvent('stencila-view-change', {
               currentView: 'split',
             })
           }}
@@ -64,7 +66,13 @@ export class UISidebar extends LitElement {
     </div> `
   }
 
-  private createEvent<T extends keyof SidebarContext>(
+  /**
+   * Emit a `MainContextEvent`
+   * 
+   * @param name The name of the event
+   * @param detail The event details
+   */
+  private emitEvent<T extends keyof SidebarContext>(
     name: MainContextEvent,
     detail?: Pick<SidebarContext, T>
   ): void {
