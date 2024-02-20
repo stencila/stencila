1e25633e8c9a305b7c07a9c4ddcc40adc87e8c0c
Nokome Bentley <nokome@stenci.la>
2024-02-20 21:57:13 +1300

    feat(cli utils): add `ToStdout` trait and `rpassword`

--- rust/cli-utils/src/table.rs
+++ rust/cli-utils/src/table.rs
@@ -1,11 +1,10 @@
 use comfy_table::{
     modifiers::{UTF8_ROUND_CORNERS, UTF8_SOLID_INNER_BORDERS},
     presets::UTF8_FULL,
-    Table,
 };
 
 // Re-exports
-pub use comfy_table::{Attribute, Cell, Color};
+pub use comfy_table::{Attribute, Cell, CellAlignment, Color, Table};
 
 /// Create a new table for displaying data in the terminal
 ///
