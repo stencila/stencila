fd04d056fa2535c6052bf58d10484ec5c96261ba
Nokome Bentley <nokome@stenci.la>
2024-03-15 17:35:15 +1300

    chore(markdown): formatting

--- rust/codec-markdown-trait/src/lib.rs
+++ rust/codec-markdown-trait/src/lib.rs
@@ -12,7 +12,7 @@ pub trait MarkdownCodec {
 }
 
 /// Encode a node that implements `MarkdownCodec` to Markdown
-/// 
+///
 /// A convenience function to save the caller from having to create a context etc.
 pub fn to_markdown<T>(node: &T) -> String
 where
