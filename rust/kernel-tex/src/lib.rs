18e8f734e89fdc87192010bf9d47422317a7d849
Nokome Bentley <nokome@stenci.la>
2024-03-17 16:52:23 +1300

    feat(tex kernel): add kernel for transpiling TeX to MathML

---
+++ rust/kernel-tex/src/lib.rs
@@ -0,0 +1,236 @@
+use std::path::Path;
+
+use kernel::{
+    common::{
+        async_trait::async_trait,
+        eyre::{bail, Result},
+        once_cell::sync::Lazy,
+        regex::Regex,
+        tokio::sync::{mpsc, watch},
+        tracing,
+    },
+    format::Format,
+    schema::{
+        ExecutionMessage, MessageLevel, Node, SoftwareApplication, SoftwareApplicationOptions,
+        SoftwareSourceCode, Variable,
+    },
+    Kernel, KernelAvailability, KernelForks, KernelInstance, KernelInterrupt, KernelKill,
+    KernelSignal, KernelStatus, KernelTerminate,
+};
+use latex2mathml::{latex_to_mathml, DisplayStyle};
+
+/// A kernel for compiling TeX math to MathML.
+///
+/// Note that although this is all about converting TeX to MathML it is implemented
+/// as a kernel, rather than a codec, because the conversion is at the level of
+/// an individual node (i.e. `MathBlock` or `MatchInline`) rather than at the document level.
+#[derive(Default)]
+pub struct TexKernel {}
+
+impl Kernel for TexKernel {
+    fn name(&self) -> String {
+        "tex".to_string()
+    }
+
+    fn availability(&self) -> KernelAvailability {
+        KernelAvailability::Available
+    }
+
+    fn supports_languages(&self) -> Vec<Format> {
+        vec![Format::Tex, Format::Latex]
+    }
+
+    fn supports_interrupt(&self) -> KernelInterrupt {
+        KernelInterrupt::No
+    }
+
+    fn supports_terminate(&self) -> KernelTerminate {
+        KernelTerminate::No
+    }
+
+    fn supports_kill(&self) -> KernelKill {
+        KernelKill::No
+    }
+
+    fn supports_forks(&self) -> KernelForks {
+        KernelForks::No
+    }
+
+    fn create_instance(&self) -> Result<Box<dyn KernelInstance>> {
+        Ok(Box::new(TexKernelInstance {}))
+    }
+}
+
+pub struct TexKernelInstance {}
+
+impl TexKernelInstance {
+    /// Transpile TeX to MathML
+    fn transpile(&self, tex: &str, style: DisplayStyle) -> (Option<String>, Vec<ExecutionMessage>) {
+        match latex_to_mathml(tex, style) {
+            Ok(mathml) => {
+                // Some (most?) errors are embedded into the MathML so we attempt to regex them out
+                static REGEX: Lazy<Regex> =
+                    Lazy::new(|| Regex::new(r"\[PARSE ERROR: (.*?)\]").expect("invalid regex"));
+
+                if let Some(matches) = REGEX.captures(&mathml) {
+                    (
+                        None,
+                        vec![ExecutionMessage {
+                            level: MessageLevel::Exception,
+                            message: matches.get(1).map_or_else(
+                                || "Parse error".to_string(),
+                                |group| group.as_str().to_string(),
+                            ),
+                            error_type: Some("Syntax error".to_string()),
+                            ..Default::default()
+                        }],
+                    )
+                } else {
+                    (Some(mathml), vec![])
+                }
+            }
+            Err(error) => (
+                None,
+                vec![ExecutionMessage::new(
+                    MessageLevel::Exception,
+                    error.to_string(),
+                )],
+            ),
+        }
+    }
+}
+
+#[async_trait]
+impl KernelInstance for TexKernelInstance {
+    fn name(&self) -> String {
+        "tex".to_string()
+    }
+
+    async fn status(&self) -> Result<KernelStatus> {
+        Ok(KernelStatus::Ready)
+    }
+
+    fn watcher(&self) -> Result<watch::Receiver<KernelStatus>> {
+        bail!("Not implemented")
+    }
+
+    fn signaller(&self) -> Result<mpsc::Sender<KernelSignal>> {
+        bail!("Not implemented")
+    }
+
+    async fn start(&mut self, _directory: &Path) -> Result<()> {
+        Ok(())
+    }
+
+    async fn stop(&mut self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn execute(&mut self, code: &str) -> Result<(Vec<Node>, Vec<ExecutionMessage>)> {
+        tracing::trace!("Transpiling TeX to MathML");
+
+        let (mathml, messages) = self.transpile(code, DisplayStyle::Block);
+        let mathml = mathml.map_or_else(|| Vec::new(), |mathml| vec![Node::String(mathml)]);
+
+        Ok((mathml, messages))
+    }
+
+    async fn evaluate(&mut self, code: &str) -> Result<(Node, Vec<ExecutionMessage>)> {
+        tracing::trace!("Transpiling TeX to MathML");
+
+        let (mathml, messages) = self.transpile(code, DisplayStyle::Inline);
+        let mathml = mathml.map_or_else(
+            || Node::String(String::new()),
+            |mathml| Node::String(mathml),
+        );
+
+        Ok((mathml, messages))
+    }
+
+    async fn info(&mut self) -> Result<SoftwareApplication> {
+        tracing::trace!("Getting TeX runtime info");
+
+        Ok(SoftwareApplication {
+            name: "Tex".to_string(),
+            options: Box::new(SoftwareApplicationOptions {
+                operating_system: Some(std::env::consts::OS.to_string()),
+                ..Default::default()
+            }),
+            ..Default::default()
+        })
+    }
+
+    async fn packages(&mut self) -> Result<Vec<SoftwareSourceCode>> {
+        Ok(vec![])
+    }
+
+    async fn list(&mut self) -> Result<Vec<Variable>> {
+        Ok(vec![])
+    }
+
+    async fn get(&mut self, _name: &str) -> Result<Option<Node>> {
+        Ok(None)
+    }
+
+    async fn set(&mut self, _name: &str, _node: &Node) -> Result<()> {
+        Ok(())
+    }
+
+    async fn remove(&mut self, _name: &str) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use common_dev::pretty_assertions::assert_eq;
+    use kernel::{common::tokio, schema::Node};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn execute() -> Result<()> {
+        let mut instance = TexKernelInstance {};
+
+        let (outputs, messages) = instance.execute(r"a = \pi r^2").await?;
+        assert_eq!(messages, vec![]);
+        assert_eq!(outputs, vec![Node::String("<math xmlns=\"http://www.w3.org/1998/Math/MathML\" display=\"block\"><mi>a</mi><mo>=</mo><mi>π</mi><msup><mi>r</mi><mn>2</mn></msup></math>".to_string())]);
+
+        let (outputs, messages) = instance.execute(r"\foo").await?;
+        assert_eq!(
+            messages,
+            vec![ExecutionMessage {
+                level: MessageLevel::Exception,
+                message: "Undefined(\"Command(\\\"foo\\\")\")".to_string(),
+                error_type: Some("Syntax error".to_string()),
+                ..Default::default()
+            }]
+        );
+        assert_eq!(outputs, vec![]);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn evaluate() -> Result<()> {
+        let mut instance = TexKernelInstance {};
+
+        let (output, messages) = instance.evaluate(r"\pi r^2").await?;
+        assert_eq!(messages, vec![]);
+        assert_eq!(output, Node::String("<math xmlns=\"http://www.w3.org/1998/Math/MathML\" display=\"inline\"><mi>π</mi><msup><mi>r</mi><mn>2</mn></msup></math>".to_string()));
+
+        let (output, messages) = instance.evaluate(r"\foo").await?;
+        assert_eq!(
+            messages,
+            vec![ExecutionMessage {
+                level: MessageLevel::Exception,
+                message: "Undefined(\"Command(\\\"foo\\\")\")".to_string(),
+                error_type: Some("Syntax error".to_string()),
+                ..Default::default()
+            }]
+        );
+        assert_eq!(output, Node::String(String::new()));
+
+        Ok(())
+    }
+}
