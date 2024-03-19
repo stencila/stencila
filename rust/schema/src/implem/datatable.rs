c3fd4997dfc3827f1cb559fffad918a835d39588
Nokome Bentley <nokome@stenci.la>
2024-03-19 18:55:46 +1300

    feat(dom html): add `data-type` to `<td>`s for `Datatable`; do not quote strings

--- rust/schema/src/implem/datatable.rs
+++ rust/schema/src/implem/datatable.rs
@@ -1,4 +1,4 @@
-use crate::{prelude::*, Datatable};
+use crate::{prelude::*, ArrayValidator, Datatable, Primitive};
 
 impl Datatable {
     /// Get the number of rows in the `Datatable`
@@ -25,14 +25,41 @@ impl DomCodec for Datatable {
         }
         context.exit_elem().exit_elem();
 
+        // Get a name for the type of each column
+        let data_types = self
+            .columns
+            .iter()
+            .map(|column| match &column.validator {
+                Some(ArrayValidator {
+                    items_validator: Some(items_validator),
+                    ..
+                }) => Some(
+                    items_validator
+                        .to_string()
+                        .trim_end_matches("Validator")
+                        .to_lowercase(),
+                ),
+                _ => None,
+            })
+            .collect_vec();
+
         // Create a <tbody> elem with a <td> for each value in each column
         context.enter_elem("tbody");
         for row in 0..self.rows().min(context.max_datatable_rows) {
             context.enter_elem("tr");
-            for column in &self.columns {
+            for (column_index, column) in self.columns.iter().enumerate() {
                 context.enter_elem("td");
+
+                if let Some(Some(data_type)) = data_types.get(column_index) {
+                    context.push_attr("data-type", data_type);
+                }
+
                 if let Some(value) = column.values.get(row) {
-                    let text = serde_json::to_string(value).unwrap_or_default();
+                    let text = if let Primitive::String(value) = &value {
+                        value.clone()
+                    } else {
+                        serde_json::to_string(value).unwrap_or_default()
+                    };
                     context.push_text(&text);
                 }
                 context.exit_elem();
