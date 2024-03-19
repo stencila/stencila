78f64ca8b5ad9e9e9de7b5b2b476ae5ab63c7b48
Nokome Bentley <nokome@stenci.la>
2024-03-19 18:01:04 +1300

    reactor(web): tidy up `<stencila-ui-node-outputs>` for consistency

--- web/src/ui/nodes/properties/outputs.ts
+++ web/src/ui/nodes/properties/outputs.ts
@@ -3,23 +3,22 @@ import { LitElement, html } from 'lit'
 import { customElement, property } from 'lit/decorators'
 
 import { withTwind } from '../../../twind'
-import { nodeUi } from '../icons-and-colours'
+
+import './generic/collapsible'
 
 @customElement('stencila-ui-node-outputs')
 @withTwind()
-export class NodeOutputs extends LitElement {
-  @property({ type: String })
+export class UiNodeOutputs extends LitElement {
+  @property()
   type: NodeType
 
   override render() {
-    const { borderColour } = nodeUi(this.type)
-
     return html`
       <stencila-ui-node-collapsible-property
-        .collapsed=${false}
+        type=${this.type}
         icon-name="output"
         icon-library="stencila"
-        header-bg=${borderColour}
+        .collapsed=${false}
       >
         <span slot="title">Outputs</span>
         <div class="px-6 py-3 flex flex-col gap-y-3" slot="content">
