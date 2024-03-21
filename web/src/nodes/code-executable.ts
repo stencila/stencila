7d81b08e0b6a9feb847fbf60a52bebeb2d5293a3
Nokome Bentley <nokome@stenci.la>
2024-03-21 15:20:30 +1300

    chore(web): formatting

--- web/src/nodes/code-executable.ts
+++ web/src/nodes/code-executable.ts
@@ -4,7 +4,7 @@ import { Executable } from './executable'
 
 /**
  * Abstract base class for web components representing Stencila Schema `CodeExecutable` node types
- * 
+ *
  * Note that `code` and `authors` are encoded as slots.
  *
  * @see https://github.com/stencila/stencila/blob/main/docs/reference/schema/code/code-executable.md
--- web/src/nodes/code-static.ts
+++ web/src/nodes/code-static.ts
@@ -4,7 +4,7 @@ import { Entity } from './entity'
 
 /**
  * Abstract base class for web components representing Stencila Schema `CodeStatic` node types
- * 
+ *
  * Note that `code` and `authors` are encoded as slots.
  *
  * @see https://github.com/stencila/stencila/blob/main/docs/reference/schema/code/code-static.md
