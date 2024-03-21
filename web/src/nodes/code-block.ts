fcb6507da7ecc56ddb7e74c55e82e1140234b412
Nokome Bentley <nokome@stenci.la>
2024-03-21 13:45:01 +1300

    feat(code block): implement node cards

--- web/src/nodes/code-block.ts
+++ web/src/nodes/code-block.ts
@@ -1,24 +1,51 @@
 import { html } from 'lit'
 import { customElement } from 'lit/decorators'
 
-import { withTwind } from '../twind'
-
 import '../ui/nodes/card'
 import '../ui/nodes/properties/authors'
+import '../ui/nodes/properties/code'
 
-import { Entity } from './entity'
+import { CodeStatic } from './code-static'
 
 @customElement('stencila-code-block')
-@withTwind()
-export class CodeBlock extends Entity {
+export class CodeBlock extends CodeStatic {
+  /**
+   * In static view, render the code and programming language
+   *
+   * TODO: display code as a read-only CodeMirror editor for syntax
+   * highlighting and show language.
+   */
   override renderStaticView() {
     return html`<slot name="code"></slot>`
   }
 
+  /**
+   * In dynamic view, also render the authors
+   */
   override renderDynamicView() {
-    return this.renderStaticView()
+    return html`
+      <stencila-ui-node-card type="CodeBlock" view="dynamic">
+        <div slot="body">
+          <stencila-ui-node-authors>
+            <slot name="authors"></slot>
+          </stencila-ui-node-authors>
+
+          <stencila-ui-node-code
+            type="CodeBlock"
+            language=${this.programmingLanguage}
+            read-only
+          >
+            <slot name="code"></slot>
+          </stencila-ui-node-code>
+        </div>
+      </stencila-ui-node-card>
+    `
   }
 
+  /**
+   * In dynamic view, render the authors but not code since that is
+   * usually already rendered in the source.
+   */
   override renderSourceView() {
     return html`
       <stencila-ui-node-card type="CodeBlock" view="source">
---
+++ web/src/nodes/code-static.ts
@@ -0,0 +1,15 @@
+import { property } from 'lit/decorators.js'
+
+import { Entity } from './entity'
+
+/**
+ * Abstract base class for web components representing Stencila Schema `CodeStatic` node types
+ * 
+ * Note that `code` and `authors` are encoded as slots.
+ *
+ * @see https://github.com/stencila/stencila/blob/main/docs/reference/schema/code/code-static.md
+ */
+export abstract class CodeStatic extends Entity {
+  @property({ attribute: 'programming-language' })
+  programmingLanguage?: string
+}
