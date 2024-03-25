e131877f41a9bdbd2a25533e0df952fc7b46b1e0
Nokome Bentley <nokome@stenci.la>
2024-03-25 14:06:55 +1300

    feat(parsers): initial implementation of parsers

---
+++ rust/parsers/src/lib.rs
@@ -0,0 +1,17 @@
+use parser::{common::once_cell::sync::Lazy, format::Format, DefaultParser};
+
+pub use parser::{ParseInfo, Parser};
+
+/// Parse some code in a language
+pub fn parse(code: &str, language: &str) -> ParseInfo {
+    static PARSERS: Lazy<Vec<Box<dyn Parser>>> = Lazy::new(|| vec![]);
+
+    let format = Format::from_name(language);
+    for parser in PARSERS.iter() {
+        if parser.supports_language(&format) {
+            return parser.parse(code, &format);
+        }
+    }
+
+    DefaultParser::default().parse(code, &format)
+}
