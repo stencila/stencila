b5efea10022476c7c323b3c86f0645b41707ecb7
Nokome Bentley <nokome@stenci.la>
2024-03-25 16:44:24 +1300

    refactor(kernels cli): organize commands as in other modules

--- rust/kernels/src/cli.rs
+++ rust/kernels/src/cli.rs
@@ -11,7 +11,7 @@ use kernel::{
     },
     format::Format,
     schema::StringOrNumber,
-    KernelAvailability, KernelForks, KernelInterrupt, KernelKill, KernelTerminate,
+    KernelAvailability, KernelForks, KernelInterrupt, KernelKill, KernelProvider, KernelTerminate,
 };
 
 use crate::{list, Kernels};
@@ -23,34 +23,132 @@ pub struct Cli {
     command: Option<Command>,
 }
 
-/// A command to perform with kernels
-#[derive(Debug, Subcommand, Default)]
+#[derive(Debug, Subcommand)]
 enum Command {
-    /// List the kernels available
-    #[default]
-    List,
-
-    /// Get information about a kernel
-    ///
-    /// Mainly used to check the version of the kernel runtime and
-    /// operating system for debugging purpose.
+    List(List),
     Info(Info),
-
-    /// List packages available to a kernel
-    ///
-    /// Mainly used to check libraries available to a kernel
-    /// for debugging purpose.
     Packages(Packages),
-
     Execute(Execute),
 }
 
+impl Cli {
+    pub async fn run(self) -> Result<()> {
+        let Some(command) = self.command else {
+            return List {}.run().await;
+        };
+
+        match command {
+            Command::List(list) => list.run().await,
+            Command::Info(info) => info.run().await,
+            Command::Packages(pkgs) => pkgs.run().await,
+            Command::Execute(exec) => exec.run().await,
+        }
+    }
+}
+
+/// List the kernels available
+#[derive(Debug, Args)]
+struct List;
+
+impl List {
+    async fn run(self) -> Result<()> {
+        let mut table = table::new();
+        table.set_header([
+            "Name",
+            "Provider",
+            "Availability",
+            "Languages",
+            "Fork",
+            "Interrupt",
+            "Terminate",
+            "Kill",
+        ]);
+
+        for kernel in list().await {
+            use KernelAvailability::*;
+
+            let provider = kernel.provider();
+            let availability = kernel.availability();
+            let langs = kernel
+                .supports_languages()
+                .iter()
+                .map(|format| format.name())
+                .join(", ");
+            let forks = kernel.supports_forks();
+            let interrupt = kernel.supports_interrupt();
+            let terminate = kernel.supports_terminate();
+            let kill = kernel.supports_kill();
+
+            table.add_row([
+                Cell::new(kernel.name()).add_attribute(Attribute::Bold),
+                match provider {
+                    KernelProvider::Builtin => Cell::new("builtin").fg(Color::Green),
+                    KernelProvider::Plugin(name) => {
+                        Cell::new(format!("plugin \"{name}\"")).fg(Color::Blue)
+                    }
+                },
+                match availability {
+                    Available => Cell::new(availability).fg(Color::Green),
+                    Installable => Cell::new(availability).fg(Color::DarkYellow),
+                    Unavailable => Cell::new(availability).fg(Color::Grey),
+                },
+                Cell::new(langs),
+                match forks {
+                    KernelForks::Yes => Cell::new(forks).fg(Color::Green),
+                    KernelForks::No => Cell::new(forks).fg(Color::DarkGrey),
+                },
+                match interrupt {
+                    KernelInterrupt::Yes => Cell::new(interrupt).fg(Color::Green),
+                    KernelInterrupt::No => Cell::new(interrupt).fg(Color::DarkGrey),
+                },
+                match terminate {
+                    KernelTerminate::Yes => Cell::new(terminate).fg(Color::Green),
+                    KernelTerminate::No => Cell::new(terminate).fg(Color::DarkGrey),
+                },
+                match kill {
+                    KernelKill::Yes => Cell::new(kill).fg(Color::Green),
+                    KernelKill::No => Cell::new(kill).fg(Color::DarkGrey),
+                },
+            ]);
+        }
+
+        println!("{table}");
+
+        Ok(())
+    }
+}
+
+/// Get information about a kernel
+///
+/// Mainly used to check the version of the kernel runtime and
+/// operating system for debugging purpose.
 #[derive(Debug, Args)]
 struct Info {
     /// The name of the kernel to get information for
     name: String,
 }
 
+impl Info {
+    async fn run(self) -> Result<()> {
+        let mut kernels = Kernels::new_here();
+        let instance = kernels.create_instance(Some(&self.name)).await?;
+
+        let info = instance.lock().await.info().await?;
+        println!(
+            "Name: {}\nVersion: {}\nOperating system: {}\n",
+            info.name,
+            info.options.software_version.as_deref().unwrap_or("?"),
+            info.options.operating_system.as_deref().unwrap_or("?"),
+        );
+
+        Ok(())
+    }
+}
+
+/// List packages available to a kernel
+///
+/// Mainly used to check libraries available to a kernel
+/// for debugging purpose.
 #[derive(Debug, Args)]
 struct Packages {
     /// The name of the kernel to list packages for
@@ -61,6 +159,43 @@ struct Packages {
     /// Only packages whose name contains this string will be included
     /// (case insensitive)
     filter: Option<String>,
+}
+
+impl Packages {
+    async fn run(self) -> Result<()> {
+        let mut kernels = Kernels::new_here();
+        let instance = kernels.create_instance(Some(&self.name)).await?;
+
+        let packages = instance.lock().await.packages().await?;
+        let packages = packages
+            .into_iter()
+            .filter(|package| {
+                if let Some(filter) = &self.filter {
+                    package.name.to_lowercase().contains(&filter.to_lowercase())
+                } else {
+                    true
+                }
+            })
+            .sorted_by(|a, b| a.name.cmp(&b.name));
+
+        let mut table = table::new();
+        table.set_header(["Package", "Version"]);
+
+        for package in packages {
+            let version = match package.options.version.unwrap_or_default() {
+                StringOrNumber::String(version) => version,
+                StringOrNumber::Number(version) => version.to_string(),
+            };
+            table.add_row([
+                Cell::new(package.name).add_attribute(Attribute::Bold),
+                Cell::new(version).set_alignment(CellAlignment::Right),
+            ]);
+        }
+
+        println!("{table}");
+
+        Ok(())
+    }
 }
 
 /// Execute some code in a kernel
@@ -82,126 +217,22 @@ struct Execute {
     code: String,
 }
 
-impl Cli {
-    // Run the CLI
-    pub async fn run(self) -> Result<()> {
-        match self.command.unwrap_or_default() {
-            Command::List => {
-                let mut table = table::new();
-                table.set_header([
-                    "Name",
-                    "Availability",
-                    "Languages",
-                    "Fork",
-                    "Interrupt",
-                    "Terminate",
-                    "Kill",
-                ]);
+impl Execute {
+    async fn run(self) -> Result<()> {
+        let mut kernels = Kernels::new_here();
+        let instance = kernels.create_instance(Some(&self.name)).await?;
 
-                for kernel in list().await {
-                    use KernelAvailability::*;
+        let code = self.code.replace("\\n", "\n");
+        let (outputs, messages) = instance.lock().await.execute(&code).await?;
 
-                    let availability = kernel.availability();
-                    let langs = kernel
-                        .supports_languages()
-                        .iter()
-                        .map(|format| format.name())
-                        .join(", ");
-                    let forks = kernel.supports_forks();
-                    let interrupt = kernel.supports_interrupt();
-                    let terminate = kernel.supports_terminate();
-                    let kill = kernel.supports_kill();
+        // TODO: creates a `Map` output type that can be used to display sections with headers
+        // instead of the following printlns
 
-                    table.add_row([
-                        Cell::new(kernel.name()).add_attribute(Attribute::Bold),
-                        match availability {
-                            Available => Cell::new(availability).fg(Color::Green),
-                            Installable => Cell::new(availability).fg(Color::DarkYellow),
-                            Unavailable => Cell::new(availability).fg(Color::Grey),
-                        },
-                        Cell::new(langs),
-                        match forks {
-                            KernelForks::Yes => Cell::new(forks).fg(Color::Green),
-                            KernelForks::No => Cell::new(forks).fg(Color::DarkGrey),
-                        },
-                        match interrupt {
-                            KernelInterrupt::Yes => Cell::new(interrupt).fg(Color::Green),
-                            KernelInterrupt::No => Cell::new(interrupt).fg(Color::DarkGrey),
-                        },
-                        match terminate {
-                            KernelTerminate::Yes => Cell::new(terminate).fg(Color::Green),
-                            KernelTerminate::No => Cell::new(terminate).fg(Color::DarkGrey),
-                        },
-                        match kill {
-                            KernelKill::Yes => Cell::new(kill).fg(Color::Green),
-                            KernelKill::No => Cell::new(kill).fg(Color::DarkGrey),
-                        },
-                    ]);
-                }
+        println!("Outputs");
+        Code::new(Format::Yaml, &serde_yaml::to_string(&outputs)?).to_stdout();
 
-                println!("{table}");
-            }
-            Command::Info(Info { name }) => {
-                let mut kernels = Kernels::new_here();
-                let instance = kernels.create_instance(Some(&name)).await?;
-
-                let info = instance.lock().await.info().await?;
-                println!(
-                    "Name: {}\nVersion: {}\nOperating system: {}\n",
-                    info.name,
-                    info.options.software_version.as_deref().unwrap_or("?"),
-                    info.options.operating_system.as_deref().unwrap_or("?"),
-                );
-            }
-            Command::Packages(Packages { name, filter }) => {
-                let mut kernels = Kernels::new_here();
-                let instance = kernels.create_instance(Some(&name)).await?;
-
-                let packages = instance.lock().await.packages().await?;
-                let packages = packages
-                    .into_iter()
-                    .filter(|package| {
-                        if let Some(filter) = &filter {
-                            package.name.to_lowercase().contains(&filter.to_lowercase())
-                        } else {
-                            true
-                        }
-                    })
-                    .sorted_by(|a, b| a.name.cmp(&b.name));
-
-                let mut table = table::new();
-                table.set_header(["Package", "Version"]);
-
-                for package in packages {
-                    let version = match package.options.version.unwrap_or_default() {
-                        StringOrNumber::String(version) => version,
-                        StringOrNumber::Number(version) => version.to_string(),
-                    };
-                    table.add_row([
-                        Cell::new(package.name).add_attribute(Attribute::Bold),
-                        Cell::new(version).set_alignment(CellAlignment::Right),
-                    ]);
-                }
-
-                println!("{table}");
-            }
-            Command::Execute(Execute { name, code }) => {
-                let mut kernels = Kernels::new_here();
-                let instance = kernels.create_instance(Some(&name)).await?;
-
-                let code = code.replace("\\n", "\n");
-                let (outputs, messages) = instance.lock().await.execute(&code).await?;
-
-                // TODO: creates a `Map` output type that can be used to display sections with headers
-                // instead of the following printlns
-
-                println!("Outputs");
-                Code::new(Format::Yaml, &serde_yaml::to_string(&outputs)?).to_stdout();
-
-                println!("Messages");
-                Code::new(Format::Yaml, &serde_yaml::to_string(&messages)?).to_stdout();
-            }
-        }
+        println!("Messages");
+        Code::new(Format::Yaml, &serde_yaml::to_string(&messages)?).to_stdout();
 
         Ok(())
     }
