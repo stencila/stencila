98d61f5eee02cb23a938e4509273ea02ee50b936
Nokome Bentley <nokome@stenci.la>
2024-04-17 21:44:03 +1200

    test(node patches): consolidate to one file and add metrics
    [skip ci]

--- rust/schema/tests/patch.rs
+++ rust/schema/tests/patch.rs
@@ -1,13 +1,214 @@
-//! Unit tests of diffing and patching for various types
+use std::{collections::BTreeMap, fs::read_to_string};
 
-use common::eyre::Result;
-use common_dev::pretty_assertions::assert_eq;
+use codec::{format::Format, Codec, DecodeOptions};
+use codec_markdown::MarkdownCodec;
+use common::{eyre::Result, glob::glob, serde::Serialize, tokio};
+use common_dev::{insta::assert_yaml_snapshot, pretty_assertions::assert_eq};
+use node_authorship::author_roles;
 use schema::{
     diff, patch,
     shortcuts::{art, p, sec, t},
-    Article, Block, Figure, Inline, Node, NodeProperty, Paragraph, PatchNode, PatchOp, PatchPath,
-    PatchSlot, PatchValue, Primitive, Strong, Text, TimeUnit,
+    Article, Author, AuthorRole, Block, Figure, Inline, Node, NodeProperty, Paragraph, PatchNode,
+    PatchOp, PatchPath, PatchSlot, PatchValue, Primitive, Strong, Text, TimeUnit, Visitor,
+    WalkControl,
 };
+
+/// An individual fixture
+#[derive(Debug, Serialize)]
+#[serde(crate = "common::serde")]
+struct Fixture {
+    /// The old node read from the fixture file
+    old: Node,
+
+    /// The new node read from the fixture file
+    new: Node,
+
+    /// The operations required to go from old to new
+    ops: Vec<(PatchPath, PatchOp)>,
+
+    /// The number of author roles before applying ops
+    authors_before: usize,
+
+    /// The number of author roles after applying ops
+    authors_after: usize,
+}
+
+/// A summary of all the fixtures in this test
+/// This is snap shotted so we can catch changes in key metrics
+#[derive(Debug, Serialize)]
+#[serde(crate = "common::serde")]
+struct Summary {
+    /// The number of operations for each fixture to go from old to new
+    ops_count: BTreeMap<String, usize>,
+
+    /// The total number of operations across all fixtures
+    ops_total: usize,
+
+    /// The number of operations for each fixture to go from old to new
+    author_diffs: BTreeMap<String, isize>,
+
+    /// The total number of operations across all fixtures
+    author_diffs_sum: isize,
+}
+
+/// A visitor to count the author roles in a node
+struct Authors(usize);
+
+impl Authors {
+    fn count(&mut self, authors: &Option<Vec<Author>>) {
+        self.0 += authors.as_ref().map(|vec| vec.len()).unwrap_or_default()
+    }
+}
+
+impl Visitor for Authors {
+    fn visit_inline(&mut self, inline: &Inline) -> WalkControl {
+        macro_rules! apply {
+            ($($variant:ident),*) => {
+                use Inline::*;
+                match inline {
+                    $($variant(node) => self.count(&node.options.authors),)*
+                    _ => {}
+                }
+            };
+        }
+
+        // Should be applied to all inlines with the `authors` property
+        apply!(
+            AudioObject,
+            CodeExpression,
+            CodeInline,
+            ImageObject,
+            InstructionInline,
+            MathInline,
+            MediaObject,
+            StyledInline,
+            VideoObject
+        );
+
+        WalkControl::Continue
+    }
+
+    fn visit_block(&mut self, block: &Block) -> WalkControl {
+        macro_rules! apply {
+            ($($variant:ident),*) => {
+                use Block::*;
+                match block {
+                    $($variant(node) => self.count(&node.options.authors),)*
+                    _ => {}
+                }
+            };
+        }
+
+        // Should be applied to all blocks with the `authors` property
+        apply!(
+            Admonition,
+            Claim,
+            CodeBlock,
+            CodeChunk,
+            Figure,
+            ForBlock,
+            Heading,
+            IfBlock,
+            InstructionBlock,
+            List,
+            MathBlock,
+            Paragraph,
+            QuoteBlock,
+            StyledBlock,
+            Table
+        );
+
+        WalkControl::Continue
+    }
+}
+
+// Count the authors in a node
+fn authors(node: &Node) -> usize {
+    let mut authors = Authors(0);
+    authors.visit(node);
+    authors.0
+}
+
+/// Snapshot tests of the `MergeNode::diff` method
+#[tokio::test]
+async fn fixtures() -> Result<()> {
+    let mut ops_count = BTreeMap::new();
+    let mut ops_total = 0;
+    let mut author_diffs = BTreeMap::new();
+    let mut author_diffs_sum = 0;
+    for path in glob("tests/fixtures/*.md")?.flatten() {
+        let name = path
+            .file_stem()
+            .and_then(|name| name.to_str())
+            .unwrap()
+            .to_string();
+
+        // Read in the fixture and split into old and new Markdown
+        let content = read_to_string(path)?;
+        let mut parts = content.splitn(2, "===\n").map(String::from);
+        let (old, new) = (
+            parts.next().unwrap_or_default(),
+            parts.next().unwrap_or_default(),
+        );
+
+        // Decode the old and new Markdown into nodes
+        let options = Some(DecodeOptions {
+            format: Some(Format::Markdown),
+            ..Default::default()
+        });
+        let codec = MarkdownCodec {};
+        let (old, ..) = codec.from_str(&old, options.clone()).await?;
+        let (new, ..) = codec.from_str(&new, options).await?;
+
+        // To test the retention of properties not represented in source Markdown
+        // apply the default author role recursively to the old node
+        let mut enriched = old.clone();
+        author_roles(&mut enriched, vec![AuthorRole::default()]);
+        let authors_before = authors(&enriched);
+
+        // Calculate the ops
+        let ops = diff(&old, &new)?;
+        ops_count.insert(name.clone(), ops.len());
+        ops_total += ops.len();
+
+        // Apply ops and assert that get new node
+        let mut merged = old.clone();
+        patch(&mut merged, ops.clone())?;
+        assert_eq!(merged, new, "{name}\n{ops:#?}");
+
+        // Apply the ops to enriched node and count authors after
+        patch(&mut enriched, ops.clone())?;
+        let authors_after = authors(&enriched);
+        let author_diff = authors_after as isize - authors_before as isize;
+        author_diffs.insert(name.clone(), author_diff);
+        author_diffs_sum += author_diff;
+
+        // Snapshot the fixture
+        assert_yaml_snapshot!(
+            name,
+            Fixture {
+                old,
+                new,
+                ops,
+                authors_before,
+                authors_after
+            }
+        );
+    }
+
+    // Snapshot summary
+    assert_yaml_snapshot!(
+        "summary",
+        Summary {
+            ops_count,
+            ops_total,
+            author_diffs,
+            author_diffs_sum
+        }
+    );
+
+    Ok(())
+}
 
 #[test]
 fn atoms() -> Result<()> {
@@ -57,9 +258,6 @@ fn atoms() -> Result<()> {
 
     Ok(())
 }
-
-#[test]
-fn options() {}
 
 #[test]
 fn vecs() -> Result<()> {
