baaf0570ec3b6d788efcfd4c022a58a3f3575b34
Nokome Bentley <nokome@stenci.la>
2024-05-08 17:34:53 +1200

    refactor(myst plugin): use `mys-spec` instead of `mdast`

--- plugins/myst/src/decode/blocks.ts
+++ plugins/myst/src/decode/blocks.ts
@@ -1,10 +1,10 @@
 import { Block, Paragraph } from "@stencila/types";
-import type { BlockContent } from "mdast";
+import type { Directive, FlowContent } from "myst-spec";
 
 import { mdsToInlines } from "./inlines.js";
 
 /**
- * Transform MDAST `BlockContent` nodes to Stencila Schema `Block` nodes
+ * Transform MyST `Block` nodes to Stencila Schema `Block` nodes
  *
  * This is equivalent to the Rust `mds_to_blocks` function in
  * `rust/codec-markdown/src/decode/blocks.rs`.
@@ -12,20 +12,38 @@ import { mdsToInlines } from "./inlines.js";
  * This is also an update of code in
  * https://github.com/stencila/encoda/blob/master/src/codecs/md/index.ts.
  */
-export function mdsToBlocks(mds: BlockContent[]): Block[] {
+export function mdsToBlocks(mds: FlowContent[]): Block[] {
   return mds.map((md) => {
     switch (md.type) {
       case "paragraph":
         return new Paragraph(mdsToInlines(md.children));
-      case "heading":
-      case "thematicBreak":
+      case "mystDirective":
+        return directiveToBlock(md);
+      case "admonition":
       case "blockquote":
-      case "list":
-      case "table":
-      case "html":
       case "code":
-      default:
+      case "container":
+      case "definition":
+      case "footnoteDefinition":
+      case "heading":
+      case "html":
+      case "list":
+      case "math":
+      case "mystComment":
+      case "mystTarget":
+      case "table":
+      case "thematicBreak":
         throw new Error(`Not yet implemented: ${md.type}`);
     }
   });
+}
+
+/**
+ * Transform a MyST `Directive` into a Stencila `Block` node
+ */
+function directiveToBlock(directive: Directive): Block {
+  switch (directive.name) {
+    default:
+      throw new Error(`mystRole not yet implemented: ${directive.name}`);
+  }
 }
--- plugins/myst/src/decode/inlines.ts
+++ plugins/myst/src/decode/inlines.ts
@@ -6,25 +6,10 @@ import {
   Text,
   Underline,
 } from "@stencila/types";
-import type { PhrasingContent } from "mdast";
+import type { PhrasingContent, Role } from "myst-spec";
 
 /**
- * An interface for a MyST role https://mystmd.org/spec/overview#roles
- *
- * TODO: There is probably a published type for this amongst the MyST packages.
- * (and for MyST directives). This is just an initial stab to get things type checking,
- * look for published type, and if not available implement one based on above spec.
- */
-interface MySTRole {
-  type: "mystRole";
-  name: string;
-  value: string;
-}
-
-type MySTInline = PhrasingContent | MySTRole;
-
-/**
- * Transform MDAST `PhrasingContent` to Stencila Schema `Inline` nodes
+ * Transform MyST `PhrasingContent` to Stencila Schema `Inline` nodes
  *
  * This is equivalent to the Rust `mds_to_inlines` function in
  * `rust/codec-markdown/src/decode/inlines.rs`.
@@ -32,7 +17,7 @@ type MySTInline = PhrasingContent | MySTRole;
  * This is also an update of code in
  * https://github.com/stencila/encoda/blob/master/src/codecs/md/index.ts.
  */
-export function mdsToInlines(mds: MySTInline[]): Inline[] {
+export function mdsToInlines(mds: PhrasingContent[]): Inline[] {
   return mds.map((md) => {
     switch (md.type) {
       case "text":
@@ -44,32 +29,46 @@ export function mdsToInlines(mds: MySTInline[]): Inline[] {
       case "inlineCode":
         return new CodeInline(md.value);
       case "mystRole":
-        return mystRole(md);
-      case "link":
-      case "linkReference":
-      case "delete":
-      case "html":
-      case "inlineCode":
+        return roleToInline(md);
+      case "abbreviation":
       case "break":
+      case "crossReference":
+      case "footnoteReference":
+      case "html":
       case "image":
       case "imageReference":
-      case "footnote":
-      case "footnoteReference":
-      default:
+      case "inlineCode":
+      case "inlineMath":
+      case "link":
+      case "linkReference":
+      case "subscript":
+      case "superscript":
+      case "underline":
         throw new Error(`mdast inline type not yet implemented: ${md.type}`);
     }
   });
 }
 
 /**
- * Transform a `MySTRole` into a Stencila `Inline` node
+ * Transform a MyST `Role` into a Stencila `Inline` node
  */
-function mystRole(inline: MySTRole): Inline {
-  switch (inline.name) {
+function roleToInline(role: Role): Inline {
+  switch (role.name) {
     case "u":
     case "underline":
-      return new Underline([new Text(inline.value)]);
+      return underlineRoleToUnderline(role);
     default:
-      throw new Error(`mystRole not yet implemented: ${inline.name}`);
+      throw new Error(`mystRole not yet implemented: ${role.name}`);
+  }
+}
+
+/**
+ * Transform a MyST `Role` for underline into a Stencila `Underline` node
+ */
+function underlineRoleToUnderline(role: Role): Underline {
+  if (role.children) {
+    return new Underline(mdsToInlines(role.children));
+  } else {
+    return new Underline([new Text(role.value ?? "")]);
   }
 }
