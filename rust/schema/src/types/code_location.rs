920fc8b41d6f6da940903a9715a701adac3fe8f1
Nokome Bentley <nokome@stenci.la>
2024-06-27 11:41:00 +1200

    docs(schema): change docs for `CodeLocation` to indicate zero-based

--- rust/schema/src/types/code_location.rs
+++ rust/schema/src/types/code_location.rs
@@ -24,19 +24,19 @@ pub struct CodeLocation {
     /// The source of the code, a file path, label or URL.
     pub source: Option<String>,
 
-    /// The 1-based index if the first line on which the error occurred.
+    /// The 0-based index if the first line on which the error occurred.
     #[serde(alias = "start-line", alias = "start_line")]
     pub start_line: Option<UnsignedInteger>,
 
-    /// The 1-based index if the first column on which the error occurred.
+    /// The 0-based index if the first column on which the error occurred.
     #[serde(alias = "start-column", alias = "start_column")]
     pub start_column: Option<UnsignedInteger>,
 
-    /// The 1-based index if the last line on which the error occurred.
+    /// The 0-based index if the last line on which the error occurred.
     #[serde(alias = "end-line", alias = "end_line")]
     pub end_line: Option<UnsignedInteger>,
 
-    /// The 1-based index if the last column on which the error occurred.
+    /// The 0-based index if the last column on which the error occurred.
     #[serde(alias = "end-column", alias = "end_column")]
     pub end_column: Option<UnsignedInteger>,
 
