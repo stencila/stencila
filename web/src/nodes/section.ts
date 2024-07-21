26bb9aa6316b04bc2a51ce4330521cdb35d88926
Nokome Bentley <nokome@stenci.la>
2024-07-21 19:48:54 +1200

    fix(for block): improve styling

--- web/src/nodes/section.ts
+++ web/src/nodes/section.ts
@@ -1,14 +1,13 @@
 import { html } from 'lit'
-import { customElement } from 'lit/decorators'
+import { customElement, property } from 'lit/decorators'
 
 import { withTwind } from '../twind'
-
 import '../ui/nodes/node-card/on-demand/block'
 import '../ui/nodes/properties/authors'
+import '../ui/nodes/properties/provenance/provenance'
+import { nodeUi } from '../ui/nodes/icons-and-colours'
 
 import { Entity } from './entity'
-
-import '../ui/nodes/properties/provenance/provenance'
 
 /**
  * Web component representing a Stencila Schema `Section` node
@@ -18,17 +17,22 @@ import '../ui/nodes/properties/provenance/provenance'
 @customElement('stencila-section')
 @withTwind()
 export class Section extends Entity {
-  /**
-   * render `content` and summary stats in a node
-   * card that is shown on hover.
-   */
-  override render() {
-    // TODO: Add summary stats to card
+  @property({ attribute: 'section-type' })
+  sectionType?: string
 
+  override render() {
+    return this.sectionType === 'Iteration'
+      ? this.renderIteration()
+      : this.renderSection()
+  }
+
+  /**
+   * Render a normal section
+   */
+  private renderSection() {
     return html`
       <stencila-ui-block-on-demand
         type="Section"
-        view="dynamic"
         depth=${this.depth}
         ancestors=${this.ancestors}
       >
@@ -45,5 +49,24 @@ export class Section extends Entity {
         </div>
       </stencila-ui-block-on-demand>
     `
+  }
+
+  /**
+   * Render a section when it is an iteration of a `ForBlock`
+   */
+  private renderIteration() {
+    const { colour, borderColour, textColour } = nodeUi('ForBlock')
+
+    const siblings = [...this.parentElement.children]
+    const index = siblings.findIndex((elem) => elem === this)
+
+    return html`<div
+        class="px-4 py-2 flex items-center text-[${textColour}] bg-[${colour}] border-[${borderColour}] font-sans text-sm"
+      >
+        Iteration ${index + 1}
+      </div>
+      <div class="p-3">
+        <slot name="content"></slot>
+      </div>`
   }
 }
