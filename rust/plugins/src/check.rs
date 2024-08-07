3f1b96e505486f59d0cb0d1ea520f91e4f217f6b
Nokome Bentley <nokome@stenci.la>
2024-08-07 13:01:28 +1200

    feat(plugins): allow skipping of some tests

--- rust/plugins/src/check.rs
+++ rust/plugins/src/check.rs
@@ -15,7 +15,12 @@ use crate::Plugin;
 
 /// Check a plugin
 #[tracing::instrument]
-pub async fn check(name: &str) -> Result<Message> {
+pub async fn check(
+    name: &str,
+    skip_codecs: bool,
+    skip_kernels: bool,
+    skip_models: bool,
+) -> Result<Message> {
     tracing::info!("Checking plugin `{name}`");
 
     let plugin = Plugin::read_manifest(name)?;
@@ -40,6 +45,11 @@ pub async fn check(name: &str) -> Result<Message> {
 
     // Codecs
     for codec in plugin.codecs() {
+        if skip_codecs {
+            tracing::warn!("Skipping plugin `{name}` codec `{}`", codec.name());
+            continue;
+        }
+
         tracing::info!("Checking plugin `{name}` codec `{}`", codec.name());
 
         // Create an article with a single paragraph (because it should be handled
@@ -61,6 +71,11 @@ pub async fn check(name: &str) -> Result<Message> {
 
     // Kernels
     for kernel in plugin.kernels() {
+        if skip_kernels {
+            tracing::warn!("Skipping plugin `{name}` kernel `{}`", kernel.name());
+            continue;
+        }
+
         tracing::info!("Checking plugin `{name}` kernel `{}`", kernel.name());
 
         // Start an instance of the kernel
@@ -84,6 +99,11 @@ pub async fn check(name: &str) -> Result<Message> {
 
     // Models
     for model in plugin.models() {
+        if skip_models {
+            tracing::warn!("Skipping plugin `{name}` model `{}`", model.name());
+            continue;
+        }
+
         tracing::info!("Checking plugin `{name}` model `{}`", model.id());
 
         // Create a task for the model
@@ -112,10 +132,28 @@ pub async fn check(name: &str) -> Result<Message> {
 pub struct CheckArgs {
     /// The name of the plugin to install
     pub name: String,
+
+    /// Skip checking codecs
+    #[arg(long)]
+    pub skip_codecs: bool,
+
+    /// Skip checking kernels
+    #[arg(long)]
+    pub skip_kernels: bool,
+
+    /// Skip checking models
+    #[arg(long)]
+    pub skip_models: bool,
 }
 
 impl CheckArgs {
     pub async fn run(self) -> Result<Message> {
-        check(&self.name).await
+        check(
+            &self.name,
+            self.skip_codecs,
+            self.skip_kernels,
+            self.skip_models,
+        )
+        .await
     }
 }
