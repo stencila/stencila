780ec8b1e365bbd441a7ca70a2ca8a73beb65848
Nokome Bentley <nokome@stenci.la>
2024-08-24 17:05:40 +1200

    feat(models): add `execute` command for testing models

--- rust/models/src/cli.rs
+++ rust/models/src/cli.rs
@@ -1,12 +1,20 @@
-use cli_utils::table::{self, Attribute, Cell, Color};
+use cli_utils::{
+    table::{self, Attribute, Cell, Color},
+    Code, ToStdout,
+};
 use model::{
     common::{
         clap::{self, Args, Parser, Subcommand},
         eyre::Result,
         itertools::Itertools,
+        serde_yaml,
     },
-    ModelAvailability, ModelType,
+    format::Format,
+    schema::{InstructionMessage, InstructionModel},
+    ModelAvailability, ModelTask, ModelType,
 };
+
+use crate::select;
 
 /// Manage generative models
 #[derive(Debug, Parser)]
@@ -18,6 +26,7 @@ pub struct Cli {
 #[derive(Debug, Subcommand)]
 enum Command {
     List(List),
+    Execute(Execute),
 }
 
 impl Cli {
@@ -29,6 +38,7 @@ impl Cli {
 
         match command {
             Command::List(list) => list.run().await?,
+            Command::Execute(execute) => execute.run().await?,
         }
 
         Ok(())
@@ -43,7 +53,13 @@ impl List {
     async fn run(self) -> Result<()> {
         let mut table = table::new();
         table.set_header([
-            "Id", "Type", "Provider", "Name", "Version", "Inputs", "Outputs",
+            "Id",
+            "Type",
+            "Availability",
+            "Provider",
+            "Name",
+            "Version",
+            "I/O",
         ]);
 
         for model in super::list().await {
@@ -62,35 +78,99 @@ impl List {
                 .map(|output| output.to_string())
                 .join(", ");
 
+            let io = [&inputs, "/", &outputs].concat();
+
             table.add_row([
                 Cell::new(model.id()).add_attribute(Attribute::Bold),
                 match model.r#type() {
-                    ModelType::Builtin => Cell::new("builtin").fg(Color::Blue),
-                    ModelType::Local => Cell::new("local").fg(Color::Yellow),
+                    ModelType::Builtin => Cell::new("builtin").fg(Color::DarkBlue),
+                    ModelType::Local => Cell::new("local").fg(Color::Blue),
                     ModelType::Router => Cell::new("router").fg(Color::Green),
-                    ModelType::Proxy => Cell::new("proxy").fg(Color::Cyan),
+                    ModelType::Proxied => Cell::new("proxied").fg(Color::Cyan),
                     ModelType::Remote => Cell::new("remote").fg(Color::Magenta),
                     ModelType::Plugin(name) => {
                         Cell::new(format!("plugin \"{name}\"")).fg(Color::DarkCyan)
                     }
                 },
+                match availability {
+                    Available => Cell::new(availability).fg(Color::Green),
+                    Disabled => Cell::new(availability).fg(Color::DarkYellow),
+                    RequiresKey => Cell::new(availability).fg(Color::Yellow),
+                    Installable => Cell::new(availability).fg(Color::Cyan),
+                    Unavailable => Cell::new(availability).fg(Color::Grey),
+                },
                 Cell::new(model.provider()),
                 Cell::new(model.name()),
-                match availability {
-                    Available => Cell::new(model.version()),
-                    _ => match availability {
-                        Available => Cell::new(availability).fg(Color::Green),
-                        Disabled => Cell::new(availability).fg(Color::DarkBlue),
-                        Installable => Cell::new(availability).fg(Color::Cyan),
-                        Unavailable => Cell::new(availability).fg(Color::Grey),
-                    },
-                },
-                Cell::new(inputs),
-                Cell::new(outputs),
+                Cell::new(model.version()),
+                Cell::new(io),
             ]);
         }
 
         println!("{table}");
+
+        Ok(())
+    }
+}
+
+/// Execute a model task
+///
+/// Mainly intended for testing of model selection and routing.
+#[derive(Debug, Args)]
+struct Execute {
+    prompt: String,
+
+    /// The id pattern to specify the model to use
+    #[arg(long, short)]
+    model: Option<String>,
+
+    /// Perform a dry run
+    #[arg(long)]
+    dry_run: bool,
+}
+
+impl Execute {
+    async fn run(self) -> Result<()> {
+        let message = InstructionMessage::from(self.prompt);
+
+        let model = if self.model.is_some() {
+            Some(InstructionModel {
+                id_pattern: self.model,
+                ..Default::default()
+            })
+        } else {
+            None
+        };
+
+        let task = ModelTask {
+            messages: vec![message],
+            instruction_model: model,
+            dry_run: self.dry_run,
+            ..Default::default()
+        };
+
+        Code::new(Format::Markdown, "# Constructed task\n").to_stdout();
+        Code::new(Format::Yaml, &serde_yaml::to_string(&task)?).to_stdout();
+
+        let model = select(&task).await?;
+
+        Code::new(Format::Markdown, "# Selected model\n").to_stdout();
+        Code::new(
+            Format::Yaml,
+            &format!(
+                "id: {}\nprovider: {}\nname: {}\nversion: {}\ntype: {}",
+                model.id(),
+                model.provider(),
+                model.name(),
+                model.version(),
+                model.r#type()
+            ),
+        )
+        .to_stdout();
+
+        let output = model.perform_task(&task).await?;
+
+        Code::new(Format::Markdown, "# Generated output\n").to_stdout();
+        Code::new(Format::Yaml, &serde_yaml::to_string(&output)?).to_stdout();
 
         Ok(())
     }
