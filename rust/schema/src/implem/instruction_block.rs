04474a1e08e0261ec27ead947eadeeacc14d5fbd
Nokome Bentley <nokome@stenci.la>
2024-09-23 09:04:16 +1200

    fix(instruction block): correctly handle edit instructions

--- rust/schema/src/implem/instruction_block.rs
+++ rust/schema/src/implem/instruction_block.rs
@@ -1,7 +1,7 @@
 use codec_info::{lost_exec_options, lost_options};
 use common::tracing;
 
-use crate::{patch, prelude::*, InstructionBlock, InstructionType, SuggestionStatus};
+use crate::{merge, patch, prelude::*, InstructionBlock, InstructionType, SuggestionStatus};
 
 impl InstructionBlock {
     pub fn apply_patch_op(
@@ -10,84 +10,134 @@ impl InstructionBlock {
         op: &PatchOp,
         context: &mut PatchContext,
     ) -> Result<bool> {
-        if path.is_empty() {
-            if let PatchOp::Accept(suggestion_id) = op {
-                if let Some(suggestions) = &mut self.suggestions {
-                    for suggestion in suggestions.iter_mut() {
-                        if &suggestion.node_id() == suggestion_id {
-                            // Mark the accepted suggestion as such
-                            suggestion.suggestion_status = Some(SuggestionStatus::Accepted);
-
-                            // Record the patcher as the acceptor
-                            let accepter_patch = context.authors_as_acceptors();
-                            let mut content = suggestion.content.clone();
-                            for node in &mut content {
-                                if let Err(error) = patch(node, accepter_patch.clone()) {
-                                    tracing::error!("While accepting block suggestion: {error}");
-                                }
-                            }
-                        } else {
-                            // Implicitly reject other suggestions
-                            suggestion.suggestion_status = Some(SuggestionStatus::Rejected);
-                        }
-                    }
-                }
-
-                return Ok(true);
-            } else if matches!(op, PatchOp::Archive) {
-                // Add this instruction to the root's archive
-                context.op_additional(
-                    PatchPath::from(NodeProperty::Archive),
-                    PatchOp::Push(self.to_value()?),
-                );
-
-                // If the instruction has an accepted suggestion then replace it with that,
-                // otherwise delete it.
-                let mut path = context.path();
-                let index = match path.pop_back() {
-                    Some(PatchSlot::Index(index)) => index,
-                    slot => bail!("Expected index slot, got: {slot:?}"),
-                };
-                let accepted = self.suggestions.iter().flatten().find_map(|suggestion| {
-                    matches!(
-                        suggestion.suggestion_status,
-                        Some(SuggestionStatus::Accepted)
-                    )
-                    .then_some(suggestion.content.clone())
-                });
-                match &accepted {
-                    Some(content) => {
-                        if content.is_empty() {
-                            // No content, so just delete
-                            context.op_additional(path, PatchOp::Remove(vec![index]));
-                        } else if content.len() == 1 {
-                            // Just one block, so replace it
-                            context.op_additional(
-                                path,
-                                PatchOp::Replace(vec![(index, content[0].to_value()?)]),
-                            );
-                        } else {
-                            // More than one block so remove instruction and insert blocks in its place
-                            let mut blocks = Vec::with_capacity(content.len());
-                            for (offset, block) in content.iter().enumerate() {
-                                blocks.push((index + offset, block.to_value()?))
-                            }
-                            context
-                                .op_additional(path.clone(), PatchOp::Remove(vec![index]))
-                                .op_additional(path, PatchOp::Insert(blocks));
-                        }
-                    }
-                    None => {
-                        // No accepted suggestion, so just delete
-                        context.op_additional(path, PatchOp::Remove(vec![index]));
-                    }
-                }
-
-                return Ok(true);
-            }
+        if !path.is_empty() {
+            // Early return for ops that have a path (i.e. that target a descendant in the tree)
+            return Ok(false);
         }
 
-        Ok(false)
+        if let PatchOp::Accept(suggestion_id) = op {
+            if let Some(suggestions) = &mut self.suggestions {
+                for suggestion in suggestions.iter_mut() {
+                    if &suggestion.node_id() == suggestion_id {
+                        // Mark the accepted suggestion as such
+                        suggestion.suggestion_status = Some(SuggestionStatus::Accepted);
+
+                        // Record the patcher as the acceptor
+                        let accepter_patch = context.authors_as_acceptors();
+                        let mut content = suggestion.content.clone();
+                        for node in &mut content {
+                            if let Err(error) = patch(node, accepter_patch.clone()) {
+                                tracing::error!("While accepting block suggestion: {error}");
+                            }
+                        }
+                    } else {
+                        // Implicitly reject other suggestions
+                        suggestion.suggestion_status = Some(SuggestionStatus::Rejected);
+                    }
+                }
+            }
+
+            Ok(true)
+        } else if matches!(op, PatchOp::Archive) {
+            // Add this instruction to the root's archive
+            context.op_additional(
+                PatchPath::from(NodeProperty::Archive),
+                PatchOp::Push(self.to_value()?),
+            );
+
+            // Get the path and index for applying the additional op
+            let mut path = context.path();
+            let index = match path.pop_back() {
+                Some(PatchSlot::Index(index)) => index,
+                slot => bail!("Expected index slot, got: {slot:?}"),
+            };
+
+            // Get the accepted suggestion
+            let accepted = self.suggestions.iter().flatten().find_map(|suggestion| {
+                matches!(
+                    suggestion.suggestion_status,
+                    Some(SuggestionStatus::Accepted)
+                )
+                .then_some(suggestion.content.clone())
+            });
+
+            let Some(accepted) = accepted else {
+                // No accepted suggestion, so just delete instruction
+                context.op_additional(path, PatchOp::Remove(vec![index]));
+                return Ok(true);
+            };
+
+            if accepted.is_empty() {
+                // No content in accepted suggestion, so just delete instruction
+                context.op_additional(path, PatchOp::Remove(vec![index]));
+                return Ok(true);
+            }
+
+            match &self.instruction_type {
+                InstructionType::New => {
+                    if accepted.len() == 1 {
+                        // Just one block in accepted suggestion, so replace with it
+                        context.op_additional(
+                            path,
+                            PatchOp::Replace(vec![(index, accepted[0].to_value()?)]),
+                        );
+                    } else {
+                        // More than one block in accepted suggestion so remove instruction and
+                        // insert blocks in its place
+                        let mut blocks = Vec::with_capacity(accepted.len());
+                        for (offset, block) in accepted.iter().enumerate() {
+                            blocks.push((index + offset, block.to_value()?))
+                        }
+                        context
+                            .op_additional(path.clone(), PatchOp::Remove(vec![index]))
+                            .op_additional(path, PatchOp::Insert(blocks));
+                    }
+                }
+                InstructionType::Edit => {
+                    // Merge the accepted content into the existing content and replace
+                    // the instruction with that merged content
+                    let merged = if let Some(content) = self.content.as_ref() {
+                        let len = content.len().max(accepted.len());
+                        let mut merged = Vec::with_capacity(len);
+                        for index in 0..len {
+                            let old = content.get(index);
+                            let new = accepted.get(index);
+                            match (old, new) {
+                                (Some(old), Some(new)) => {
+                                    let mut old = old.clone();
+                                    merge(&mut old, new, None, None)?;
+                                    merged.push(old);
+                                }
+                                (Some(old), None) => {
+                                    merged.push(old.clone());
+                                }
+                                (None, Some(new)) => {
+                                    merged.push(new.clone());
+                                }
+                                (None, None) => {}
+                            }
+                        }
+                        merged
+                    } else {
+                        accepted
+                    };
+
+                    let mut blocks = Vec::with_capacity(merged.len());
+                    for (offset, block) in merged.iter().enumerate() {
+                        blocks.push((index + offset, block.to_value()?))
+                    }
+
+                    context
+                        .op_additional(path.clone(), PatchOp::Remove(vec![index]))
+                        .op_additional(path, PatchOp::Insert(blocks));
+                }
+                _ => {}
+            };
+
+            Ok(true)
+        } else {
+            Ok(false)
+        }
     }
 }
 
