9b45f7e3d1bbdea0cc6ef09d17a0357b6c369cde
Nokome Bentley <nokome@stenci.la>
2024-09-30 08:00:01 +1300

    fix(include block): alter exec phases and record compilation messages

--- rust/schema/src/implem/include_block.rs
+++ rust/schema/src/implem/include_block.rs
@@ -5,7 +5,17 @@ use crate::{prelude::*, IncludeBlock};
 impl MarkdownCodec for IncludeBlock {
     fn to_markdown(&self, context: &mut MarkdownEncodeContext) {
         if context.render || matches!(context.format, Format::Llmd) {
-            // Record any execution messages
+            // Record any compilation or execution messages
+            if let Some(messages) = &self.options.compilation_messages {
+                for message in messages {
+                    context.add_message(
+                        self.node_type(),
+                        self.node_id(),
+                        message.level.clone().into(),
+                        message.message.to_string(),
+                    );
+                }
+            }
             if let Some(messages) = &self.options.execution_messages {
                 for message in messages {
                     context.add_message(
