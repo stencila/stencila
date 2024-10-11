38fa7f25058398542ffd6d132e80da1f3df12fb7
mike-parkin <mike@dragonfly.co.nz>
2024-10-11 15:42:30 +1300

    feat(e2e): install and config playwright

---
+++ e2e-tests/paragraph.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/examples/nodes/paragraph/paragraph');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Stencila/);
+});
+
+
+
