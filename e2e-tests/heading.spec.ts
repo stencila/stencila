58c1ee3e5fe578444a62944d1709ec89b309623b
mike-parkin <mike@dragonfly.co.nz>
2024-10-16 09:41:15 +1300

    reafctor(tests): add argos reporter

--- e2e-tests/heading.spec.ts
+++ e2e-tests/heading.spec.ts
@@ -1,11 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('/examples/tests/paragraph/paragraph');
+  await page.goto('/examples/tests/heading/heading');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Stencila/);
 });
-
-
-
--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,6 @@ export default defineConfig({
       "@argos-ci/playwright/reporter",
       {
         uploadToArgos: !!process.env.CI,
-        // token: "<YOUR-ARGOS-TOKEN>",
       },
     ],
   ],
@@ -41,14 +40,12 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure'
   },
-
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
@@ -58,28 +55,7 @@ export default defineConfig({
     //   name: 'webkit',
     //   use: { ...devices['Desktop Safari'] },
     // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'cargo run -p cli serve --no-auth',
