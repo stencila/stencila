7973deb092485d0ff7eecbf800a02e54924bc16d
Nokome Bentley <nokome@stenci.la>
2024-11-04 15:26:05 +1300

    fix(web): update link to web fonts
    Fixes an issue on Ubuntu >23 where Inter was not being used (due to invalid/inappropriate font variants?)

--- rust/server/src/documents.rs
+++ rust/server/src/documents.rs
@@ -369,7 +369,7 @@ pub async fn serve_path(
         format!(
             r#"<link rel="preconnect" href="https://fonts.googleapis.com">
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                <link href="https://fonts.googleapis.com/css2?family=Inter:slnt,wght@-10..0,100..900&family=IBM+Plex+Mono:wght@400&display=swap" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
                 <link rel="stylesheet" type="text/css" href="/~static/{version}/views/{view}.css">
                 <script type="module" src="/~static/{version}/views/{view}.js"></script>"#
         )
--- vscode/src/webviews.ts
+++ vscode/src/webviews.ts
@@ -82,7 +82,7 @@ export async function createDocumentViewPanel(
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Inter:slnt,wght@-10..0,100..900&family=IBM+Plex+Mono:wght@400&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
             <link title="theme:${themeName}" rel="stylesheet" type="text/css" href="${themeCss}">
             <link rel="stylesheet" type="text/css" href="${viewCss}">
             <script type="text/javascript" src="${viewJs}"></script>
