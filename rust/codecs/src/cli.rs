4c2b19833c1f4df939aed1b0ff934e5f53d37f96
micah <micah@duckles.org>
2024-11-29 09:27:16 +1300

    input and output avaliblity of formats to table of codecs command with colors

--- rust/codecs/src/cli.rs
+++ rust/codecs/src/cli.rs
@@ -1,8 +1,9 @@
-use cli_utils::table::{self, Attribute, Cell};
+use cli_utils::table::{self, Attribute, Cell, Color};
 use codec::common::{
     clap::{self, Args, Parser, Subcommand},
     eyre::Result,
 };
+use codec::status::Status;
 
 /// Manage format conversion codecs
 #[derive(Debug, Parser)]
@@ -38,12 +39,30 @@ struct List;
 impl List {
     async fn run(self) -> Result<()> {
         let mut table = table::new();
-        table.set_header(["Name", "Status"]);
+        table.set_header(["Name", "Input formats", "Output formats", "Status"]);
 
         for codec in super::list() {
+            let supports_input = codec.supports_from_path();
+            let supports_output = codec.supports_to_path();
+            let status = codec.status();
             table.add_row([
                 Cell::new(codec.name()).add_attribute(Attribute::Bold),
-                Cell::new(codec.status()),
+                match supports_input {
+                    true => Cell::new("Yes").fg(Color::Green),
+                    false => Cell::new("No"),
+                },
+                match supports_output {
+                    true => Cell::new("Yes").fg(Color::Green),
+                    false => Cell::new("No"),
+                },
+                match status {
+                    Status::Planned => Cell::new(status).fg(Color::Red),
+                    Status::UnderDevelopment => Cell::new(status).fg(Color::Yellow),
+                    Status::Stable => Cell::new(status).fg(Color::Green),
+                    Status::Beta => Cell::new(status).fg(Color::DarkBlue),
+                    Status::Alpha => Cell::new(status).fg(Color::Magenta),
+                    Status::Experimental => Cell::new(status).fg(Color::DarkRed),
+                },
             ]);
         }
 
