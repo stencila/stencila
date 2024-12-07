23e593e206e21bdef670e40a6730e6216b1b8c04
Nokome Bentley <nokome@stenci.la>
2024-12-08 08:27:04 +1300

    fix(markdown codec): support quote blocks with empty content

--- rust/schema/src/implem/quote_block.rs
+++ rust/schema/src/implem/quote_block.rs
@@ -6,17 +6,22 @@ impl MarkdownCodec for QuoteBlock {
     fn to_markdown(&self, context: &mut MarkdownEncodeContext) {
         context
             .enter_node(self.node_type(), self.node_id())
-            .merge_losses(lost_options!(self, id, cite))
-            .push_line_prefix("> ")
-            .prefix_empty_lines(true)
-            .push_prop_fn(NodeProperty::Content, |context| {
-                self.content.to_markdown(context)
-            })
-            .trim_end_matches(|char| char == '\n' || char == ' ' || char == '>')
-            .prefix_empty_lines(false)
-            .pop_line_prefix()
-            .newline()
-            .exit_node()
-            .newline();
+            .merge_losses(lost_options!(self, id, cite));
+
+        if self.content.is_empty() {
+            context.push_str(">");
+        } else {
+            context
+                .push_line_prefix("> ")
+                .prefix_empty_lines(true)
+                .push_prop_fn(NodeProperty::Content, |context| {
+                    self.content.to_markdown(context)
+                })
+                .trim_end_matches(|char| char == '\n' || char == ' ' || char == '>')
+                .prefix_empty_lines(false)
+                .pop_line_prefix();
+        }
+
+        context.newline().exit_node().newline();
     }
 }
