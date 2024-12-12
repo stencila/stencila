4a6330f0f642897811c705c7a7bac7e32fbbd448
micah <micah@duckles.org>
2024-12-13 11:41:45 +1300

    fix(jats codec): decoding align and valign

--- rust/schema/src/types/horizontal_alignment.rs
+++ rust/schema/src/types/horizontal_alignment.rs
@@ -3,24 +3,51 @@
 use crate::prelude::*;
 
 /// The horizontal alignment of content.
-#[derive(Debug, strum::Display, Clone, PartialEq, Serialize, Deserialize, StripNode, WalkNode, WriteNode, SmartDefault, strum::EnumString, Eq, PartialOrd, Ord, ReadNode, PatchNode, DomCodec, HtmlCodec, JatsCodec, MarkdownCodec, TextCodec)]
+#[derive(
+    Debug,
+    strum::Display,
+    Clone,
+    PartialEq,
+    Serialize,
+    Deserialize,
+    StripNode,
+    WalkNode,
+    WriteNode,
+    SmartDefault,
+    strum::EnumString,
+    Eq,
+    PartialOrd,
+    Ord,
+    ReadNode,
+    PatchNode,
+    DomCodec,
+    HtmlCodec,
+    JatsCodec,
+    MarkdownCodec,
+    TextCodec,
+)]
 #[serde(crate = "common::serde")]
 #[strum(ascii_case_insensitive, crate = "common::strum")]
 #[cfg_attr(feature = "proptest", derive(Arbitrary))]
 pub enum HorizontalAlignment {
     /// Left align content.
     #[default]
+    #[strum(serialize = "left")]
     AlignLeft,
 
     /// Right align content.
+    #[strum(serialize = "right")]
     AlignRight,
 
     /// Fully justify cell content.
+    #[strum(serialize = "justify")]
     AlignJustify,
 
     /// Center align the cell content.
+    #[strum(serialize = "center")]
     AlignCenter,
 
     /// Align the content on a character.
+    #[strum(serialize = "char")]
     AlignCharacter,
 }
