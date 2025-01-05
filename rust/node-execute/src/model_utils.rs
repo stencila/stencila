733c7260696bf6328ffd3f8a145cae8e893c594f
Nokome Bentley <nokome@stenci.la>
2025-01-05 18:23:27 +1300

    feat(model tasks): add handling of media files

--- rust/node-execute/src/model_utils.rs
+++ rust/node-execute/src/model_utils.rs
@@ -5,7 +5,8 @@ use common::{
 };
 use models::{ModelOutput, ModelOutputKind, ModelTask};
 use schema::{
-    shortcuts::p, Article, AudioObject, AuthorRole, Block, File, ImageObject, Inline, Link, MessagePart, Node, Text, VideoObject
+    shortcuts::p, Article, AudioObject, AuthorRole, Block, File, ImageObject, Inline, Link,
+    MessagePart, Node, Text, VideoObject,
 };
 
 /// Performs a model task and converts the output to blocks
@@ -87,15 +88,50 @@ pub(super) fn file_to_message_part(file: &File) -> Option<MessagePart> {
         .and_then(|media_type| Format::from_media_type(media_type).ok())
         .unwrap_or_else(|| Format::from_name(&file.name));
 
-    if format.is_image() {
-        None // TODO
-    } else if format.is_audio() {
-        None // TODO
-    } else if format.is_video() {
-        None // TODO
+    if format.is_image() || format.is_audio() || format.is_video() {
+        let Some(content) = file
+            .content
+            .as_ref()
+            .and_then(|content| (!content.trim().is_empty()).then_some(content))
+        else {
+            return None;
+        };
+
+        let mut content_url = content.clone();
+        let media_type = file.media_type.clone();
+
+        if file.options.transfer_encoding.as_deref() == Some("base64")
+            && !content_url.starts_with("data:")
+        {
+            let media_type = media_type.clone().unwrap_or_else(|| format.media_type());
+            content_url.insert_str(0, &["data:", &media_type, ";base64,"].concat());
+        }
+
+        let object = if format.is_audio() {
+            MessagePart::AudioObject(AudioObject {
+                media_type,
+                content_url,
+                ..Default::default()
+            })
+        } else if format.is_video() {
+            MessagePart::VideoObject(VideoObject {
+                media_type,
+                content_url,
+                ..Default::default()
+            })
+        } else {
+            MessagePart::ImageObject(ImageObject {
+                media_type,
+                content_url,
+                ..Default::default()
+            })
+        };
+
+        Some(object)
     } else {
         file.content
             .as_ref()
-            .map(|value| MessagePart::Text(Text::from(value)))
+            .and_then(|content| (!content.trim().is_empty()).then_some(content))
+            .map(|content| MessagePart::Text(Text::from(content)))
     }
 }
