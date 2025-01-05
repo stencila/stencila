388c1a61569fb53904059b5c36fe77a4d7689906
Nokome Bentley <nokome@stenci.la>
2025-01-05 17:38:28 +1300

    fix(web): add chat commands to list

--- web/src/nodes/list.ts
+++ web/src/nodes/list.ts
@@ -30,6 +30,15 @@ export class List extends Entity {
         node-id=${this.id}
         depth=${this.depth}
       >
+        <div slot="header-right">
+          <stencila-ui-node-chat-commands
+            type="List"
+            node-id=${this.id}
+            depth=${this.depth}
+          >
+          </stencila-ui-node-chat-commands>
+        </div>
+
         <div slot="body">
           <stencila-ui-node-authors type="List">
             <stencila-ui-node-provenance slot="provenance">
@@ -38,6 +47,7 @@ export class List extends Entity {
             <slot name="authors"></slot>
           </stencila-ui-node-authors>
         </div>
+
         <slot name="items" slot="content"></slot>
       </stencila-ui-block-on-demand>
     `
