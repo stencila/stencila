3552ee73e7f593ad43381b32cb5a6e61f826f577
Nokome Bentley <nokome@stenci.la>
2025-01-08 08:16:03 +1300

    fix(lexical codec): pass format from options to encode context

--- rust/codec-lexical/src/nodes.rs
+++ rust/codec-lexical/src/nodes.rs
@@ -1,11 +1,14 @@
 use codec::{
     common::eyre::{bail, Result},
+    format::Format,
     schema::{Article, Node},
-    DecodeInfo, EncodeInfo,
+    DecodeInfo, EncodeInfo, EncodeOptions,
 };
 
 use crate::{
-    blocks::{blocks_from_lexical, blocks_to_lexical}, lexical::{LexicalDoc, RootNode}, shared::{LexicalDecodeContext, LexicalEncodeContext}
+    blocks::{blocks_from_lexical, blocks_to_lexical},
+    lexical::{LexicalDoc, RootNode},
+    shared::{LexicalDecodeContext, LexicalEncodeContext},
 };
 
 /// Decode a Lexical document to a Stencila root node
@@ -23,8 +26,20 @@ pub(crate) fn root_from_lexical(lexical: LexicalDoc) -> Result<(Node, DecodeInfo
 }
 
 /// Encode a Stencila root node to a Lexical document
-pub(crate) fn root_to_lexical(root: &Node) -> Result<(LexicalDoc, EncodeInfo)> {
-    let mut context = LexicalEncodeContext::default();
+pub(crate) fn root_to_lexical(
+    root: &Node,
+    options: &Option<EncodeOptions>,
+) -> Result<(LexicalDoc, EncodeInfo)> {
+    let format = options
+        .as_ref()
+        .and_then(|options| options.format.clone())
+        .unwrap_or(Format::Lexical);
+
+    let mut context = LexicalEncodeContext {
+        format,
+        ..Default::default()
+    };
+
     let lexical = node_to_lexical(root, &mut context)?;
 
     Ok((
