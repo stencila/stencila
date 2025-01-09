08c67aaa7f69674c5659a53fd7ecc766438c9613
Nokome Bentley <nokome@stenci.la>
2025-01-10 10:38:55 +1300

    chore(lsp): warn if document is not article

--- rust/lsp/src/commands.rs
+++ rust/lsp/src/commands.rs
@@ -866,18 +866,21 @@ pub(super) async fn execute_command(
                         let node_id = Some(node_id.clone());
                         temporary.retain(|node| node.node_id() != node_id);
 
-                        if temporary.len() == len_before {
-                            Err(invalid_request(format!(
-                                "Chat with id not found in temporaries: {node_id:?}"
-                            )))
-                        } else {
-                            Ok(None)
-                        }
-                    } else {
-                        Err(invalid_request("Root node is not an article"))
-                    }
-                })
-                .await;
+                let len_before = temporary.len();
+                let node_id = Some(node_id);
+                temporary.retain(|node| node.node_id() != node_id);
+
+                return if temporary.len() == len_before {
+                    Err(invalid_request(format!(
+                        "Chat with id not found in temporaries: {node_id:?}"
+                    )))
+                } else {
+                    Ok(None)
+                };
+            } else {
+                tracing::warn!("Root node is not an article");
+                return Ok(None);
+            }
         }
         SAVE_DOC => (
             "Saving document with sidecar".to_string(),
