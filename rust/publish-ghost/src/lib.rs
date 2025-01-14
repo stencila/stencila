65a64d7aa437d1893387b9a5dd0ead9b4b8da38b
micah <micah@duckles.org>
2025-01-15 11:54:46 +1300

    feat(publish ghost cli): removed comma delimited tag input for multiple tag flags

--- rust/publish-ghost/src/lib.rs
+++ rust/publish-ghost/src/lib.rs
@@ -18,7 +18,7 @@ use common::{
     tracing,
 };
 use document::{
-    schema::{shortcuts::t, Node, PropertyValueOrString,Primitive},
+    schema::{shortcuts::t, Node, Primitive, PropertyValueOrString},
     CommandWait, DecodeOptions, Document, EncodeOptions, Format, LossesResponse,
 };
 
@@ -78,7 +78,7 @@ pub struct Cli {
     pull: bool,
 
     /// Set slug(URL slug the page or post will be avalible at)
-    #[arg(long,requires = "push")]
+    #[arg(long, requires = "push")]
     slug: Option<String>,
 
     /// Push as draft
@@ -99,7 +99,7 @@ pub struct Cli {
     schedule: Option<DateTime<Utc>>,
 
     /// Tags for ghost page or post
-    #[arg(long = "tag", value_delimiter = ',', requires = "push")]
+    #[arg(long = "tag", requires = "push")]
     tags: Option<Vec<String>>,
 
     /// excerpt in ghost
@@ -612,12 +612,11 @@ struct Resource {
     tags: Option<Vec<Tag>>,
 }
 
-
 #[derive(Debug, Default, Serialize, Deserialize)]
 #[serde(crate = "common::serde")]
-struct Tag{
+struct Tag {
     // TODO: can add description and so on from https://ghost.org/docs/admin-api/
-    name:String,
+    name: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -656,7 +655,7 @@ impl Payload {
         codeinjection_head: Option<String>,
         codeinjection_foot: Option<String>,
         slug: Option<String>,
-        tags:Option<Vec<String>>,
+        tags: Option<Vec<String>>,
     ) -> Result<Self> {
         // Get document title and other metadata
         // TODO: other metadata such as authors, excerpt (from abstract?)
@@ -687,7 +686,6 @@ impl Payload {
         } else {
             excerpt
         };
-
 
         // Get tags from yaml header if it's Array of Strings
         let tags = if tags.is_none() {
@@ -741,7 +739,7 @@ impl Payload {
             codeinjection_head,
             codeinjection_foot,
             slug: slug.or_else(|| title),
-            tags: tags.map(|tag| tag.into_iter().map(|name| Tag{name,}).collect()),
+            tags: tags.map(|tag| tag.into_iter().map(|name| Tag { name }).collect()),
             ..Default::default()
         };
 
