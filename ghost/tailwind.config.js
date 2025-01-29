2003dd6d2ff956d61db3b1641327082350bef379
mike-parkin <mike@dragonfly.co.nz>
2025-01-29 17:05:24 +1300

    wip: set basic tailwind structs

--- ghost/tailwind.config.js
+++ ghost/tailwind.config.js
@@ -5,8 +5,63 @@ export default {
     "./**/*.hbs",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [require('autoprefixer')],
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          // The default Tailwind font stack from https://tailwindcss.com/docs/font-family
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+      colors: {
+        transparent: 'transparent',
+        white: '#fff',
+        brand: {
+          blue: '#2568ef',
+          green: '#66ff66',
+          red: '#e53e3e',
+          yellow: '#ecc94b',
+        },
+        grey: {
+          'wild-sand': '#f5f5f5',
+          shady: '#9d9d9d',
+          aluminium: '#999999',
+          'mine-shaft': '#333333',
+          '100': '#fefefe',
+          '150': '#f2f2f2',
+          '200': '#dedede',
+          '300': '#c7c7c7',
+          '400': '#b0b0b0',
+          '500': '#999999',
+          '600': '#808080',
+          '700': '#666666',
+          '800': '#4d4d4d',
+          '900': '#333333',
+        },
+        black: '#171817',
+        greene: {
+          '000': '#f5fff5',
+          100: '#e5ffe5',
+          200: '#d9f2d9',
+          900: '#0B4C46',
+        },
+        blue: {
+          50: '#f1f5fe',
+          200: '#b1c9fa',
+          400: '#77a0f5',
+          700: '#2568ef',
+          800: '#174bb3',
+          900: '#092d77',
+        },
+      },
+    },
+    plugins: [require('autoprefixer')],
+  }
 }
 
