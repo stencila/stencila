a747f685cacfb2fa754bd6414651c41fe9555d99
Nokome Bentley <nokome@stenci.la>
2025-02-05 07:28:40 +1300

    chore(web): remove unused views and clients

--- web/src/nodes/parameter.ts
+++ web/src/nodes/parameter.ts
@@ -21,37 +21,10 @@ export class Parameter extends Executable {
   @property({ type: Object })
   validator?: Validator
 
-  constructor() {
-    super()
-
-    this.addEventListener('input', (event: Event) => {
-      const target = event.target as HTMLInputElement
-
-      const value = target.value
-
-      // TODO: Handle different types of values
-      // using target.valueAsNumber and target.valueAsDate
-
-      this.patchNode({
-        op: 'replace',
-        id: this._id,
-        path: 'value',
-        value,
-      })
-    })
-  }
-
   override render() {
     return html`
       <label for="${this.name}">${this.label ?? this.name}</label>
       <input name="${this.name}" />
     `
-  }
-
-  /**
-   * This accessor exists to allow us to get around the eslint rule: wc/no-constructor-attributes (as called in the constructor) - via: plugin:wc/recommended
-   */
-  get _id() {
-    return this.id
   }
 }
