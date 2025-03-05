ac402cd55488f246c48f83ced7e05cb2618edfd3
Nokome Bentley <nokome@stenci.la>
2025-03-05 17:54:54 +1300

    fix(vscode): use embedded spinner to avoid delays in loading
    Adds async to loading of view javascript so that spinner shows immediately
    and is then removed when loading finished.

--- web/src/views/vscode.ts
+++ web/src/views/vscode.ts
@@ -1,14 +1,11 @@
-import { html, LitElement } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 import { VSCodeClient } from '../clients/vscode'
-import { withTwind } from '../twind'
 
 import { DocumentView } from './document'
 
 import '../nodes'
 import '../shoelace'
-import '@shoelace-style/shoelace/dist/components/spinner/spinner'
 import '../ui/document/menu'
 
 /**
@@ -28,22 +25,5 @@ export class VsCodeView extends DocumentView {
     const renderRoot = super.createRenderRoot()
     this.client = new VSCodeClient(renderRoot)
     return renderRoot
-  }
-}
-
-/**
- * Loader element to display whilst webview window is loading
- */
-@customElement('stencila-vscode-loader')
-@withTwind()
-export class VsCodeLoader extends LitElement {
-  protected override render(): unknown {
-    return html`
-      <div
-        class="fixed inset-0 w-screen h-screen flex items-center justify-center bg-white z-10"
-      >
-        <sl-spinner style="font-size: 5rem; --track-width: 5px;"></sl-spinner>
-      </div>
-    `
   }
 }
