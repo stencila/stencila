f5d2b477d27fca83facbf330608f7376b44e6d24
Nokome Bentley <nokome@stenci.la>
2025-03-21 00:22:44 +1300

    fix(cli utils): improve datatable pretty print

--- rust/cli-utils/src/datatable.rs
+++ rust/cli-utils/src/datatable.rs
@@ -2,7 +2,7 @@ use comfy_table::{Cell, CellAlignment, Color, Row};
 
 use common::{itertools::Itertools, serde_json};
 use format::Format;
-use schema::{Datatable, Primitive};
+use schema::{Datatable, Primitive, Validator};
 
 use crate::{table, Code, ToStdout};
 
@@ -13,50 +13,69 @@ impl ToStdout for Datatable {
         let header = self
             .columns
             .iter()
-            .map(|column| column.name.as_str())
+            .map(|column| {
+                let (color, alignment) = match column
+                    .validator
+                    .as_ref()
+                    .and_then(|validator| validator.items_validator.as_deref())
+                {
+                    Some(Validator::BooleanValidator(..)) => (Color::Blue, CellAlignment::Right),
+                    Some(Validator::IntegerValidator(..)) => (Color::Cyan, CellAlignment::Right),
+                    Some(Validator::NumberValidator(..)) => (Color::Green, CellAlignment::Right),
+                    Some(Validator::StringValidator(..)) => (Color::Yellow, CellAlignment::Left),
+                    _ => (Color::Magenta, CellAlignment::Right),
+                };
+
+                Cell::new(column.name.as_str())
+                    .fg(color)
+                    .set_alignment(alignment)
+            })
             .collect_vec();
         table.set_header(header);
 
         let mut rows = Vec::new();
         for row in 0..self.rows() {
             let cells = self.columns.iter().map(|column| {
-                column.values.get(row).map_or_else(
-                    || Cell::new(""),
-                    |value| match value {
-                        Primitive::Null(cell) => Cell::new(cell.to_string())
-                            .fg(Color::DarkGrey)
-                            .set_alignment(CellAlignment::Right),
-                        Primitive::Boolean(cell) => Cell::new(cell.to_string())
-                            .fg(Color::Magenta)
-                            .set_alignment(CellAlignment::Right),
-                        Primitive::Integer(cell) => Cell::new(cell.to_string())
-                            .fg(Color::Green)
-                            .set_alignment(CellAlignment::Right),
-                        Primitive::UnsignedInteger(cell) => Cell::new(cell.to_string())
-                            .fg(Color::Cyan)
-                            .set_alignment(CellAlignment::Right),
-                        Primitive::Number(cell) => Cell::new(cell.to_string())
-                            .fg(Color::Blue)
-                            .set_alignment(CellAlignment::Right),
-                        Primitive::String(cell) => Cell::new(cell.to_string())
-                            .fg(Color::DarkYellow)
-                            .set_alignment(CellAlignment::Left),
-                        Primitive::Array(cell) => Cell::new(
-                            Code::new(
-                                Format::Json,
-                                &serde_json::to_string(cell).unwrap_or_default(),
-                            )
-                            .to_terminal(),
-                        ),
-                        Primitive::Object(cell) => Cell::new(
-                            Code::new(
-                                Format::Json,
-                                &serde_json::to_string(cell).unwrap_or_default(),
-                            )
-                            .to_terminal(),
-                        ),
-                    },
-                )
+                let alignment = match column
+                    .validator
+                    .as_ref()
+                    .and_then(|validator| validator.items_validator.as_deref())
+                {
+                    Some(Validator::StringValidator(..)) => CellAlignment::Left,
+                    _ => CellAlignment::Right,
+                };
+
+                column
+                    .values
+                    .get(row)
+                    .map_or_else(
+                        || Cell::new(""),
+                        |value| match value {
+                            Primitive::Null(cell) => {
+                                Cell::new(cell.to_string()).fg(Color::DarkGrey)
+                            }
+                            Primitive::Boolean(cell) => Cell::new(cell.to_string()),
+                            Primitive::Integer(cell) => Cell::new(cell.to_string()),
+                            Primitive::UnsignedInteger(cell) => Cell::new(cell.to_string()),
+                            Primitive::Number(cell) => Cell::new(cell.to_string()),
+                            Primitive::String(cell) => Cell::new(cell.to_string()),
+                            Primitive::Array(cell) => Cell::new(
+                                Code::new(
+                                    Format::Json,
+                                    &serde_json::to_string(cell).unwrap_or_default(),
+                                )
+                                .to_terminal(),
+                            ),
+                            Primitive::Object(cell) => Cell::new(
+                                Code::new(
+                                    Format::Json,
+                                    &serde_json::to_string(cell).unwrap_or_default(),
+                                )
+                                .to_terminal(),
+                            ),
+                        },
+                    )
+                    .set_alignment(alignment)
             });
             rows.push(Row::from(cells))
         }
