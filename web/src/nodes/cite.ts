ad45c1ee118245be6da087d1c1dcd8614eedd5a2
Nokome Bentley <nokome@stenci.la>
2025-04-15 13:48:41 +1200

    fix(web): fallback to using content if appropriate

--- web/src/nodes/cite.ts
+++ web/src/nodes/cite.ts
@@ -1,6 +1,6 @@
 import { CitationMode } from '@stencila/types'
 import { html } from 'lit'
-import { customElement, property } from 'lit/decorators'
+import { customElement, property, state } from 'lit/decorators'
 
 import { withTwind } from '../twind'
 
@@ -14,21 +14,49 @@ import { Entity } from './entity'
 @customElement('stencila-cite')
 @withTwind()
 export class Cite extends Entity {
+  @property({ attribute: 'target' })
+  target?: string
+
   @property({ attribute: 'citation-mode' })
   citationMode?: CitationMode
 
+  /**
+   * Whether the `Cite` has a resolved `Reference`
+   *
+   * `Cite` nodes that originate from sources such as JATS can has both a resolved `reference`
+   * (based on `target`) and `content`. The `content` is treated as a fallback and will not be shown if the cite has
+   * a resolved `reference`.
+   */
+  @state()
+  hasReference: boolean = false
+
+  onReferencesSlotChange({ target: slot }: Event) {
+    const referenceElem = (slot as HTMLSlotElement).assignedElements({
+      flatten: true,
+    })[0]
+    this.hasReference = !!referenceElem
+  }
+
   override render() {
+    const inner = html`<slot
+        name="reference"
+        @slotchange=${this.onReferencesSlotChange}
+      ></slot
+      ><span class=${this.hasReference ? 'hidden' : ''}
+        ><slot name="content"></slot
+      ></span>`
+
     if (this.citationMode == 'Parenthetical') {
-      return html`(<slot name="reference"></slot>)`
+      return html`(${inner})`
     }
 
     const items = this.closestGlobally('stencila-cite-group [slot=items]')
     if (items) {
       if (this != items.lastElementChild) {
-        return html`<slot name="reference"></slot>; `
+        return html`${inner}; `
       }
     }
 
-    return html`<slot name="reference"></slot>`
+    return html`${inner}`
   }
 }
