3f3fbb962e7a5581e669030d480c81fb20d096db
Nokome Bentley <nokome@stenci.la>
2025-05-08 05:50:53 +1200

    refactor(article): factor out `Article::is_course` method

--- rust/schema/src/implem/article.rs
+++ rust/schema/src/implem/article.rs
@@ -1,6 +1,17 @@
 use crate::{prelude::*, Article, Block, RawBlock};
 
 impl Article {
+    /// Does tha article appear to be have been decoded from the format using the `--coarse` option
+    ///
+    /// Checks whether the first block in the content of the article is a `RawBlock` of the given formats
+    pub fn is_coarse(&self, format: &Format) -> bool {
+        if let Some(Block::RawBlock(raw)) = self.content.first() {
+            &Format::from_name(&raw.format) == format
+        } else {
+            false
+        }
+    }
+
     pub fn to_jats_special(&self) -> (String, Losses) {
         use codec_jats_trait::encode::{elem, elem_no_attrs};
 
@@ -52,19 +63,17 @@ impl LatexCodec for Article {
     fn to_latex(&self, context: &mut LatexEncodeContext) {
         context.enter_node(self.node_type(), self.node_id());
 
-        let mut wrap = context.standalone;
-        if context.standalone {
-            // Only wrap if necessary i.e. first block is not a RawBlock with preamble
-            if let Some(Block::RawBlock(RawBlock {
-                format, content, ..
-            })) = self.content.first()
-            {
-                if format.to_lowercase() == "latex" && content.contains(r"\documentclass") {
-                    context.coarse = true;
-                    wrap = false;
-                }
-            }
-        }
+        let wrap = context.standalone
+            && !self
+                .content
+                .first()
+                .map(|block| match block {
+                    Block::RawBlock(RawBlock { content, .. }) => {
+                        content.contains(r"\documentclass")
+                    }
+                    _ => false,
+                })
+                .unwrap_or_default();
 
         if wrap {
             context.str(
