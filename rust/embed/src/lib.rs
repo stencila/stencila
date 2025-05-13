c4a34a4d2a27d49469d6325584d8f82984c1ec14
Nokome Bentley <nokome@stenci.la>
2025-05-13 22:17:26 +1200

    feat(embeddings): initial iteration of crate

---
+++ rust/embed/src/lib.rs
@@ -0,0 +1,37 @@
+use common::eyre::{Result, eyre};
+use dirs::{DirType, get_app_dir};
+use fastembed::{EmbeddingModel, InitOptions, TextEmbedding};
+
+/// Generate embeddings for more that one text
+///
+/// TODO: add model as an argument
+fn embed(texts: Vec<String>) -> Result<Vec<Vec<f32>>> {
+    let options = InitOptions::new(EmbeddingModel::BGESmallENV15)
+        .with_cache_dir(get_app_dir(DirType::Models, true)?)
+        .with_show_download_progress(false);
+
+    let model = TextEmbedding::try_new(options).map_err(|error| eyre!(error))?;
+
+    let vecs = model.embed(texts, None).map_err(|error| eyre!(error))?;
+    Ok(vecs)
+}
+
+/// Generate embeddings for passages
+///
+/// Adds the "passage:" prefix which is recommended by `fastembed`.
+pub fn passages(texts: Vec<String>) -> Result<Vec<Vec<f32>>> {
+    embed(
+        texts
+            .into_iter()
+            .map(|text| format!("passage: {text}"))
+            .collect(),
+    )
+}
+
+/// Generate embeddings for a query
+///
+/// Adds the "query:" prefix which is recommended by `fastembed`.
+pub fn query(text: &str) -> Result<Vec<f32>> {
+    let mut vecs = embed(vec![format!("query: {text}")])?;
+    Ok(vecs.swap_remove(0))
+}
