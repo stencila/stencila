46f127c98503cc0aeba6aea08eaea95b4e96d505
Nokome Bentley <nokome@stenci.la>
2025-07-20 23:35:34 +1200

    fix(cli): simplify error message when not in debug mode

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -1,7 +1,8 @@
 #![recursion_limit = "256"]
 
-use std::env::set_var;
+use std::{env::set_var, process::exit};
 
+use cli_utils::message;
 use common::{clap::Parser, eyre::Result, tokio};
 
 use cli::{
@@ -22,17 +23,25 @@ async fn main() -> Result<()> {
     }
 
     let (log_level, log_format, error_details) = if cli.debug {
-        (LoggingLevel::Debug, LoggingFormat::Pretty, "all")
+        (
+            LoggingLevel::Debug,
+            LoggingFormat::Pretty,
+            "all".to_string(),
+        )
     } else if cli.trace {
-        (LoggingLevel::Trace, LoggingFormat::Pretty, "all")
+        (
+            LoggingLevel::Trace,
+            LoggingFormat::Pretty,
+            "all".to_string(),
+        )
     } else {
-        (cli.log_level, cli.log_format, cli.error_details.as_str())
+        (cli.log_level, cli.log_format, cli.error_details.clone())
     };
 
     if matches!(cli.command, Command::Lsp) {
         lsp::run(log_level.into(), &cli.log_filter).await?
     } else {
-        errors::setup(error_details, cli.error_link)?;
+        errors::setup(&error_details, cli.error_link)?;
         logging::setup(log_level, &cli.log_filter, log_format)?;
         ask::setup_cli(cli.assume_answer()).await?;
 
@@ -41,7 +50,14 @@ async fn main() -> Result<()> {
             upgrade::check(false);
         }
 
-        cli.run().await?;
+        if let Err(error) = cli.run().await {
+            if error_details == "auto" && !cfg!(debug_assertions) {
+                message(&error.to_string(), Some("💥"));
+                exit(1);
+            } else {
+                return Err(error);
+            }
+        }
 
         if !skip_upgrade {
             upgrade::notify();
