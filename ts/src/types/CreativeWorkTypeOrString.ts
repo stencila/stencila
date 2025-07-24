33e1d4e340086f80b9883e664291634b46c8a6a7
Nokome Bentley <nokome@stenci.la>
2025-07-24 12:40:16 +1200

    fix(schema gen): generate new license type for typescript properly

--- ts/src/types/CreativeWorkTypeOrString.ts
+++ ts/src/types/CreativeWorkTypeOrString.ts
@@ -1,7 +1,4 @@
 // Generated file; do not edit. See https://github.com/stencila/stencila/tree/main/rust/schema-gen
-
-import { hydrate } from "../hydrate.js";
-
 import { type CreativeWorkType } from "./CreativeWorkType.js";
 
 /**
@@ -11,17 +8,4 @@ export type CreativeWorkTypeOrString =
   CreativeWorkType |
   string;
 
-/**
- * Create a `CreativeWorkTypeOrString` from an object
- */
-export function creativeWorkTypeOrString(other: CreativeWorkTypeOrString): CreativeWorkTypeOrString {
-  if (other == null || typeof other !== "object" || Array.isArray(other) || typeof other.type === "undefined") {
-    return other as CreativeWorkTypeOrString;
-  }
-  switch(other.type) {
-    case "CreativeWorkType":
-      return hydrate(other) as CreativeWorkTypeOrString
-    default:
-      throw new Error(`Unexpected type for CreativeWorkTypeOrString: ${other.type}`);
-  }
-}
+
