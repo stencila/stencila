2910eecefa105e85862c3a4406c6d2b9566d1a57
Nokome Bentley <nokome@stenci.la>
2025-07-27 09:48:00 +1200

    chore(cli): update log filter to avoid headless chrome messages

--- rust/cli/src/cli.rs
+++ rust/cli/src/cli.rs
@@ -117,7 +117,7 @@ pub struct Cli {
     /// syntax such as `tokio=debug`.
     #[arg(
         long,
-        default_value = "globset=warn,hyper=info,hyper_util=info,ignore=warn,keyring=info,mio=info,notify=warn,ort=error,reqwest=info,rustls=info,sled=info,tokenizers:=info,tokio=info,tungstenite=info",
+        default_value = "globset=warn,headless_chrome=warn,hyper=info,hyper_util=info,ignore=warn,keyring=info,mio=info,notify=warn,ort=error,reqwest=info,rustls=info,sled=info,tokenizers:=info,tokio=info,tungstenite=info",
         global = true,
         hide = true
     )]
