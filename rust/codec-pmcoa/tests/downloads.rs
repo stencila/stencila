f01c0a48fce156bc7c78eeb6ea341f27fec340a0
Nokome Bentley <nokome@stenci.la>
2025-08-22 15:29:08 +1200

    test(pmcoa codec): skip download test

--- rust/codec-pmcoa/tests/downloads.rs
+++ rust/codec-pmcoa/tests/downloads.rs
@@ -6,7 +6,10 @@ use common_dev::insta::{assert_json_snapshot, assert_yaml_snapshot};
 
 use codec_pmcoa::PmcOaCodec;
 
-/// Download and decode from PMCID
+/// Download and decode from PMCID Currently skipping this test because request
+/// fails with 500 status code (including when using `xh` and Firefox, so not
+/// just our client)
+#[ignore = "frequently 500s"]
 #[tokio::test]
 #[allow(clippy::print_stderr)]
 async fn downloads() -> Result<()> {
