f9e1f259de0aaeb9a07c924f6bd7e4ad2eb1ceb6
Nokome Bentley <nokome@stenci.la>
2025-09-04 10:16:27 +1200

    fix(server): improve display of internal errors

--- rust/server/src/errors.rs
+++ rust/server/src/errors.rs
@@ -15,19 +15,24 @@ pub(crate) struct InternalError;
 impl InternalError {
     /// Create a new internal error
     ///
-    /// Creates an error log entry with all the debugging niceties
-    /// of `eyre`.
+    /// Creates an error log entry with all the debugging niceties of `eyre`.
+    ///
+    /// Note that `tower_http::trace::on_failure` will crate a `tracing` log
+    /// entry for the 500 code so that is not done here to avoid duplication.
+    /// Also `tracing::error("{error:?}")` escape ASCII colors which messes up
+    /// the colored display.
     pub fn new<T>(error: T) -> Self
     where
         T: Display + Debug,
     {
-        tracing::error!("{error:?}");
+        eprintln!("{error:?}\n");
         Self
     }
 }
 
 impl Display for InternalError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Do not leak any details in display
         write!(f, "InternalError")
     }
 }
