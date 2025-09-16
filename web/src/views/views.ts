35d224b7c593c5e6e0ba77a26b5df5700ef141e1
Nokome Bentley <nokome@stenci.la>
2025-09-17 09:46:42 +1200

    refactor(web): remove unused module

--- web/src/views/views.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentView } from '../types'
-
-export const VIEWS: Record<Exclude<DocumentView, 'directory'>, string> = {
-  dynamic: 'Live updating and able to see and interact with code',
-  interactive: 'Live updating and able to interact with parameters',
-  live: 'Live updating view',
-  static: 'Fixed, read-only view',
-}
