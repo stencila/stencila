7a2b72301e2fe20023bfccbf83841b3939e7e528
Nokome Bentley <nokome@stenci.la>
2025-10-04 14:23:01 +1300

    fix(themes): improvements to code

--- web/src/ui/nodes/properties/code/theme.ts
+++ web/src/ui/nodes/properties/code/theme.ts
@@ -91,76 +91,76 @@ export function createTheme(): Extension {
   // Syntax highlighting theme - maps @lezer/highlight tags to CSS variables
   const syntaxHighlightStyle = HighlightStyle.define([
     // Comments
-    { tag: t.comment, color: 'var(--syntax-comment)' },
-    { tag: t.blockComment, color: 'var(--syntax-comment-block)' },
-    { tag: t.lineComment, color: 'var(--syntax-comment-line)' },
-    { tag: t.docComment, color: 'var(--syntax-comment-doc)' },
+    { tag: t.comment, color: 'var(--code-comment)' },
+    { tag: t.blockComment, color: 'var(--code-comment-block)' },
+    { tag: t.lineComment, color: 'var(--code-comment-line)' },
+    { tag: t.docComment, color: 'var(--code-comment-doc)' },
 
     // Keywords
-    { tag: t.keyword, color: 'var(--syntax-keyword)' },
-    { tag: t.controlKeyword, color: 'var(--syntax-keyword-control)' },
-    { tag: t.operatorKeyword, color: 'var(--syntax-keyword-operator)' },
-    { tag: t.modifier, color: 'var(--syntax-keyword-modifier)' },
-    { tag: t.definitionKeyword, color: 'var(--syntax-keyword-definition)' },
+    { tag: t.keyword, color: 'var(--code-keyword)' },
+    { tag: t.controlKeyword, color: 'var(--code-keyword-control)' },
+    { tag: t.operatorKeyword, color: 'var(--code-keyword-operator)' },
+    { tag: t.modifier, color: 'var(--code-keyword-modifier)' },
+    { tag: t.definitionKeyword, color: 'var(--code-keyword-definition)' },
 
     // Strings and literals
-    { tag: t.string, color: 'var(--syntax-string)' },
-    { tag: t.special(t.string), color: 'var(--syntax-string-special)' },
-    { tag: t.regexp, color: 'var(--syntax-string-regex)' },
-    { tag: t.escape, color: 'var(--syntax-string-escape)' },
+    { tag: t.string, color: 'var(--code-string)' },
+    { tag: t.special(t.string), color: 'var(--code-string-special)' },
+    { tag: t.regexp, color: 'var(--code-string-regex)' },
+    { tag: t.escape, color: 'var(--code-string-escape)' },
 
     // Numbers and values
-    { tag: t.number, color: 'var(--syntax-number)' },
-    { tag: t.integer, color: 'var(--syntax-number-integer)' },
-    { tag: t.float, color: 'var(--syntax-number-float)' },
-    { tag: t.bool, color: 'var(--syntax-boolean)' },
-    { tag: t.null, color: 'var(--syntax-constant)' },
+    { tag: t.number, color: 'var(--code-number)' },
+    { tag: t.integer, color: 'var(--code-number-integer)' },
+    { tag: t.float, color: 'var(--code-number-float)' },
+    { tag: t.bool, color: 'var(--code-boolean)' },
+    { tag: t.null, color: 'var(--code-constant)' },
 
     // Constants and built-ins
-    { tag: t.constant(t.name), color: 'var(--syntax-constant)' },
-    { tag: t.standard(t.name), color: 'var(--syntax-constant-builtin)' },
+    { tag: t.constant(t.name), color: 'var(--code-constant)' },
+    { tag: t.standard(t.name), color: 'var(--code-constant-builtin)' },
 
     // Variables
-    { tag: t.variableName, color: 'var(--syntax-variable)' },
-    { tag: t.standard(t.variableName), color: 'var(--syntax-variable-builtin)' },
-    { tag: t.special(t.variableName), color: 'var(--syntax-variable-special)' },
+    { tag: t.variableName, color: 'var(--code-variable)' },
+    { tag: t.standard(t.variableName), color: 'var(--code-variable-builtin)' },
+    { tag: t.special(t.variableName), color: 'var(--code-variable-special)' },
 
     // Functions and methods
-    { tag: t.function(t.variableName), color: 'var(--syntax-function)' },
-    { tag: t.function(t.propertyName), color: 'var(--syntax-function-method)' },
-    { tag: t.standard(t.function(t.variableName)), color: 'var(--syntax-function-builtin)' },
+    { tag: t.function(t.variableName), color: 'var(--code-function)' },
+    { tag: t.function(t.propertyName), color: 'var(--code-function-method)' },
+    { tag: t.standard(t.function(t.variableName)), color: 'var(--code-function-builtin)' },
 
     // Classes and types
-    { tag: t.className, color: 'var(--syntax-class)' },
-    { tag: t.standard(t.className), color: 'var(--syntax-class-builtin)' },
-    { tag: t.typeName, color: 'var(--syntax-type)' },
-    { tag: t.standard(t.typeName), color: 'var(--syntax-type-builtin)' },
+    { tag: t.className, color: 'var(--code-class)' },
+    { tag: t.standard(t.className), color: 'var(--code-class-builtin)' },
+    { tag: t.typeName, color: 'var(--code-type)' },
+    { tag: t.standard(t.typeName), color: 'var(--code-type-builtin)' },
 
     // Namespaces and modules
-    { tag: t.namespace, color: 'var(--syntax-namespace)' },
-    { tag: t.macroName, color: 'var(--syntax-namespace)' },
+    { tag: t.namespace, color: 'var(--code-namespace)' },
+    { tag: t.macroName, color: 'var(--code-namespace)' },
 
     // Properties and attributes
-    { tag: t.propertyName, color: 'var(--syntax-property)' },
-    { tag: t.attributeName, color: 'var(--syntax-attribute)' },
+    { tag: t.propertyName, color: 'var(--code-property)' },
+    { tag: t.attributeName, color: 'var(--code-attribute)' },
 
     // Tags (for markup languages)
-    { tag: t.tagName, color: 'var(--syntax-tag)' },
-    { tag: t.angleBracket, color: 'var(--syntax-tag-angle)' },
+    { tag: t.tagName, color: 'var(--code-tag)' },
+    { tag: t.angleBracket, color: 'var(--code-tag-angle)' },
 
     // Operators and punctuation
-    { tag: t.operator, color: 'var(--syntax-operator)' },
-    { tag: t.punctuation, color: 'var(--syntax-punctuation)' },
-    { tag: t.bracket, color: 'var(--syntax-punctuation-bracket)' },
-    { tag: t.separator, color: 'var(--syntax-punctuation-delimiter)' },
+    { tag: t.operator, color: 'var(--code-operator)' },
+    { tag: t.punctuation, color: 'var(--code-punctuation)' },
+    { tag: t.bracket, color: 'var(--code-punctuation-bracket)' },
+    { tag: t.separator, color: 'var(--code-punctuation-delimiter)' },
 
     // Meta and special
-    { tag: t.meta, color: 'var(--syntax-meta)' },
-    { tag: t.processingInstruction, color: 'var(--syntax-meta)' },
+    { tag: t.meta, color: 'var(--code-meta)' },
+    { tag: t.processingInstruction, color: 'var(--code-meta)' },
 
     // Invalid and deprecated
-    { tag: t.invalid, color: 'var(--syntax-invalid)' },
-    { tag: t.deleted, color: 'var(--syntax-invalid-deprecated)' },
+    { tag: t.invalid, color: 'var(--code-invalid)' },
+    { tag: t.deleted, color: 'var(--code-invalid-deprecated)' },
 
     // Text emphasis (for markdown, etc.)
     { tag: t.emphasis, fontStyle: 'italic' },
