0920c70d21c7ef8bf240f8495041b2836fb7e0d5
Nokome Bentley <nokome@stenci.la>
2025-11-21 21:19:38 +1300

    feat(codec utils): add `modification_time` function

--- rust/codec-utils/src/lib.rs
+++ rust/codec-utils/src/lib.rs
@@ -1,8 +1,10 @@
 mod git_info;
+mod modification_time;
 mod move_file;
 mod rebase_edits;
 pub mod split_paragraph;
 
 pub use git_info::*;
+pub use modification_time::*;
 pub use move_file::*;
 pub use rebase_edits::*;
