6cef7a00f22629d3d024427825c00c0a58e3c9f1
Nokome Bentley <nokome@stenci.la>
2026-04-03 19:11:31 +1300

    fix(schema gen): avoid deleting config schema

--- rust/schema-gen/src/json_schema.rs
+++ rust/schema-gen/src/json_schema.rs
@@ -1,6 +1,6 @@
 //! Generation of a JSON Schema from Stencila Schema
 
-use std::path::PathBuf;
+use std::{ffi::OsStr, path::PathBuf};
 
 use eyre::{ErrReport, Result};
 use futures::future::try_join_all;
@@ -24,7 +24,7 @@ impl Schemas {
         // Remove all existing *.schema.json files
         let futures = glob(&dir.join("*.schema.json").to_string_lossy())?
             .flatten()
-            .filter(|file| file != &PathBuf::from("stencila-config.schema.json"))
+            .filter(|file| file.file_name() != Some(OsStr::new("stencila-config.schema.json")))
             .map(|file| async { remove_file(file).await });
         try_join_all(futures).await?;
 
